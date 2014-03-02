91e93899ed171b111851c9fa58e47e57174c9e5c
John Blum <jblum0@gmail.com>
2014-03-01 16:26:17 -0800

    Refactored the SorterFactory introducing the createSorterElseDefault method to return a Sorter implementation based on the SortType or return the given default Sorter if the SortType is not mapped to a Sorter implementation.  Added associated test cases to the SorterFactoryTest class.  Refactored the test cases in the SortTypeTest class.

--- src/test/java/org/cp/elements/util/sort/SortTypeTest.java
+++ src/test/java/org/cp/elements/util/sort/SortTypeTest.java
@@ -30,6 +30,7 @@ import org.junit.Test;
  * SorterType enumeration type.
  * <p/>
  * @author John J. Blum
+ * @see java.lang.Enum
  * @see org.cp.elements.util.sort.SortType
  * @see org.junit.Test
  * @since 1.0.0
@@ -45,14 +46,10 @@ public class SortTypeTest {
 
   @Test
   public void testValueOfAbbreviation() {
-    assertEquals(SortType.BUBBLE_SORT, SortType.valueOfAbbreviation(SortType.BUBBLE_SORT.getAbbreviation()));
-    assertEquals(SortType.COMB_SORT, SortType.valueOfAbbreviation(SortType.COMB_SORT.getAbbreviation()));
-    assertEquals(SortType.HEAP_SORT, SortType.valueOfAbbreviation(SortType.HEAP_SORT.getAbbreviation()));
-    assertEquals(SortType.INSERTION_SORT, SortType.valueOfAbbreviation(SortType.INSERTION_SORT.getAbbreviation()));
-    assertEquals(SortType.MERGE_SORT, SortType.valueOfAbbreviation(SortType.MERGE_SORT.getAbbreviation()));
-    assertEquals(SortType.QUICK_SORT, SortType.valueOfAbbreviation(SortType.QUICK_SORT.getAbbreviation()));
-    assertEquals(SortType.SELECTION_SORT, SortType.valueOfAbbreviation(SortType.SELECTION_SORT.getAbbreviation()));
-    assertEquals(SortType.SHELL_SORT, SortType.valueOfAbbreviation(SortType.SHELL_SORT.getAbbreviation()));
+    for (SortType value : SortType.values()) {
+      assertEquals(value, SortType.valueOfAbbreviation(value.getAbbreviation()));
+    }
+
     assertEquals(SortType.HEAP_SORT, SortType.valueOfAbbreviation("heap"));
     assertEquals(SortType.MERGE_SORT, SortType.valueOfAbbreviation("Merge"));
     assertEquals(SortType.QUICK_SORT, SortType.valueOfAbbreviation("QUICK"));
@@ -69,14 +66,10 @@ public class SortTypeTest {
 
   @Test
   public void testValueOfName() {
-    assertEquals(SortType.BUBBLE_SORT, SortType.valueOfName(SortType.BUBBLE_SORT.getName()));
-    assertEquals(SortType.COMB_SORT, SortType.valueOfName(SortType.COMB_SORT.getName()));
-    assertEquals(SortType.HEAP_SORT, SortType.valueOfName(SortType.HEAP_SORT.getName()));
-    assertEquals(SortType.INSERTION_SORT, SortType.valueOfName(SortType.INSERTION_SORT.getName()));
-    assertEquals(SortType.MERGE_SORT, SortType.valueOfName(SortType.MERGE_SORT.getName()));
-    assertEquals(SortType.QUICK_SORT, SortType.valueOfName(SortType.QUICK_SORT.getName()));
-    assertEquals(SortType.SELECTION_SORT, SortType.valueOfName(SortType.SELECTION_SORT.getName()));
-    assertEquals(SortType.SHELL_SORT, SortType.valueOfName(SortType.SHELL_SORT.getName()));
+    for (SortType value : SortType.values()) {
+      assertEquals(value, SortType.valueOfName(value.getName()));
+    }
+
     assertEquals(SortType.HEAP_SORT, SortType.valueOfName("heap sort"));
     assertEquals(SortType.MERGE_SORT, SortType.valueOfName("Merge Sort"));
     assertEquals(SortType.QUICK_SORT, SortType.valueOfName("QUICK SORT"));
