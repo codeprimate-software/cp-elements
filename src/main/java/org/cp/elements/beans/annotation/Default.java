216008a7c87265af3293a5ac71f848ef6d479211
John Blum <jblum0@gmail.com>
2014-12-21 22:27:10 -0800

    Created the org.cp.elements.beans.annotation packaging containing the @Default and @Required annotations representing bean properties with default values or required properties, respectively.

---
+++ src/main/java/org/cp/elements/beans/annotation/Default.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2011-Present. Codeprimate, LLC and authors.  All Rights Reserved.
+ * <p/>
+ * This software is licensed under the Codeprimate End User License Agreement (EULA).
+ * This software is proprietary and confidential in addition to an intellectual asset
+ * of the aforementioned authors.
+ * <p/>
+ * By using the software, the end-user implicitly consents to and agrees to be in compliance
+ * with all terms and conditions of the EULA.  Failure to comply with the EULA will result in
+ * the maximum penalties permissible by law.
+ * <p/>
+ * In short, this software may not be reverse engineered, reproduced, copied, modified
+ * or distributed without prior authorization of the aforementioned authors, permissible
+ * and expressed only in writing.  The authors grant the end-user non-exclusive, non-negotiable
+ * and non-transferable use of the software "as is" without expressed or implied WARRANTIES,
+ * EXTENSIONS or CONDITIONS of any kind.
+ * <p/>
+ * For further information on the software license, the end user is encouraged to read
+ * the EULA @ ...
+ */
+
+package org.cp.elements.beans.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The Default annotation defines bean property meta-data for specifying default values when a value
+ * is not explicitly provided for the property when the corresponding setter is called.
+ *
+ * @author John J. Blum
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.ElementType#METHOD
+ * @see java.lang.annotation.Inherited
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.RetentionPolicy#RUNTIME
+ * @see java.lang.annotation.Target
+ * @since 1.0.0
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Default {
+
+  String value();
+
+}
