17409ffcfc2bebb6a3b4df09727c0babcb726203
John Blum <jblum0@gmail.com>
2015-01-18 13:10:04 -0800

    Refactored the org.cp.elements.lang.MathUtils class adding many mathematical and geometry calculations, such as areas, volumes and dimensions of various geometric shapes, min and max values, fibonacci sequences, summations, multiplications, etc.

--- src/main/java/org/cp/elements/lang/MathUtils.java
+++ src/main/java/org/cp/elements/lang/MathUtils.java
@@ -22,18 +22,353 @@
 package org.cp.elements.lang;
 
 /**
- * The MathUtils class is a utility class encapsulating common mathematical operations.
+ * The MathUtils class is a utility class encapsulating common mathematical operations and calculations.
  * 
  * @author John J. Blum
  * @see java.lang.Math
- * @since 1.0.0
  * @link http://www.math.com
+ * @since 1.0.0
  */
+@SuppressWarnings("unused")
 public abstract class MathUtils {
 
   /**
+   * Calculates the area of a circle.
+   *
+   * @param radius a double value indicating the radius of the circle.
+   * @return the area of a circle given the radius.
+   * @see #ellipseArea(double, double)
+   */
+  public static double circleArea(final double radius) {
+    return ellipseArea(radius, radius);
+  }
+
+  /**
+   * Calculates the circumference of a circle.
+   *
+   * @param radius a double value indicating the radius of the circle.
+   * @return the circumference of a circle given the radius.
+   * @see java.lang.Math#PI
+   */
+  public static double circleCircumference(final double radius) {
+    return (2.0d * Math.PI * radius);
+  }
+
+  /**
+   * Calculates the diameter of a circle.
+   *
+   * @param radius a double value indicating the radius of the circle.
+   * @return the diameter of a circle given the radius.
+   */
+  public static double circleDiameter(final double radius) {
+    return (2.0d * radius);
+  }
+
+  /**
+   * Calculates the volume of a cone.
+   *
+   * @param radius a double value indicating the radius of the cone's base.
+   * @param height a double value indicating the height of the cone.
+   * @return the volume of a cone given the radius and height.
+   * @see java.lang.Math#PI
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double coneVolume(final double radius, final double height) {
+    return ((Math.PI * Math.pow(radius, 2) * height) / 3.0d);
+  }
+
+  /**
+   * Calculates the surface area of a cube.
+   *
+   * @param side a double value indicating the side length of the cube.
+   * @return the surface area of a cube given a side.
+   * @see #squareArea(double)
+   */
+  public static double cubeSurfaceArea(final double side) {
+    return (6.0d * squareArea(side));
+  }
+
+  /**
+   * Calculates the volume of a cube.
+   *
+   * @param side a double value indicating the length of the cube's side.
+   * @return the volume of a cube given the length of a side.
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double cubeVolume(final double side) {
+    return Math.pow(side, 3);
+  }
+
+  /**
+   * Calculates the surface area of a cylinder.
+   *
+   * @param radius a double value indicating the radius of the cylinder.
+   * @param height a double value indicating the height of the cylinder.
+   * @return the surface area of a cylinder given the radius and height.
+   * @see java.lang.Math#PI
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double cylinderSurfaceArea(final double radius, final double height) {
+    return ((2.0d * Math.PI * Math.pow(radius, 2)) + (2.0d * Math.PI * radius * height));
+  }
+
+  /**
+   * Calculates the volume of a cylinder.
+   *
+   * @param radius a double value indicating the radius of the cylinder's end(s).
+   * @param height a double value indicating the height of the cylinder.
+   * @return the volume of a cylinder given the radius and height.
+   * @see java.lang.Math#PI
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double cylinderVolume(final double radius, final double height) {
+    return (Math.PI * Math.pow(radius, 2) * height);
+  }
+
+  /**
+   * Calculates the area of an ellipse.
+   *
+   * @param radiusLength a double value indicating the radius length of the ellipse (x axis).
+   * @param radiusHeight a double value indicating the radius height of the ellipse (y axis).
+   * @return the area of an ellipse given the radius length and height.
+   * @see java.lang.Math#PI
+   */
+  public static double ellipseArea(final double radiusLength, final double radiusHeight) {
+    return (Math.PI * radiusLength * radiusHeight);
+  }
+
+  /**
+   * Calculates the volume of an ellipsoid.
+   *
+   * @param radiusLength a double value indicating the radius length of the ellipsoid (x axis).
+   * @param radiusHeight a double value indicating the radius height of the ellipsoid (y axis).
+   * @param radiusWidth a double value indicating the radius width of the ellipsoid (z axis).
+   * @return the volume of the ellipsoid given the radius length, height and width.
+   * @see java.lang.Math#PI
+   */
+  public static double ellipsoidVolume(final double radiusLength, final double radiusHeight, final double radiusWidth) {
+    return ((4.0d * Math.PI * radiusLength * radiusHeight * radiusWidth) / 3.0d);
+  }
+
+  /**
+   * Calculates the area of a equilateral triangle.
+   *
+   * @param side a double value indicating the length of the equilateral triangle's side.
+   * @return the area of a equilateral triangle given the length of a side.
+   * @see java.lang.Math#pow(double, double)
+   * @see java.lang.Math#sqrt(double)
+   */
+  public static double equilateralTriangleArea(final double side) {
+    return ((Math.sqrt(3.0d) / 4.0d) * Math.pow(side, 2));
+  }
+
+  /**
+   * Calculates the factorial of the given number.
+   *
+   * @param number an integer value indicating the number to calculate the factorial of.
+   * @return the factorial of the given number.
+   * @throws IllegalArgumentException if the number is less than 0.
+   */
+  public static int factorial(int number) {
+    Assert.argument(number >= 0, "The number to calculate the factorial of must be greater than equal to 0!");
+
+    int result = 1;
+
+    while (number > 0) {
+      result *= number--;
+    }
+
+    return result;
+  }
+
+  /**
+   * Gets the Fibonacci number at position n in the Fibonacci Sequence.
+   *
+   * @param n an integer value indicating the nth number in the Fibonacci Sequence.
+   * @return the nth number in the Fibonacci Sequence.
+   * @throws IllegalArgumentException if the position (n) is less than 1.
+   * @see #fibonacciSequence(int)
+   */
+  public static int fibonacciNumber(final int n) {
+    return fibonacciSequence(n)[n - 1];
+  }
+
+  /**
+   * Calculates the Fibonacci Sequence to the nth position.
+   *
+   * @param n an integer value indicating the position of the nth element in the Fibonacci Sequence.
+   * @return an integer array containing n elements of the Fibonacci Sequence.
+   * @throws IllegalArgumentException if the position (n) is less than 1.
+   */
+  public static int[] fibonacciSequence(final int n) {
+    Assert.argument(n > 0, "The number of elements from the Fibonacci Sequence to calculate must be greater than equal to 0!");
+
+    int[] fibonacciNumbers = new int[n];
+
+    for (int position = 0; position < n; position++) {
+      if (position == 0) {
+        fibonacciNumbers[position] = 0;
+      }
+      else if (position < 2) {
+        fibonacciNumbers[position] = 1;
+      }
+      else {
+        fibonacciNumbers[position] = (fibonacciNumbers[position - 1] + fibonacciNumbers[position - 2]);
+      }
+    }
+
+    return fibonacciNumbers;
+  }
+
+  /**
+   * Calculates the volume of an irregular prism.
+   *
+   * @param base a double value indicating the prism's base (width).
+   * @param height a double value indicating the prism's height.
+   * @return the volume of an irregular prism given the base and height.
+   */
+  public static double irregularPrismVolume(final double base, final double height) {
+    return (base * height);
+  }
+
+  /**
+   * Determines the maximum numerical value in an array of values.
+   *
+   * @param values an array of numerical values from which to determine the maximum value.
+   * @return the maximum numerical value in the array of numerical values.  Returns Double.NaN
+   * if the array of values is null.
+   * @see java.lang.Double#NaN
+   * @see java.lang.Math#max(double, double)
+   */
+  @NullSafe
+  public static double max(final double... values) {
+    double maxValue = Double.NaN;
+
+    if (values != null) {
+      for (double value : values) {
+        maxValue = (Double.isNaN(maxValue) ? value : Math.max(maxValue, value));
+      }
+    }
+
+    return maxValue;
+  }
+
+  /**
+   * Determines the minimum numerical value in an array of values.
+   *
+   * @param values an array of numerical values from which to determine the minimum value.
+   * @return the minimum numerical value in the array of numerical values.  Returns Double.NaN
+   * if the array of values is null.
+   * @see java.lang.Double#NaN
+   * @see java.lang.Math#min(double, double)
+   */
+  @NullSafe
+  public static double min(final double... values) {
+    double minValue = Double.NaN;
+
+    if (values != null) {
+      for (double value : values) {
+        minValue = (Double.isNaN(minValue) ? value : Math.min(minValue, value));
+      }
+    }
+
+    return minValue;
+  }
+
+  /**
+   * Multiplies the array of numbers.
+   *
+   * @param numbers the array of integer values to multiply.
+   * @return the array of integer numbers multiplied together.
+   */
+  @NullSafe
+  public static int multiply(final int... numbers) {
+    int result = 0;
+
+    if (numbers != null) {
+      result = (numbers.length > 0 ? 1 : 0);
+      for (int number : numbers) {
+        result *= number;
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Calculates the area of a parallelogram.
+   *
+   * @param base a double value indicating the base length of the parallelogram.
+   * @param height a double value indicating the height of the parallelogram.
+   * @return the area of a parallelogram given the base length and height.
+   * @see #rectangleArea(double, double)
+   */
+  public static double parallelogramArea(final double base, final double height) {
+    return rectangleArea(base, height);
+  }
+
+  /**
+   * Calculates the volume of a pyramid.
+   *
+   * @param base a double value indicating the base length of the pyramid.
+   * @param height a double value indicating the height of the pyramid.
+   * @return the volume of a pyramid given the base length and height.
+   */
+  public static double pyramidVolume(final double base, final double height) {
+    return ((base * height) / 3.0d);
+  }
+
+  /**
+   * Calculates the Pythagorean Theorem (c2 = a2 + b2).
+   *
+   * @param a double value operand.
+   * @param b double value operand.
+   * @return the value for c using the Pythagorean Theorem
+   * @see java.lang.Math#pow(double, double)
+   * @see java.lang.Math#sqrt(double)
+   */
+  public static double pythagoreanTheorem(final double a, final double b) {
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+  }
+
+  /**
+   * Calculates the area of a rectangle.
+   *
+   * @param length a double value indicating the length of the rectangle.
+   * @param height a double value indicating the height of the rectangle.
+   * @return the area of a rectangle given the length and height.
+   */
+  public static double rectangleArea(final double length, final double height) {
+    return (length * height);
+  }
+
+  /**
+   * Calculates the surface area of a rectangular prism;
+   *
+   * @param length a double value indicating the length of the rectangular prism (x axis).
+   * @param height a double value indicating the height of the rectangular prism (y axis).
+   * @param width a double value indicating the width of the rectangular prism (z axis).
+   * @return the surface area of a rectangular prism given the length, height and width of the sides.
+   */
+  public static double rectangularPrismSurfaceArea(final double length, final double height, final double width) {
+    return ((2 * length * height) + (2 * length * width) + (2 * height * width));
+  }
+
+  /**
+   * Calculates the volume of a rectangular prism.
+   *
+   * @param length a double value indicating the length of the rectangular prism (x axis).
+   * @param height a double value indicating the height of the rectangular prism (y axis).
+   * @param width a double value indicating the width of the rectangular prism (z axis).
+   * @return the volume of a rectangular prism given the length, height and width.
+   */
+  public static double rectangularPrismVolume(final double length, final double height, final double width) {
+    return (length * height * width);
+  }
+
+  /**
    * Rounds the specified double value to the nearest tenth.
-   * 
+   *
    * @param value the double value to round to the nearest tenth.
    * @return the double value rounded to the nearest tenth.
    * @see java.lang.Math#round(double)
@@ -43,6 +378,83 @@ public abstract class MathUtils {
     value = Math.round(value);
     value /= 10.0d;
     return value;
+  }
+
+  /**
+   * Calculates the surface area of a sphere.
+   *
+   * @param radius a double value indicating the radius of the sphere.
+   * @return the surface area of a sphere given a radius
+   * @see java.lang.Math#PI
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double sphereSurfaceArea(final double radius) {
+    return (4.0d * Math.PI * Math.pow(radius, 2));
+  }
+
+  /**
+   * Calculates the volume of a sphere.
+   *
+   * @param radius a double value indicating the radius of the sphere.
+   * @return the volume of a sphere given the radius.
+   * @see java.lang.Math#PI
+   * @see java.lang.Math#pow(double, double)
+   */
+  public static double sphereVolume(final double radius) {
+    return ((4.0d * Math.PI * Math.pow(radius, 3)) / 3.0d);
+  }
+
+  /**
+   * Calculates the area of a square.
+   *
+   * @param side a double value indicating the length of the square's side.
+   * @return the area of a square given the length of a side.
+   * @see #rectangleArea(double, double)
+   */
+  public static double squareArea(final double side) {
+    return rectangleArea(side, side);
+  }
+
+  /**
+   * Calculates the sum of all integer values in the array.
+   *
+   * @param numbers an array of integer values to sum up.
+   * @return the sum of all integer values in the array.
+   */
+  @NullSafe
+  public static int sum(final int... numbers) {
+    int sum = 0;
+
+    if (numbers != null) {
+      for (int number : numbers) {
+        sum += number;
+      }
+    }
+
+    return sum;
+  }
+
+  /**
+   * Calculates the area of a trapezoid.
+   *
+   * @param height a double value indicating the height of the trapezoid.
+   * @param baseOne a double value indicating the length of one of the trapezoid's bases.
+   * @param baseTwo a double value indicating the length of the other trapezoid's bases.
+   * @return the area of a trapezoid given the height and length of the 2 bases.
+   */
+  public static double trapezoidArea(final double height, final double baseOne, final double baseTwo) {
+    return ((height / 2.0d) * (baseOne + baseTwo));
+  }
+
+  /**
+   * Calculates the area of a triangle.
+   *
+   * @param base a double value indicating the length of the triangle's base.
+   * @param height a double value indicating the height of one of the triangle's sides.
+   * @return the area of a triangle given the base length and height.
+   */
+  public static double triangleArea(final double base, final double height) {
+    return ((base * height) / 2.0d);
   }
 
 }
--- src/test/java/org/cp/elements/lang/MathUtilsTest.java
+++ src/test/java/org/cp/elements/lang/MathUtilsTest.java
@@ -28,14 +28,206 @@ import org.junit.Test;
 /**
  * The MathUtilsTest class is a test suite of test cases testing the contract and functionality of the MathUtils 
  * utility class.
- * <p/>
+ *
  * @author John J. Blum
  * @see org.cp.elements.lang.MathUtils
- * @see org.junit.Assert
  * @see org.junit.Test
  * @since 1.0.0
  */
 public class MathUtilsTest {
+
+  protected void assertEqualIntArrays(final int[] expected, final int[] actual) {
+    assertEquals(expected.length, actual.length);
+
+    for (int index = 0; index < expected.length; index++) {
+      assertEquals(expected[index], actual[index]);
+    }
+  }
+
+  protected int[] toIntArray(final int... numbers) {
+    return numbers;
+  }
+
+  @Test
+  public void testCircleArea() {
+    assertEquals(78.5398d, MathUtils.circleArea(5.0d), 0.0001d);
+  }
+
+  @Test
+  public void testCircleCircumference() {
+    assertEquals(31.41592d, MathUtils.circleCircumference(5.0d), 0.000007d);
+  }
+
+  @Test
+  public void testCircleDiameter() {
+    assertEquals(10.0d, MathUtils.circleDiameter(5.0d), 0.0d);
+  }
+
+  @Test
+  public void testConeVolume() {
+    assertEquals(104.7d, MathUtils.coneVolume(5.0d, 4.0d), 0.02d);
+  }
+
+  @Test
+  public void testCubeSurfaceArea() {
+    assertEquals(150.0d, MathUtils.cubeSurfaceArea(5.0d), 0.0d);
+  }
+
+  @Test
+  public void testCubeVolume() {
+    assertEquals(125.0d, MathUtils.cubeVolume(5.0d), 0.0d);
+  }
+
+  @Test
+  public void testCylinderSurfaceArea() {
+    assertEquals(471.2388d, MathUtils.cylinderSurfaceArea(5.0d, 10.d), 0.000099d);
+  }
+
+  @Test
+  public void testCylinderVolume() {
+    assertEquals(314.1592d, MathUtils.cylinderVolume(5.0d, 4.0d), 0.00007d);
+  }
+
+  @Test
+  public void testEllipseArea() {
+    assertEquals(31.41592d, MathUtils.ellipseArea(5.0d, 2.0d), 0.000007d);
+  }
+
+  @Test
+  public void testEllipsoidVolume() {
+    assertEquals(251.32d, MathUtils.ellipsoidVolume(5.0d, 4.0d, 3.0d), 0.008d);
+  }
+
+  @Test
+  public void testEquilateralTriangleArea() {
+    assertEquals(10.825d, MathUtils.equilateralTriangleArea(5.0d), 0.0004d);
+  }
+
+  @Test
+  public void testFactorial() {
+    assertEquals(1, MathUtils.factorial(0));
+    assertEquals(1, MathUtils.factorial(1));
+    assertEquals(2, MathUtils.factorial(2));
+    assertEquals(6, MathUtils.factorial(3));
+    assertEquals(24, MathUtils.factorial(4));
+    assertEquals(120, MathUtils.factorial(5));
+    assertEquals(720, MathUtils.factorial(6));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testFactorialWithIllegalArgument() {
+    MathUtils.factorial(-1);
+  }
+
+  @Test
+  public void testFibonacciNumber() {
+    assertEquals(0, MathUtils.fibonacciNumber(1));
+    assertEquals(1, MathUtils.fibonacciNumber(2));
+    assertEquals(1, MathUtils.fibonacciNumber(3));
+    assertEquals(2, MathUtils.fibonacciNumber(4));
+    assertEquals(3, MathUtils.fibonacciNumber(5));
+    assertEquals(5, MathUtils.fibonacciNumber(6));
+    assertEquals(8, MathUtils.fibonacciNumber(7));
+    assertEquals(13, MathUtils.fibonacciNumber(8));
+    assertEquals(21, MathUtils.fibonacciNumber(9));
+    assertEquals(514229, MathUtils.fibonacciNumber(30));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testFibonacciNumberWithIllegalArgument() {
+    MathUtils.fibonacciSequence(-1);
+  }
+
+  @Test
+  public void testFibonacciSequence() {
+    assertEqualIntArrays(toIntArray(0), MathUtils.fibonacciSequence(1));
+    assertEqualIntArrays(toIntArray(0, 1), MathUtils.fibonacciSequence(2));
+    assertEqualIntArrays(toIntArray(0, 1, 1), MathUtils.fibonacciSequence(3));
+    assertEqualIntArrays(toIntArray(0, 1, 1, 2, 3), MathUtils.fibonacciSequence(5));
+    assertEqualIntArrays(toIntArray(0, 1, 1, 2, 3, 5, 8, 13), MathUtils.fibonacciSequence(8));
+    assertEqualIntArrays(toIntArray(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144), MathUtils.fibonacciSequence(13));
+    assertEqualIntArrays(toIntArray(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181,
+        6765), MathUtils.fibonacciSequence(21));
+    assertEqualIntArrays(toIntArray(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181,
+      6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811), MathUtils.fibonacciSequence(29));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testFibonacciSequenceWithAnIllegalArgument() {
+    MathUtils.fibonacciSequence(0);
+  }
+
+  @Test
+  public void testIrregularPrismVolume() {
+    assertEquals(20.0d, MathUtils.irregularPrismVolume(5.0d, 4.0d), 0.0d);
+  }
+
+  @Test
+  public void testMax() {
+    assertEquals(Double.NaN, MathUtils.max((double[]) null), 0.0d);
+    assertEquals(0.0d, MathUtils.max(0), 0.0d);
+    assertEquals(0.0d, MathUtils.max(0, 0, 0), 0.0d);
+    assertEquals(2.0d, MathUtils.max(0, 1, 2), 0.0d);
+    assertEquals(2.0d, MathUtils.max(2, 1, 0), 0.0d);
+    assertEquals(2.0d, MathUtils.max(1, 2, 0), 0.0d);
+    assertEquals(1.0d, MathUtils.max(-100, -1, 1), 0.0d);
+    assertEquals(1.0d, MathUtils.max(Double.NaN, -1, 1), 0.0d);
+  }
+
+  @Test
+  public void testMin() {
+    assertEquals(Double.NaN, MathUtils.min((double[]) null), 0.0d);
+    assertEquals(0.0d, MathUtils.min(0), 0.0d);
+    assertEquals(0.0d, MathUtils.min(0, 0, 0), 0.0d);
+    assertEquals(0.0d, MathUtils.min(0, 1, 2), 0.0d);
+    assertEquals(0.0d, MathUtils.min(2, 1, 0), 0.0d);
+    assertEquals(0.0d, MathUtils.min(1, 2, 0), 0.0d);
+    assertEquals(-100.0d, MathUtils.min(-100, -1, 1), 0.0d);
+    assertEquals(-1.0d, MathUtils.min(Double.NaN, -1, 1), 0.0d);
+  }
+
+  @Test
+  public void testMultiply() {
+    assertEquals(0, MathUtils.multiply((int[]) null));
+    assertEquals(0, MathUtils.multiply());
+    assertEquals(0, MathUtils.multiply(0));
+    assertEquals(0, MathUtils.multiply(0, 0, 0));
+    assertEquals(0, MathUtils.multiply(0, 0, 1));
+    assertEquals(1, MathUtils.multiply(1));
+    assertEquals(1, MathUtils.multiply(1, 1, 1));
+    assertEquals(6, MathUtils.multiply(1, 2, 3));
+    assertEquals(64, MathUtils.multiply(2, 4, 8));
+  }
+
+  @Test
+  public void testParallelogramArea() {
+    assertEquals(30.0d, MathUtils.parallelogramArea(5.0d, 6.0d), 0.0d);
+  }
+
+  @Test
+  public void testPyramidVolume() {
+    assertEquals(10.d, MathUtils.pyramidVolume(5.0d, 6.0d), 0.0d);
+  }
+
+  @Test
+  public void testPythagoreanTheorem() {
+    assertEquals(5.0d, MathUtils.pythagoreanTheorem(3.0d, 4.0d), 0.0d);
+  }
+
+  @Test
+  public void testRectangleArea() {
+    assertEquals(40.0d, MathUtils.rectangleArea(5.0d, 8.0d), 0.0d);
+  }
+
+  @Test
+  public void testRectangularPrismSurfaceArea() {
+    assertEquals(94.0d, MathUtils.rectangularPrismSurfaceArea(5.0d, 4.0d, 3.0d), 0.0d);
+  }
+
+  @Test
+  public void testRectanglePrismVolume() {
+    assertEquals(240.0d, MathUtils.rectangularPrismVolume(5.0d, 6.0d, 8.0d), 0.0d);
+  }
 
   @Test
   public void testRoundToNearestTenth() {
@@ -46,6 +238,41 @@ public class MathUtilsTest {
     assertEquals(Double.valueOf(19.4d), Double.valueOf(MathUtils.roundToNearestTenth(19.419d)));
     assertEquals(Double.valueOf(19.4d), Double.valueOf(MathUtils.roundToNearestTenth(19.449d)));
     assertEquals(Double.valueOf(19.6d), Double.valueOf(MathUtils.roundToNearestTenth(19.55d)));
+  }
+
+  @Test
+  public void testSphereSurfaceArea() {
+    assertEquals(314.1592d, MathUtils.sphereSurfaceArea(5.0d), 0.00007d);
+  }
+
+  @Test
+  public void testSphereVolume() {
+    assertEquals(113.09d, MathUtils.sphereVolume(3.0d), 0.008d);
+  }
+
+  @Test
+  public void testSquareArea() {
+    assertEquals(25.0d, MathUtils.squareArea(5.0d), 0.0d);
+  }
+
+  @Test
+  public void testSum() {
+    assertEquals(0, MathUtils.sum((int[]) null));
+    assertEquals(0, MathUtils.sum());
+    assertEquals(0, MathUtils.sum(0));
+    assertEquals(0, MathUtils.sum(0, 0, 0));
+    assertEquals(6, MathUtils.sum(1, 2, 3));
+    assertEquals(21, MathUtils.sum(0, 1, 2, 3, 4, 5, 6));
+  }
+
+  @Test
+  public void testTrapezoidArea() {
+    assertEquals(25.0d, MathUtils.trapezoidArea(10.0d, 2.0d, 3.0d), 0.0d);
+  }
+
+  @Test
+  public void testTriangleArea() {
+    assertEquals(15.0d, MathUtils.triangleArea(5.0d, 6.0d), 0.0d);
   }
 
 }
