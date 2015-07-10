6c60db6d3ddd9b869c925240cf2c028a50481fc6
John Blum <jblum0@gmail.com>
2015-07-10 01:36:07 -0700

    Implemented the ValueHolder class for holding Comparable, Immutable, Non-Null, Serializable or any arbitrary, non-bounded value in a holder type object.

---
+++ src/main/java/org/cp/elements/lang/ValueHolder.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2011-Present. Codeprimate, LLC and authors.  All Rights Reserved.
+ *
+ * This software is licensed under the Codeprimate End User License Agreement (EULA).
+ * This software is proprietary and confidential in addition to an intellectual asset
+ * of the aforementioned authors.
+ *
+ * By using the software, the end-user implicitly consents to and agrees to be in compliance
+ * with all terms and conditions of the EULA.  Failure to comply with the EULA will result in
+ * the maximum penalties permissible by law.
+ *
+ * In short, this software may not be reverse engineered, reproduced, copied, modified
+ * or distributed without prior authorization of the aforementioned authors, permissible
+ * and expressed only in writing.  The authors grant the end-user non-exclusive, non-negotiable
+ * and non-transferable use of the software "as is" without expressed or implied WARRANTIES,
+ * EXTENSIONS or CONDITIONS of any kind.
+ *
+ * For further information on the software license, the end user is encouraged to read
+ * the EULA @ ...
+ */
+
+package org.cp.elements.lang;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+import org.cp.elements.util.ComparatorUtils;
+
+/**
+ * The ValueHolder class is a value holding type.
+ *
+ * @author John Blum
+ * @param <T> the Class type of the value held by this ValueHolder.
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ValueHolder<T> {
+
+  private T value;
+
+  /**
+   * Factory method to construct an instance of the ValueHolder class with a Comparable value.  The ValueHolder
+   * implementation itself implements the Comparable interface.
+   *
+   * @param <T> the Class type of the Comparable value.
+   * @param value the Comparable value to hold.
+   * @return a ValueHolder implementation that holds Comparable values.
+   * @see java.lang.Comparable
+   */
+  public static <T extends Comparable<T>> ComparableValueHolder<T> withComparableValue(final T value) {
+    return new ComparableValueHolder<T>(value);
+  }
+
+  /**
+   * Factory method to construct an instance of the ValueHolder class with an immutable Object value.
+   *
+   * @param <T> the Class type of the Cloneable value.
+   * @param value the Cloneable, immutable value to hold.
+   * @return a ValueHolder implementation that enforces the immutability of values it holds
+   * by way of the Object.clone() method for values that implement the java.lang.Cloneable interface.
+   * @see java.lang.Cloneable
+   */
+  public static <T extends Cloneable> ValueHolder<T> withImmutableValue(final T value) {
+    return new ValueHolder<T>(ObjectUtils.clone(value)) {
+      @Override public T getValue() {
+        return ObjectUtils.clone(super.getValue());
+      }
+
+      @Override public void setValue(final T value) {
+        super.setValue(ObjectUtils.clone(value));
+      }
+    };
+  }
+
+  /**
+   * Factory method to construct an instance of the ValueHolder class with a non-null value.
+   *
+   * @param <T> the Class type of the Object value.
+   * @param value the non-null value to hold.
+   * @return a ValueHolder instance that enforces non-null values.
+   * @throws NullPointerException if the value is null.
+   */
+  public static <T> ValueHolder<T> withNonNullValue(final T value) {
+    Assert.notNull(value, "The value must not be null!");
+
+    return new ValueHolder<T>(value) {
+      @Override
+      public void setValue(final T value) {
+        Assert.notNull(value, "The value must not be null!");
+        super.setValue(value);
+      }
+    };
+  }
+
+  /**
+   * Factory method to construct in instance of the ValueHolder class with a Serializable value.
+   *
+   * @param <T> the Class type of the Serializable value.
+   * @param value the Serializable value to be held.
+   * @return a ValueHolder implementation that holds Serializable values.
+   * @see java.io.Serializable
+   */
+  public static <T extends Serializable> SerializableValueHolder<T> withSerializableValue(final T value) {
+    return new SerializableValueHolder<T>(value);
+  }
+
+  /**
+   * Constructs an instance of the ValueHolder class with a null value.
+   *
+   * @see #ValueHolder(Object)
+   */
+  public ValueHolder() {
+  }
+
+  /**
+   * Constructs an instance of the ValueHolder class initialized with the specified value.
+   *
+   * @param value the Object value held by this ValueHolder.
+   * @see #ValueHolder()
+   */
+  public ValueHolder(final T value) {
+    this.value = value;
+  }
+
+  /**
+   * Gets the value held by this ValueHolder.
+   *
+   * @return the value of this ValueHolder.
+   */
+  public T getValue() {
+    return value;
+  }
+
+  /**
+   * Sets the value to be held by this ValueHolder.
+   *
+   * @param value the value of this ValueHolder.
+   */
+  public void setValue(final T value) {
+    this.value = value;
+  }
+
+  /* (non-Javadoc) */
+  @Override
+  public boolean equals(final Object obj) {
+    if (obj == this) {
+      return true;
+    }
+
+    if (!(obj instanceof ValueHolder)) {
+      return false;
+    }
+
+    ValueHolder that = (ValueHolder) obj;
+
+    return ObjectUtils.equalsIgnoreNull(this.getValue(), that.getValue());
+  }
+
+  /* (non-Javadoc) */
+  @Override
+  public int hashCode() {
+    int hashValue = 17;
+    hashValue = 37 * hashValue + ObjectUtils.hashCode(getValue());
+    return hashValue;
+  }
+
+  /* (non-Javadoc) */
+  @Override
+  public String toString() {
+    return String.valueOf(getValue());
+  }
+
+  /**
+   * The ComparableValueHolder class is an extension of ValueHolder that holds Comparable values.
+   *
+   * @param <T> the Class type of the Comparable value.
+   * @see java.lang.Comparable
+   */
+  public static class ComparableValueHolder<T extends Comparable<T>> extends ValueHolder<T> implements Comparable<T> {
+
+    public ComparableValueHolder() {
+    }
+
+    public ComparableValueHolder(final T value) {
+      super(value);
+    }
+
+    @Override
+    public int compareTo(final T value) {
+      return ComparatorUtils.compareIgnoreNull(getValue(), value);
+    }
+  }
+
+  /**
+   * The SerializableValueHolder class is an extension of ValueHolder that holds Serializable values.
+   *
+   * @param <T> the Class type of the Serializable value.
+   * @see java.io.Serializable
+   */
+  public static class SerializableValueHolder<T extends Serializable> extends ValueHolder<T> implements Serializable {
+
+    private static final long serialVersionUID = 421081248;
+
+    public SerializableValueHolder() {
+    }
+
+    public SerializableValueHolder(final T value) {
+      super(value);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void readObject(final ObjectInputStream in) throws ClassNotFoundException, IOException {
+      setValue((T) in.readObject());
+    }
+
+    private void readObjectNoData() throws ObjectStreamException {
+      setValue(null);
+    }
+
+    private void writeObject(final ObjectOutputStream out) throws IOException {
+      out.writeObject(getValue());
+    }
+  }
+
+}
---
+++ src/test/java/org/cp/elements/lang/ValueHolderTest.java
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2011-Present. Codeprimate, LLC and authors.  All Rights Reserved.
+ *
+ * This software is licensed under the Codeprimate End User License Agreement (EULA).
+ * This software is proprietary and confidential in addition to an intellectual asset
+ * of the aforementioned authors.
+ *
+ * By using the software, the end-user implicitly consents to and agrees to be in compliance
+ * with all terms and conditions of the EULA.  Failure to comply with the EULA will result in
+ * the maximum penalties permissible by law.
+ *
+ * In short, this software may not be reverse engineered, reproduced, copied, modified
+ * or distributed without prior authorization of the aforementioned authors, permissible
+ * and expressed only in writing.  The authors grant the end-user non-exclusive, non-negotiable
+ * and non-transferable use of the software "as is" without expressed or implied WARRANTIES,
+ * EXTENSIONS or CONDITIONS of any kind.
+ *
+ * For further information on the software license, the end user is encouraged to read
+ * the EULA @ ...
+ */
+
+package org.cp.elements.lang;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Calendar;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * The ValueHolderTest class is a test suite of test cases testing the contract and functionality
+ * of the ValueHolder class.
+ *
+ * @author John J. Blum
+ * @see org.cp.elements.lang.ValueHolder
+ * @see org.junit.Test
+ * @since 1.0.0
+ */
+public class ValueHolderTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void constructWithNullValue() {
+    ValueHolder<Object> valueHolder = new ValueHolder<Object>(null);
+
+    assertThat(valueHolder, is(not(nullValue())));
+    assertThat(valueHolder.getValue(), is(nullValue()));
+  }
+
+  @Test
+  public void constructWithNonNullValue() {
+    ValueHolder<String> valueHolder = new ValueHolder<String>("test");
+
+    assertThat(valueHolder, is(not(nullValue())));
+    assertThat(valueHolder.getValue(), is(equalTo("test")));
+  }
+
+  @Test
+  public void setAndGetValue() {
+    ValueHolder<String> valueHolder = new ValueHolder<String>();
+
+    assertThat(valueHolder.getValue(), is(nullValue(String.class)));
+
+    valueHolder.setValue("test");
+
+    assertThat(valueHolder.getValue(), is(equalTo("test")));
+
+    valueHolder.setValue(null);
+
+    assertThat(valueHolder.getValue(), is(nullValue()));
+
+    valueHolder.setValue("mock");
+
+    assertThat(valueHolder.getValue(), is(equalTo("mock")));
+  }
+
+  @Test
+  public void equalValues() {
+    ValueHolder<String> testOne = new ValueHolder<String>("test");
+    ValueHolder<String> testTwo = new ValueHolder<String>("test");
+
+    assertThat(testOne, is(not(sameInstance(testTwo))));
+    assertThat(testOne, is(equalTo(testTwo)));
+  }
+
+  @Test
+  public void unequalValues() {
+    ValueHolder<String> testOne = new ValueHolder<String>("test");
+    ValueHolder<String> mockOne = new ValueHolder<String>("mock");
+
+    assertThat(testOne, is(not(sameInstance(mockOne))));
+    assertThat(testOne, is(not(equalTo(mockOne))));
+  }
+
+  @Test
+  public void hashCodeValue() {
+    ValueHolder<String> valueHolder = new ValueHolder<String>("test");
+
+    assertThat(valueHolder.hashCode(), is(not(equalTo(0))));
+    assertThat(valueHolder.hashCode(), is(not(equalTo("test".hashCode()))));
+  }
+
+  @Test
+  public void testToString() {
+    ValueHolder<Object> valueHolder = new ValueHolder<Object>("test");
+
+    assertThat(valueHolder.toString(), is(equalTo("test")));
+
+    valueHolder.setValue(2l);
+
+    assertThat(valueHolder.toString(), is(equalTo("2")));
+
+    valueHolder.setValue(Math.PI);
+
+    assertThat(valueHolder.toString(), is(equalTo(String.valueOf(Math.PI))));
+
+    valueHolder.setValue(true);
+
+    assertThat(valueHolder.toString(), is(equalTo("true")));
+  }
+
+  @Test
+  public void withComparableValue() {
+    ValueHolder.ComparableValueHolder<String> valueOne = ValueHolder.withComparableValue("one");
+
+    assertThat(valueOne, is(instanceOf(Comparable.class)));
+    assertThat(valueOne.getValue(), is(instanceOf(Comparable.class)));
+    assertThat(valueOne.getValue(), is(equalTo("one")));
+    assertTrue(valueOne.compareTo("two") < 0);
+    assertTrue(valueOne.compareTo("none") > 0);
+    assertTrue(valueOne.compareTo("one") == 0);
+  }
+
+  @Test
+  public void withImmutableValue() {
+    Calendar today = Calendar.getInstance();
+    ValueHolder<Calendar> dayHolder = ValueHolder.withImmutableValue(today);
+
+    assertThat(dayHolder, is(not(nullValue())));
+
+    Calendar todayClone = dayHolder.getValue();
+
+    assertThat(todayClone, is(instanceOf(Calendar.class)));
+    assertThat(todayClone, is(not(sameInstance(today))));
+    assertThat(todayClone, is(equalTo(today)));
+
+    todayClone.add(Calendar.DAY_OF_MONTH, -1); // yesterday
+
+    Calendar anotherTodayClone = dayHolder.getValue();
+
+    assertThat(anotherTodayClone, is(instanceOf(Calendar.class)));
+    assertThat(anotherTodayClone, is(not(sameInstance(todayClone))));
+    assertThat(anotherTodayClone, is(not(equalTo(todayClone))));
+    assertThat(anotherTodayClone, is(not(sameInstance(today))));
+    assertThat(anotherTodayClone, is(equalTo(today)));
+
+    Calendar tomorrow = Calendar.getInstance();
+    tomorrow.add(Calendar.DAY_OF_MONTH, 1);
+    dayHolder.setValue(tomorrow);
+
+    Calendar tomorrowClone = dayHolder.getValue();
+
+    assertThat(tomorrowClone, is(instanceOf(Calendar.class)));
+    assertThat(tomorrowClone, is(not(sameInstance(tomorrow))));
+    assertThat(tomorrowClone, is(equalTo(tomorrow)));
+
+    Calendar tomorrowCopy = (Calendar) tomorrow.clone();
+
+    tomorrow.add(Calendar.YEAR, 1);
+
+    Calendar anotherTomorrowClone = dayHolder.getValue();
+
+    assertThat(anotherTomorrowClone, is(instanceOf(Calendar.class)));
+    assertThat(anotherTomorrowClone, is(not(sameInstance(tomorrow))));
+    assertThat(anotherTomorrowClone, is(not(equalTo(tomorrow))));
+    assertThat(anotherTomorrowClone, is(not(sameInstance(tomorrowCopy))));
+    assertThat(anotherTomorrowClone, is(equalTo(tomorrowCopy)));
+  }
+
+  @Test
+  public void withNonNullValue() {
+    ValueHolder<String> valueHolder = ValueHolder.withNonNullValue("test");
+
+    assertThat(valueHolder, is(not(nullValue())));
+    assertThat(valueHolder.getValue(), is(equalTo("test")));
+
+    valueHolder.setValue("null");
+
+    assertThat(valueHolder.getValue(), is(equalTo("null")));
+
+    valueHolder.setValue("mock");
+
+    assertThat(valueHolder.getValue(), is(equalTo("mock")));
+
+    valueHolder.setValue("nil");
+
+    assertThat(valueHolder.getValue(), is(equalTo("nil")));
+  }
+
+  @Test
+  public void withNonNullValueHolderConstructedWithNullValue() {
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage("The value must not be null!");
+
+    ValueHolder.withNonNullValue(null);
+  }
+
+  @Test
+  public void withNonNullValueHolderSettingNullValue() {
+    ValueHolder<String> valueHolder = ValueHolder.withNonNullValue("test");
+
+    assertThat(valueHolder, is(not(nullValue())));
+    assertThat(valueHolder.getValue(), is(equalTo("test")));
+
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage("The value must not be null!");
+
+    valueHolder.setValue(null);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void withSerializableValue() throws Exception {
+    ValueHolder<Integer> twentyOne = ValueHolder.withSerializableValue(21);
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    ObjectOutputStream objectOut = new ObjectOutputStream(out);
+
+    objectOut.writeObject(twentyOne);
+    objectOut.flush();
+    objectOut.close();
+
+    byte[] twentyOneBytes = out.toByteArray();
+
+    assertThat(twentyOneBytes, is(not(nullValue())));
+    assertThat(twentyOneBytes.length, is(not(equalTo(0))));
+
+    ObjectInputStream objectIn = new ObjectInputStream(new ByteArrayInputStream(twentyOneBytes));
+
+    ValueHolder<Integer> twentyOneCopy = (ValueHolder<Integer>) objectIn.readObject();
+
+    objectIn.close();
+
+    assertThat(twentyOneCopy, is(not(nullValue())));
+    assertThat(twentyOneCopy, is(not(sameInstance(twentyOne))));
+    assertThat(twentyOneCopy, is(equalTo(twentyOne)));
+  }
+
+}
