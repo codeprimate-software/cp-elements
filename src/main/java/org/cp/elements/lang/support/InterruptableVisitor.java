3b4883eeac5e12f506b0290c7fccaf14032ea3e8
John Blum <jblum0@gmail.com>
2015-12-29 23:55:56 -0800

    Create the InterruptableVisitor class in the org.cp.elements.lang.support package implementing the Visitor interface to traverse an object graph interrupting any objects the implement the Interruptable interface.

---
+++ src/main/java/org/cp/elements/lang/support/InterruptableVisitor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import org.cp.elements.lang.Interruptable;
+import org.cp.elements.lang.Visitable;
+import org.cp.elements.lang.Visitor;
+
+/**
+ * The InterruptableVisitor class is a {@link Visitor} implementation that visits and object graph and interrupts
+ * any {@link Interruptable} objects in the graph visited.
+ *
+ * @author John J. Blum
+ * @see org.cp.elements.lang.Interruptable
+ * @see org.cp.elements.lang.Visitable
+ * @see org.cp.elements.lang.Visitor
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class InterruptableVisitor implements Visitor {
+
+  /**
+   * Visits {@link Visitable} objects in a object graph in search of {@link Interruptable} objects
+   * in order to interrupt them.
+   *
+   * @param visitable the Visitable object visited by this Visitor.
+   * @see org.cp.elements.lang.Visitable
+   */
+  @Override
+  public void visit(final Visitable visitable) {
+    if (visitable instanceof Interruptable) {
+      ((Interruptable) visitable).interrupt();
+    }
+  }
+
+}
---
+++ src/test/java/org/cp/elements/lang/support/InterruptableVisitorTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.cp.elements.lang.Interruptable;
+import org.cp.elements.lang.Visitable;
+import org.junit.Test;
+
+/**
+ * The InterruptableVisitorTest class is a test suite of test cases testing the contract and functionality
+ * of the InterruptableVisitor class.
+ *
+ * @author John J. Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.support.InterruptableVisitor
+ * @since 1.0.0
+ */
+public class InterruptableVisitorTest {
+
+  private InterruptableVisitor visitor = new InterruptableVisitor();
+
+  @Test
+  public void visitNonInterruptableObjectWithNoConsequences() {
+    visitor.visit(mock(Visitable.class));
+  }
+
+  @Test
+  public void visitNullWithNoConsequences() {
+    visitor.visit(null);
+  }
+
+  @Test
+  public void visitInterruptableObjectInvokesInterrupt() {
+    VisitableInterruptable mockInterruptable = mock(VisitableInterruptable.class);
+
+    visitor.visit(mockInterruptable);
+
+    verify(mockInterruptable, times(1)).interrupt();
+  }
+
+  interface VisitableInterruptable extends Interruptable, Visitable {
+  }
+
+}
