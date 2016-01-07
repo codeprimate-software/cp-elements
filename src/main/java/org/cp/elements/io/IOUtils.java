9148546233c13991ec6bd265c8b9a0f29a8728aa
John Blum <jblum0@gmail.com>
2016-01-07 01:24:16 -0800

    Review, refactor and enhance the org.cp.elements.io.IOUtils class and associated test class. Add the copy(..), deserializeObject(:byte[]), deserializeObject(:byte[], :ClassLoader), serializeObject(:Object} and toByteArray(..) methods.

--- src/main/java/org/cp/elements/io/IOUtils.java
+++ src/main/java/org/cp/elements/io/IOUtils.java
@@ -16,24 +16,38 @@
 
 package org.cp.elements.io;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
+import java.io.OutputStream;
 
+import org.cp.elements.lang.Assert;
 import org.cp.elements.lang.NullSafe;
+import org.cp.elements.lang.ObjectUtils;
 
 /**
  * The IOUtils class provides basic input and output utility operations.
  *
  * @author John J. Blum
+ * @see java.io.InputStream
+ * @see java.io.OutputStream
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
 public abstract class IOUtils {
 
+  protected static final int DEFAULT_BUFFER_SIZE = 32768;
+
   /**
-   * Attempts to close the Closeable object ignoring any IOException that may occur as a result of the close operation.
+   * Attempts to close the {@link Closeable} object, ignoring any {@link IOException} that may occur as a result
+   * of the close operation.
    *
-   * @param obj the Closeable object who's close method will be called.
+   * @param obj the {@link Closeable} object who's {@code close} method will be called.
    * @return a boolean value indicating if the close operation was successful or not.
    * @see java.io.Closeable
    */
@@ -49,6 +63,174 @@ public abstract class IOUtils {
     }
 
     return false;
+  }
+
+  /**
+   * Copies the contents of the source {@link InputStream} to the target {@link OutputStream}.
+   *
+   * @param in the source {@link InputStream} to copy bytes from.
+   * @param out the target {@link OutputStream} to copy bytes to.
+   * @throws IOException if the copy operation results in an I/O error.
+   * @see java.io.InputStream
+   * @see java.io.OutputStream
+   */
+  public static void copy(final InputStream in, final OutputStream out) throws IOException {
+    byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+    for (int length = in.read(buffer); length > 0; length = in.read(buffer)) {
+      out.write(buffer, 0, length);
+      out.flush();
+    }
+  }
+
+  /**
+   * Deserializes the given byte array back into an {@link Object}.
+   *
+   * @param serializedObjectBytes an array containing the bytes of a serialized object.
+   * @return a {@link java.io.Serializable} object from the array of bytes.
+   * @throws ClassNotFoundException if the class type of the serialized object cannot be resolved.
+   * @throws IOException if an I/O error occurs during the deserialization process.
+   * @throws NullPointerException if the serialized object byte array is null.
+   * @see #deserializeObject(byte[], ClassLoader)
+   * @see #serializeObject(Object)
+   * @see java.io.ByteArrayInputStream
+   * @see java.io.ObjectInputStream
+   * @see java.io.Serializable
+   */
+  public static Object deserializeObject(final byte[] serializedObjectBytes) throws ClassNotFoundException, IOException {
+    ObjectInputStream in = null;
+
+    try {
+      in = new ObjectInputStream(new ByteArrayInputStream(serializedObjectBytes));
+      return in.readObject();
+    }
+    finally {
+      close(in);
+    }
+  }
+
+  /**
+   * Deserializes the given byte array back into an {@link Object} who's {@link Class} type is resolvable
+   * with the specified {@link ClassLoader}.
+   *
+   * @param serializedObjectBytes an array containing the bytes of a serialized object.
+   * @param classLoader the Java {@link ClassLoader} used to resolve the {@link Class} type
+   * of the serialized {@link Object}.
+   * @return a {@link java.io.Serializable} object from the array of bytes.
+   * @throws ClassNotFoundException if the class type of the serialized object cannot be resolved
+   * by the specified Java {@link ClassLoader}.
+   * @throws IOException if an I/O error occurs while deserializing the object from the array of bytes.
+   * @throws NullPointerException if the serialized object byte array is null.
+   * @see #deserializeObject(byte[])
+   * @see #serializeObject(Object)
+   * @see IOUtils.ClassLoaderObjectInputStream
+   * @see java.lang.ClassLoader
+   * @see java.io.ByteArrayInputStream
+   * @see java.io.ObjectInputStream
+   * @see java.io.Serializable
+   */
+  public static Object deserializeObject(final byte[] serializedObjectBytes, final ClassLoader classLoader)
+    throws ClassNotFoundException, IOException
+  {
+    ObjectInputStream in = null;
+
+    try {
+      in = new ClassLoaderObjectInputStream(new ByteArrayInputStream(serializedObjectBytes), classLoader);
+      return in.readObject();
+    }
+    finally {
+      close(in);
+    }
+  }
+
+  /**
+   * Serializes the given {@link java.io.Serializable} object into an array fo bytes.
+   *
+   * @param obj the {@link java.io.Serializable} object to serialize into an array of bytes.
+   * @return the byte array of the serialized object.
+   * @throws IOException if an I/O error occurs during the serialization process.
+   * @see #deserializeObject(byte[])
+   * @see java.io.ByteArrayOutputStream
+   * @see java.io.ObjectOutputStream
+   * @see java.io.Serializable
+   */
+  public static byte[] serializeObject(final Object obj) throws IOException {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ObjectOutputStream objOut = null;
+
+    try {
+      objOut = new ObjectOutputStream(out);
+      objOut.writeObject(obj);
+      objOut.flush();
+
+      return out.toByteArray();
+    }
+    finally {
+      close(objOut);
+    }
+  }
+
+  /**
+   * Reads the contents of the specified @{link InputStream} into a byte array.
+   *
+   * @param in the {@link InputStream} to read content from.
+   * @return a byte array containing the contents of the given {@link InputStream}.
+   * @throws IOException if an I/O error occurs while reading the {@link InputStream}.
+   * @see java.io.ByteArrayOutputStream
+   * @see java.io.InputStream
+   */
+  @NullSafe
+  public static byte[] toByteArray(final InputStream in) throws IOException {
+    Assert.notNull(in, "The InputStream from which to read bytes from cannot be null");
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+    int bytesRead;
+
+    try {
+      while ((bytesRead = in.read(buffer)) != -1) {
+        out.write(buffer, 0, bytesRead);
+        out.flush();
+      }
+    }
+    finally {
+      close(in);
+      close(out);
+    }
+
+    return out.toByteArray();
+  }
+
+  /**
+   * The ClassLoaderObjectInputStream class is a {@link ObjectInputStream} implementation that resolves
+   * the {@link Class} type of the {@link Object} being deserialized with the specified Java {@link ClassLoader}.
+   *
+   * @see java.lang.ClassLoader
+   * @see java.lang.Thread#getContextClassLoader()
+   * @see java.io.ObjectInputStream
+   */
+  protected static class ClassLoaderObjectInputStream extends ObjectInputStream {
+
+    private final ClassLoader classLoader;
+
+    /* (non-Javadoc) */
+    public ClassLoaderObjectInputStream(final InputStream in, final ClassLoader classLoader) throws IOException {
+      super(in);
+      this.classLoader = ObjectUtils.defaultIfNull(classLoader, Thread.currentThread().getContextClassLoader());
+    }
+
+    /* (non-Javadoc) */
+    protected ClassLoader getClassLoader() {
+      return classLoader;
+    }
+
+    /* (non-Javadoc) */
+    @Override
+    protected Class<?> resolveClass(final ObjectStreamClass descriptor) throws ClassNotFoundException , IOException {
+      return Class.forName(descriptor.getName(), false, getClassLoader());
+    }
   }
 
 }
--- src/test/java/org/cp/elements/io/IOUtilsTest.java
+++ src/test/java/org/cp/elements/io/IOUtilsTest.java
@@ -16,49 +16,151 @@
 
 package org.cp.elements.io;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
 
-import org.cp.elements.test.AbstractMockingTestSuite;
-import org.jmock.Expectations;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.invocation.InvocationOnMock;
 
 /**
- * The IOUtilsTest class is a test suite for testing the contract and functionality of the IOUtils class.
+ * The IOUtilsTest class is a test suite of test cases testing the contract and functionality of the IOUtils class.
  *
  * @author John J. Blum
- * @since 1.0.0
- * @see org.cp.elements.io.IOUtils
- * @see org.cp.elements.test.AbstractMockingTestSuite
- * @see org.easymock.EasyMock
+ * @see java.io.Closeable
+ * @see java.io.InputStream
+ * @see java.io.OutputStream
+ * @see org.junit.Rule
  * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.io.IOUtils
+ * @since 1.0.0
  */
-public class IOUtilsTest extends AbstractMockingTestSuite {
+public class IOUtilsTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
 
   @Test
-  public void testClose() throws Exception {
-    final Closeable mockCloseable = mockContext.mock(Closeable.class);
-
-    mockContext.checking(new Expectations() {{
-      oneOf(mockCloseable).close();
-    }});
-
+  public void closeIsSuccessful() throws Exception {
+    Closeable mockCloseable = mock(Closeable.class);
     assertTrue(IOUtils.close(mockCloseable));
+    verify(mockCloseable, times(1)).close();
   }
 
   @Test
-  public void testCloseThrowsIOException() throws Exception {
-    final Closeable mockCloseable = mockContext.mock(Closeable.class);
+  public void closeThrowsIOException() throws Exception {
+    Closeable mockCloseable = mock(Closeable.class);
 
-    mockContext.checking(new Expectations() {{
-      oneOf(mockCloseable).close();
-      will(throwException(new IOException("i/o error!")));
-    }});
+    doThrow(new IOException("test")).when(mockCloseable).close();
 
     assertFalse(IOUtils.close(mockCloseable));
+
+    verify(mockCloseable, times(1)).close();
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void copyIsSuccessful() throws IOException {
+    InputStream mockInputStream = mock(InputStream.class);
+    OutputStream mockOutputStream = mock(OutputStream.class);
+
+    when(mockInputStream.read(any(byte[].class))).thenReturn(IOUtils.DEFAULT_BUFFER_SIZE).thenReturn(8192).thenReturn(0);
+
+    IOUtils.copy(mockInputStream, mockOutputStream);
+
+    verify(mockInputStream, times(3)).read(any(byte[].class));
+    verify(mockOutputStream, times(1)).write(any(byte[].class), eq(0), eq(IOUtils.DEFAULT_BUFFER_SIZE));
+    verify(mockOutputStream, times(1)).write(any(byte[].class), eq(0), eq(8192));
+    verify(mockOutputStream, never()).write(any(byte[].class), eq(0), eq(0));
+  }
+
+  @Test
+  public void copyActualIsSuccessful() throws IOException {
+    InputStream source = new ByteArrayInputStream("This is a test!".getBytes());
+    OutputStream target = new ByteArrayOutputStream(source.available());
+
+    IOUtils.copy(source, target);
+
+    assertThat(new String(((ByteArrayOutputStream) target).toByteArray(), Charset.forName("UTF-8")),
+      is(equalTo("This is a test!")));
+  }
+
+  @Test
+  public void serializationDeserializationIsSuccessful() throws ClassNotFoundException, IOException {
+    assertThat(IOUtils.deserializeObject(IOUtils.serializeObject("TEST")), is(equalTo("TEST")));
+  }
+
+  @Test
+  public void serializationDeserializationWithClassLoaderIsSuccessful() throws ClassNotFoundException, IOException {
+    assertThat(IOUtils.deserializeObject(IOUtils.serializeObject("TEST"), Thread.currentThread().getContextClassLoader()),
+      is(equalTo("TEST")));
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void toByteArrayIsSuccessful() throws IOException {
+    InputStream mockInputStream = mock(InputStream.class);
+
+    when(mockInputStream.read(any(byte[].class))).thenAnswer((InvocationOnMock invocation) -> {
+      byte[] buffer = invocation.getArgumentAt(0, byte[].class);
+
+      buffer[0] = (byte) 0xCA;
+      buffer[1] = (byte) 0xFE;
+      buffer[2] = (byte) 0xBA;
+      buffer[3] = (byte) 0xBE;
+
+      return 4;
+    }).thenAnswer((InvocationOnMock invocation) -> -1);
+
+    byte[] bytes = IOUtils.toByteArray(mockInputStream);
+
+    assertThat(bytes, is(notNullValue()));
+    assertThat(bytes.length, is(equalTo(4)));
+    assertThat(bytes[0], is(equalTo((byte) 0xCA)));
+    assertThat(bytes[1], is(equalTo((byte) 0xFE)));
+    assertThat(bytes[2], is(equalTo((byte) 0xBA)));
+    assertThat(bytes[3], is(equalTo((byte) 0xBE)));
+
+    verify(mockInputStream, times(2)).read(any(byte[].class));
+    verify(mockInputStream, times(1)).close();
+  }
+
+  @Test
+  public void toByteArrayThrowsIOException() throws IOException {
+    InputStream mockInputStream = mock(InputStream.class);
+
+    when(mockInputStream.read(any(byte[].class))).thenThrow(new IOException("test"));
+
+    expectedException.expect(IOException.class);
+    expectedException.expectCause(is(nullValue(Throwable.class)));
+    expectedException.expectMessage("test");
+
+    IOUtils.toByteArray(mockInputStream);
+
+    verify(mockInputStream, times(1)).close();
   }
 
 }
