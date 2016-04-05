b0ff03d94d1c383ac469533e2de9b472018e6e30
John Blum <jblum0@gmail.com>
2016-04-04 23:56:11 -0700

    Create the PropertiesAdapter class in the org.cp.elements.util package for adapting and wrapping a standard Java API java.util.Properties object for convenient property access operations.  Create a PropertiesBuilder class in the org.cp.elements.util package for conveniently constructing an instance of a Java API java.util.Properties object using the Builder design pattern.

---
+++ src/main/java/org/cp/elements/util/PropertiesAdapter.java
@@ -0,0 +1,242 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.util;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Filter;
+import org.cp.elements.util.convert.ConversionService;
+import org.cp.elements.util.convert.provider.DefaultConversionService;
+
+/**
+ * The PropertiesAdapter class is a wrapper around a {@link java.util.Properties} object encapsulating functionality
+ * to conveniently access properties and convert to a specific value type.
+ *
+ * @author John J. Blum
+ * @see java.lang.Iterable
+ * @see java.util.Properties
+ * @see org.cp.elements.util.convert.ConversionService
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class PropertiesAdapter implements Iterable<String> {
+
+  private final ConversionService conversionService;
+
+  private final Properties delegate;
+
+  /**
+   * Factory method to get an instance of the PropertiesAdapter class initialized with the given {@link Properties}.
+   *
+   * @param properties the {@link Properties} to wrap.
+   * @return an instance of the {@link PropertiesAdapter} initialized with the given {@link Properties}.
+   */
+  public static PropertiesAdapter from(Properties properties) {
+    return new PropertiesAdapter(properties);
+  }
+
+  /**
+   * Constructs an instance of the PropertiesAdapter class initialized with the given {@link Properties}.
+   *
+   * @param properties the {@link Properties} to wrap with this wrapper.
+   */
+  public PropertiesAdapter(Properties properties) {
+    Assert.notNull(properties, "The Properties to wrap cannot be null");
+    this.delegate = properties;
+    this.conversionService = new DefaultConversionService();
+  }
+
+  /**
+   * Returns a reference to the {@link ConversionService} used to convert the property value
+   * into a value of the desired class type.
+   *
+   * @return a reference to the {@link ConversionService} used ot convert the property value
+   * into a value of the desired class type.
+   * @see org.cp.elements.util.convert.ConversionService
+   */
+  protected ConversionService getConversionService() {
+    return conversionService;
+  }
+
+  /**
+   * Returns the {@link Properties} wrapped by this wrapper.
+   *
+   * @return the {@link Properties} wrapped by this wrapper.
+   * @see java.util.Properties
+   */
+  public Properties getProperties() {
+    return delegate;
+  }
+
+  /**
+   * Determines whether the named property is present in this {@link Properties} adapter.
+   *
+   * @param propertyName the name of the property to check for presence in this adapter.
+   * @return a boolean value indicating whether the named property is present in this set of {@link Properties}.
+   * @see java.util.Properties#containsKey(Object)
+   * @see #getProperties()
+   */
+  public boolean contains(String propertyName) {
+    return getProperties().containsKey(propertyName);
+  }
+
+  /**
+   * Converts the value of the named property into an instance of the given {@link Class} type.
+   *
+   * @param <T> Class type of the return value.
+   * @param propertyName the name of the property to get.
+   * @param type the desired {@link Class} type to convert the value of the named property to.
+   * @return the value of the named property converted to an instance of the given {@link Class} type.
+   * @see org.cp.elements.util.convert.ConversionService#convert(Object, Class)
+   * @see java.lang.Class
+   * @see #getConversionService()
+   * @see #get(String) \
+   */
+  protected <T> T convert(String propertyName, Class<T> type) {
+    return getConversionService().convert(get(propertyName), type);
+  }
+
+  /**
+   * Defaults of the value for the named property if the property does not exist.
+   *
+   * @param <T> {@link Class} type of the return value.
+   * @param propertyName the name of the property to get.
+   * @param defaultValue the default value to return if the named property does not exist.
+   * @param type the desired {@link Class} type of the named property value.
+   * @return the value of the named property as a instance of the specified {@link Class} type
+   * or return the default value if the named property does not exist.
+   * @see java.lang.Class
+   * @see #contains(String)
+   * @see #convert(String, Class)
+   */
+  protected <T> T defaultIfNotExists(String propertyName, T defaultValue, Class<T> type) {
+    return (contains(propertyName) ? convert(propertyName, type) : defaultValue);
+  }
+
+  /**
+   * Filters the properties from this adapter by name.
+   *
+   * @param filter the {@link Filter} used to filter the properties of this adapter.
+   * @return a newly constructed instance of the {@link PropertiesAdapter} containing only the filtered properties.
+   * @see org.cp.elements.lang.Filter
+   * @see java.util.Properties
+   */
+  public PropertiesAdapter filter(Filter<String> filter) {
+    Properties properties = new Properties();
+
+    for (String propertyName : this) {
+      if (filter.accept(propertyName)) {
+        properties.setProperty(propertyName, get(propertyName));
+      }
+    }
+
+    return from(properties);
+  }
+
+  /**
+   * Gets the assigned value of named property as a {@link String}.
+   *
+   * @param propertyName the name of the property to get.
+   * @return the assigned value of the named property as a {@link String}.
+   * @see #get(String, String)
+   */
+  public String get(String propertyName) {
+    return get(propertyName, null);
+  }
+
+  /**
+   * Gets the assigned value of the named property as a {@link String} or returns the default value
+   * if the named property does not exist.
+   *
+   * @param propertyName the name of the property to get.
+   * @param defaultValue the default value to return if the named property does not exist.
+   * @return the assigned value of the named property as a {@link String} or default value
+   * if the named property does not exist.
+   * @see java.util.Properties#getProperty(String)
+   * @see #getProperties()
+   */
+  public String get(String propertyName, String defaultValue) {
+    return getProperties().getProperty(propertyName, defaultValue);
+  }
+
+  /**
+   * Gets the assigned value of the named property as an instance of the specified {@link Class} type.
+   *
+   * @param <T> {@link Class} type of the return value.
+   * @param propertyName the name of the property to get.
+   * @return the assigned value of the named property as an instance of the specified {@link Class} type.
+   * @see #getAsType(String, Object, Class)
+   */
+  public <T> T getAsType(String propertyName, Class<T> type) {
+    return getAsType(propertyName, null, type);
+  }
+
+  /**
+   * Gets the assigned value of the named property as an instance of the specified {@link Class} type
+   * or the default value if the named property does not exist.
+   *
+   * @param <T> {@link Class} type of the return value.
+   * @param propertyName the name of the property to get.
+   * @param defaultValue the default value to return if the named property does not exist.
+   * @return the assigned value of the named property as an instance of the specified {@link Class} type
+   * or the default value if the named property does not exist.
+   * @see #defaultIfNotExists(String, Object, Class)
+   * @see java.lang.Class
+   */
+  public <T> T getAsType(String propertyName, T defaultValue, Class<T> type) {
+    return defaultIfNotExists(propertyName, defaultValue, type);
+  }
+
+  /**
+   * Determines whether this {@link Properties} object contains any properties.
+   *
+   * @return a boolean value indicating whether this {@link Properties} object contains any properties.
+   * @see java.util.Properties#isEmpty()
+   * @see #getProperties()
+   */
+  public boolean isEmpty() {
+    return getProperties().isEmpty();
+  }
+
+  /**
+   * Iterates the property names in this {@link Properties} object.
+   *
+   * @return an {@link Iterator} to iterate the name of the properties in this {@link Properties} object.
+   * @see java.util.Properties#stringPropertyNames()
+   * @see java.util.Iterator
+   * @see #getProperties()
+   */
+  @Override
+  public Iterator<String> iterator() {
+    return Collections.unmodifiableSet(getProperties().stringPropertyNames()).iterator();
+  }
+
+  /**
+   * Returns the number of properties in this {@link Properties} object.
+   *
+   * @return an integer value indicating the number of properties in this {@link Properties} object.
+   * @see java.util.Properties#size()
+   * @see #getProperties()
+   */
+  public int size() {
+    return getProperties().size();
+  }
+
+}
--- src/main/java/org/cp/elements/util/PropertiesBuilder.java
+++ src/main/java/org/cp/elements/util/PropertiesBuilder.java
@@ -16,10 +16,18 @@
 
 package org.cp.elements.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Map;
 import java.util.Properties;
 
+import org.cp.elements.io.NoSuchFileException;
+
 /**
- * The PropertiesBuilder class is a
+ * The PropertiesBuilder class is a Builder object for constructing an instance of the {@link Properties} class.
  *
  * @author John J. Blum
  * @see java.util.Properties
@@ -30,27 +38,148 @@ public class PropertiesBuilder {
 
   private final Properties properties;
 
+  /**
+   * Factory method to load the {@link Properties} contained in the specified {@link File}.
+   *
+   * @param properties the properties {@link File} to load.
+   * @return an instance of the {@link PropertiesBuilder} class initialized with the properties
+   * contained in the given {@link File}.
+   * @throws NoSuchFileException if the specified {@link File} cannot be found.
+   * @throws SystemException if the properties from the specified {@link File} cannot be loaded.
+   * @see java.util.Properties#load(Reader)
+   * @see java.io.File
+   * @see #from(Properties)
+   */
+  public static PropertiesBuilder from(File properties) {
+    try {
+      Properties defaults = new Properties();
+      defaults.load(new FileReader(properties));
+      return from(defaults);
+    }
+    catch (FileNotFoundException e) {
+      throw new NoSuchFileException(String.format("[$1%s] not found", properties), e);
+    }
+    catch (IOException e) {
+      throw new SystemException(String.format("failed to load properties from file [%1$s]", properties));
+    }
+  }
+
+  /**
+   * Factory method to initialize a new instance of the {@link PropertiesBuilder} with the key/values
+   * from the given {@link Map}.
+   *
+   * @param map the {@link Map} used to initialize the new {@link PropertiesBuilder}.
+   * @return a new {@link PropertiesBuilder} initialized with the key/values from the given {@link Map}.
+   * @see java.util.Properties
+   * @see java.util.Map
+   * @see #from(Properties)
+   */
+  public static PropertiesBuilder from(Map<String, Object> map) {
+    Properties properties = new Properties();
+
+    for (Map.Entry<String, Object> entry : map.entrySet()) {
+      properties.setProperty(entry.getKey(), String.valueOf(entry.getValue()));
+    }
+
+    return from(properties);
+  }
+
+  /**
+   * Factory method to construct a new instance of the {@link PropertiesBuilder} class
+   * initialized with the specified {@link Properties}.
+   *
+   * @param properties the {@link Properties} used to initialize the {@link PropertiesBuilder}.
+   * @return a newly constructed {@link PropertiesBuilder} initialized with the specified {@link Properties}.
+   * @see java.util.Properties
+   * @see #PropertiesBuilder(Properties)
+   */
+  public static PropertiesBuilder from(Properties properties) {
+    return new PropertiesBuilder(properties);
+  }
+
+  /**
+   * Factory method to construct a new isntance of the {@link PropertiesBuilder} class initialized
+   * from the System properties.
+   *
+   * @return a newly constructed {@link PropertiesBuilder} initialized with the System properties.
+   * @see java.lang.System#getProperties()
+   * @see #from(Properties)
+   */
+  public static PropertiesBuilder fromSystemProperties() {
+    return from(System.getProperties());
+  }
+
+  /**
+   * Constructs an instance of the PropertiesBuilder class with an empty set of {@link Properties}.
+   */
   public PropertiesBuilder() {
     properties = new Properties();
   }
 
-  public PropertiesBuilder(final Properties defaults) {
+  /**
+   * Constructs an instance of the PropertiesBuilder class initialized with the given set of default {@link Properties}.
+   *
+   * @param defaults the {@link Properties} used as defaults.
+   * @see java.util.Properties
+   */
+  public PropertiesBuilder(Properties defaults) {
     properties = new Properties(defaults);
   }
 
+  /**
+   * Returns the raw {@link Properties} object being constructed by this builder.
+   *
+   * @return a reference to the {@link Properties} object under construction.
+   * @see java.util.Properties
+   */
   protected Properties getProperties() {
     return properties;
   }
 
-  public PropertiesBuilder setProperty(final String name, final String value) {
-    getProperties().setProperty(name, value);
+  /**
+   * Sets the named property to the given value.
+   *
+   * @param propertyName the name of the property to set.
+   * @param propertyValue the value to assign to the property.
+   * @return a reference to this builder.
+   * @see #set(String, String)
+   */
+  public PropertiesBuilder set(String propertyName, Object propertyValue) {
+    return set(propertyName, String.valueOf(propertyValue));
+  }
+
+  /**
+   * Sets the named property to the given value.
+   *
+   * @param propertyName the name of the property to set.
+   * @param propertyValue the value to assign to the property.
+   * @return a reference to this builder.
+   * @see java.util.Properties#setProperty(String, String)
+   * @see #getProperties()
+   */
+  public PropertiesBuilder set(String propertyName, String propertyValue) {
+    getProperties().setProperty(propertyName, propertyValue);
     return this;
   }
 
+  /**
+   * Returns the constructed {@link Properties} object.
+   *
+   * @return the constructed {@link Properties} object.
+   * @see java.util.Properties
+   * @see #getProperties()
+   */
   public Properties build() {
     return getProperties();
   }
 
+  /**
+   * Returns a String description of the constructed {@link Properties}.
+   *
+   * @return a String describing the constructed {@link Properties}.
+   * @see java.util.Properties#toString()
+   * @see #getProperties()
+   */
   @Override
   public String toString() {
     return getProperties().toString();
