9baa4422a08bee3e221c33518459194a13d64c37
John Blum <jblum0@gmail.com>
2016-06-25 23:59:48 -0700

    Add the StreamUtils class in the org.cp.elements.util.stream package for processing instance of java.util.stream.Stream.

---
+++ src/main/java/org/cp/elements/util/stream/StreamUtils.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.util.stream;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.util.CollectionUtils;
+
+/**
+ * The StreamUtils class is a collection of utility methods for working with Java 8 {@link Stream}s
+ *
+ * @author John J. Blum
+ * @see java.util.stream.Stream
+ * @see org.cp.elements.util.CollectionUtils
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public abstract class StreamUtils {
+
+  /**
+   * Returns a {@link Stream} from the array of elements.
+   *
+   * @param <T> Class type of the elements in the array.
+   * @param elements array of elements to convert into a {@link Stream}.
+   * @return a {@link Stream} for the elements in the array.
+   * @see java.util.stream.Stream
+   */
+  @SafeVarargs
+  public static <T> Stream<T> stream(T... elements) {
+    return Arrays.asList(elements).stream();
+  }
+
+  /**
+   * Returns a {@link Stream} from the {@link Iterable} collection of elements.
+   *
+   * @param <T> Class type of the elements in the {@link Iterable}.
+   * @param iterable {@link Iterable} collection of elements to convert into a {@link Stream}.
+   * @return a {@link Stream} for the elements in the {@link Iterable}.
+   * @throws IllegalArgumentException if {@link Iterable} is null.
+   * @see java.util.stream.Stream
+   * @see java.lang.Iterable
+   */
+  public static <T> Stream<T> stream(Iterable<T> iterable) {
+    Assert.notNull(iterable, "Iterable cannot be null");
+
+    return (iterable instanceof Collection ? ((Collection) iterable).stream()
+      : CollectionUtils.findAll(iterable, (element) -> true).stream());
+  }
+}
---
+++ src/test/java/org/cp/elements/util/stream/StreamUtilsTests.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.util.stream;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.cp.elements.util.ArrayUtils;
+import org.junit.Test;
+
+/**
+ * Test suite of test cases testing the contract and functionality of the {@link StreamUtils} class.
+ *
+ * @author John J. Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.util.stream.StreamUtils
+ * @since 1.0.0
+ */
+public class StreamUtilsTests {
+
+  @Test
+  public void streamFromArray() {
+    String[] array = { "test", "testing", "tested" };
+    Stream<String> stream = StreamUtils.stream(array);
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+
+    List<String> list = stream.collect(Collectors.toList());
+
+    assertThat(list, is(notNullValue(List.class)));
+    assertThat(list, is(equalTo(array.length)));
+    assertThat(list.containsAll(Arrays.asList(array)), is(true));
+  }
+
+  @Test
+  public void streamFromOneElementArray() {
+    Stream<String> stream = StreamUtils.stream("test");
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+
+    List<String> list = stream.collect(Collectors.toList());
+
+    assertThat(list, is(notNullValue(List.class)));
+    assertThat(list.size(), is(equalTo(1)));
+    assertThat(list.get(0), is(equalTo("test")));
+  }
+
+  @Test
+  public void streamFromEmptyArray() {
+    Stream<Object> stream = StreamUtils.stream();
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+    assertThat(stream.count(), is(equalTo(0)));
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void streamFromNullArray() {
+    StreamUtils.stream((Object[]) null);
+  }
+
+  @Test
+  public void streamFromIterable() {
+    Stream<Integer> stream = StreamUtils.stream(ArrayUtils.iterable(1, 2, 3));
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+
+    Object[] array = stream.toArray();
+
+    assertThat(array, is(notNullValue(Object[].class)));
+    assertThat(array, is(equalTo(3)));
+
+    for (int index = 0; index < array.length; index++) {
+      assertThat(array[index], is(equalTo(index + 1)));
+    }
+  }
+
+  @Test
+  public void streamFromOneElementIterable() {
+    Stream<Integer> stream = StreamUtils.stream(ArrayUtils.iterable(1));
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+
+    Object[] array = stream.toArray();
+
+    assertThat(array, is(notNullValue(Object[].class)));
+    assertThat(array.length, is(equalTo(1)));
+    assertThat(array[0], is(equalTo(1)));
+  }
+
+  @Test
+  public void streamFromEmptyIterable() {
+    Stream<Object> stream = StreamUtils.stream(ArrayUtils.iterable());
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+    assertThat(stream.count(), is(equalTo(0)));
+  }
+
+  @Test
+  public void streamFromNullIterable() {
+    Stream<?> stream = StreamUtils.stream((Iterable) null);
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+    assertThat(stream.count(), is(equalTo(0)));
+  }
+
+  @Test
+  public void streamFromCollection() {
+    List<Integer> list = Arrays.asList(1, 2, 3);
+    Stream<Integer> stream = StreamUtils.stream(list);
+
+    assertThat(stream, is(notNullValue(Stream.class)));
+
+    List<Integer> result = stream.collect(Collectors.toList());
+
+    assertThat(result, is(notNullValue(List.class)));
+    assertThat(result.size(), is(equalTo(list.size())));
+    assertThat(result.containsAll(list), is(true));
+  }
+}
