db103cd66c6baf8718ac0c73d1c9af98ed945824
John Blum <jblum@pivotal.io>
2017-02-07 23:01:23 -0800

    Add the Experimental annotation to the org.cp.elements.lang.annotation package to annotate experimental features, such as classes or methods in the API.

---
+++ src/main/java/org/cp/elements/management/Manageable.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.management;
+
+/**
+ * The {@link Manageable} interface is a marker interface to indicate that some {@link Class}
+ * or component can be managed in a management context, such as with JMX, or with the SNMP protocol,
+ * or using another management interface/system.
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public interface Manageable {
+
+}
