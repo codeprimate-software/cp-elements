a30a524d126eccda55a19a64ff22febe94f50ef9
John Blum <jblum@pivotal.io>
2017-02-20 22:29:57 -0800

    Rename org.cp.elements.biz.rules.RuleFailureException to org.cp.elements.biz.rules.RuleException.
    Refactor o.c.e.biz.rules.RuleException to extend java.lang.RuntimeException.
    
    Refactor the o.c.e.biz.rules.RuleException Javadoc.

---
+++ src/main/java/org/cp/elements/biz/rules/RuleException.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.biz.rules;
+
+/**
+ * The {@link RuleException} class is a {@link RuntimeException} indicating that an error occurred
+ * evaluating a business rule.
+ *
+ * @author John J. Blum
+ * @see java.lang.RuntimeException
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class RuleException extends RuntimeException {
+
+  /**
+   * Constructs an uninitialized instance of the {@link RuleException} class with not {@link Throwable cause}
+   * or {@link String message}.
+
+   * @see java.lang.RuntimeException()
+   */
+  public RuleException() {
+  }
+
+  /**
+   * Constructs an instance of {@link RuleException} initialized with the given {@link String message}
+   * to describe the business rule error.
+   *
+   * @param message {@link String} describing the business rule error.
+   * @see java.lang.RuntimeException(String)
+   */
+  public RuleException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs an instance of {@link RuleException} initialized with the underlying {@link Throwable cause}
+   * of the business rule error.
+   *
+   * @param cause {@link Throwable} indicating the underlying cause of the business rule error.
+   * @see java.lang.RuntimeException(Throwable)
+   */
+  public RuleException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs an instance of {@link RuleException} initialized with the given {@link String message} to describe
+   * the business rule error along with the underlying {@link Throwable cause} of the business rule error.
+   *
+   * @param message {@link String} describing the business rule error.
+   * @param cause {@link Throwable} indicating the underlying cause of the business rule error.
+   * @see java.lang.RuntimeException(String, Throwable)
+   */
+  public RuleException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
