0f956d2ad850e0d74a8076aca6c3de81fa79c23a
John Blum <jblum@pivotal.io>
2017-02-24 00:20:07 -0800

    Create the org.cp.elements.lang.annotation.Immutable stereotype annotation to annotate and identify application classes or components that are immutable.

---
+++ src/main/java/org/cp/elements/lang/annotation/Immutable.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The {@link Immutable} annotation is a stereotype {@link java.lang.annotation.Annotation} to annotate
+ * and identify any application class or component that is immutable.
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.RetentionPolicy#RUNTIME
+ * @see java.lang.annotation.Target
+ * @see java.lang.annotation.ElementType#TYPE
+ * @since 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@SuppressWarnings("unused")
+public @interface Immutable {
+
+}
