cdcaf81f4a8953cd7cdb4e67bb1379d54cd4455e
John Blum <jblum@pivotal.io>
2017-02-24 22:37:53 -0800

    Create the org.cp.elements.test.annotation.SubjectUnderTest stereotype annotation to annotate and identify the object that is the subject of a test case or test class.

---
+++ src/main/java/org/cp/elements/test/annotation/SubjectUnderTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.test.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The {@link SubjectUnderTest} annotation is a stereotype {@link Annotation} to identify an object
+ * that is the subject of a test case, or an entire test class, i.e. the Subject Under Test (SUT).
+ *
+ * This {@link Annotation} can be used on instance fields, instance methods, local variables or method parameters.
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Annotation
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.RetentionPolicy#RUNTIME
+ * @see java.lang.annotation.Target
+ * @since 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.METHOD, ElementType.PARAMETER })
+@SuppressWarnings("unused")
+public @interface SubjectUnderTest {
+
+}
