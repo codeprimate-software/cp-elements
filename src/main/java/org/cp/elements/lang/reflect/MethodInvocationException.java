d130643655f2d728e54dd83b4ac9178a79bedc91
John Blum <jblum@pivotal.io>
2017-02-28 14:53:11 -0800

    Refactor Javadoc comments.

--- src/main/java/org/cp/elements/lang/reflect/MethodInvocationException.java
+++ src/main/java/org/cp/elements/lang/reflect/MethodInvocationException.java
@@ -16,54 +16,59 @@
 
 package org.cp.elements.lang.reflect;
 
+import java.lang.reflect.Method;
+
 /**
- * The MethodInvocationException class is a RuntimeException that is thrown during a method invocation
- * when a checked Exception or Error occurs.
+ * The {@link MethodInvocationException} class is a {@link RuntimeException} that is thrown during
+ * a {@link Method} invocation when a checked {@link Exception} or {@link Error} occurs.
  *
  * @author John J. Blum
  * @see java.lang.RuntimeException
+ * @see java.lang.reflect.Method
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
 public class MethodInvocationException extends RuntimeException {
 
   /**
-   * Constructs an uninitialized instance of the MethodInvocationException.
+   * Constructs an uninitialized instance of {@link MethodInvocationException}.
    */
   public MethodInvocationException() {
   }
 
   /**
-   * Constructs an instance of the MethodInvocationException initialized with the given message describing
-   * the method invocation error.
+   * Constructs an instance of {@link MethodInvocationException} initialized with the given {@link String message}
+   * to describe the method invocation error.
    *
-   * @param message a String describing the nature of the method invocation error.
+   * @param message {@link String} describing the method invocation error.
+   * @see java.lang.String
    */
-  public MethodInvocationException(final String message) {
+  public MethodInvocationException(String message) {
     super(message);
   }
 
   /**
-   * Constructs an instance of the MethodInvocationException initialized with the given Throwable to indicate
-   * the cause of the method invocation error.
+   * Constructs an instance of {@link MethodInvocationException} initialized with the given {@link Throwable cause}
+   * indicating the underlying reason for the method invocatino error.
    *
-   * @param cause the Throwable indicating the cause of the method invocation error.
+   * @param cause {@link Throwable} indicating the underlying reason for the method invocation error.
    * @see java.lang.Throwable
    */
-  public MethodInvocationException(final Throwable cause) {
+  public MethodInvocationException(Throwable cause) {
     super(cause);
   }
 
   /**
-   * Constructs an instance of the MethodInvocationException initialized with the given message describing
-   * the method invocation error along with the Throwable indicating the probable cause of the method invocation error.
+   * Constructs an instance of {@link MethodInvocationException} initialized with the given {@link String message}
+   * to describe the method invocation error along with the {@link Throwable probable cause} of why
+   * the method invocation error occurred.
    *
-   * @param message a String describing the nature of the method invocation error.
-   * @param cause the Throwable indicating the cause of the method invocation error.
+   * @param message {@link String} describing the method invocation error.
+   * @param cause {@link Throwable} indicating the underlying reason for the method invocation error.
+   * @see java.lang.String
    * @see java.lang.Throwable
    */
-  public MethodInvocationException(final String message, final Throwable cause) {
+  public MethodInvocationException(String message, Throwable cause) {
     super(message, cause);
   }
-
 }
