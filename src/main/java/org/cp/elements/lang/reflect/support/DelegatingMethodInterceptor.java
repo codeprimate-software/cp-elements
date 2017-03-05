eb23e2ec2bdf3d82a87ae2666ae02d7afee934da
John Blum <jblum@pivotal.io>
2017-03-04 16:26:34 -0800

    Create the org.cp.elements.lang.reflect.support.DelegatingMethodInterceptor class to wrap an existing MethodInterceptor for delegating all MethodInterceptor operations.
    This is useful in use cases where the MethodInterceptor must be changed at runtime.

---
+++ src/main/java/org/cp/elements/lang/reflect/support/DelegatingMethodInterceptor.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.reflect.support;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
+
+import java.util.Optional;
+
+import org.cp.elements.lang.concurrent.GuardedBy;
+import org.cp.elements.lang.reflect.MethodInterceptor;
+import org.cp.elements.lang.reflect.MethodInvocation;
+
+/**
+ * The DelegatingMethodInterceptor class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class DelegatingMethodInterceptor<T> implements MethodInterceptor<T> {
+
+  private MethodInterceptor<T> delegate;
+
+  /**
+   * Factory method used to construct an uninitialized instance of the {@link DelegatingMethodInterceptor}.
+   *
+   * @return a new, uninitialized instance of the {@link DelegatingMethodInterceptor}.
+   * @see org.cp.elements.lang.reflect.support.DelegatingMethodInterceptor
+   * @see #DelegatingMethodInterceptor()
+   */
+  public static DelegatingMethodInterceptor newDelegatingMethodInterceptor() {
+    return new DelegatingMethodInterceptor();
+  }
+
+  /**
+   * Factory method used to construct an instance of the {@link DelegatingMethodInterceptor} class initialized with
+   * the given {@link MethodInterceptor} used as the delegate in all {@link MethodInterceptor} operations.
+   *
+   * @param <T> {{@link Class} type of the target {@link Object}.
+   * @param delegate {@link MethodInterceptor} wrapped as the delegate for the {@link DelegatingMethodInterceptor}.
+   * @return
+   */
+  public static <T> DelegatingMethodInterceptor newDelegatingMethodInterceptor(MethodInterceptor<T> delegate) {
+    return new DelegatingMethodInterceptor<T>(delegate);
+  }
+
+  /**
+   * Default constructor for {@link DelegatingMethodInterceptor}constructing an uninitialized instance
+   * of {@link DelegatingMethodInterceptor}.
+   */
+  public DelegatingMethodInterceptor() {
+  }
+
+  /**
+   * Constructs an instance of the {@link DelegatingMethodInterceptor} initialized with
+   * the given {@link MethodInterceptor} serving as the delegate.
+   *
+   * @param delegate {@link MethodInterceptor} wrapped by this {@link DelegatingMethodInterceptor}.
+   * May be {@literal null} but must be {@literal non-null} when the delegate is used.
+   * @see org.cp.elements.lang.reflect.MethodInterceptor
+   */
+  public DelegatingMethodInterceptor(MethodInterceptor<T> delegate) {
+    this.delegate =  delegate;
+  }
+
+  /**
+   * Sets the delegate used in the {@link MethodInterceptor} operations.
+   *
+   * @param delegate {@link MethodInterceptor} used in operations on this {@link MethodInterceptor}.
+   * @see org.cp.elements.lang.reflect.MethodInterceptor
+   */
+  @GuardedBy("this")
+  public synchronized void setDelegate(MethodInterceptor<T> delegate) {
+    this.delegate = delegate;
+  }
+
+  /**
+   * Returns the delegate used in the {@link MethodInterceptor} operations.
+   *
+   * @return the {@link MethodInterceptor} used in operations on this {@link MethodInterceptor}.
+   * @throws IllegalStateException if the {@code delegate} is {@literal null}.
+   * @see org.cp.elements.lang.reflect.MethodInterceptor
+   */
+  @GuardedBy("this")
+  protected synchronized MethodInterceptor<T> getDelegate() {
+    return Optional.ofNullable(this.delegate).orElseThrow(
+      () -> newIllegalStateException("The delegate MethodInterceptor was not properly initialized"));
+  }
+
+  /**
+   * @inheritDoc
+   */
+  @Override
+  public T getTarget() {
+    return getDelegate().getTarget();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  @Override
+  public <R> Optional<R> intercept(MethodInvocation methodInvocation) {
+    return getDelegate().intercept(methodInvocation);
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/reflect/support/DelegatingMethodInterceptorTests.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.reflect.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.reflect.support.DelegatingMethodInterceptor.newDelegatingMethodInterceptor;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.cp.elements.lang.reflect.MethodInterceptor;
+import org.cp.elements.lang.reflect.MethodInvocation;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Unit tests for {@link DelegatingMethodInterceptor}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.junit.runner.RunWith
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.mockito.runners.MockitoJUnitRunner
+ * @see org.cp.elements.lang.reflect.MethodInterceptor
+ * @see org.cp.elements.lang.reflect.MethodInvocation
+ * @see org.cp.elements.lang.reflect.support.DelegatingMethodInterceptor
+ * @since 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DelegatingMethodInterceptorTests {
+
+  @Mock
+  private MethodInterceptor<Object> mockDelegate;
+
+  @Mock
+  private MethodInvocation mockMethodInvocation;
+
+  @Test
+  public void newDelegatingMethodInterceptorWithDelegateIsSuccessful() {
+    DelegatingMethodInterceptor<?> methodInterceptor = newDelegatingMethodInterceptor(mockDelegate);
+
+    assertThat(methodInterceptor).isNotNull();
+    assertThat(methodInterceptor.getDelegate()).isSameAs(mockDelegate);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void newDelegatingMethodInterceptorWithoutDelegateIsSuccessful() {
+    DelegatingMethodInterceptor<?> methodInterceptor = newDelegatingMethodInterceptor();
+
+    assertThat(methodInterceptor).isNotNull();
+
+    methodInterceptor.getDelegate();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void setAndGetDelegateIsSuccessful() {
+    MethodInterceptor<?> mockMethodInterceptor = mock(MethodInterceptor.class);
+    DelegatingMethodInterceptor methodInterceptor = newDelegatingMethodInterceptor(mockMethodInterceptor);
+
+    assertThat(methodInterceptor).isNotNull();
+    assertThat(methodInterceptor.getDelegate()).isSameAs(mockMethodInterceptor);
+
+    methodInterceptor.setDelegate(mockDelegate);
+
+    assertThat(methodInterceptor.getDelegate()).isSameAs(mockDelegate);
+
+    methodInterceptor.setDelegate(mockMethodInterceptor);
+
+    assertThat(methodInterceptor.getDelegate()).isSameAs(mockMethodInterceptor);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void getUninitializedDelegateThrowsIllegalStateException() {
+    try {
+      newDelegatingMethodInterceptor().getDelegate();
+    }
+    catch (IllegalStateException expected) {
+      assertThat(expected).hasMessage("The delegate MethodInterceptor was not properly initialized");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void getTargetDelegatesToDelegateGetTarget() {
+    when(mockDelegate.getTarget()).thenReturn("test");
+
+    assertThat(newDelegatingMethodInterceptor(mockDelegate).getTarget()).isEqualTo("test");
+
+    verify(mockDelegate, times(1)).getTarget();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void interceptDelegatesToDelegateInvoke() {
+    when(mockDelegate.intercept(any(MethodInvocation.class))).thenReturn(Optional.of("test"));
+
+    assertThat(newDelegatingMethodInterceptor(mockDelegate).intercept(mockMethodInvocation).orElse(null))
+      .isEqualTo("test");
+
+    verify(mockDelegate, times(1)).intercept(eq(mockMethodInvocation));
+  }
+}
