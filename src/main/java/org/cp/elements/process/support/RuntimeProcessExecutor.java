16d388b1b9b3f2f90b95176eb17929cfc667d8bf
John Blum <jblum@pivotal.io>
2017-03-12 13:21:43 -0700

    Review, refactor and test the o.c.e.process.support.RuntimeProcessExecutor class.

--- src/main/java/org/cp/elements/process/support/RuntimeProcessExecutor.java
+++ src/main/java/org/cp/elements/process/support/RuntimeProcessExecutor.java
@@ -16,16 +16,17 @@
 
 package org.cp.elements.process.support;
 
-import static org.cp.elements.lang.ObjectUtils.defaultIfNull;
+import static org.cp.elements.io.FileUtils.isDirectory;
 import static org.cp.elements.process.ProcessAdapter.newProcessAdapter;
 import static org.cp.elements.process.ProcessContext.newProcessContext;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Optional;
 
-import org.cp.elements.io.FileSystemUtils;
 import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.NullSafe;
 import org.cp.elements.lang.SystemUtils;
 import org.cp.elements.process.ProcessAdapter;
 import org.cp.elements.process.ProcessContext;
@@ -39,17 +40,21 @@ import org.cp.elements.util.Environment;
  * to the running OS {@link Process}.
  *
  * @author John Blum
+ * @see java.io.File
  * @see java.lang.Process
  * @see java.lang.Runtime
+ * @see org.cp.elements.process.ProcessAdapter
+ * @see org.cp.elements.process.ProcessContext
  * @see org.cp.elements.process.ProcessExecutor
+ * @see org.cp.elements.util.Environment
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
 public class RuntimeProcessExecutor implements ProcessExecutor<ProcessAdapter> {
 
   /**
-   * Factory method to construct a new instance of {@link RuntimeProcessExecutor} used to execute and run a {@link Process}
-   * using {@link Runtime#exec(String[], String[], File)}.
+   * Factory method to construct a new instance of {@link RuntimeProcessExecutor} used to execute and run
+   * a {@link Process} with {@link Runtime#exec(String[], String[], File)}.
    *
    * @return a new instance of {@link RuntimeProcessExecutor}.
    * @see org.cp.elements.process.support.RuntimeProcessExecutor
@@ -65,19 +70,21 @@ public class RuntimeProcessExecutor implements ProcessExecutor<ProcessAdapter> {
    * in the given {@link File directory}.
    *
    * @param directory {@link File directory} in which the program will run.
-   * @param commandLine array of {@link String} values constituting the program and it's runtime arguments.
+   * @param commandLine array of {@link String} values constituting the program and its runtime arguments.
    * @return a {@link Process} object representing the running program.
    * @throws IllegalArgumentException if the {@link File} reference is not a valid, existing directory
    * or the command-line is unspecified.
+   * @see org.cp.elements.process.ProcessAdapter
+   * @see org.cp.elements.process.ProcessContext
    * @see java.lang.Process
    * @see java.io.File
    */
   @Override
   public ProcessAdapter execute(File directory, String... commandLine) {
 
-    Assert.isTrue(FileSystemUtils.isDirectory(directory), "[%s] is not a valid directory", directory);
+    Assert.isTrue(isDirectory(directory), "[%s] is not a valid directory", directory);
 
-    Assert.notEmpty(commandLine, "The command-line [%s] must contain at least 1 command",
+    Assert.notEmpty(commandLine, "The command-line %s must contain at least 1 command",
       Arrays.toString(commandLine));
 
     try {
@@ -94,19 +101,47 @@ public class RuntimeProcessExecutor implements ProcessExecutor<ProcessAdapter> {
       return newProcessAdapter(process, processContext);
     }
     catch (IOException e) {
-      throw new ProcessExecutionException(String.format("Failed to execute program [%1$s] in directory [%2$s]",
+      throw new ProcessExecutionException(String.format("Failed to execute program %1$s in directory [%2$s]",
         Arrays.toString(commandLine), directory), e);
     }
   }
 
+  /**
+   * Executes the {@code commandLine} with the Java {@link Runtime} class in the given {@link File directory}
+   * with the provided {@link Environment} configuration.
+   *
+   * @param commandLine array of {@link String} values constituting the program and its runtime arguments.
+   * @param directory {@link File directory} in which the program will run.
+   * @param environment {@link Environment} configuration to set when the program runs.
+   * @return a {@link Process} object representing the running program.
+   * @throws IOException if the {@link Process} cannot be started.
+   * @see java.lang.Runtime#exec(String, String[], File)
+   * @see java.lang.Process
+   */
   protected Process doExecute(String[] commandLine, File directory, Environment environment) throws IOException {
     return Runtime.getRuntime().exec(commandLine, environment.toAssociativeArray(), directory);
   }
 
+  /**
+   * Null-safe operation to get a reference to the configured {@link Environment} used when running the program.
+   *
+   * @return a reference to the {@link Environment} configuration used when running the the program.
+   * If the configured {@link Environment} is {@literal null}, then this method
+   * returns {@link Environment#fromEnvironmentVariables()}.
+   * @see org.cp.elements.util.Environment
+   */
+  @NullSafe
   protected Environment getEnvironment() {
-    return defaultIfNull(this.environment, Environment.fromEnvironmentVariables());
+    return Optional.ofNullable(this.environment).orElseGet(Environment::fromEnvironmentVariables);
   }
 
+  /**
+   * Sets the {@link Environment} configuration used when running the program.
+   *
+   * @param environment {@link Environment} configuration used when running the program.
+   * @return this {@link RuntimeProcessExecutor}.
+   * @see org.cp.elements.util.Environment
+   */
   public RuntimeProcessExecutor using(Environment environment) {
     this.environment = environment;
     return this;
---
+++ src/test/java/org/cp/elements/process/support/RuntimeProcessExecutorTests.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.process.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.process.support.RuntimeProcessExecutor.newRuntimeProcessExecutor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.cp.elements.io.FileSystemUtils;
+import org.cp.elements.lang.SystemUtils;
+import org.cp.elements.process.ProcessAdapter;
+import org.cp.elements.process.ProcessExecutionException;
+import org.cp.elements.util.Environment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Unit tests for {@link RuntimeProcessExecutor}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.junit.runner.RunWith
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.mockito.runners.MockitoJUnitRunner
+ * @see org.cp.elements.process.support.RuntimeProcessExecutor
+ * @since 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RuntimeProcessExecutorTests {
+
+  @Mock
+  private Process mockProcess;
+
+  @Test
+  public void newRuntimeProcessExecutorIsNotNull() {
+    RuntimeProcessExecutor processExecutor = newRuntimeProcessExecutor();
+
+    assertThat(processExecutor).isNotNull();
+  }
+
+  @Test
+  public void executeIsSuccessful() {
+    RuntimeProcessExecutor processExecutor = new RuntimeProcessExecutor() {
+      @Override
+      protected Process doExecute(String[] commandLine, File directory, Environment environment) throws IOException {
+        return RuntimeProcessExecutorTests.this.mockProcess;
+      }
+    };
+
+    String[] expectedCommandLine = { "java", "-server", "-ea", "-classpath",
+      "/class/path/to/application.jar", "example.Application" };
+
+    ProcessAdapter process = processExecutor.execute(FileSystemUtils.USER_HOME_DIRECTORY, expectedCommandLine);
+
+    assertThat(process).isNotNull();
+    assertThat(process.getProcess()).isSameAs(this.mockProcess);
+    assertThat(process.getProcessContext()).isNotNull();
+    assertThat(process.getProcessContext().getCommandLine()).isEqualTo(Arrays.asList(expectedCommandLine));
+    assertThat(process.getProcessContext().getDirectory()).isEqualTo(FileSystemUtils.USER_HOME_DIRECTORY);
+    assertThat(process.getProcessContext().getEnvironment()).isEqualTo(Environment.fromEnvironmentVariables());
+    assertThat(process.getProcessContext().getUsername()).isEqualTo(SystemUtils.USERNAME);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void executeWithInvalidDirectoryThrowsIllegalArgumentException() {
+    try {
+      newRuntimeProcessExecutor().execute(new File("/path/to/non/existing/diretory"),
+        "java", "-server", "-ea", "-classpath", "/class/path/to/application.jar", "example.Application");
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("[/path/to/non/existing/diretory] is not a valid directory");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void executeWithNoCommandThrowsIllegalArgumentException() {
+    try {
+      newRuntimeProcessExecutor().execute(FileSystemUtils.WORKING_DIRECTORY);
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("The command-line [] must contain at least 1 command");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = ProcessExecutionException.class)
+  public void executeFailedProcessHandlesIOException() {
+    RuntimeProcessExecutor processExecutor = new RuntimeProcessExecutor() {
+      @Override
+      protected Process doExecute(String[] commandLine, File directory, Environment environment) throws IOException {
+        throw new IOException("test");
+      }
+    };
+
+    String[] expectedCommandLine = { "java", "-server", "-ea", "-classpath",
+      "/class/path/to/application.jar", "example.Application" };
+
+    try {
+      processExecutor.execute(FileSystemUtils.WORKING_DIRECTORY, expectedCommandLine);
+    }
+    catch (ProcessExecutionException expected) {
+      assertThat(expected).hasMessage("Failed to execute program %1$s in directory [%2$s]",
+        Arrays.toString(expectedCommandLine), FileSystemUtils.WORKING_DIRECTORY);
+      assertThat(expected).hasCauseInstanceOf(IOException.class);
+      assertThat(expected.getCause()).hasMessage("test");
+      assertThat(expected.getCause()).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void usingEnvironmentVariables() {
+    assertThat(newRuntimeProcessExecutor().getEnvironment()).isEqualTo(Environment.fromEnvironmentVariables());
+  }
+
+  @Test
+  public void usingProvidedEnvironment() {
+    Environment expectedEnvironment = Environment.from(Collections.singletonMap("testKey", "testValue"));
+    RuntimeProcessExecutor processExecutor = newRuntimeProcessExecutor();
+
+    assertThat(processExecutor.using(expectedEnvironment)).isSameAs(processExecutor);
+    assertThat(processExecutor.getEnvironment()).isEqualTo(expectedEnvironment);
+  }
+}
