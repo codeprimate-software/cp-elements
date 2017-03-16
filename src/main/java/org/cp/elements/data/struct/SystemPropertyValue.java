8a65c647ba464424fed9c967e2af180755476fec
John Blum <jblum@pivotal.io>
2017-03-16 13:43:00 -0700

    Create the SystemPropertyValue data structure and KeyValue subclass in the org.cp.elements.data.struct package to model a single system property and its value.

---
+++ src/main/java/org/cp/elements/data/struct/SystemPropertyValue.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.struct;
+
+import java.util.Optional;
+
+import org.cp.elements.lang.concurrent.ThreadSafe;
+
+/**
+ * The {@link SystemPropertyValue} class is {@link KeyValue} data structure providing access to the value
+ * of a given Java System Property.
+ *
+ * @author John Blum
+ * @see java.lang.System#getProperty(String)
+ * @see org.cp.elements.data.struct.KeyValue
+ * @since 1.0.0
+ */
+@ThreadSafe
+@SuppressWarnings("unused")
+public class SystemPropertyValue extends KeyValue<String, String> {
+
+  /**
+   * Factory method used to construct a new instance of {@link SystemPropertyValue} initialized with
+   * the given system property.
+   *
+   * @param systemProperty Java system property.
+   * @return a new instance of the {@link SystemPropertyValue} initialized with the given system property.
+   * @throws IllegalArgumentException if system property is {@literal null}.
+   * @see #SystemPropertyValue(String)
+   */
+  public static SystemPropertyValue newSystemPropertyValue(String systemProperty) {
+    return new SystemPropertyValue(systemProperty);
+  }
+
+  /**
+   * Constructs a new instance of {@link SystemPropertyValue} initialized with the given system property.
+   *
+   * @param systemProperty Java system property.
+   * @throws IllegalArgumentException if system property is {@literal null}.
+   */
+  public SystemPropertyValue(String systemProperty) {
+    super(systemProperty);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  @Override
+  public Optional<String> getValue() {
+    return Optional.ofNullable(System.getProperty(getKey()));
+  }
+}
---
+++ src/test/java/org/cp/elements/data/struct/SystemPropertyValueTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.struct;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.data.struct.SystemPropertyValue.newSystemPropertyValue;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link SystemPropertyValue}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.data.struct.SystemPropertyValue
+ * @since 1.0.0
+ */
+public class SystemPropertyValueTests {
+
+  @Test
+  public void newSystemPropertyValueIsSuccessful() {
+    SystemPropertyValue systemPropertyValue = newSystemPropertyValue("user.name");
+
+    assertThat(systemPropertyValue).isNotNull();
+    assertThat(systemPropertyValue.getKey()).isEqualTo("user.name");
+    assertThat(systemPropertyValue.getValue()).isInstanceOf(Optional.class);
+    assertThat(systemPropertyValue.getValue("default")).isNotEqualTo("default");
+  }
+
+  @Test
+  public void getValueForExistingSystemProperty() {
+    assertThat(newSystemPropertyValue("user.name").getValue("default"))
+      .isEqualTo(System.getProperty("user.name"));
+  }
+
+  @Test
+  public void getValueForNonExistingSystemProperty() {
+    SystemPropertyValue systemPropertyValue = newSystemPropertyValue("non.existing.property");
+
+    assertThat(systemPropertyValue.getValue()).isInstanceOf(Optional.class);
+    assertThat(systemPropertyValue.getValue("default"))
+      .isEqualTo(System.getProperty("non.existing.property", "default"));
+  }
+}
