ef0d7de21a42506cc0be807aa89eb5da67d46ea9
John Blum <jblum@pivotal.io>
2017-03-16 00:09:36 -0700

    Create the Tester functional interface in the org.cp.elements.test package to define a contract for implementing Objects that perform a single test and return a boolean value to indicate pass for fail.

---
+++ src/main/java/org/cp/elements/test/Tester.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.test;
+
+/**
+ * The {@link Tester} interface defines a contract for an implementing {@link Object} to perform a single test.
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface Tester {
+
+  /**
+   * Performs the test defined by the implementing {@link Object}.
+   *
+   * @return a boolean value indicating whether the test passed or failed.
+   */
+  boolean test();
+
+}
