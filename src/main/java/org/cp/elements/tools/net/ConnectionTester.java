02b58d91265347db484875cf4d4ed3b6424b1635
John Blum <jblum@pivotal.io>
2017-03-17 23:02:10 -0700

    Create the ConnectionTester tool class in the org.cp.elements.tools.net package to test network Socket connections for connectivity.

---
+++ src/main/java/org/cp/elements/tools/net/ConnectionTester.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.tools.net;
+
+import static org.cp.elements.lang.LangExtensions.assertThat;
+import static org.cp.elements.lang.NumberUtils.intValue;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
+import static org.cp.elements.net.NetworkUtils.close;
+import static org.cp.elements.net.NetworkUtils.lenientParsePort;
+import static org.cp.elements.net.NetworkUtils.newSocketAddress;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.cp.elements.lang.Condition;
+import org.cp.elements.lang.StringUtils;
+import org.cp.elements.net.ServicePort;
+import org.cp.elements.test.Tester;
+
+/**
+ * The {@link ConnectionTester} class is used to test a network connection to a given network endpoint.
+ *
+ * @author John Blum
+ * @see java.net.Socket
+ * @see org.cp.elements.lang.Condition
+ * @see org.cp.elements.test.Tester
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ConnectionTester implements Condition, Tester {
+
+  protected static final boolean SO_REUSEADDR = true;
+
+  protected static final long CONNECT_TIMEOUT = TimeUnit.SECONDS.toMillis(1L);
+
+  protected static final String HOST_PORT_SEPARATOR = ":";
+
+  /* (non-Javadoc) */
+  public static void main(String[] args) {
+    Arrays.stream(args).forEach(hostPort -> {
+      String[] hostPortSplit = hostPort.split(HOST_PORT_SEPARATOR);
+      String host = (hostPortSplit.length > 1 ? hostPortSplit[0] : null);
+      int port = lenientParsePort(hostPortSplit.length > 1 ? hostPortSplit[1] : hostPortSplit[0]);
+
+      System.out.printf("Connection to [%1$s:%2$d] was %3$ssuccessful%n",
+        Optional.ofNullable(host).orElse("localhost"), port,
+          newConnectionTester(host, port).test() ? StringUtils.EMPTY_STRING : "not ");
+    });
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link ConnectionTester} in order to test a connection
+   * to {@literal localhost} on the specified {@link Integer port}.
+   *
+   * @param port {@link Integer} value specifying the port number to which the connection is bound.
+   * @return a new instance of {@link ConnectionTester} bound to the specified {@link Integer port}.
+   * @throws IllegalArgumentException if the {@code port} number is not valid.
+   * @see #ConnectionTester(String, int)
+   */
+  public static ConnectionTester newConnectionTester(int port) {
+    return new ConnectionTester(null, port);
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link ConnectionTester} in order to test a connection
+   * to the specified {@link String host} on the specified {@link Integer port}.
+   *
+   * @param host {@link String name} of the host to which the connection is bound.
+   * @param port {@link Integer} value specifying the port number to which the connection is bound.
+   * @return a new instance of {@link ConnectionTester} bound to the specified {@link String host}
+   * and {@link Integer port}.
+   * @throws IllegalArgumentException if the {@code port} number is not valid.
+   * @see #ConnectionTester(String, int)
+   */
+  public static ConnectionTester newConnectionTester(String host, int port) {
+    return new ConnectionTester(host, port);
+  }
+
+  private final AtomicBoolean connected = new AtomicBoolean(false);
+
+  private final String host;
+
+  private final int port;
+
+  /**
+   * Constructs a new instance of the {@link ConnectionTester} bound to the given {@link String host}
+   * and {@link Integer port}.
+   *
+   * @param host {@link String name} of the host to which the connection is bound.
+   * @param port {@link Integer} value specifying the port number to which the connection is bound.
+   * @throws IllegalArgumentException if the {@code port} number is not valid.
+   */
+  public ConnectionTester(String host, int port) {
+    assertThat(port).throwing(newIllegalArgumentException("[%d] is not a valid port number", port))
+      .isGreaterThanAndLessThanEqualTo(0, ServicePort.MAX_PORT);
+
+    this.host = (host == null || host.trim().isEmpty() ? null : host);
+    this.port = port;
+  }
+
+  /**
+   * Determines whether a network connection to the specified {@link #getHost()} and {@link #getPort()} was established.
+   *
+   * @return a boolean value indicating whether a network connection to the specified {@link #getHost()}
+   * and {@link #getPort()} was successfully established.
+   * @see #isNotConnected()
+   */
+  public boolean isConnected() {
+    return connected.get();
+  }
+
+  /**
+   * Determines whether a network connection to the specified {@link #getHost()} and {@link #getPort()} was established.
+   *
+   * @return a boolean value indicating whether a network connection to the specified {@link #getHost()}
+   * and {@link #getPort()} was successfully established.
+   * @see #isConnected()
+   */
+  public boolean isNotConnected() {
+    return !isConnected();
+  }
+
+  /**
+   * Sets whether a network connection to the specified {@link #getHost()} and {@link #getPort()} could be established.
+   *
+   * @param connected boolean value indicating whether a network connection to the specified {@link #getHost()}
+   * and {@link #getPort()} was successfully established.
+   * @see #isNotConnected()
+   * @see #isConnected()
+   */
+  protected ConnectionTester setConnected(boolean connected) {
+    this.connected.set(connected);
+    return this;
+  }
+
+  /**
+   * Return the {@link String name} of the host tested in the connection.
+   *
+   * @return the {@link String name} of the host tested in the connection.
+   * @see #getPort()
+   */
+  public String getHost() {
+    return this.host;
+  }
+
+  /**
+   * Return the {@link Integer port number} tested in the connection.
+   *
+   * @return the {@link Integer port number} tested in the connection.
+   * @see #getHost()
+   */
+  public int getPort() {
+    return this.port;
+  }
+
+  /**
+   * @see #test()
+   */
+  @Override
+  public boolean evaluate() {
+    return test();
+  }
+
+  /**
+   * Resets the state of the {@link #isConnected() connected property} and retests the connection.
+   *
+   * @return a boolean value indicating whether a connection to the specified network {@link #getHost()}
+   * and {@link #getPort()} was successful.
+   * @see #setConnected(boolean)
+   * @see #test()
+   */
+  public boolean retest() {
+    return setConnected(false).test();
+  }
+
+  /**
+   * Tests whether a connection to the specified network {@link #getHost()} and {@link #getPort()} can be established.
+   *
+   * @return a boolean value indicating whether a connection to the specified network {@link #getHost()}
+   * and {@link #getPort()} was successful.
+   * @see java.net.Socket#isConnected()
+   * @see org.cp.elements.net.NetworkUtils#newSocketAddress(String, int)
+   * @see #newSocket()
+   * @see #getHost()
+   * @see #getPort()
+   */
+  @Override
+  public boolean test() {
+    if (isNotConnected()) {
+      Socket socket = null;
+
+      try {
+        socket = newSocket();
+        socket.setReuseAddress(SO_REUSEADDR);
+        socket.connect(newSocketAddress(getHost(), getPort()), intValue(CONNECT_TIMEOUT));
+        setConnected(socket.isConnected());
+      }
+      catch (IOException ignore) {
+      }
+      finally {
+        close(socket);
+      }
+    }
+
+    return isConnected();
+  }
+
+  /**
+   * Constructs a new, uninitialized {@link Socket}.
+   *
+   * @return a new, uninitialized {@link Socket}.
+   * @see java.net.Socket#Socket()
+   */
+  protected Socket newSocket() {
+    return new Socket();
+  }
+}
---
+++ src/test/java/org/cp/elements/tools/net/ConnectionTesterTests.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.tools.net;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.CheckedExceptionsFactory.newIOException;
+import static org.cp.elements.lang.NumberUtils.intValue;
+import static org.cp.elements.tools.net.ConnectionTester.newConnectionTester;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link ConnectionTester}.
+ *
+ * @author John Blum
+ * @see java.net.Socket
+ * @see java.net.SocketAddress
+ * @see org.junit.Test
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.mockito.Spy
+ * @see org.cp.elements.tools.net.ConnectionTester
+ * @since 1.0.0
+ */
+public class ConnectionTesterTests {
+
+  @Test
+  public void newConnectionTestWithPort() {
+    ConnectionTester connectionTester = newConnectionTester(1234);
+
+    assertThat(connectionTester).isNotNull();
+    assertThat(connectionTester.getHost()).isNull();
+    assertThat(connectionTester.getPort()).isEqualTo(1234);
+  }
+
+  @Test
+  public void newConnectionTesterWithHostAndPort() {
+    ConnectionTester connectionTester = newConnectionTester("skullbox", 1234);
+
+    assertThat(connectionTester).isNotNull();
+    assertThat(connectionTester.getHost()).isEqualTo("skullbox");
+    assertThat(connectionTester.getPort()).isEqualTo(1234);
+  }
+
+  @Test
+  public void constructConnectionTester() {
+    ConnectionTester connectionTester = new ConnectionTester("localhost", 9876);
+
+    assertThat(connectionTester.getHost()).isEqualTo("localhost");
+    assertThat(connectionTester.getPort()).isEqualTo(9876);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructConnectionTesterWithNegativePortThrowsIllegalArgumentException() {
+    try {
+      new ConnectionTester("localhost", -1);
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("[-1] is not a valid port number");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructConnectionTesterWithPortExceedingMaximumThrowsIllegalArgumentException() {
+    try {
+      new ConnectionTester("localhost", 123456789);
+    }
+    catch (IllegalArgumentException expected) {
+      assertThat(expected).hasMessage("[123456789] is not a valid port number");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void constructConnectionTesterWithBlankHostReturnsNullHost() {
+    assertThat(new ConnectionTester("  ", 1234).getHost()).isNull();
+  }
+
+  @Test
+  public void constructConnectionTesterWithEmptyHostReturnsNullHost() {
+    assertThat(new ConnectionTester("", 1234).getHost()).isNull();
+  }
+
+  @Test
+  public void setGetAndIsConnected() {
+    ConnectionTester connectionTester = newConnectionTester(1234);
+
+    assertThat(connectionTester).isNotNull();
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.isNotConnected()).isTrue();
+    assertThat(connectionTester.setConnected(true)).isSameAs(connectionTester);
+    assertThat(connectionTester.isConnected()).isTrue();
+    assertThat(connectionTester.isNotConnected()).isFalse();
+    assertThat(connectionTester.setConnected(false)).isSameAs(connectionTester);
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.isNotConnected()).isTrue();
+  }
+
+  @Test
+  public void evaluateCallsTest() {
+    ConnectionTester connectionTester = spy(newConnectionTester(1234));
+
+    assertThat(connectionTester).isNotNull();
+
+    doReturn(true).when(connectionTester).test();
+
+    assertThat(connectionTester.evaluate()).isTrue();
+
+    verify(connectionTester, times(1)).test();
+  }
+
+  @Test
+  public void retestCallsSetConnectedWithFalseAndTest() {
+    ConnectionTester connectionTester = spy(newConnectionTester(1234));
+
+    assertThat(connectionTester).isNotNull();
+    assertThat(connectionTester.setConnected(true)).isSameAs(connectionTester);
+    assertThat(connectionTester.isConnected()).isTrue();
+
+    doReturn(true).when(connectionTester).test();
+
+    assertThat(connectionTester.retest()).isTrue();
+
+    assertThat(connectionTester.isConnected()).isFalse();
+
+    verify(connectionTester, times(1)).setConnected(eq(false));
+    verify(connectionTester, times(1)).test();
+  }
+
+  @Test
+  public void testConnectsSuccessfully() throws IOException {
+    ConnectionTester connectionTester = spy(newConnectionTester(1234));
+    Socket mockSocket = mock(Socket.class);
+
+    assertThat(connectionTester).isNotNull();
+
+    doReturn(mockSocket).when(connectionTester).newSocket();
+    when(mockSocket.isConnected()).thenReturn(true);
+
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.test()).isTrue();
+    assertThat(connectionTester.isConnected()).isTrue();
+    assertThat(connectionTester.test()).isTrue();
+    assertThat(connectionTester.isConnected()).isTrue();
+
+    verify(connectionTester, times(1)).newSocket();
+    verify(connectionTester, times(1)).setConnected(eq(true));
+    verify(mockSocket, times(1)).setReuseAddress(eq(ConnectionTester.SO_REUSEADDR));
+    verify(mockSocket, times(1))
+      .connect(isA(SocketAddress.class), eq(intValue(ConnectionTester.CONNECT_TIMEOUT)));
+    verify(mockSocket, times(1)).isConnected();
+    verify(mockSocket, times(1)).close();
+    verifyNoMoreInteractions(mockSocket);
+  }
+
+  @Test
+  public void testFailsToConnect() throws IOException {
+    ConnectionTester connectionTester = spy(newConnectionTester(1234));
+    Socket mockSocket = mock(Socket.class);
+
+    assertThat(connectionTester).isNotNull();
+
+    doReturn(mockSocket).when(connectionTester).newSocket();
+    when(mockSocket.isConnected()).thenReturn(false);
+
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.test()).isFalse();
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.test()).isFalse();
+    assertThat(connectionTester.isConnected()).isFalse();
+
+    verify(connectionTester, times(2)).newSocket();
+    verify(connectionTester, times(2)).setConnected(eq(false));
+    verify(mockSocket, times(2)).setReuseAddress(eq(ConnectionTester.SO_REUSEADDR));
+    verify(mockSocket, times(2))
+      .connect(isA(SocketAddress.class), eq(intValue(ConnectionTester.CONNECT_TIMEOUT)));
+    verify(mockSocket, times(2)).isConnected();
+    verify(mockSocket, times(2)).close();
+    verifyNoMoreInteractions(mockSocket);
+  }
+
+  @Test
+  public void testHandlesIOException() throws IOException {
+    ConnectionTester connectionTester = spy(newConnectionTester(1234));
+    Socket mockSocket = mock(Socket.class);
+
+    assertThat(connectionTester).isNotNull();
+
+    doReturn(mockSocket).when(connectionTester).newSocket();
+    doThrow(newIOException("test")).when(mockSocket).connect(any(SocketAddress.class), anyInt());
+
+    assertThat(connectionTester.isConnected()).isFalse();
+    assertThat(connectionTester.test()).isFalse();
+    assertThat(connectionTester.isConnected()).isFalse();
+
+    verify(connectionTester, times(1)).newSocket();
+    verify(connectionTester, never()).setConnected(eq(false));
+    verify(mockSocket, times(1)).setReuseAddress(eq(ConnectionTester.SO_REUSEADDR));
+    verify(mockSocket, times(1))
+      .connect(isA(SocketAddress.class), eq(intValue(ConnectionTester.CONNECT_TIMEOUT)));
+    verify(mockSocket, never()).isConnected();
+    verify(mockSocket, times(1)).close();
+    verifyNoMoreInteractions(mockSocket);
+  }
+}
