4b37580555f8b28a8ae1e00db087a12766723d80
John Blum <jblum@pivotal.io>
2017-04-01 11:58:50 -0700

    Create the org.cp.elements.lang.Executable, Functional interface as a contract for implementing objects encapsulating execution logic.

---
+++ src/main/java/org/cp/elements/lang/Executable.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang;
+
+/**
+ * The {@link Executable} interface defines a contract for implementing objects enabling them to be executed.
+ * This {@link Class interface} is similar in purpose and function to the {@link Runnable}
+ * and {@link java.util.concurrent.Callable} {@link Class interfaces}.
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@FunctionalInterface
+@SuppressWarnings("unused")
+public interface Executable {
+
+  /**
+   * Callback method to execute the intended function/logic encapsulated in the execution.
+   *
+   * @param <T> {@link Class} type of the executions return value.
+   * @return the result of the execution.
+   */
+  <T> T execute();
+
+}
