59cb74a66b259da31685a88cbd7d8de66a36b808
John Blum <jblum@pivotal.io>
2017-10-31 22:50:27 -0700

    Add additional java.lang.Runnable utility methods, e.g. runWithSleepThrowOnInterrupt(..) and runWithSleepUniterrupted(..).
    Add o.c.e.lang.RunnableUtils.ReturningRunnable utility methods, e.g. runWithSleepThenReturnValue(..), runWithSleepThenReturnValueThrowOnInterrupt(..) and runWithSleepThenReturnValueUninterrupted(..).
    
    Add unit tests for o.c.e.lang.RunnableUtils.

--- src/main/java/org/cp/elements/lang/RunnableUtils.java
+++ src/main/java/org/cp/elements/lang/RunnableUtils.java
@@ -25,28 +25,188 @@ import org.cp.elements.lang.concurrent.ThreadUtils;
  *
  * @author John Blum
  * @see java.lang.Runnable
+ * @see org.cp.elements.lang.concurrent.ThreadUtils
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
 public abstract class RunnableUtils {
 
-  public static boolean runWithPause(long milliseconds, Runnable runnable) {
+  /**
+   * Runs the given {@link Runnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link Runnable} object to run; must not be {@literal null}.
+   * @return a boolean value indicating whether the {@link Runnable} ran successfully
+   * and whether the current {@link Thread} slept for the given number of milliseconds.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @see org.cp.elements.lang.concurrent.ThreadUtils#sleep(long, int)
+   * @see java.lang.Runnable#run()
+   */
+  public static boolean runWithSleep(long milliseconds, Runnable runnable) {
+
     Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
     runnable.run();
+
     return ThreadUtils.sleep(milliseconds, 0);
   }
 
-  public static boolean runWithPauseUninterrupted(long milliseconds, Runnable runnable) {
+  /**
+   * Runs the given {@link Runnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link Runnable} object to run; must not be {@literal null}.
+   * @return a boolean value indicating whether the {@link Runnable} ran successfully
+   * and whether the current {@link Thread} slept for the given number of milliseconds.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @throws SleepDeprivedException if the current {@link Thread} is interrupted while sleeping.
+   * @see org.cp.elements.lang.concurrent.ThreadUtils#sleep(long, int)
+   * @see java.lang.Runnable#run()
+   */
+  public static boolean runWithSleepThrowOnInterrupt(long milliseconds, Runnable runnable) {
+
     Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
     runnable.run();
+
+    if (!ThreadUtils.sleep(milliseconds, 0)) {
+      throw new SleepDeprivedException(String.format("Failed to wait for [%d] millisecond(s)", milliseconds));
+    }
+
+    return true;
+  }
+
+  /**
+   * Runs the given {@link Runnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds.
+   *
+   * This utility method sleeps uninterrupted, resetting the interrupt bit if the current, calling {@link Thread}
+   * is interrupted during sleep.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link Runnable} object to run; must not be {@literal null}.
+   * @return a boolean value indicating whether the {@link Runnable} ran successfully
+   * and whether the current {@link Thread} slept for the given number of milliseconds.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @see java.lang.Runnable#run()
+   * @see #safeSleep(long)
+   */
+  public static boolean runWithSleepUninterrupted(long milliseconds, Runnable runnable) {
+
+    Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
+    runnable.run();
+
     return safeSleep(milliseconds);
   }
 
+  /**
+   * Runs the given {@link ReturningRunnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds before returning a value.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param <T> {{@link Class} type of the {@link ReturningRunnable} return value.
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link ReturningRunnable} object to run; must not be {@literal null}.
+   * @return the value returned by the {@link ReturningRunnable}.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @see org.cp.elements.lang.RunnableUtils.ReturningRunnable#run()
+   * @see org.cp.elements.lang.concurrent.ThreadUtils#sleep(long, int)
+   */
+  public static <T> T runWithSleepThenReturnValue(long milliseconds, ReturningRunnable<T> runnable) {
+
+    Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
+    T returnValue = runnable.run();
+
+    ThreadUtils.sleep(milliseconds, 0);
+
+    return returnValue;
+  }
+
+  /**
+   * Runs the given {@link ReturningRunnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds before returning a value.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param <T> {{@link Class} type of the {@link ReturningRunnable} return value.
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link ReturningRunnable} object to run; must not be {@literal null}.
+   * @return the value returned by the {@link ReturningRunnable}.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @throws SleepDeprivedException if the current {@link Thread} is interrupted while sleeping.
+   * @see org.cp.elements.lang.RunnableUtils.ReturningRunnable#run()
+   * @see org.cp.elements.lang.concurrent.ThreadUtils#sleep(long, int)
+   */
+  public static <T> T runWithSleepThenReturnValueThrowOnInterrupt(long milliseconds, ReturningRunnable<T> runnable) {
+
+    Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
+    T returnValue = runnable.run();
+
+    if (!ThreadUtils.sleep(milliseconds, 0)) {
+      throw new SleepDeprivedException(String.format("Failed to wait for [%d] millisecond(s)", milliseconds));
+    }
+
+    return returnValue;
+  }
+
+  /**
+   * Runs the given {@link ReturningRunnable} object and then causes the current, calling {@link Thread} to sleep
+   * for the given number of milliseconds before returning a value.
+   *
+   * This utility method sleeps uninterrupted, resetting the interrupt bit if the current, calling {@link Thread}
+   * is interrupted during sleep.
+   *
+   * This utility method can be used to simulate a long running, expensive operation.
+   *
+   * @param <T> {{@link Class} type of the {@link ReturningRunnable} return value.
+   * @param milliseconds a long value with the number of milliseconds for the current {@link Thread} to sleep.
+   * @param runnable {@link ReturningRunnable} object to run; must not be {@literal null}.
+   * @return the value returned by the {@link ReturningRunnable}.
+   * @throws IllegalArgumentException if milliseconds is less than equal to 0.
+   * @see org.cp.elements.lang.RunnableUtils.ReturningRunnable#run()
+   * @see #safeSleep(long)
+   */
+  public static <T> T runWithSleepThenReturnValueUninterrupted(long milliseconds, ReturningRunnable<T> runnable) {
+
+    Assert.isTrue(milliseconds > 0, "Milliseconds [%d] must be greater than 0", milliseconds);
+
+    T returnValue = runnable.run();
+
+    safeSleep(milliseconds);
+
+    return returnValue;
+  }
+
+  /**
+   * Safely sleeps for the given amount of milliseconds.
+   *
+   * If the {@link Thread#currentThread() current Thread} is {@link Thread#isInterrupted() interrupted} while
+   * {@link Thread#sleep(long, int) sleeping}, then the {@link Thread#currentThread() current Thread} will continue
+   * to {@link Thread#sleep(long, int) sleep} until the given number of milliseconds have expired and the interrupt bit
+   * will be set on return.
+   *
+   * @param milliseconds the number of milliseconds that the {@link Thread#currentThread() current Thread} will sleep.
+   * @return {@literal true} if the {@link Thread#currentThread() current Thread} was able to
+   * {@link Thread#sleep(long, int)} for the given number of milliseconds uninterrupted.
+   * @see java.lang.Thread#sleep(long, int)
+   */
   private static boolean safeSleep(long milliseconds) {
 
     boolean interrupted = false;
 
-    long timeout = System.currentTimeMillis() + milliseconds;
+    long timeout = (System.currentTimeMillis() + milliseconds);
 
     while (System.currentTimeMillis() < timeout) {
       try {
@@ -55,13 +215,16 @@ public abstract class RunnableUtils {
       catch (InterruptedException cause) {
         interrupted = true;
       }
+      finally {
+        milliseconds = Math.min(timeout - System.currentTimeMillis(), 0);
+      }
     }
 
     if (interrupted) {
       Thread.currentThread().interrupt();
     }
 
-    return true;
+    return !Thread.currentThread().isInterrupted();
   }
 
   /**
@@ -75,5 +238,70 @@ public abstract class RunnableUtils {
     long t0 = System.currentTimeMillis();
     runnable.run();
     return Optional.of(System.currentTimeMillis() - t0);
+  }
+
+  /**
+   * The {@link ReturningRunnable} interface defines a contract for implementing objects that run a computation
+   * and return a value.
+   *
+   * @param <T> {@link Class} type of the return value.
+   */
+  public interface ReturningRunnable<T> {
+
+    /**
+     * Computation to run.
+     *
+     * @return the result of the computation.
+     */
+    T run();
+  }
+
+  /**
+   * The {@link SleepDeprivedException} is a {@link RuntimeException} that is thrown when a {@link Thread}
+   * did not get all of its {@link Thread#sleep(long, int) sleep}.
+   *
+   * @see java.lang.RuntimeException
+   */
+  public static final class SleepDeprivedException extends RuntimeException {
+
+    /**
+     * Construct a new instance of the {@link SleepDeprivedException} with no {@link String message} or no known
+     * {@link Throwable cause}.
+     */
+    public SleepDeprivedException() {
+    }
+
+    /**
+     * Construct a new instance of the {@link SleepDeprivedException} initialized with given {@link String message}
+     * describing the reason for this exception.
+     *
+     * @param message {@link String} describing the reason for this exception.
+     */
+    public SleepDeprivedException(String message) {
+      super(message);
+    }
+
+    /**
+     * Construct a new instance of the {@link SleepDeprivedException} initialized with given {@link Throwable cause}
+     * of this exception.
+     *
+     * @param cause {@link Throwable} object as the underlying cause of this exception.
+     * @see java.lang.Throwable
+     */
+    public SleepDeprivedException(Throwable cause) {
+      super(cause);
+    }
+
+    /**
+     * Construct a new instance of the {@link SleepDeprivedException} initialized with given {@link String message}
+     * describing the reason for this exception along with the underlying {@link Throwable cause} of this exception.
+     *
+     * @param message {@link String} describing the reason for this exception.
+     * @param cause {@link Throwable} object as the underlying cause of this exception.
+     * @see java.lang.Throwable
+     */
+    public SleepDeprivedException(String message, Throwable cause) {
+      super(message, cause);
+    }
   }
 }
--- src/test/java/org/cp/elements/lang/RunnableUtilsTests.java
+++ src/test/java/org/cp/elements/lang/RunnableUtilsTests.java
@@ -18,20 +18,389 @@ package org.cp.elements.lang;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.assertj.core.api.Assertions;
 import org.cp.elements.lang.concurrent.ThreadUtils;
 import org.junit.Test;
 
+import edu.umd.cs.mtc.MultithreadedTestCase;
+import edu.umd.cs.mtc.TestFramework;
+
 /**
- * The RunnableUtilsTests class...
+ * Unit tests for {@link RunnableUtils}.
  *
  * @author John Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.lang.RunnableUtils
+ * @see edu.umd.cs.mtc.MultithreadedTestCase
+ * @see edu.umd.cs.mtc.TestFramework
  * @since 1.0.0
  */
 public class RunnableUtilsTests {
 
+  private void testRunWithSleepUsingIllegalMilliseconds(RunnableUtilsInvoker invoker, long milliseconds) {
+
+    AtomicBoolean ran = new AtomicBoolean(false);
+
+    try {
+      invoker.invoke(milliseconds, () -> ran.set(true));
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Milliseconds [%d] must be greater than 0", milliseconds);
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(ran.get()).isFalse();
+    }
+  }
+
+  private void testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtilsReturningInvoker invoker,
+      long milliseconds) {
+
+    AtomicBoolean ran = new AtomicBoolean(false);
+
+    try {
+      invoker.invoke(milliseconds, () -> {
+        ran.set(true);
+        return "test";
+      });
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Milliseconds [%d] must be greater than 0", milliseconds);
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(ran.get()).isFalse();
+    }
+  }
+
   @Test
-  public void timeRunIsCorrect() {
-    assertThat(RunnableUtils.timedRun(() -> ThreadUtils.sleep(1000, 0)).orElse(0L))
-      .isGreaterThan(1000L);
+  public void runWithSleep() {
+
+    AtomicBoolean ran = new AtomicBoolean(false);
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleep(50L, () -> ran.set(true))).isTrue();
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+    assertThat(ran.get()).isTrue();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepUsingNegativeMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleep, -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepUsingZeroMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleep, 0L);
+  }
+
+  @Test
+  public void runWithSleepThrowOnInterrupt() {
+
+    AtomicBoolean ran = new AtomicBoolean(false);
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleepThrowOnInterrupt(50L, () -> ran.set(true))).isTrue();
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+    assertThat(ran.get()).isTrue();
+  }
+
+  @Test
+  public void runWithSleepThrowOnInterruptThrowsSleepDeprivedException() throws Throwable {
+    TestFramework.runOnce(new RunWithSleepThrowOnInterruptThrowsSleepDeprivedException());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThrowOnInterruptUsingNegativeMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleepThrowOnInterrupt, -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThrowOnInterruptUsingZeroMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleepThrowOnInterrupt, 0L);
+  }
+
+  @Test
+  public void runWithSleepUninterrupted() {
+
+    AtomicBoolean ran = new AtomicBoolean(false);
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleepUninterrupted(50L, () -> ran.set(true))).isTrue();
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+    assertThat(ran.get()).isTrue();
+  }
+
+  @Test
+  public void runWithSleepUninterruptedHandlesInterruptedThread() throws Throwable {
+    TestFramework.runOnce(new RunWithSleepUninterruptedHandlesInterruptedThread());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepUninterruptedUsingNegativeMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleepUninterrupted, -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepUninterruptedUsingZeroMilliseconds() {
+    testRunWithSleepUsingIllegalMilliseconds(RunnableUtils::runWithSleepUninterrupted, 0L);
+  }
+
+  @Test
+  public void runWithSleepThenReturnValue() {
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleepThenReturnValue(50L, () -> "test")).isEqualTo("test");
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueWithNegativeMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValue, -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueWithZeroMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValue, -0L);
+  }
+
+  @Test
+  public void runWithSleepThenReturnValueThrowOnInterrupt() {
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleepThenReturnValueThrowOnInterrupt(50L, () -> "test"))
+      .isEqualTo("test");
+
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+  }
+
+  @Test
+  public void runWithSleepThenReturnValueThrowOnInterruptThrowsSleepDeprivedException() throws Throwable {
+    TestFramework.runOnce(new RunWithSleepThenReturnValueThrowOnInterruptThrowsSleepDeprivedException());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueThrowOnInterruptWithNegativeMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValueThrowOnInterrupt,
+      -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueThrowOnInterruptWithZeroMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValueThrowOnInterrupt,
+      -0L);
+  }
+
+  @Test
+  public void runWithSleepThenReturnValueUninterrupted() {
+
+    long t0 = System.currentTimeMillis();
+
+    assertThat(RunnableUtils.runWithSleepThenReturnValueUninterrupted(50L, () -> "test")).isEqualTo("test");
+    assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(50L);
+  }
+
+  @Test
+  public void runWithSleepThenReturnValueUninterruptedHandlesInterruptedThread() throws Throwable {
+    TestFramework.runOnce(new RunWithSleepThenReturnValueUninterruptedHandlesInterruptedThread());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueUninterruptedWithNegativeMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValueUninterrupted,
+      -1L);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void runWithSleepThenReturnValueUninterruptedWithZeroMilliseconds() {
+    testRunWithSleepThenReturnValueUsingIllegalMilliseconds(RunnableUtils::runWithSleepThenReturnValueUninterrupted,
+      -0L);
+  }
+
+  @Test
+  public void timedRunIsCorrect() {
+    assertThat(RunnableUtils.timedRun(() -> ThreadUtils.sleep(100L, 0)).orElse(0L))
+      .isGreaterThanOrEqualTo(100L);
+  }
+
+  @SuppressWarnings("unused")
+  private static final class RunWithSleepThrowOnInterruptThrowsSleepDeprivedException extends MultithreadedTestCase {
+
+    private volatile Thread sleepingThread;
+
+    public void thread1() {
+
+      assertTick(0);
+
+      this.sleepingThread = Thread.currentThread();
+      this.sleepingThread.setName("Sleeping Thread");
+
+      AtomicBoolean ran = new AtomicBoolean(false);
+
+      try {
+        RunnableUtils.runWithSleepThrowOnInterrupt(2000, () -> ran.set(true));
+        Assertions.fail("Current Thread was not interrupted");
+      }
+      catch (RunnableUtils.SleepDeprivedException expected) {
+        assertThat(expected).hasMessage("Failed to wait for [2000] millisecond(s)");
+        assertThat(expected).hasNoCause();
+      }
+      finally {
+        assertThat(ran.get()).isTrue();
+        assertThat(Thread.currentThread().isInterrupted()).isTrue();
+      }
+    }
+
+    public void thread2() {
+
+      Thread.currentThread().setName("Interrupting Thread");
+
+      waitForTick(1);
+
+      Optional.ofNullable(this.sleepingThread).ifPresent(Thread::interrupt);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static final class RunWithSleepUninterruptedHandlesInterruptedThread extends MultithreadedTestCase {
+
+    private volatile Thread sleepingThread;
+
+    public void thread1() {
+
+      assertTick(0);
+
+      this.sleepingThread = Thread.currentThread();
+      this.sleepingThread.setName("Sleeping Thread");
+
+      AtomicBoolean ran = new AtomicBoolean(false);
+
+      long t0 = System.currentTimeMillis();
+
+      try {
+        assertThat(RunnableUtils.runWithSleepUninterrupted(200L, () -> ran.set(true))).isFalse();
+        assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(200L);
+      }
+      catch (Throwable cause) {
+        Assertions.fail("Current Thread was interrupted and the interrupt was not properly handled", cause);
+      }
+      finally {
+        assertThat(ran.get()).isTrue();
+        assertThat(Thread.currentThread().isInterrupted()).isTrue();
+      }
+    }
+
+    public void thread2() {
+
+      Thread.currentThread().setName("Interrupting Thread");
+
+      waitForTick(1);
+
+      Optional.ofNullable(this.sleepingThread).ifPresent(Thread::interrupt);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static final class RunWithSleepThenReturnValueThrowOnInterruptThrowsSleepDeprivedException
+      extends MultithreadedTestCase {
+
+    private volatile Thread sleepingThread;
+
+    public void thread1() {
+
+      assertTick(0);
+
+      this.sleepingThread = Thread.currentThread();
+      this.sleepingThread.setName("Sleeping Thread");
+
+      try {
+        RunnableUtils.runWithSleepThenReturnValueThrowOnInterrupt(2000, () -> "test");
+        Assertions.fail("Current Thread was not interrupted");
+      }
+      catch (RunnableUtils.SleepDeprivedException expected) {
+        assertThat(expected).hasMessage("Failed to wait for [2000] millisecond(s)");
+        assertThat(expected).hasNoCause();
+      }
+      finally {
+        assertThat(Thread.currentThread().isInterrupted()).isTrue();
+      }
+    }
+
+    public void thread2() {
+
+      Thread.currentThread().setName("Interrupting Thread");
+
+      waitForTick(1);
+
+      Optional.ofNullable(this.sleepingThread).ifPresent(Thread::interrupt);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static final class RunWithSleepThenReturnValueUninterruptedHandlesInterruptedThread
+      extends MultithreadedTestCase {
+
+    private volatile Thread sleepingThread;
+
+    public void thread1() {
+
+      assertTick(0);
+
+      this.sleepingThread = Thread.currentThread();
+      this.sleepingThread.setName("Sleeping Thread");
+
+      AtomicBoolean ran = new AtomicBoolean(false);
+
+      long t0 = System.currentTimeMillis();
+
+      try {
+        assertThat(RunnableUtils.runWithSleepThenReturnValueUninterrupted(200L, () -> {
+          ran.set(true);
+          return "test";
+        })).isEqualTo("test");
+
+        assertThat(System.currentTimeMillis() - t0).isGreaterThanOrEqualTo(200L);
+      }
+      catch (Throwable cause) {
+        Assertions.fail("Current Thread was interrupted and the interrupt was not properly handled", cause);
+      }
+      finally {
+        assertThat(ran.get()).isTrue();
+        assertThat(Thread.currentThread().isInterrupted()).isTrue();
+      }
+    }
+
+    public void thread2() {
+
+      Thread.currentThread().setName("Interrupting Thread");
+
+      waitForTick(1);
+
+      Optional.ofNullable(this.sleepingThread).ifPresent(Thread::interrupt);
+    }
+  }
+
+  @FunctionalInterface
+  interface RunnableUtilsInvoker {
+    void invoke(long milliseconds, Runnable runnable);
+  }
+
+  @FunctionalInterface
+  interface RunnableUtilsReturningInvoker {
+    void invoke(long milliseconds, RunnableUtils.ReturningRunnable<?> runnable);
   }
 }
