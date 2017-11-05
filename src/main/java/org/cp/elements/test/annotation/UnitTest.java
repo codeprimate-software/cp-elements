c6945dd58270055c1321c07db377c8490e8c9a54
John Blum <jblum@pivotal.io>
2017-11-04 22:58:27 -0700

    Add UnitTest annotation to denot Unit Tests.

---
+++ src/main/java/org/cp/elements/test/annotation/UnitTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.test.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The {@link UnitTest} annotation is a marker {@link Annotation} indicating that the annotated test case
+ * or test class is Unit Test.
+ *
+ * @author John J. Blum
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.Target
+ * @see org.cp.elements.test.annotation.FunctionalTest
+ * @since 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+public @interface UnitTest {
+
+}
