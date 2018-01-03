5d647d6c3c2617e128917595a683c70559d4c8b2
John Blum <jblum@pivotal.io>
2018-01-03 10:00:02 -0800

    Define a new Registry interface in the org.cp.elements.lang package that defines a contract for classes implementing some type of service object registry.

---
+++ src/main/java/org/cp/elements/lang/Registry.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang;
+
+import java.util.Iterator;
+import java.util.Optional;
+
+import org.cp.elements.lang.annotation.NullSafe;
+
+/**
+ * The {@link Registry} interface defines a contract for implementing {@link Object objects} that can register
+ * and unregister other interesting {@link Object Objects}.
+ *
+ * @author John Blum
+ * @see java.lang.Iterable
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public interface Registry<T> extends Iterable<T> {
+
+  /**
+   * Determines whether the {@link Object} is registered with this {@link Registry}.
+   *
+   * @param obj {@link Object} to evaluate.
+   * @return a boolean value indicating whether the {@link Object} is registered with this {@link Registry}.
+   * @see #register(Object)
+   */
+  @NullSafe
+  default boolean isRegistered(T obj) {
+
+    return Optional.ofNullable(obj)
+      .map(it -> {
+
+        for (T registeredObject : this) {
+          if (it.equals(registeredObject)) {
+            return true;
+          }
+        }
+
+        return false;
+
+      })
+      .orElse(false);
+  }
+
+  /**
+   * Registers the {@link Object} with this {@link Registry}.
+   *
+   * @param <R> {@link Class concrete type} of this {@link Registry} implementation.
+   * @param obj {@link Object} to register.
+   * @return this {@link Registry}.
+   * @see #unregister(Object)
+   */
+  <R extends Registry<T>> R register(T obj);
+
+  /**
+   * Unregisters the {@link Object} from this {@link Registry}.
+   *
+   * @param <R> {@link Class concrete type} of this {@link Registry} implementation.
+   * @param obj {@link Object} to unregister.
+   * @return this {@link Registry}.
+   * @see #register(Object)
+   */
+  @NullSafe
+  @SuppressWarnings("unchecked")
+  default <R extends Registry<T>> R unregister(T obj) {
+
+    Iterator<T> registeredObjects = this.iterator();
+
+    while (registeredObjects.hasNext()) {
+      if (registeredObjects.next().equals(obj)) {
+        registeredObjects.remove();
+      }
+    }
+
+    return (R) this;
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/RegistryTests.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.util.ArrayUtils.asIterator;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link Registry}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.Registry
+ * @since 1.0.0
+ */
+public class RegistryTests {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void isRegisteredWithNullObjectReturnsFalse() {
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.isRegistered(any())).thenCallRealMethod();
+
+    assertThat(mockRegistry.isRegistered(null)).isFalse();
+
+    verify(mockRegistry, never()).iterator();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void isRegisteredWithRegisteredObjectReturnsTrue() {
+
+    Object objectOne = new Object();
+    Object objectTwo = new Object();
+    Object objectThree = new Object();
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.isRegistered(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(asIterator(objectOne, objectTwo, objectThree));
+
+    assertThat(mockRegistry.isRegistered(objectOne)).isTrue();
+    assertThat(mockRegistry.isRegistered(objectTwo)).isTrue();
+
+    verify(mockRegistry, times(2)).iterator();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void isRegisteredWithUnregisteredObjectReturnsFalse() {
+
+    Object object = new Object();
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.isRegistered(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(Collections.emptyIterator());
+
+    assertThat(mockRegistry.isRegistered(object)).isFalse();
+
+    verify(mockRegistry, times(1)).iterator();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void unregisterRegisteredObject() {
+
+    Object registeredObject = new Object();
+
+    Iterator<Object> mockIterator = mock(Iterator.class);
+
+    when(mockIterator.hasNext()).thenReturn(true, false);
+    when(mockIterator.next()).thenReturn(registeredObject);
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.unregister(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(mockIterator);
+
+    assertThat(mockRegistry.<Registry>unregister(registeredObject)).isEqualTo(mockRegistry);
+
+    verify(mockIterator, times(2)).hasNext();
+    verify(mockIterator, times(1)).next();
+    verify(mockIterator, times(1)).remove();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void unregisterUnregisteredObject() {
+
+    Object unregisteredObject = new Object();
+
+    Iterator<Object> mockIterator = mock(Iterator.class);
+
+    when(mockIterator.hasNext()).thenReturn(true, false);
+    when(mockIterator.next()).thenReturn("test");
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.unregister(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(mockIterator);
+
+    assertThat(mockRegistry.<Registry>unregister(unregisteredObject)).isEqualTo(mockRegistry);
+
+    verify(mockIterator, times(2)).hasNext();
+    verify(mockIterator, times(1)).next();
+    verify(mockIterator, never()).remove();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void unregisterWhenNoObjectsAreRegistered() {
+
+    Iterator<Object> mockIterator = mock(Iterator.class);
+
+    when(mockIterator.hasNext()).thenReturn(false);
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.unregister(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(mockIterator);
+
+    assertThat(mockRegistry.<Registry>unregister("test")).isEqualTo(mockRegistry);
+
+    verify(mockIterator, times(1)).hasNext();
+    verify(mockIterator, never()).next();
+    verify(mockIterator, never()).remove();
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void unregisterWithNullObject() {
+
+    Iterator<Object> mockIterator = mock(Iterator.class);
+
+    when(mockIterator.hasNext()).thenReturn(true, false);
+    when(mockIterator.next()).thenReturn("null");
+
+    Registry<Object> mockRegistry = mock(Registry.class);
+
+    when(mockRegistry.unregister(any())).thenCallRealMethod();
+    when(mockRegistry.iterator()).thenReturn(mockIterator);
+
+    assertThat(mockRegistry.<Registry>unregister(null)).isEqualTo(mockRegistry);
+
+    verify(mockIterator, times(2)).hasNext();
+    verify(mockIterator, times(1)).next();
+    verify(mockIterator, never()).remove();
+  }
+}
