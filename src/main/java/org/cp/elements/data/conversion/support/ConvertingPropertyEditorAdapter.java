097e2be9d3017e866254c5fbc3ab8cf746c4bdcc
John Blum <jblum@pivotal.io>
2018-01-14 20:59:37 -0800

    Create the o.c.e.data.conversion.support.ConvertingPropertyEditorAdapter class to adapt an o.c.e.data.conversion.Converter as a java.beans.PropertyEditor.

---
+++ src/main/java/org/cp/elements/data/conversion/support/ConvertingPropertyEditorAdapter.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.conversion.support;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
+
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorSupport;
+
+import org.cp.elements.data.conversion.ConversionException;
+import org.cp.elements.data.conversion.Converter;
+import org.cp.elements.lang.Assert;
+
+/**
+ * The {@link ConvertingPropertyEditorAdapter} class is an Adapter adapting the {@link Converter} interface
+ * into an instance of {@link PropertyEditor}.
+ *
+ * @author John Blum
+ * @see java.beans.PropertyEditor
+ * @see org.cp.elements.data.conversion.Converter
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ConvertingPropertyEditorAdapter extends PropertyEditorSupport {
+
+  private final Converter<String, ?> converter;
+
+  /**
+   * Constructs a new instance of the {@link ConvertingPropertyEditorAdapter} initialized with
+   * the given {@link Converter}.
+   *
+   * @param converter {@link Converter} backing this {@link PropertyEditor}.
+   * @return a new instance of {@link ConvertingPropertyEditorAdapter} initialized with
+   * the given {@link Converter}.
+   * @throws IllegalArgumentException if {@link Converter} is {@literal null}.
+   * @see #ConvertingPropertyEditorAdapter(Converter)
+   * @see org.cp.elements.data.conversion.Converter
+   */
+  public static ConvertingPropertyEditorAdapter of(Converter<String, ?> converter) {
+    return new ConvertingPropertyEditorAdapter(converter);
+  }
+
+  /**
+   * Constructs a new instance of the {@link ConvertingPropertyEditorAdapter} initialized with
+   * the given {@link Converter}.
+   *
+   * @param converter {@link Converter} backing this {@link PropertyEditor}.
+   * @throws IllegalArgumentException if {@link Converter} is {@literal null}.
+   * @see org.cp.elements.data.conversion.Converter
+   */
+  protected ConvertingPropertyEditorAdapter(Converter<String, ?> converter) {
+
+    Assert.notNull(converter, "Converter is required");
+
+    this.converter = converter;
+  }
+
+  /**
+   * Returns a reference to the configured {@link Converter}.
+   *
+   * @return a reference to the configured {@link Converter}.
+   * @see org.cp.elements.data.conversion.Converter
+   */
+  protected Converter<String, ?> getConverter() {
+    return this.converter;
+  }
+
+  /**
+   * Converts the given {@link String text} into an {@link Object}.
+   *
+   * @param text {@link String} to convert.
+   * @throws IllegalArgumentException if the given {@link String text}
+   * cannot be {@link Converter#convert(Object) converted} into an {@link Object}.
+   * @see org.cp.elements.data.conversion.Converter#convert(Object)
+   * @see #getConverter()
+   */
+  @Override
+  public void setAsText(String text) throws IllegalArgumentException {
+
+    try {
+      setValue(getConverter().convert(text));
+    }
+    catch (ConversionException cause) {
+      throw newIllegalArgumentException(cause, "Could not set text [%s] as value", text);
+    }
+  }
+}
---
+++ src/test/java/org/cp/elements/data/conversion/support/ConvertingPropertyEditorAdapterTests.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.conversion.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.ElementsExceptionsFactory.newConversionException;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.cp.elements.data.conversion.ConversionException;
+import org.cp.elements.data.conversion.Converter;
+import org.cp.elements.enums.Gender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Unit tests for {@link ConvertingPropertyEditorAdapter}.
+ *
+ * @author John Blum
+ * @see java.beans.PropertyEditor
+ * @see org.junit.Test
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.data.conversion.Converter
+ * @see org.cp.elements.data.conversion.support.ConvertingPropertyEditorAdapter
+ * @since 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConvertingPropertyEditorAdapterTests {
+
+  @Mock
+  private Converter<String, Object> mockConverter;
+
+  @Test
+  public void constructsNewConvertingPropertyEditorAdapter() {
+
+    ConvertingPropertyEditorAdapter propertyEditor = new ConvertingPropertyEditorAdapter(this.mockConverter);
+
+    assertThat(propertyEditor).isNotNull();
+    assertThat(propertyEditor.getConverter()).isEqualTo(this.mockConverter);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructsNewConvertingPropertyEditorWithNullConverterThrowsException() {
+
+    try {
+      new ConvertingPropertyEditorAdapter(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Converter is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void setAsTextIsSuccessful() {
+
+    when(this.mockConverter.convert(eq("female"))).thenReturn(Gender.FEMALE);
+
+    ConvertingPropertyEditorAdapter propertyEditor = ConvertingPropertyEditorAdapter.of(this.mockConverter);
+
+    assertThat(propertyEditor).isNotNull();
+    assertThat(propertyEditor.getConverter()).isEqualTo(this.mockConverter);
+
+    propertyEditor.setAsText("female");
+
+    assertThat(propertyEditor.getValue()).isEqualTo(Gender.FEMALE);
+
+    verify(this.mockConverter, times(1)).convert(eq("female"));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setAsTextHandlesConversionExceptionThrowsIllegalArgumentException() {
+
+    when(this.mockConverter.convert(anyString())).thenThrow(newConversionException("test"));
+
+    ConvertingPropertyEditorAdapter propertyEditor = ConvertingPropertyEditorAdapter.of(this.mockConverter);
+
+    try {
+      assertThat(propertyEditor).isNotNull();
+      assertThat(propertyEditor.getConverter()).isEqualTo(this.mockConverter);
+      assertThat(propertyEditor.getValue()).isNull();
+
+      propertyEditor.setAsText("test");
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Could not set text [test] as value");
+      assertThat(expected).hasCauseInstanceOf(ConversionException.class);
+      assertThat(expected.getCause()).hasMessage("test");
+      assertThat(expected.getCause()).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verify(this.mockConverter, times(1)).convert(eq("test"));
+      assertThat(propertyEditor.getValue()).isNull();
+    }
+  }
+}
