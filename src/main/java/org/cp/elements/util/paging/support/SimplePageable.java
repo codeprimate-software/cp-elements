7c10f75f53031ffe06db7dde49cba53aa8e4d2b5
John Blum <jblum@pivotal.io>
2018-01-22 22:32:11 -0800

    Create the o.c.e.util.paging.support.SimplePageable class to wrap an array or java.util.List to provide paging capabilities.

---
+++ src/main/java/org/cp/elements/util/paging/support/SimplePageable.java
@@ -0,0 +1,423 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.util.paging.support;
+
+import static java.util.Arrays.asList;
+import static org.cp.elements.lang.ElementsExceptionsFactory.newPageNotFoundException;
+import static org.cp.elements.util.ArrayUtils.nullSafeArray;
+import static org.cp.elements.util.CollectionUtils.nullSafeList;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.util.paging.Page;
+import org.cp.elements.util.paging.PageNotFoundException;
+import org.cp.elements.util.paging.Pageable;
+
+/**
+ * The SimplePageable class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class SimplePageable<T> implements Pageable<T> {
+
+  protected static final int DEFAULT_PAGE_SIZE = 20;
+
+  /**
+   * Factory method used to construct a new instance of {@link SimplePageable} from the given array.
+   *
+   * The returned {@link Pageable} object wraps the given array to provide paging functionality.
+   * The {@link Pageable} object uses the default page size of 20 elements per {@link Page}.
+   *
+   * @param <T> {@link Class type} of elements contained in the array.
+   * @param array {@link Object} array to wrap and provide paging functionality for.
+   * @return a new instance of {@link Pageable} wrapping the given array to provide paging functionality.
+   * @see #SimplePageable(List)
+   */
+  @SafeVarargs
+  public static <T> SimplePageable<T> of(T... array) {
+    return new SimplePageable<>(asList(nullSafeArray(array)));
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link SimplePageable} for the given {@link List}.
+   *
+   * The returned {@link Pageable} object wraps the given {@link List} to provide paging functionality.
+   * The {@link Pageable} object uses the default page size of 20 elements per {@link Page}.
+   *
+   * @param <T> {@link Class type} of elements contained in the {@link List}.
+   * @param list {@link List} to wrap and provide paging functionality for.
+   * @return a new instance of {@link Pageable} wrapping the given {@link List} to provide paging functionality.
+   * @see #of(List, int)
+   * @see java.util.List
+   */
+  public static <T> SimplePageable<T> of(List<T> list) {
+    return of(list, DEFAULT_PAGE_SIZE);
+  }
+
+  /**
+   * Factory method used to construct a new instance of {@link SimplePageable} for the given {@link List}
+   * with the configured {@link Page} size.
+   *
+   * The returned {@link Pageable} object wraps the given {@link List} to provide paging functionality.
+   * The {@link Pageable} object uses the given page size.
+   *
+   * @param <T> {@link Class type} of elements contained in the {@link List}.
+   * @param list {@link List} to wrap and provide paging functionality for.
+   * @param pageSize integer indicating the desired number of elements per {@link Page}
+   * @return a new instance of {@link Pageable} wrapping the given {@link List} to provide paging functionality.
+   * @see #SimplePageable(List, int)
+   * @see java.util.List
+   */
+  public static <T> SimplePageable<T> of(List<T> list, int pageSize) {
+    return new SimplePageable<>(nullSafeList(list), pageSize);
+  }
+
+  private final int pageSize;
+
+  private final List<T> list;
+
+  /**
+   * Constructs a new instance of {@link SimplePageable} initialized with the given {@link List}
+   * and default page size of 20 elements per {@link Page}.
+   *
+   * @param list {@link List} to wrap and provide paging functionality for.
+   * @throws IllegalArgumentException if the {@link List} is {@literal null}.
+   * @see #SimplePageable(List, int)
+   */
+  public SimplePageable(List<T> list) {
+    this(list, DEFAULT_PAGE_SIZE);
+  }
+
+  /**
+   * Constructs a new instance of {@link SimplePageable} initialized with the given {@link List} and given page size.
+   *
+   * @param list {@link List} to wrap and provide paging functionality for.
+   * @param pageSize integer indicating the desired number of elements per {@link Page}
+   * @throws IllegalArgumentException if the {@link List} is {@literal null} or the {@code pageSize}
+   * is less than equal to {@literal 0}.
+   */
+  public SimplePageable(List<T> list, int pageSize) {
+
+    Assert.notNull(list, "List is required");
+    Assert.isTrue(pageSize > 0, "Page size [%d] must be greater than 0", pageSize);
+
+    this.list = list;
+    this.pageSize = pageSize;
+  }
+
+  /**
+   * Returns a reference to the wrapped {@link List} for which paging functionality is provided.
+   *
+   * @return a reference to the wrapped {@link List} for which paging functionality is provided.
+   * @see java.util.List
+   */
+  protected List<T> getList() {
+    return this.list;
+  }
+
+  /**
+   * Returns the configured page size for this {@link Pageable}, which determines
+   * the number of elements per {@link Page}.
+   *
+   * @return an integer value indicating the configured number of elements per {@link Page}.
+   */
+  protected int getPageSize() {
+    return this.pageSize;
+  }
+
+  /**
+   * Get the total number of {@link Page pages} in this {@link Pageable} object.
+   *
+   * @return an int value indicating the total number of {@link Page pages} in this {@link Pageable} object.
+   */
+  @Override
+  public int count() {
+
+    int listSize = getList().size();
+    int pageSize = getPageSize();
+    int pageCount = listSize / pageSize;
+
+    pageCount += listSize % pageSize > 0 ? 1 : 0;
+
+    return pageCount;
+  }
+
+  /**
+   * Determines whether this {@link Pageable} object contains any {@link Page Pages}.
+   *
+   * @return a boolean value indicating whether this {@link Pageable} object contains any {@link Page Pages}.
+   * @see #count()
+   */
+  @Override
+  public boolean isEmpty() {
+    return getList().isEmpty();
+  }
+
+  /**
+   * Iterates over the {@link Page Pages} contained in this {@link Pageable object}.
+   *
+   * @return an {@link Iterator} over the {@link Page Pages} contained in this {@link Pageable object}.
+   * @see java.util.Iterator
+   */
+  @Override
+  public Iterator<Page<T>> iterator() {
+
+    return new Iterator<Page<T>>() {
+
+      int currentIndex = 0;
+      int currentPageNumber = 1;
+      int pageSize = getPageSize();
+
+      @Override
+      public boolean hasNext() {
+        return this.currentIndex < getList().size();
+      }
+
+      @Override
+      public Page<T> next() {
+
+        Assert.isTrue(hasNext(), new NoSuchElementException("No more pages"));
+
+        Page<T> page = SimplePage.of(SimplePageable.this, this.currentPageNumber);
+
+        this.currentIndex += this.pageSize;
+        this.currentPageNumber++;
+
+        return page;
+      }
+    };
+  }
+
+  /**
+   * Sorts all the elements contained in this {@link Pageable} object.
+   *
+   * @param orderBy {@link Comparator} used to sort (order) all the elements across all the {@link Page Pages}
+   * contained by this {@link Pageable}.
+   * @see java.util.Comparator
+   * @see #getList()
+   */
+  @Override
+  public void sort(Comparator<T> orderBy) {
+    getList().sort(orderBy);
+  }
+
+  /**
+   * {@link SimplePage} is an Abstract Data Type (ADT) modeling a single {@link Page} contained by
+   * this {@link SimplePageable} object.
+   *
+   * @param <T> {@link Class type} of the elements contained in this {@link Page}.
+   * @see org.cp.elements.util.paging.Page
+   */
+  protected static class SimplePage<T> implements Page<T> {
+
+    private final int pageNumber;
+
+    private final List<T> elements;
+
+    private final SimplePageable<T> pageable;
+
+    /**
+     * Factory method used to construct a new instance of {@link SimplePage} initialized with the source,
+     * underlying {@link SimplePageable} object containing this {@link Page} and this {@link Page Page's}
+     * page number.
+     *
+     * This factory method constructs a new {@link SimplePage} for the first {@link Page}
+     * in the {@link Pageable} object.
+     *
+     * @param pageable reference to the {@link SimplePageable} object that contains this {@link Page}.
+     * @throws IllegalArgumentException if the {@link SimplePageable} object is {@literal null}
+     * or the {@link Pageable} object is empty.
+     * @see org.cp.elements.util.paging.support.SimplePageable
+     * @see #of(SimplePageable, int)
+     */
+    protected static <T> SimplePage<T> of(SimplePageable<T> pageable) {
+      return of(pageable, 1);
+    }
+
+    /**
+     * Factory method used to construct a new instance of {@link SimplePage} initialized with the source,
+     * underlying {@link SimplePageable} object containing this {@link Page} and this {@link Page Page's}
+     * page number.
+     *
+     * @param pageable reference to the {@link SimplePageable} object that contains this {@link Page}.
+     * @param pageNumber integer value indicating this {@link Page Page's} page number in
+     * the collection of {@link Page Pages} contained by the {@link SimplePageable} object.
+     * @throws IllegalArgumentException if the {@link SimplePageable} object is {@literal null},
+     * the {@link Pageable} object is empty or the page number exceeds the number of {@link Page Pages}
+     * contained by the given {@link Pageable} object.
+     * @see org.cp.elements.util.paging.support.SimplePageable
+     * @see #SimplePage(SimplePageable, int)
+     */
+    protected static <T> SimplePage<T> of(SimplePageable<T> pageable, int pageNumber) {
+      return new SimplePage<>(pageable, pageNumber);
+    }
+
+    /**
+     * Constructs a new instance of {@link SimplePage} initialized with the source, underlying {@link SimplePageable}
+     * object containing this {@link Page} and this {@link Page Page's} page number.
+     *
+     * @param pageable reference to the {@link SimplePageable} object that contains this {@link Page}.
+     * @param pageNumber integer value indicating this {@link Page Page's} page number in
+     * the collection of {@link Page Pages} contained by the {@link SimplePageable} object.
+     * @throws IllegalArgumentException if the {@link SimplePageable} object is {@literal null},
+     * the {@link Pageable} object is empty or the page number exceeds the number of {@link Page Pages}
+     * contained by the given {@link Pageable} object.
+     * @see org.cp.elements.util.paging.support.SimplePageable
+     */
+    protected SimplePage(SimplePageable<T> pageable, int pageNumber) {
+
+      Assert.notNull(pageable, "Pageable is required");
+      Assert.isFalse(pageable.isEmpty(), "Pageable object must contain pages");
+
+      int pageCount = pageable.count();
+
+      Assert.isTrue(pageNumber <= pageCount,
+        "Page number must less than equal to the number of pages [%d]", pageCount);
+
+      List<T> list = pageable.getList();
+
+      int pageSize = pageable.getPageSize();
+      int fromIndex = Math.max((pageNumber - 1) * pageSize, 0);
+      int toIndex = Math.min(fromIndex + pageSize, list.size());
+
+      this.pageable = pageable;
+      this.pageNumber = pageNumber;
+      this.elements = pageable.getList().subList(fromIndex, toIndex);
+    }
+
+    /**
+     * Returns a {@link List} of elements contained in this {@link Page}.
+     *
+     * @return a {@link List} of elements contained in this {@link Page}.
+     * @see java.util.List
+     */
+    protected List<T> getElements() {
+      return this.elements;
+    }
+
+    /**
+     * Returns this {@link Page Page's} page number.
+     *
+     * @return an integer value with this {@link Page Page's} page number.
+     */
+    @Override
+    public int getNumber() {
+      return this.pageNumber;
+    }
+
+    /**
+     * Returns a reference to the backing {@link Pageable} object.
+     *
+     * @return a reference to the backing {@link Pageable} object.
+     * @see org.cp.elements.util.paging.support.SimplePageable
+     */
+    protected SimplePageable<T> getPageable() {
+      return this.pageable;
+    }
+
+    /**
+     * Determines whether there is a next {@link Page}.
+     *
+     * @return a boolean value indicating whether there is a next {@link Page}.
+     */
+    @Override
+    public boolean hasNext() {
+
+      int pageSize = getPageable().getPageSize();
+
+      return (getNumber() * pageSize + pageSize) < getPageable().getList().size();
+    }
+
+    /**
+     * Determines whether there is a previous {@link Page}.
+     *
+     * @return a boolean value indicating whether there is a previous {@link Page}.
+     */
+    @Override
+    public boolean hasPrevious() {
+      return getNumber() > 1;
+    }
+
+    /**
+     * Returns an {@link Iterator} over the elements contained this {@link Page}.
+     *
+     * @return an {@link Iterator} over the elements contained this {@link Page}.
+     * @see java.util.Iterator
+     */
+    @Override
+    public Iterator<T> iterator() {
+      return getElements().iterator();
+    }
+
+    /**
+     * Returns the next {@link Page} in the sequence of {@link Page Pages}.
+     *
+     * @return the next {@link Page} in the sequence of {@link Page Pages}.
+     * @throws PageNotFoundException if there is no next {@link Page}.
+     * @see org.cp.elements.util.paging.Page
+     */
+    @Override
+    public Page<T> next() {
+
+      Assert.isTrue(hasNext(), newPageNotFoundException("No next page after [%d]", getNumber()));
+
+      return getPageable().getPage(getNumber() + 1);
+    }
+
+    /**
+     * Returns the previous {@link Page} in the sequence of {@link Page Pages}.
+     *
+     * @return the previous {@link Page} in the sequence of {@link Page Pages}.
+     * @throws PageNotFoundException if there is no previous {@link Page}.
+     * @see org.cp.elements.util.paging.Page
+     */
+    @Override
+    public Page<T> previous() {
+
+      Assert.isTrue(hasPrevious(), newPageNotFoundException("No previous page before [%d]", getNumber()));
+
+      return getPageable().getPage(getNumber() - 1);
+    }
+
+    /**
+     * Indicates the number of elements or items contained in this {@link Page}.
+     *
+     * @return an integer value indicating the number of elements or items contained in this {@link Page}.
+     */
+    @Override
+    public int size() {
+      return getElements().size();
+    }
+
+    /**
+     * Sorts (orders) only the elements contained in this {@link Page}.
+     *
+     * @param orderBy {@link Comparator} used to sort (order) only the elements contained in this {@link Page}.
+     * @see java.util.Comparator
+     */
+    @Override
+    public void sort(Comparator<T> orderBy) {
+      getElements().sort(orderBy);
+    }
+  }
+}
---
+++ src/test/java/org/cp/elements/util/paging/support/SimplePageableTests.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.util.paging.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.cp.elements.util.paging.Page;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link SimplePageable}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.util.paging.support.SimplePageable
+ * @since 1.0.0
+ */
+public class SimplePageableTests {
+
+  @Test
+  public void constructSimplePageableWithListAndDefaultPageSize() {
+
+    List<Object> list = Arrays.asList("test", "testing", "tested");
+
+    SimplePageable<Object> pageable = new SimplePageable<>(list);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.getList()).isEqualTo(list);
+    assertThat(pageable.getPageSize()).isEqualTo(SimplePageable.DEFAULT_PAGE_SIZE);
+  }
+
+  @Test
+  public void constructSimplePageableWithListAndPageSize() {
+
+    List<Object> list = Arrays.asList("test", "testing", "tested");
+
+    SimplePageable<Object> pageable = new SimplePageable<>(list, 5);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.getList()).isEqualTo(list);
+    assertThat(pageable.getPageSize()).isEqualTo(5);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructSimplePageableWithInvalidPageSize() {
+
+    try {
+      new SimplePageable<>(Collections.emptyList(), -1);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Page size [-1] must be greater than 0");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructSimplePageableWithNullList() {
+
+    try {
+      new SimplePageable<>(null, 5);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("List is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void simplePageableOfNullArray() {
+
+    SimplePageable<Object> pageable = SimplePageable.of((Object[]) null);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.getList()).isEmpty();
+    assertThat(pageable.getPageSize()).isEqualTo(SimplePageable.DEFAULT_PAGE_SIZE);
+  }
+
+  @Test
+  public void simplePageableOfNullList() {
+
+    SimplePageable<Object> pageable = SimplePageable.of(null, 10);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.getList()).isEmpty();
+    assertThat(pageable.getPageSize()).isEqualTo(10);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void pageCountForListSizeOfZeroIsZero() {
+
+    SimplePageable pageable = SimplePageable.of();
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.count()).isEqualTo(0);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void pageCountForListSizeOfOneUsingDefaultPageSizeIsOne() {
+
+    SimplePageable pageable = SimplePageable.of(Collections.singletonList("test"));
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.count()).isEqualTo(1);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void pageCountForListSizeOfTwoUsingPageSizeOfTwoIsOne() {
+
+    SimplePageable pageable = SimplePageable.of(Arrays.asList("test", "testing"), 2);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.count()).isEqualTo(1);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void pageCountForListSizeOfTwoUsingPageSizeOfOneIsTwo() {
+
+    SimplePageable pageable = SimplePageable.of(Arrays.asList("test", "testing"), 1);
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable.count()).isEqualTo(2);
+  }
+
+  @Test
+  public void simplePageableWithNoPages() {
+
+    SimplePageable<Object> pageable = SimplePageable.of();
+
+    assertThat(pageable).isNotNull();
+    assertThat(pageable).isEmpty();
+    assertThat(pageable.count()).isEqualTo(0);
+
+    Iterator<Page<Object>> pages = pageable.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isFalse();
+  }
+
+  @Test
+  public void simplePageableWithSinglePage() {
+
+    List<String> strings = Arrays.asList("test", "testing", "tested");
+
+    SimplePageable<String> pageableStrings = SimplePageable.of(strings);
+
+    assertThat(pageableStrings).isNotNull();
+    assertThat(pageableStrings).isNotEmpty();
+    assertThat(pageableStrings.count()).isEqualTo(1);
+    assertThat(pageableStrings.getPageSize()).isEqualTo(SimplePageable.DEFAULT_PAGE_SIZE);
+
+    Iterator<Page<String>> pages = pageableStrings.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<String> page = pages.next();
+
+    assertThat(page).isNotNull();
+    assertThat(page).containsExactly("test", "testing", "tested");
+    assertThat(pages.hasNext()).isFalse();
+  }
+
+  @Test
+  public void simplePageableWithThreeFullPages() {
+
+    List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    SimplePageable<Integer> pageableIntegers = SimplePageable.of(integers, 3);
+
+    assertThat(pageableIntegers).isNotNull();
+    assertThat(pageableIntegers).isNotEmpty();
+    assertThat(pageableIntegers.count()).isEqualTo(3);
+
+    Iterator<Page<Integer>> pages = pageableIntegers.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageOne = pages.next();
+
+    assertThat(pageOne).isNotNull();
+    assertThat(pageOne).containsExactly(1, 2, 3);
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageTwo = pages.next();
+
+    assertThat(pageTwo).isNotNull();
+    assertThat(pageTwo).containsExactly(4, 5, 6);
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageThree = pages.next();
+
+    assertThat(pageThree).isNotNull();
+    assertThat(pageThree).containsExactly(7, 8, 9);
+    assertThat(pages.hasNext()).isFalse();
+  }
+
+  @Test
+  public void simplePageableWithTwoFullPagesAndOnePartialPage() {
+
+    List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    SimplePageable<Integer> pageableIntegers = SimplePageable.of(integers, 4);
+
+    assertThat(pageableIntegers).isNotNull();
+    assertThat(pageableIntegers).isNotEmpty();
+    assertThat(pageableIntegers.count()).isEqualTo(3);
+
+    Iterator<Page<Integer>> pages = pageableIntegers.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageOne = pages.next();
+
+    assertThat(pageOne).isNotNull();
+    assertThat(pageOne).containsExactly(1, 2, 3, 4);
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageTwo = pages.next();
+
+    assertThat(pageTwo).isNotNull();
+    assertThat(pageTwo).containsExactly(5, 6, 7, 8);
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageThree = pages.next();
+
+    assertThat(pageThree).isNotNull();
+    assertThat(pageThree).containsExactly(9);
+    assertThat(pages.hasNext()).isFalse();
+  }
+
+  @Test
+  public void sortPageableIsCorrect() {
+
+    List<Integer> integers = Arrays.asList(4, 9, 6, 2, 8, 5, 1, 3, 7);
+
+    SimplePageable<Integer> pageableIntegers = SimplePageable.of(integers, 5);
+
+    assertThat(pageableIntegers).isNotNull();
+    assertThat(pageableIntegers).isNotEmpty();
+    assertThat(pageableIntegers.count()).isEqualTo(2);
+
+    Iterator<Page<Integer>> pages = pageableIntegers.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageOne = pages.next();
+
+    assertThat(pageOne).isNotNull();
+    assertThat(pageOne).containsExactly(4, 9, 6, 2, 8);
+    assertThat(pages.hasNext()).isTrue();
+
+    Page<Integer> pageTwo = pages.next();
+
+    assertThat(pageTwo).isNotNull();
+    assertThat(pageTwo).containsExactly(5, 1, 3, 7);
+    assertThat(pages.hasNext()).isFalse();
+
+    pageableIntegers.sort(Comparator.naturalOrder());
+
+    assertThat(pageableIntegers.count()).isEqualTo(2);
+
+    pages = pageableIntegers.iterator();
+
+    assertThat(pages).isNotNull();
+    assertThat(pages.hasNext()).isTrue();
+
+    pageOne = pages.next();
+
+    assertThat(pageOne).isNotNull();
+    assertThat(pageOne).containsExactly(1, 2, 3, 4, 5);
+    assertThat(pages.hasNext()).isTrue();
+
+    pageTwo = pages.next();
+
+    assertThat(pageTwo).isNotNull();
+    assertThat(pageTwo).containsExactly(6, 7, 8, 9);
+    assertThat(pages.hasNext()).isFalse();
+  }
+}
