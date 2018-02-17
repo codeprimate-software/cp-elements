91989ed22d0fa2c1f13df93b2eb2e3bed63d382f
John Blum <jblum@pivotal.io>
2018-02-16 23:41:39 -0800

    Add the o.c.e.lang.support.IdentifiableSupport abstract class to support implementations of the o.c.e.lang.Identifiable interface.

---
+++ src/main/java/org/cp/elements/lang/support/IdentifiableSupport.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import org.cp.elements.lang.Identifiable;
+import org.cp.elements.lang.annotation.Id;
+
+/**
+ * {@link IdentifiableSupport} is an abstract {@link Class} supporting implementations of the {@link Identifiable}
+ * {@link Class interface}.
+ *
+ * @author John Blum
+ * @see java.lang.Comparable
+ * @see org.cp.elements.lang.Identifiable
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public abstract class IdentifiableSupport<ID extends Comparable<ID>> implements Identifiable<ID> {
+
+  @Id
+  private ID id;
+
+  /**
+   * Returns the identifier uniquely identifying this {@link Object}.
+   *
+   * @return the value uniquely identifying this {@link Object}.
+   */
+  @Override
+  public ID getId() {
+    return this.id;
+  }
+
+  /**
+   * Sets the identifier uniquely identifying this {@link Object}.
+   *
+   * @param id value of {@link Class type T} assigned as this {@link Object object's} unique identifier.
+   */
+  @Override
+  public void setId(ID id) {
+    this.id = id;
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/support/IdentifiableSupportTests.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link IdentifiableSupport}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.lang.support.IdentifiableSupport
+ * @since 1.0.0
+ */
+public class IdentifiableSupportTests {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void setAndGetId() {
+
+    IdentifiableSupport<Integer> mockIdentifiableSupport = new TestIdentifiableSupport();
+
+    assertThat(mockIdentifiableSupport.getId()).isNull();
+
+    mockIdentifiableSupport.setId(1);
+
+    assertThat(mockIdentifiableSupport.getId()).isEqualTo(1);
+
+    mockIdentifiableSupport.setId(null);
+
+    assertThat(mockIdentifiableSupport.getId()).isNull();
+  }
+
+  static final class TestIdentifiableSupport extends IdentifiableSupport<Integer> { }
+
+}
