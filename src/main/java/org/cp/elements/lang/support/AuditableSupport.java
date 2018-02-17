0b1d1d3589e1f650baee6f48a34a842ec0c8686a
John Blum <jblum@pivotal.io>
2018-02-16 23:38:58 -0800

    Add the o.c.e.lang.support.AuditableSupport abstract class to support implementations of the o.c.e.lang.Auditable interface.

---
+++ src/main/java/org/cp/elements/lang/support/AuditableSupport.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import static org.cp.elements.lang.ObjectUtils.defaultIfNull;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Auditable;
+
+/**
+ * {@link AuditableSupport} is an abstract {@link Class} supporting implementations of the {@link Auditable}
+ * {@link Class interface}.
+ *
+ * @author John Blum
+ * @see java.time.LocalDateTime
+ * @see org.cp.elements.lang.Auditable
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public abstract class AuditableSupport<USER, PROCESS, ID extends Comparable<ID>> extends IdentifiableSupport<ID>
+    implements Auditable<USER, PROCESS, ID>  {
+
+  private static final String USER_NAME_SYSTEM_PROPERTY = "user.name";
+
+  private LocalDateTime createdOn;
+  private LocalDateTime lastModifiedOn;
+  private LocalDateTime modifiedOn;
+
+  private PROCESS createdWith;
+  private PROCESS lastModifiedWith;
+  private PROCESS modifiedWith;
+
+  private USER createdBy;
+  private USER lastModifiedBy;
+  private USER modifiedBy;
+
+  /**
+   * Assert the given {@link Object value} is not {@literal null}.
+   *
+   * @param <T> {@link Class type} of the {@link Object value}.
+   * @param value {@link Object} to evaluate.
+   * @param message {@link Supplier} containing the message used in the {@link IllegalArgumentException}.
+   * @return the given {@link Object value} if not {@literal null}.
+   * @throws IllegalArgumentException with {@link Supplier message} if the {@link Object value} is {@literal null}.
+   * @see java.util.function.Supplier
+   */
+  private <T> T assertNotNull(T value, Supplier<String> message) {
+    Assert.notNull(value, message);
+    return value;
+  }
+
+  /**
+   * Gets the user responsible for creating this object.
+   *
+   * @return an object representing the user who created this object.
+   */
+  @Override
+  public USER getCreatedBy() {
+    return this.createdBy;
+  }
+
+  /**
+   * Sets the user responsible for creating this object.
+   *
+   * @param createdBy object representing the user who created this object.
+   */
+  @Override
+  public void setCreatedBy(USER createdBy) {
+    this.createdBy = assertNotNull(createdBy, () -> "Created by is required");
+  }
+
+  /**
+   * Gets the date and time when this object was created.
+   *
+   * @return a {@link LocalDateTime} capturing the date and time when this object was created.
+   */
+  @Override
+  public LocalDateTime getCreatedOn() {
+    return this.createdOn;
+  }
+
+  /**
+   * Sets the date and time when this object was created.
+   *
+   * @param createdOn {@link LocalDateTime} capturing the date and time when this object was created.
+   */
+  @Override
+  public void setCreatedOn(LocalDateTime createdOn) {
+    this.createdOn = assertNotNull(createdOn, () -> "Created on is required");
+  }
+
+  /**
+   * Gets the process (application) used by the user to create this object.
+   *
+   * @return an object representing the process (application) used by the user to create this object.
+   */
+  @Override
+  public PROCESS getCreatedWith() {
+    return this.createdWith;
+  }
+
+  /**
+   * Sets the process (application) used by the user to create this object.
+   *
+   * @param createdWith object representing the process (application) used by the user to create this object.
+   */
+  @Override
+  public void setCreatedWith(PROCESS createdWith) {
+    this.createdWith = assertNotNull(createdWith, () -> "Created with is required");
+  }
+
+  /**
+   * Gets the last user who modified this object.
+   *
+   * @return an object representing the last user responsible for modifying this object.
+   */
+  @Override
+  public USER getLastModifiedBy() {
+    return this.lastModifiedBy;
+  }
+
+  /**
+   * Gets the date and time when this object was last modified.
+   *
+   * @return a {@link LocalDateTime} capturing the date and time when this object was last modified.
+   */
+  @Override
+  public LocalDateTime getLastModifiedOn() {
+    return this.lastModifiedOn;
+  }
+
+  /**
+   * Gets the last process (application) used by the user to modify this object.
+   *
+   * @return an object representing the last process (application) used by the user to modify this object.
+   */
+  @Override
+  public PROCESS getLastModifiedWith() {
+    return this.lastModifiedWith;
+  }
+
+  /**
+   * Gets the user responsible for modifying this object.
+   *
+   * @return an object representing the user responsible for modifying this object.
+   */
+  @Override
+  public USER getModifiedBy() {
+    return Optional.ofNullable(this.modifiedBy).orElseGet(this::getCreatedBy);
+  }
+
+  /**
+   * Sets the user responsible for modifying this object.
+   *
+   * @param modifiedBy object representing the user responsible for modifying this object.
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public void setModifiedBy(USER modifiedBy) {
+    this.modifiedBy = assertNotNull(modifiedBy, () -> "Modified by is required");
+    this.lastModifiedBy = defaultIfNull(this.lastModifiedBy, this.modifiedBy);
+  }
+
+  /**
+   * Gets the date and time when this object was modified.
+   *
+   * @return a {@link LocalDateTime} capturing the date and time when this object was modified.
+   */
+  @Override
+  public LocalDateTime getModifiedOn() {
+    return Optional.ofNullable(this.modifiedOn).orElseGet(this::getCreatedOn);
+  }
+
+  /**
+   * Sets the date and time when this object was modified.
+   *
+   * @param modifiedOn {@link LocalDateTime} capturing the date and time when this object was modified.
+   */
+  @Override
+  public void setModifiedOn(LocalDateTime modifiedOn) {
+    this.modifiedOn = assertNotNull(modifiedOn, () -> "Modified on is required");
+    this.lastModifiedOn = defaultIfNull(this.lastModifiedOn, this.modifiedOn);
+  }
+
+  /**
+   * Gets the process (application) used by the user to modify this object.
+   *
+   * @return an object representing the process (application) used by the user to modify this object.
+   */
+  @Override
+  public PROCESS getModifiedWith() {
+    return Optional.ofNullable(this.modifiedWith).orElseGet(this::getCreatedWith);
+  }
+
+  /**
+   * Sets the process (application) used by the user to modify this object.
+   *
+   * @param modifiedWith object representing the process (application) used by the user to modify this object.
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public void setModifiedWith(PROCESS modifiedWith) {
+    this.modifiedWith = assertNotNull(modifiedWith, () -> "Modified with is required");
+    this.lastModifiedWith = defaultIfNull(this.lastModifiedWith, this.modifiedWith);
+  }
+
+  /**
+   * Returns the {@link String name} of the {@link USER} running the application.
+   *
+   * @return a {@link String} contain the name of the {@link USER} running the application
+   * @see java.lang.System#getProperty(String)
+   */
+  protected String getUsername() {
+    return System.getProperty(USER_NAME_SYSTEM_PROPERTY);
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/support/AuditableSupportTests.java
@@ -0,0 +1,347 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.cp.elements.lang.Constants;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AuditableSupport}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.lang.support.AuditableSupport
+ * @since 1.0.0
+ */
+public class AuditableSupportTests {
+
+  private AuditableSupport<String, String, Long> auditableSupport;
+
+  @Before
+  @SuppressWarnings("unchecked")
+  public void setup() {
+    this.auditableSupport = new TestAuditableSupport();
+  }
+
+  @Test
+  public void setAndGetCreatedBy() {
+
+    this.auditableSupport.setCreatedBy("testUser");
+
+    assertThat(this.auditableSupport.getCreatedBy()).isEqualTo("testUser");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setCreatedByToNullThrowsIllegalArgumentException() {
+
+    try {
+      this.auditableSupport.setCreatedBy("testUser");
+      this.auditableSupport.setCreatedBy(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Created by is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getCreatedBy()).isEqualTo("testUser");
+    }
+  }
+
+  @Test
+  public void setAndGetCreatedOn() {
+
+    LocalDateTime now = LocalDateTime.now();
+
+    this.auditableSupport.setCreatedOn(now);
+
+    assertThat(this.auditableSupport.getCreatedOn()).isEqualTo(now);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setCreatedOnToNullThrowsIllegalArgumentException() {
+
+    LocalDateTime createdOn = LocalDateTime.of(2018, Month.FEBRUARY, 15, 22, 30);
+
+    try {
+      this.auditableSupport.setCreatedOn(createdOn);
+      this.auditableSupport.setCreatedOn(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Created on is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getCreatedOn()).isEqualTo(createdOn);
+    }
+  }
+
+  @Test
+  public void setAndGetCreatedWith() {
+
+    this.auditableSupport.setCreatedWith("testProcess");
+
+    assertThat(this.auditableSupport.getCreatedWith()).isEqualTo("testProcess");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setCreatedWithToNullThrowsIllegalArgumentException() {
+
+    try {
+      this.auditableSupport.setCreatedWith("testProcess");
+      this.auditableSupport.setCreatedWith(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Created with is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getCreatedWith()).isEqualTo("testProcess");
+    }
+  }
+
+  @Test
+  public void setAndGetModifiedBy() {
+
+    this.auditableSupport.setModifiedBy("testUser");
+
+    assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("testUser");
+    assertThat(this.auditableSupport.getLastModifiedBy()).isEqualTo("testUser");
+  }
+
+  @Test
+  public void setModifiedByTwiceReturnsFirstUserForLastModifiedBy() {
+
+    this.auditableSupport.setModifiedBy("userOne");
+
+    assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("userOne");
+    assertThat(this.auditableSupport.getLastModifiedBy()).isEqualTo("userOne");
+
+    this.auditableSupport.setModifiedBy("userTwo");
+
+    assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("userTwo");
+    assertThat(this.auditableSupport.getLastModifiedBy()).isEqualTo("userOne");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setModifiedByToNullThrowsIllegalArgumentException() {
+
+    try {
+      this.auditableSupport.setModifiedBy("testUser");
+      this.auditableSupport.setModifiedBy(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Modified by is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("testUser");
+      assertThat(this.auditableSupport.getLastModifiedBy()).isEqualTo("testUser");
+    }
+  }
+
+  @Test
+  public void getModifiedByReturnsCreatedBy() {
+
+    this.auditableSupport.setCreatedBy("testUser");
+
+    assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("testUser");
+    assertThat(this.auditableSupport.getLastModifiedBy()).isNull();
+  }
+
+  @Test
+  public void getModifiedByReturnsModifiedBy() {
+
+    this.auditableSupport.setCreatedBy("userOne");
+    this.auditableSupport.setModifiedBy("userTwo");
+
+    assertThat(this.auditableSupport.getModifiedBy()).isEqualTo("userTwo");
+    assertThat(this.auditableSupport.getLastModifiedBy()).isEqualTo("userTwo");
+  }
+
+  @Test
+  public void setAndGetModifiedOn() {
+
+    LocalDateTime now = LocalDateTime.now();
+
+    this.auditableSupport.setModifiedOn(now);
+
+    assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(now);
+    assertThat(this.auditableSupport.getLastModifiedOn()).isEqualTo(now);
+  }
+
+  @Test
+  public void setModifiedOnTwiceReturnsFirstDateTimeForLastModifiedOn() {
+
+    LocalDateTime modifiedOnOne = LocalDateTime.of(2017, Month.FEBRUARY, 15, 22, 50);
+
+    this.auditableSupport.setModifiedOn(modifiedOnOne);
+
+    assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(modifiedOnOne);
+    assertThat(this.auditableSupport.getLastModifiedOn()).isEqualTo(modifiedOnOne);
+
+    LocalDateTime modifiedOnTwo = LocalDateTime.of(2018, Month.FEBRUARY, 15, 22, 50);
+
+    this.auditableSupport.setModifiedOn(modifiedOnTwo);
+
+    assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(modifiedOnTwo);
+    assertThat(this.auditableSupport.getLastModifiedOn()).isEqualTo(modifiedOnOne);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setModifiedOnToNullThrowsIllegalArgumentException() {
+
+    LocalDateTime now = LocalDateTime.now();
+
+    try {
+      this.auditableSupport.setModifiedOn(now);
+      this.auditableSupport.setModifiedOn(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Modified on is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(now);
+      assertThat(this.auditableSupport.getLastModifiedOn()).isEqualTo(now);
+    }
+  }
+
+  @Test
+  public void getModifiedOnReturnsCreatedOn() {
+
+    LocalDateTime createdOn = LocalDateTime.of(2018, Month.FEBRUARY, 15, 22, 45);
+
+    this.auditableSupport.setCreatedOn(createdOn);
+
+    assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(createdOn);
+    assertThat(this.auditableSupport.getLastModifiedOn()).isNull();
+  }
+
+  @Test
+  public void getModifiedOnReturnsModifiedOn() {
+
+    LocalDateTime createdOn = LocalDateTime.of(2017, Month.FEBRUARY, 15, 22, 45);
+    LocalDateTime modifiedOn = LocalDateTime.of(2018, Month.FEBRUARY, 15, 22, 45);
+
+    this.auditableSupport.setCreatedOn(createdOn);
+    this.auditableSupport.setModifiedOn(modifiedOn);
+
+    assertThat(this.auditableSupport.getModifiedOn()).isEqualTo(modifiedOn);
+    assertThat(this.auditableSupport.getLastModifiedOn()).isEqualTo(modifiedOn);
+  }
+
+  @Test
+  public void setAndGetModifiedWith() {
+
+    this.auditableSupport.setModifiedWith("testProcess");
+
+    assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("testProcess");
+    assertThat(this.auditableSupport.getLastModifiedWith()).isEqualTo("testProcess");
+  }
+
+  @Test
+  public void setModifiedWithTwiceReturnsFirstProcessForLastModifiedWith() {
+
+    this.auditableSupport.setModifiedWith("processOne");
+
+    assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("processOne");
+    assertThat(this.auditableSupport.getLastModifiedWith()).isEqualTo("processOne");
+
+    this.auditableSupport.setModifiedWith("processTwo");
+
+    assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("processTwo");
+    assertThat(this.auditableSupport.getLastModifiedWith()).isEqualTo("processOne");
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void setModifiedWithToNullThrowsIllegalArgumentException() {
+
+    try {
+      this.auditableSupport.setModifiedWith("testProcess");
+      this.auditableSupport.setModifiedWith(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Modified with is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("testProcess");
+      assertThat(this.auditableSupport.getLastModifiedWith()).isEqualTo("testProcess");
+    }
+  }
+
+  @Test
+  public void getModifiedWithReturnsCreatedWith() {
+
+    this.auditableSupport.setCreatedWith("testProcess");
+
+    assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("testProcess");
+    assertThat(this.auditableSupport.getLastModifiedWith()).isNull();
+  }
+
+  @Test
+  public void getModifiedWithReturnsModifiedWith() {
+
+    this.auditableSupport.setCreatedWith("processOne");
+    this.auditableSupport.setModifiedWith("processTwo");
+
+    assertThat(this.auditableSupport.getModifiedWith()).isEqualTo("processTwo");
+    assertThat(this.auditableSupport.getLastModifiedWith()).isEqualTo("processTwo");
+  }
+
+  @Test
+  public void getUsernameReturnsValue() {
+    assertThat(this.auditableSupport.getUsername()).isNotEmpty();
+  }
+
+  static final class TestAuditableSupport extends AuditableSupport<String, String, Long> {
+
+    @Override
+    public boolean isModified() {
+      throw new UnsupportedOperationException(Constants.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public boolean isModified(String propertyName) {
+      throw new UnsupportedOperationException(Constants.NOT_IMPLEMENTED);
+    }
+  }
+}
