57fdfa7eb3415e8dc2979a6f8670ba1c7b45bea1
John Blum <jblum@pivotal.io>
2018-02-17 00:53:59 -0800

    Add the o.c.e.lang.support.FunctionToTransformerAdapter class to adapt an instance of the java.util.function.Function interface into an instance of the o.c.e.lang.Transformer interface.

---
+++ src/main/java/org/cp/elements/lang/support/FunctionToTransformerAdapter.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import java.util.function.Function;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Transformer;
+
+/**
+ * The FunctionToTransformerAdapter class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class FunctionToTransformerAdapter<T> implements Transformer<T> {
+
+  /**
+   * Factory method use to construct a new instance of {@link FunctionToTransformerAdapter} initialized with
+   * the given {@link Function} to adapt as an instance of the {@link Transformer} interface.
+   *
+   * @param <T> {@link Class type} of {@link Object objects} processed by the {@link Function}.
+   * @param function {@link Function} to adapt as a {@link Transformer}.
+   * @return a new instance of {@link FunctionToTransformerAdapter} initialized with the given {@link Function}
+   * to adapt as a {@link Transformer}.
+   * @throws IllegalArgumentException if {@link Function} is {@literal null}.
+   * @see #FunctionToTransformerAdapter(Function)
+   * @see java.util.function.Function
+   */
+  public static <T> FunctionToTransformerAdapter<T> of(Function<T, T> function) {
+    return new FunctionToTransformerAdapter<>(function);
+  }
+
+  private final Function<T, T> function;
+
+  /**
+   * Constructs an instance of the {@link FunctionToTransformerAdapter} initialized with the given {@link Function}
+   * to adapt as an instance of the {@link Transformer} interface.
+   *
+   * @param function {@link Function} to adapt as a {@link Transformer}.
+   * @throws IllegalArgumentException if {@link Function} is {@literal null}.
+   * @see java.util.function.Function
+   */
+  public FunctionToTransformerAdapter(Function<T, T> function) {
+
+    Assert.notNull(function, "Function is required");
+
+    this.function = function;
+  }
+
+  /**
+   * Returns a reference to the adapt {@link Function} backing this {@link Transformer}.
+   *
+   * @return a reference to the adapt {@link Function} backing this {@link Transformer}.
+   * @see java.util.function.Function
+   */
+  protected Function<T, T> getFunction() {
+    return this.function;
+  }
+
+  /**
+   * Transforms the given {@link Object} by applying the {@link Function} of this {@link Transformer}.
+   *
+   * @param value {@link T value} to transform.
+   * @return the {@link T transformed value}.
+   * @see java.util.function.Function#apply(Object)
+   * @see #getFunction()
+   */
+  @Override
+  public T transform(T value) {
+    return getFunction().apply(value);
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/support/FunctionToTransformerAdapterTests.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.lang.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.function.Function;
+
+import org.cp.elements.lang.Transformer;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link FunctionToTransformerAdapter}.
+ *
+ * @author John Blum
+ * @see java.util.function.Function
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.Transformer
+ * @see org.cp.elements.lang.support.FunctionToTransformerAdapter
+ * @since 1.0.0
+ */
+public class FunctionToTransformerAdapterTests {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void constructTransformerWithFunction() {
+
+    Function<Object, Object> mockFunction = mock(Function.class);
+
+    FunctionToTransformerAdapter<Object> transformer = new FunctionToTransformerAdapter<>(mockFunction);
+
+    assertThat(transformer).isNotNull();
+    assertThat(transformer.getFunction()).isEqualTo(mockFunction);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void constructTransformerWithNullThrowsIllegalArgumentException() {
+
+    try {
+      new FunctionToTransformerAdapter<>(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Function is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void ofFunctionReturnsTransformer() {
+
+    Function<Object, Object> mockFunction = mock(Function.class);
+
+    FunctionToTransformerAdapter<Object> transformer = FunctionToTransformerAdapter.of(mockFunction);
+
+    assertThat(transformer).isNotNull();
+    assertThat(transformer.getFunction()).isEqualTo(mockFunction);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ofNullFunctionThrowsIllegalArgumentException() {
+
+    try {
+      FunctionToTransformerAdapter.of(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Function is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  public void transformerTransfomCallsFunctionApply() {
+
+    Function<String, String> toUpperCase = String::toUpperCase;
+
+    Transformer<String> transformer = FunctionToTransformerAdapter.of(toUpperCase);
+
+    assertThat(transformer).isNotNull();
+    assertThat(transformer.transform("test")).isEqualTo("TEST");
+  }
+}
