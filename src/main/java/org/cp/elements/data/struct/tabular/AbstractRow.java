3e5b35d5edaabed75314891b27bc6300f82f902e
John Blum <jblum@pivotal.io>
2018-05-06 23:29:56 -0700

    Review, refactor and polish the o.c.e.data.struct.tabular.AbstractRow abstract class.
    Add a factory method to construct a AbstractRow from an array of Object values.
    
    Add getter and setter for the View to which this Row is associated.
    
    Add a map(:Class<T>):T method to map the Row of values to an object of class type T.
    
    Override to the toString() method to return the Row index.

--- src/main/java/org/cp/elements/data/struct/tabular/AbstractRow.java
+++ src/main/java/org/cp/elements/data/struct/tabular/AbstractRow.java
@@ -16,7 +16,19 @@
 
 package org.cp.elements.data.struct.tabular;
 
+import static org.cp.elements.lang.ElementsExceptionsFactory.newMappingException;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.util.ArrayUtils;
 
 /**
  * The AbstractRow class is an abstract base class implementing the Row interface encapsulating functionality common
@@ -32,176 +44,135 @@ import org.cp.elements.lang.Assert;
 @SuppressWarnings("unused")
 public abstract class AbstractRow implements Row {
 
+  /**
+   * Constructs a new {@link AbstractRow} initialized with given array of {@link Object values}
+   * used as {@link Object values} for each {@link Column} of the new {@link AbstractRow}.
+   *
+   * @param values array of {@link Object values} used as {@link Object values} for each {@link Column}
+   * of the new {@link AbstractRow}.
+   * @return a new {@link AbstractRow} initialized with the given array of {@link Object values}.
+   */
+  @NullSafe
+  public static AbstractRow of(Object[] values) {
+
+    Object[] rowValues = ArrayUtils.nullSafeArray(values);
+
+    return new AbstractRow() {
+
+      @Override
+      @SuppressWarnings("unchecked")
+      public <T> T getValue(int columnIndex) {
+        return (T) rowValues[columnIndex];
+      }
+
+      @Override
+      @SuppressWarnings("unchecked")
+      public <T> T setValue(int columnIndex, T value) {
+
+        T currentValue = (T) rowValues[columnIndex];
+
+        rowValues[columnIndex] = value;
+
+        return currentValue;
+      }
+
+      @Override
+      public Object[] values() {
+        return rowValues;
+      }
+    };
+  }
+
   private View view;
 
   /**
-   * Gets the View to which this Row belongs.
+   * Sets the {@link View} to which this {@link Row} is associated.
    *
-   * @return the View to which this Row belongs or null if this Row is not part of any View.
-   * @throws IllegalStateException if a reference to the containing View was not properly configured.
-   * @see #setView(View)
-   * @see View
-   */
-  @Override
-  public View getView() {
-    Assert.state(view != null, "A reference to the containing View for this Row was not properly configured!");
-    return view;
-  }
-
-  /**
-   * Sets the View to which this Row belongs.
-   *
-   * @param view the View to which this Row belongs or null if this Row is not part of any View.
+   * @param view {@link View} containing this {@link Row}.
+   * @see org.cp.elements.data.struct.tabular.View
    * @see #getView()
-   * @see View
    */
-  public void setView(final View view) {
+  public void setView(View view) {
     this.view = view;
   }
 
   /**
-   * Determines whether this Row has a value at the given column index.
+   * Returns an {@link Optional} {@link View} to which this {@link Row} is associated.
    *
-   * @param columnIndex an integer value indicating the column index.
-   * @return a boolean value indicating whether this Row has a value at the given column index.
-   * @see #getValue(int)
+   * @return an {@link Optional} {@link View} containing this {@link Row}.
+   * @see org.cp.elements.data.struct.tabular.View
+   * @see java.util.Optional
    */
   @Override
-  public boolean hasValue(final int columnIndex) {
-    return (getValue(columnIndex) != null);
+  public Optional<View> getView() {
+    return Optional.ofNullable(this.view);
   }
 
   /**
-   * Determines whether this Row has a value in the given named column.
+   * Maps the {@link Object values} of this {@link Row} to the given {@link Object}.
    *
-   * @param columnName a String value indicating the name of the column.
-   * @return a boolean value indicating whether this Row has a value in the given named column.
-   * @see #getValue(String)
+   * The {@link Class type} must have a default, public no argument {@link Constructor}.
+   *
+   * @param <T> {@link Class type} of the {@link Object} to map with the {@link Object value} from this {@link Row}.
+   * @param type {@link Class type} of the {@link Object} to map with the {@link Object values} from this {@link Row}.
+   * @return the {@link Object} populated with the {@link Object values} of this {@link Row}.
+   * @see java.lang.Class
    */
   @Override
-  public boolean hasValue(final String columnName) {
-    return (getValue(columnName) != null);
+  public <T> T map(Class<T> type) {
+
+    Assert.notNull(type, "Class type of the object to map is required");
+
+    return getView().map(view -> {
+
+      try {
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(type);
+
+        T instance = type.newInstance();
+
+        Arrays.stream(beanInfo.getPropertyDescriptors()).forEach(propertyDescriptor ->
+          Optional.ofNullable(propertyDescriptor.getWriteMethod()).ifPresent(writeMethod -> {
+
+            String propertyName = propertyDescriptor.getName();
+
+            view.getColumn(propertyName).ifPresent(column -> {
+
+              Object value = getValue(column);
+
+              ObjectUtils.invoke(instance, writeMethod, ArrayUtils.asArray(value), Void.class);
+
+            });
+          }));
+
+        return instance;
+      }
+      catch (Exception cause) {
+        throw newMappingException(cause, "Failed to map object of type [%s] with values from this Row [%s]",
+          type.getName(), this);
+      }
+    })
+    .orElseThrow(() -> newIllegalStateException("This Row [%s] is not associated with a View", this));
   }
 
   /**
-   * Determines whether this Row has a value in the given Column.
+   * Returns a {@link String} representation of this {@link Row}.
    *
-   * @param column the Column in this Row of the Table.
-   * @return a boolean value indicating whether this Row has a value in the given Column.
-   * @see Column
-   * @see #getValue(Column)
+   * @return a {@link String} describing this {@link Row}.
+   * @see java.lang.String
    */
   @Override
-  public boolean hasValue(final Column column) {
-    return (getValue(column) != null);
+  public String toString() {
+    return String.format("Row %d", index());
   }
 
   /**
-   * Gets the value at the given named column in this Row.
+   * Returns the {@link Object values} of this {@link Row} in an array.
    *
-   * @param columnName a String value indicating the name of the column.
-   * @return an Object value at the given named column in this Row.
-   * @see View#indexOf(String)
-   * @see #getValue(int)
-   * @see #getView()
-   */
-  @Override
-  public Object getValue(final String columnName) {
-    return getValue(getView().indexOf(columnName));
-  }
-
-  /**
-   * Gets the value at the given Column in this Row.
-   *
-   * @param column a Column in this Row of the Table.
-   * @return an Object value at the given Column in this Row.
-   * @see Column
-   * @see View#indexOf(Column)
-   * @see #getValue(int)
-   * @see #getView()
-   */
-  @Override
-  public Object getValue(final Column column) {
-    return getValue(getView().indexOf(column));
-  }
-
-  /**
-   * Sets the value in this Row in the given named column.
-   *
-   * @param columnName a String value indicating the name of the column.
-   * @param value the Object value to set at the given named column in this Row.
-   * @return the original Object value at the given named column in this Row.
-   * @see View#indexOf(String)
-   * @see #setValue(int, Object)
-   * @see #getView()
-   */
-  @Override
-  public Object setValue(final String columnName, final Object value) {
-    return setValue(getView().indexOf(columnName), value);
-  }
-
-  /**
-   * Sets the value in this Row at the given Column.
-   *
-   * @param column the Column in this Row of the Table.
-   * @param value the Object value to set at the given Column in this Row.
-   * @return the original Object value at the given Column in this Row.
-   * @see Column
-   * @see View#indexOf(Column)
-   * @see #setValue(int, Object)
-   * @see #getView()
-   */
-  @Override
-  public Object setValue(final Column column, final Object value) {
-    return setValue(getView().indexOf(column), value);
-  }
-
-  /**
-   * Gets the index of this Row in the View.
-   *
-   * @return an integer value indicating the index of this Row in the View.  Returns a -1 if this Row has not been
-   * added to a View.
-   * @see View#indexOf(Row)
-   * @see #getView()
-   */
-  @Override
-  public int index() {
-    return getView().indexOf(this);
-  }
-
-  /**
-   * Determines the number of values in this Row (the same as the number of columns in this Row).
-   *
-   * @return an integer value indicating the number of values (columns) in this Row.
-   * @see java.lang.Iterable#iterator()
-   */
-  @Override
-  public int size() {
-    int count = 0;
-
-    for (Object value : this) {
-      count++;
-    }
-
-    return count;
-  }
-
-  /**
-   * Returns this Row as an array of Object values.
-   *
-   * @return an array of Object values contained this Row.
-   * @see java.lang.Iterable#iterator()
+   * @return the {@link Object values} of this {@link Row} in an array.
    */
   @Override
   public Object[] values() {
-    Object[] values = new Object[size()];
-    int index = 0;
-
-    for (Object value : this) {
-      values[index++] = value;
-    }
-
-    return values;
+    return ObjectUtils.EMPTY_OBJECT_ARRAY;
   }
-
 }
---
+++ src/test/java/org/cp/elements/data/struct/tabular/AbstractRowTests.java
@@ -0,0 +1,338 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.struct.tabular;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.cp.elements.data.mapping.MappingException;
+import org.cp.elements.enums.Gender;
+import org.cp.elements.lang.Constants;
+import org.cp.elements.lang.reflect.MethodInvocationException;
+import org.junit.Test;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Unit tests for {@link AbstractRow}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.data.struct.tabular.AbstractRow
+ * @since 1.0.0
+ */
+public class AbstractRowTests {
+
+  @Test
+  public void ofValuesReturnsRowOfValues() {
+
+    Object[] values = { true, 'X', 1, Math.PI, "test" };
+
+    AbstractRow row = AbstractRow.of(values);
+
+    assertThat(row).isNotNull();
+    assertThat(row.values()).isEqualTo(values);
+
+    for (int index = 0; index < values.length; index++) {
+      assertThat(row.<Object>getValue(index)).isEqualTo(values[index]);
+    }
+
+    assertThat(row.<Object>getValue(1)).isEqualTo('X');
+    assertThat(row.setValue(1, 'Y')).isEqualTo('X');
+    assertThat(row.<Object>getValue(1)).isEqualTo('Y');
+  }
+
+  @Test
+  public void ofNullValuesReturnsEmptyRow() {
+
+    AbstractRow row = AbstractRow.of(null);
+
+    assertThat(row).isNotNull();
+    assertThat(row.values()).isNotNull();
+    assertThat(row.values()).isEmpty();
+  }
+
+  @Test
+  public void setAndGetView() {
+
+    AbstractRow row = new TestRow();
+
+    View mockView = mock(View.class);
+
+    assertThat(row.getView().isPresent()).isFalse();
+
+    row.setView(mockView);
+
+    assertThat(row.getView().orElse(null)).isEqualTo(mockView);
+
+    row.setView(null);
+
+    assertThat(row.getView().isPresent()).isFalse();
+  }
+
+  @Test(expected = MappingException.class)
+  @SuppressWarnings("unchecked")
+  public void mapToExplodingTypeThrowsMappingException() {
+
+    AbstractRow row = spy(new TestRow());
+
+    Column<Object> mockValueColumn = mock(Column.class);
+
+    View mockView = mock(View.class);
+
+    row.setView(mockView);
+
+    when(mockView.getColumn(eq("value"))).thenReturn(Optional.of(mockValueColumn));
+    doReturn("test").when(row).getValue(eq(mockValueColumn));
+
+    try {
+      row.map(ExplodingType.class);
+    }
+    catch(MappingException expected) {
+
+      assertThat(expected).hasMessage("Failed to map object of type [%s] with values from this Row [%s]",
+        ExplodingType.class.getName(), row);
+
+      assertThat(expected).hasCauseInstanceOf(MethodInvocationException.class);
+      assertThat(expected.getCause().getCause()).isInstanceOf(InvocationTargetException.class);
+      assertThat(expected.getCause().getCause().getCause()).isInstanceOf(UnsupportedOperationException.class);
+      assertThat(expected.getCause().getCause().getCause()).hasMessage("test");
+
+      throw expected;
+    }
+    finally {
+      verify(mockView, times(1)).getColumn(eq("value"));
+      verify(row, times(1)).getValue(eq(mockValueColumn));
+    }
+  }
+
+  @Test(expected = MappingException.class)
+  public void mapToNonInstantiableTypeThrowsMappingException() {
+
+    AbstractRow row = spy(new TestRow());
+
+    View mockView = mock(View.class);
+
+    row.setView(mockView);
+
+    try {
+      row.map(NonInstantiableType.class);
+    }
+    catch (MappingException expected) {
+
+      assertThat(expected).hasMessage("Failed to map object of type [%s] with values from this Row [%s]",
+        NonInstantiableType.class.getName(), row);
+
+      assertThat(expected).hasCauseInstanceOf(IllegalAccessException.class);
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void mapWithNullTypeThrowsIllegalArgumentException() {
+
+    AbstractRow row = new TestRow();
+
+    try {
+      row.map(null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("Class type of the object to map is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void mapWithNullViewThrowsIllegalStateException() {
+
+    AbstractRow row = new TestRow();
+
+    assertThat(row.getView().isPresent()).isFalse();
+
+    try {
+      row.map(Person.class);
+    }
+    catch (IllegalStateException expected) {
+
+      assertThat(expected).hasMessage("This Row [%s] is not associated with a View", row);
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void rowMapsFemalePersonCorrectly() {
+
+    AbstractRow row = spy(new TestRow());
+
+    Column<Gender> mockGenderColumn = mock(Column.class);
+    Column<String> mockNameColumn = mock(Column.class);
+
+    View mockView = mock(View.class);
+
+    when(mockView.<Gender>getColumn(eq("gender"))).thenReturn(Optional.of(mockGenderColumn));
+    when(mockView.<String>getColumn(eq("name"))).thenReturn(Optional.of(mockNameColumn));
+
+    row.setView(mockView);
+
+    doReturn(Gender.MALE).when(row).getValue(eq(mockGenderColumn));
+    doReturn("Jane Doe").when(row).getValue(eq(mockNameColumn));
+
+    FemalePerson janeDoe = row.map(FemalePerson.class);
+
+    assertThat(janeDoe).isNotNull();
+    assertThat(janeDoe.getName()).isEqualTo("Jane Doe");
+    assertThat(janeDoe.getGender()).isEqualTo(Gender.FEMALE);
+
+    verify(mockView, times(1)).getColumn(eq("birthDate"));
+    verify(mockView, never()).getColumn(eq("gender"));
+    verify(mockView, times(1)).getColumn(eq("name"));
+    verify(row, never()).getValue(eq("birthDate"));
+    verify(row, never()).getValue(eq(mockGenderColumn));
+    verify(row, times(1)).getValue(eq(mockNameColumn));
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void rowMapsPersonCorrectly() {
+
+    AbstractRow row = spy(new TestRow());
+
+    Column<Gender> mockGenderColumn = mock(Column.class);
+    Column<String> mockNameColumn = mock(Column.class);
+
+    View mockView = mock(View.class);
+
+    when(mockView.<Gender>getColumn(eq("gender"))).thenReturn(Optional.of(mockGenderColumn));
+    when(mockView.<String>getColumn(eq("name"))).thenReturn(Optional.of(mockNameColumn));
+
+    row.setView(mockView);
+
+    doReturn(Gender.MALE).when(row).getValue(eq(mockGenderColumn));
+    doReturn("Jon Doe").when(row).getValue(eq(mockNameColumn));
+
+    Person jonDoe = row.map(Person.class);
+
+    assertThat(jonDoe).isNotNull();
+    assertThat(jonDoe.getName()).isEqualTo("Jon Doe");
+    assertThat(jonDoe.getGender()).isEqualTo(Gender.MALE);
+
+    verify(mockView, times(1)).getColumn(eq("gender"));
+    verify(mockView, times(1)).getColumn(eq("name"));
+    verify(row, times(1)).getValue(eq(mockGenderColumn));
+    verify(row, times(1)).getValue(eq(mockNameColumn));
+  }
+
+  @Test
+  public void toStringIsCorrect() {
+
+    AbstractRow row = spy(new TestRow());
+
+    doReturn(4).when(row).index();
+
+    assertThat(row.toString()).isEqualTo("Row 4");
+
+    verify(row, times(1)).index();
+  }
+
+  @Test
+  public void valuesReturnsEmptyObjectArray() {
+
+    AbstractRow row = new TestRow();
+
+    Object[] values = row.values();
+
+    assertThat(values).isNotNull();
+    assertThat(values).isEmpty();
+  }
+
+  private static class TestRow extends AbstractRow {
+
+    @Override
+    public <T> T getValue(int columnIndex) {
+      throw new UnsupportedOperationException(Constants.NOT_IMPLEMENTED);
+    }
+
+    @Override
+    public <T> T setValue(int columnIndex, T value) {
+      throw new UnsupportedOperationException(Constants.NOT_IMPLEMENTED);
+    }
+  }
+
+  @NoArgsConstructor
+  @SuppressWarnings("unused")
+  private static final class ExplodingType {
+
+    @Getter
+    private Object value;
+
+    public void setValue(Object value) {
+      throw new UnsupportedOperationException("test");
+    }
+  }
+
+  private static final class NonInstantiableType {
+
+    private NonInstantiableType() { }
+
+  }
+
+  @NoArgsConstructor
+  private static class FemalePerson {
+
+    @Getter
+    private Gender gender = Gender.FEMALE;
+
+    @Getter @Setter
+    private LocalDateTime birthDate;
+
+    @Getter @Setter
+    private String name;
+
+  }
+
+  @Data
+  @NoArgsConstructor
+  @SuppressWarnings("unused")
+  private static class Person {
+
+    private Gender gender;
+    private String name;
+
+  }
+}
