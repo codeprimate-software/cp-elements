4ff4e084eb5acaeb6c70699b0fe50a469944b652
John Blum <jblum@pivotal.io>
2018-05-10 01:24:55 -0700

    Review, refactor and polish the o.c.e.data.struct.tabular.AbstractTable class.
    Add unit tests.

--- src/main/java/org/cp/elements/data/struct/tabular/AbstractTable.java
+++ src/main/java/org/cp/elements/data/struct/tabular/AbstractTable.java
@@ -16,9 +16,7 @@
 
 package org.cp.elements.data.struct.tabular;
 
-import java.util.Iterator;
-
-import org.cp.elements.util.ArrayUtils;
+import org.cp.elements.lang.Constants;
 
 /**
  * The AbstractTable class is an abstract base class implementation of the Table interface encapsulating functionality
@@ -36,158 +34,54 @@ import org.cp.elements.util.ArrayUtils;
 public abstract class AbstractTable extends AbstractView implements Table {
 
   /**
-   * Adds the array of Object values as a new row in this Table.
+   * Adds the given {@link Column} to this {@link Table}.
    *
-   * @param row an array of Objects containing the values (contents) for the new row of this Table.
-   * @return a boolean value indicating whether the row added successfully modified the structure of this Table.
-   * @see AbstractRow
+   * @param column {@link Column} to add.
+   * @return a boolean value indicating whether the added {@link Column}
+   * successfully modified the structure of this {@link Table}.
+   * @see org.cp.elements.data.struct.tabular.Column
    */
   @Override
-  public boolean add(final Object... row) {
-    return add(new AbstractRow() {
-      @Override public Object getValue(final int columnIndex) {
-        return row[columnIndex];
-      }
-
-      @Override public Object setValue(final int columnIndex, final Object value) {
-        Object currentValue = row[columnIndex];
-        row[columnIndex] = value;
-        return currentValue;
-      }
-
-      @Override
-      public Iterator<Object> iterator() {
-        return ArrayUtils.iterator(row);
-      }
-    });
+  public boolean add(Column column) {
+    throw new UnsupportedOperationException(Constants.OPERATION_NOT_SUPPORTED);
   }
 
   /**
-   * Removes the column at the specified index from this Table.
+   * Adds the given {@link Row} to this {@link Table}.
    *
-   * @param index an integer value specifying the index of the column to remove from this Table.
-   * @return a boolean value indicating whether the specified column at index was successfully removed.
-   * @see #getColumn(int)
-   * @see #remove(Column)
+   * @param row {@link Row} to add.
+   * @return a boolean value indicating whether the added {@link Row}
+   * successfully modified the structure of this {@link Table}.
+   * @see org.cp.elements.data.struct.tabular.Row
    */
   @Override
-  public boolean removeColumn(final int index) {
-    return remove(getColumn(index));
+  public boolean add(Row row) {
+    throw new UnsupportedOperationException(Constants.OPERATION_NOT_SUPPORTED);
   }
 
   /**
-   * Removes the column with the specified name from this Table.
+   * Removes the {@link Column} at the given {@link Integer index} from this {@link Table}.
    *
-   * @param name a String value specifying the name of the column to remove from this Table.
-   * @return a boolean value indicating whether the specified column with name was successfully removed.
-   * @see #getColumn(String)
-   * @see #remove(Column)
+   * @param index {@link Integer} value indicating the index of the {@link Column} to remove.
+   * @return a boolean value indicating whether the {@link Column} at {@link Integer index}
+   * was successfully removed.
+   * @throws IndexOutOfBoundsException if the given {@link Integer index} is not a valid {@link Column} index
+   * in this {@link Table}.
    */
   @Override
-  public boolean removeColumn(final String name) {
-    return remove(getColumn(name));
+  public boolean removeColumn(int index) {
+    throw new UnsupportedOperationException(Constants.OPERATION_NOT_SUPPORTED);
   }
 
   /**
-   * Removes the specified Column from this Table.
+   * Removes the {@link Row} at the given {@link Integer index} from this {@link Table}.
    *
-   * @param column the Column to remove from this Table.
-   * @return a boolean value indicating whether the specified Column was successfully removed from this Table modifying
-   * it's structure.
-   * @see Column
-   * @see java.util.Iterator#remove()
-   * @see #columns()
+   * @param index {@link Integer} value indicating the index of the {@link Row} to remove.
+   * @return a boolean value indicating whether the given {@link Row} at {@link Integer index}
+   * was successfully removed.
    */
   @Override
-  public boolean remove(final Column column) {
-    for (Iterator<Column> columns = columns().iterator(); columns.hasNext(); ) {
-      Column viewColumn = columns.next();
-      if (viewColumn.equals(column)) {
-        columns.remove();
-        return true;
-      }
-    }
-
-    return false;
+  public boolean removeRow(int index) {
+    throw new UnsupportedOperationException(Constants.OPERATION_NOT_SUPPORTED);
   }
-
-  /**
-   * Removes the row at the specified index from this Table.
-   *
-   * @param index an integer value specifying the index of the row to remove from this Table.
-   * @return a boolean value indicating whether the specified row at index was successfully removed.
-   * @see #getRow(int)
-   * @see #remove(Row)
-   */
-  @Override
-  public boolean removeRow(final int index) {
-    return remove(getRow(index));
-  }
-
-  /**
-   * Removes the specified Row from this Table.
-   *
-   * @param row the Row to remove from this Table.
-   * @return a boolean value indicating whether the specified Row was successfully removed from this Table modifying
-   * it's structure.
-   * @see Row
-   * @see java.util.Iterator#remove()
-   * @see #rows()
-   */
-  @Override
-  public boolean remove(final Row row) {
-    for (Iterator<Row> rows = rows().iterator(); rows.hasNext(); ) {
-      Row viewRow = rows.next();
-      if (viewRow.equals(row)) {
-        rows.remove();
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   * Sets the value at the given row and column index in this Table.
-   *
-   * @param rowIndex an integer value indicating the row index.
-   * @param columnIndex an integer value indicating the column index.
-   * @param value the Object value to add at the given row and column in this Table.
-   * @see #getRow(int)
-   * @see Row#setValue(int, Object)
-   */
-  @Override
-  public void setValue(final int rowIndex, final int columnIndex, final Object value) {
-    getRow(rowIndex).setValue(columnIndex, value);
-  }
-
-  /**
-   * Sets the value at the given row index and named column in this Table.
-   *
-   * @param rowIndex an integer value indicating the row index.
-   * @param columnName a String value indicating the column name.
-   * @param value the Object value to add at the given row and column in this Table.
-   * @see #getRow(int)
-   * @see Row#setValue(String, Object)
-   */
-  @Override
-  public void setValue(final int rowIndex, final String columnName, final Object value) {
-    getRow(rowIndex).setValue(columnName, value);
-  }
-
-  /**
-   * Sets the value at the given row index and Column in this Table.
-   *
-   * @param rowIndex an integer value indicating the row index.
-   * @param column a Column in this Table.
-   * @param value the Object value to add at the given row and column in this Table.
-   * @see Column
-   * @see #getRow(int)
-   * @see Row#setValue(Column, Object)
-   */
-  @Override
-  public void setValue(final int rowIndex, final Column column, final Object value) {
-    getRow(rowIndex).setValue(column, value);
-  }
-
 }
---
+++ src/test/java/org/cp/elements/data/struct/tabular/AbstractTableTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cp.elements.data.struct.tabular;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link AbstractTable}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.data.struct.tabular.AbstractTable
+ * @since 1.0.0
+ */
+public class AbstractTableTests {
+
+  private AbstractTable table = new TestTable();
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void addColumnThrowsUnsupportedOperationException() {
+    this.table.add(mock(Column.class));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void addRowThrowsUnsupportedOperationException() {
+    this.table.add(mock(Row.class));
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void removeColumnAtIndexThrowsUnsupportedOperationException() {
+    this.table.removeColumn(0);
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void removeRowAtIndexThrowsUnsupportedOperationException() {
+    this.table.removeRow(0);
+  }
+
+  private static final class TestTable extends AbstractTable { }
+
+}
