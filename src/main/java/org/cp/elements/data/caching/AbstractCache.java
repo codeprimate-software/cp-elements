09efc8458089533055335740e1677120b190a4c7
John Blum <jblum@vmware.com>
2021-07-19 22:27:58 -0700

    Refactor the o.c.e.data.caching.AbstractCache class.
    Format source code.

--- src/main/java/org/cp/elements/data/caching/AbstractCache.java
+++ src/main/java/org/cp/elements/data/caching/AbstractCache.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.data.caching;
 
 import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
