2ce29b41020f10ab76592abfabb3327d14f11c2f
John Blum <jblum@vmware.com>
2021-12-22 23:41:48 -0800

    Define the @Dsl annotation and DslExtension interface to mark a class or method that implements a Domain Specific Language (DSL).

---
+++ src/main/java/org/cp/elements/lang/annotation/Dsl.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The {@link Dsl} {@link Annotation} is a Java language metadata {@link Annotation} and maker for Java classes
+ * or methods implementing a {@literal Domain Specific Language}.
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Annotation
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.Target
+ * @see org.cp.elements.lang.DslExtension
+ * @since 1.0.0
+ */
+@Target({ ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@SuppressWarnings("unused")
+public @interface Dsl {
+
+}
