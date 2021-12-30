92e7d4b688396d2b254664cb1c806f3d4e37af09
John Blum <jblum@vmware.com>
2021-12-30 15:35:44 -0800

    Review and refactor the o.c.elements.lang.support.AuditableSupport class.
    Change LocalDateTime type for the createdOn, lastModifiedOn and modifiedOn properties to java.time.Instant adapting to the Auditable API.
    
    Apply the Elements @NotNull and @Nullable annotations to the API.
    
    Edit Javadoc.

--- src/main/java/org/cp/elements/lang/support/AuditableSupport.java
+++ src/main/java/org/cp/elements/lang/support/AuditableSupport.java
@@ -13,25 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.lang.support;
 
 import static org.cp.elements.lang.ObjectUtils.defaultIfNull;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 import org.cp.elements.lang.Assert;
 import org.cp.elements.lang.Auditable;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * {@link AuditableSupport} is an abstract {@link Class} supporting implementations of the {@link Auditable}
- * {@link Class interface}.
+ * {@link AuditableSupport} is an abstract base class supporting implementations of the {@link Auditable} interface.
  *
  * @author John Blum
- * @see java.time.LocalDateTime
+ * @see java.time.Instant
  * @see org.cp.elements.lang.Auditable
+ * @see org.cp.elements.lang.support.IdentifiableSupport
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
@@ -40,9 +41,9 @@ public abstract class AuditableSupport<USER, PROCESS, ID extends Comparable<ID>>
 
   private static final String USER_NAME_SYSTEM_PROPERTY = "user.name";
 
-  private LocalDateTime createdOn;
-  private LocalDateTime lastModifiedOn;
-  private LocalDateTime modifiedOn;
+  private Instant createdOn;
+  private Instant lastModifiedOn;
+  private Instant modifiedOn;
 
   private PROCESS createdWith;
   private PROCESS lastModifiedWith;
@@ -53,7 +54,7 @@ public abstract class AuditableSupport<USER, PROCESS, ID extends Comparable<ID>>
   private USER modifiedBy;
 
   /**
-   * Assert the given {@link Object value} is not {@literal null}.
+   * Assert that the given {@link Object value} is not {@literal null}.
    *
    * @param <T> {@link Class type} of the {@link Object value}.
    * @param value {@link Object} to evaluate.
@@ -68,156 +69,129 @@ public abstract class AuditableSupport<USER, PROCESS, ID extends Comparable<ID>>
   }
 
   /**
-   * Gets the user responsible for creating this object.
-   *
-   * @return an object representing the user who created this object.
+   * @inheritDoc
    */
   @Override
-  public USER getCreatedBy() {
+  public @Nullable USER getCreatedBy() {
     return this.createdBy;
   }
 
   /**
-   * Sets the user responsible for creating this object.
-   *
-   * @param createdBy object representing the user who created this object.
+   * @inheritDoc
    */
   @Override
-  public void setCreatedBy(USER createdBy) {
+  public void setCreatedBy(@NotNull USER createdBy) {
     this.createdBy = assertNotNull(createdBy, () -> "Created by is required");
   }
 
   /**
-   * Gets the date and time when this object was created.
-   *
-   * @return a {@link LocalDateTime} capturing the date and time when this object was created.
+   * @inheritDoc
    */
   @Override
-  public LocalDateTime getCreatedOn() {
+  public @Nullable Instant getCreatedOn() {
     return this.createdOn;
   }
 
   /**
-   * Sets the date and time when this object was created.
-   *
-   * @param createdOn {@link LocalDateTime} capturing the date and time when this object was created.
+   * @inheritDoc
    */
   @Override
-  public void setCreatedOn(LocalDateTime createdOn) {
+  public void setCreatedOn(@NotNull Instant createdOn) {
     this.createdOn = assertNotNull(createdOn, () -> "Created on is required");
   }
 
   /**
-   * Gets the process (application) used by the user to create this object.
-   *
-   * @return an object representing the process (application) used by the user to create this object.
+   * @inheritDoc
    */
   @Override
-  public PROCESS getCreatedWith() {
+  public @Nullable PROCESS getCreatedWith() {
     return this.createdWith;
   }
 
   /**
-   * Sets the process (application) used by the user to create this object.
-   *
-   * @param createdWith object representing the process (application) used by the user to create this object.
+   * @inheritDoc
    */
   @Override
-  public void setCreatedWith(PROCESS createdWith) {
+  public void setCreatedWith(@NotNull PROCESS createdWith) {
     this.createdWith = assertNotNull(createdWith, () -> "Created with is required");
   }
 
   /**
-   * Gets the last user who modified this object.
-   *
-   * @return an object representing the last user responsible for modifying this object.
+   * @inheritDoc
    */
   @Override
-  public USER getLastModifiedBy() {
+  public @Nullable USER getLastModifiedBy() {
     return this.lastModifiedBy;
   }
 
   /**
-   * Gets the date and time when this object was last modified.
-   *
-   * @return a {@link LocalDateTime} capturing the date and time when this object was last modified.
+   * @inheritDoc
    */
   @Override
-  public LocalDateTime getLastModifiedOn() {
+  public @Nullable Instant getLastModifiedOn() {
     return this.lastModifiedOn;
   }
 
   /**
-   * Gets the last process (application) used by the user to modify this object.
-   *
-   * @return an object representing the last process (application) used by the user to modify this object.
+   * @inheritDoc
    */
   @Override
-  public PROCESS getLastModifiedWith() {
+  public @Nullable PROCESS getLastModifiedWith() {
     return this.lastModifiedWith;
   }
 
   /**
-   * Gets the user responsible for modifying this object.
-   *
-   * @return an object representing the user responsible for modifying this object.
+   * @inheritDoc
    */
   @Override
-  public USER getModifiedBy() {
-    return Optional.ofNullable(this.modifiedBy).orElseGet(this::getCreatedBy);
+  public @Nullable USER getModifiedBy() {
+    return Optional.ofNullable(this.modifiedBy)
+      .orElseGet(this::getCreatedBy);
   }
 
   /**
-   * Sets the user responsible for modifying this object.
-   *
-   * @param modifiedBy object representing the user responsible for modifying this object.
+   * @inheritDoc
    */
   @Override
   @SuppressWarnings("unchecked")
-  public void setModifiedBy(USER modifiedBy) {
+  public void setModifiedBy(@NotNull USER modifiedBy) {
     this.modifiedBy = assertNotNull(modifiedBy, () -> "Modified by is required");
     this.lastModifiedBy = defaultIfNull(this.lastModifiedBy, this.modifiedBy);
   }
 
   /**
-   * Gets the date and time when this object was modified.
-   *
-   * @return a {@link LocalDateTime} capturing the date and time when this object was modified.
+   * @inheritDoc
    */
   @Override
-  public LocalDateTime getModifiedOn() {
-    return Optional.ofNullable(this.modifiedOn).orElseGet(this::getCreatedOn);
+  public @Nullable Instant getModifiedOn() {
+    return Optional.ofNullable(this.modifiedOn)
+      .orElseGet(this::getCreatedOn);
   }
 
   /**
-   * Sets the date and time when this object was modified.
-   *
-   * @param modifiedOn {@link LocalDateTime} capturing the date and time when this object was modified.
+   * @inheritDoc
    */
   @Override
-  public void setModifiedOn(LocalDateTime modifiedOn) {
+  public void setModifiedOn(@NotNull Instant modifiedOn) {
     this.modifiedOn = assertNotNull(modifiedOn, () -> "Modified on is required");
     this.lastModifiedOn = defaultIfNull(this.lastModifiedOn, this.modifiedOn);
   }
 
   /**
-   * Gets the process (application) used by the user to modify this object.
-   *
-   * @return an object representing the process (application) used by the user to modify this object.
+   * @inheritDoc
    */
   @Override
-  public PROCESS getModifiedWith() {
-    return Optional.ofNullable(this.modifiedWith).orElseGet(this::getCreatedWith);
+  public @Nullable PROCESS getModifiedWith() {
+    return Optional.ofNullable(this.modifiedWith)
+      .orElseGet(this::getCreatedWith);
   }
 
   /**
-   * Sets the process (application) used by the user to modify this object.
-   *
-   * @param modifiedWith object representing the process (application) used by the user to modify this object.
+   * @inheritDoc
    */
   @Override
   @SuppressWarnings("unchecked")
-  public void setModifiedWith(PROCESS modifiedWith) {
+  public void setModifiedWith(@NotNull PROCESS modifiedWith) {
     this.modifiedWith = assertNotNull(modifiedWith, () -> "Modified with is required");
     this.lastModifiedWith = defaultIfNull(this.lastModifiedWith, this.modifiedWith);
   }
@@ -225,10 +199,13 @@ public abstract class AuditableSupport<USER, PROCESS, ID extends Comparable<ID>>
   /**
    * Returns the {@link String name} of the {@link USER} running the application.
    *
-   * @return a {@link String} contain the name of the {@link USER} running the application
+   * The {@link String name} of the {@link USER} can be defined by
+   * the Java {@link System#getProperties() System property}, {@literal user.name}.
+   *
+   * @return a {@link String} containing the {@literal name} of the {@link USER} running the application.
    * @see java.lang.System#getProperty(String)
    */
-  protected String getUsername() {
+  protected @Nullable String getUsername() {
     return System.getProperty(USER_NAME_SYSTEM_PROPERTY);
   }
 }
