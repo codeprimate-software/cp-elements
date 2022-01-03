49a9f61aec53985d9adae5e9d335c673ab82bbc2
John Blum <jblum@vmware.com>
2022-01-03 15:11:15 -0800

    Create new abstract CodeBlocks class in the org.cp.elements.lang package defining common code blocks as functions (methods).

---
+++ src/main/java/org/cp/elements/lang/CodeBlocks.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
+
+/**
+ * {@link CodeBlocks} is an abstract base class used to simplify code for common code blocks, such as
+ * {@literal if-then-else} statements, {@literal try-catch} and {@literal try-catch-finally} blocks,
+ * {@literal while-loops}, and so on.
+ *
+ * @author John Blum
+ * @see java.lang.Runnable
+ * @see java.util.function.Function
+ * @see java.util.function.Predicate
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public abstract class CodeBlocks {
+
+  public static final Runnable NO_OP_RUNNABLE = () -> {};
+
+  /**
+   * Implementation of an {@literal if-else} code block.
+   *
+   * @param <T> {@link Class type} of the {@link Object target} processed in the {@literal if} statement.
+   * @param <R> {@link Class type} of the {@link Object result} returned from the {@literal if} statement.
+   * @param target {@link Object} to evaluate in the {@literal if} statement.
+   * @param ifCondition {@link Predicate} used to evaluate the {@link Object target} in the {@literal if} condition.
+   * @param ifBlock {@link Function} encapsulating the logic in the {@literal if} block.
+   * @param elseBlock {@link Function} encapsulating the logic in the {@link else} block.
+   * @return the {@link Object result} from the evaluation of the {@literal if} statement.
+   * @throws IllegalArgumentException if the {@link Predicate if-condition}, {@link Function if-block}
+   * or the {@link Function else-block} are {@literal null}.
+   * @see java.util.function.Function
+   * @see java.util.function.Predicate
+   */
+  public static <T, R> R ifElse(@Nullable T target, @NotNull Predicate<T> ifCondition,
+      @NotNull Function<T, R> ifBlock, @NotNull Function<T, R> elseBlock) {
+
+    Assert.notNull(ifCondition, "The Predicate used in the condition of the if statement is required");
+    Assert.notNull(ifBlock, "The Function for the if block is required");
+    Assert.notNull(elseBlock, "The Function for the else block is required");
+
+    //return ifCondition.test(target) ? ifBlock.apply(target) : elseBlock.apply(target);
+
+    if (ifCondition.test(target)) {
+      return ifBlock.apply(target);
+    }
+    else {
+      return elseBlock.apply(target);
+    }
+  }
+
+  /**
+   * Implementation of the {@literal try-catch} code block, which handles the {@link Throwable} object
+   * by rethrowing a {@link RuntimeException} determined by the given, required {@link Function}.
+   *
+   * @param <T> {@link Class type} of the {@link Object result} from processing the code in the {@literal try} block.
+   * @param tryBlock {@link Function} encapsulating the logic invoked in the {@literal try} block. The processing logic
+   * in this case is capable of throwing an {@link Error} or {@link Exception}.
+   * @param catchBlock {@link Function} used to convert the {@link Throwable} into a {@link RuntimeException}.
+   * @return the {@link Object result} of processing the code in the {@literal try} block.
+   * @throws IllegalArgumentException if the {@literal try-block} {@link ThrowableOperation} or {@link Throwable}
+   * converting {@link Function} are {@literal null}.
+   * @see #tryCatchFinally(ThrowableOperation, Function, Runnable)
+   * @see org.cp.elements.lang.ThrowableOperation
+   * @see java.util.function.Function
+   */
+  public static <T> T tryCatch(@NotNull ThrowableOperation<T> tryBlock,
+      @NotNull Function<Throwable, ? extends RuntimeException> catchBlock) {
+
+    return tryCatchFinally(tryBlock, catchBlock, NO_OP_RUNNABLE);
+  }
+
+  /**
+   * Implementation of the {@literal try-catch} code block, which handles the {@link Throwable} object
+   * by rethrowing a {@link RuntimeException} determined by the given, required {@link Function}.
+   *
+   * @param <T> {@link Class type} of the {@link Object result} from processing the code in the {@literal try} block.
+   * @param tryBlock {@link Function} encapsulating the logic invoked in the {@literal try} block. The processing logic
+   * in this case is capable of throwing an {@link Error} or {@link Exception}.
+   * @param catchBlock {@link Function} used to convert the {@link Throwable} into a {@link RuntimeException}.
+   * @param finallyBlock {@link Runnable} encapsulating the logic invoked in the {@literal finally} block.
+   * @return the {@link Object result} of processing the code in the {@literal try} block.
+   * @throws IllegalArgumentException if the {@literal try-block} {@link ThrowableOperation}, {@link Throwable}
+   * converting {@link Function} or {@literal finally-block} {@link Runnable} are {@literal null}.
+   * @see org.cp.elements.lang.ThrowableOperation
+   * @see java.util.function.Function
+   * @see java.lang.Runnable
+   */
+  public static <T> T tryCatchFinally(@NotNull ThrowableOperation<T> tryBlock,
+      @NotNull Function<Throwable, ? extends RuntimeException> catchBlock, @NotNull Runnable finallyBlock) {
+
+    Assert.notNull(tryBlock, "The ThrowableOperation invoked in the try block is required");
+    Assert.notNull(catchBlock, "The Function converter invoked in the catch block is required");
+    Assert.notNull(finallyBlock, "The Runnable invoked in the finally block is required");
+
+    try {
+      return tryBlock.run(ObjectUtils.EMPTY_OBJECT_ARRAY);
+    }
+    catch (Throwable cause) {
+      throw catchBlock.apply(cause);
+    }
+    finally {
+      finallyBlock.run();
+    }
+  }
+
+  /**
+   * Implementation of the {@literal try-catch} code block, which handles the {@link Throwable} object
+   * by returning a {@link Object result}, such as an alternative {@link Object value}.
+   *
+   * @param <T> {@link Class type} of the {@link Object result} from processing the code in the {@literal try} block.
+   * @param tryBlock {@link Function} encapsulating the logic invoked in the {@literal try} block. The processing logic
+   * in this case is capable of throwing an {@link Error} or {@link Exception}.
+   * @param catchBlock {@link Function} used to handle the {@link Throwable} and return a {@link Object result}.
+   * @return the {@link Object result} of processing the code in the {@literal try} block if no {@link Error}
+   * or {@link Exception} is thrown, or returns the {@link Object value} computed from the {@link Throwable}
+   * {@link Function} handler.
+   * @throws IllegalArgumentException if the {@literal try-block} {@link ThrowableOperation} or {@link Throwable}
+   * handling {@link Function} are {@literal null}.
+   * @see #tryHandleFinally(ThrowableOperation, Function, Runnable)
+   * @see org.cp.elements.lang.ThrowableOperation
+   * @see java.util.function.Function
+   */
+  public static <T> T tryHandle(@NotNull ThrowableOperation<T> tryBlock, @NotNull Function<Throwable, T> catchBlock) {
+    return tryHandleFinally(tryBlock, catchBlock, NO_OP_RUNNABLE);
+  }
+
+  /**
+   * Implementation of the {@literal try-catch} code block, which handles the {@link Throwable} object
+   * by returning a {@link Object result}, such as an alternative {@link Object value}.
+   *
+   * @param <T> {@link Class type} of the {@link Object result} from processing the code in the {@literal try} block.
+   * @param tryBlock {@link Function} encapsulating the logic invoked in the {@literal try} block. The processing logic
+   * in this case is capable of throwing an {@link Error} or {@link Exception}.
+   * @param catchBlock {@link Function} used to handle the {@link Throwable} and return a {@link Object result}.
+   * @param finallyBlock {@link Runnable} encapsulating the logic invoked in the {@literal finally} block.
+   * @return the {@link Object result} of processing the code in the {@literal try} block if no {@link Error}
+   * or {@link Exception} is thrown, or returns the {@link Object value} computed from the {@link Throwable}
+   * {@link Function} handler.
+   * @throws IllegalArgumentException if the {@literal try-block} {@link ThrowableOperation} or {@link Throwable}
+   * handling {@link Function} are {@literal null}.
+   * @see org.cp.elements.lang.ThrowableOperation
+   * @see java.util.function.Function
+   * @see java.lang.Runnable
+   */
+  public static <T> T tryHandleFinally(@NotNull ThrowableOperation<T> tryBlock,
+      @NotNull Function<Throwable, T> catchBlock, @NotNull Runnable finallyBlock) {
+
+    Assert.notNull(tryBlock, "The ThrowableOperation invoked in the try block is required");
+    Assert.notNull(catchBlock, "The Function handler invoked in the catch block is required");
+    Assert.notNull(finallyBlock, "The Runnable invoked in the finally block is required");
+
+    try {
+      return tryBlock.run(ObjectUtils.EMPTY_OBJECT_ARRAY);
+    }
+    catch (Throwable cause) {
+      return catchBlock.apply(cause);
+    }
+    finally {
+      finallyBlock.run();
+    }
+  }
+
+  /**
+   * Implementation of the {@literal while-loop} code block.
+   *
+   * @param <T> {@link Class type} of the {@link Object target} being processed in the {@literal while-loop}.
+   * @param target {@link Object} being processed in the {@literal while-loop}
+   * @param whileCondition {@link Predicate} encapsulating the condition used in the {@literal while-loop}.
+   * @param whileBlock {@link Function} encapsulating the logic used to process the {@link Object target}
+   * for each iteration of the {@literal while-loop}.
+   * @return the {@link Object result} of processing the {@link Object target} in the {@literal while-loop}.
+   * @see java.util.function.Function
+   * @see java.util.function.Predicate
+   */
+  public static <T> T whileLoop(@Nullable T target,
+      @NotNull Predicate<T> whileCondition, @NotNull Function<T, T> whileBlock) {
+
+    Assert.notNull(whileCondition, "The condition for the while-loop is required");
+    Assert.notNull(whileBlock, "The Function of the while-loop is required");
+
+    while (whileCondition.test(target)) {
+      target = whileBlock.apply(target);
+    }
+
+    return target;
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/CodeBlocksUnitTests.java
@@ -0,0 +1,567 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import org.junit.Test;
+import org.mockito.InOrder;
+
+/**
+ * Unit Tests for {@link CodeBlocks}.
+ *
+ * @author John Blum
+ * @see java.util.function.Function
+ * @see java.util.function.Predicate
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.CodeBlocks
+ * @since 1.0.0
+ */
+@SuppressWarnings("unchecked")
+public class CodeBlocksUnitTests {
+
+  @Test
+  public void ifElseExecutesIfBlock() {
+
+    Predicate<Object> ifCondition = mock(Predicate.class);
+
+    Function<Object, Object> ifBlock = mock(Function.class);
+    Function<Object, Object> elseBlock = mock(Function.class);
+
+    doReturn(true).when(ifCondition).test(eq("mock"));
+    doReturn("test").when(ifBlock).apply(eq("mock"));
+
+    assertThat(CodeBlocks.ifElse("mock", ifCondition, ifBlock, elseBlock)).isEqualTo("test");
+
+    verify(ifCondition, times(1)).test(eq("mock"));
+    verify(ifBlock, times(1)).apply(eq("mock"));
+    verifyNoMoreInteractions(ifCondition, ifBlock);
+    verifyNoInteractions(elseBlock);
+  }
+
+  @Test
+  public void ifElseExecutesElseBlock() {
+
+    Predicate<Object> ifCondition = mock(Predicate.class);
+
+    Function<Object, Object> ifBlock = mock(Function.class);
+    Function<Object, Object> elseBlock = mock(Function.class);
+
+    doReturn(false).when(ifCondition).test(eq("mock"));
+    doReturn("test").when(elseBlock).apply(eq("mock"));
+
+    assertThat(CodeBlocks.ifElse("mock", ifCondition, ifBlock, elseBlock)).isEqualTo("test");
+
+    verify(ifCondition, times(1)).test(eq("mock"));
+    verify(elseBlock, times(1)).apply(eq("mock"));
+    verifyNoMoreInteractions(ifCondition, elseBlock);
+    verifyNoInteractions(ifBlock);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ifElseWithNullIfCondition() {
+
+    Function<Object, Object> ifBlock = mock(Function.class);
+    Function<Object, Object> elseBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.ifElse("test", null, ifBlock, elseBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Predicate used in the condition of the if statement is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(ifBlock, elseBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ifElseWithNullIfBlock() {
+
+    Predicate<Object> ifCondition = mock(Predicate.class);
+
+    Function<Object, Object> elseBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.ifElse("test", ifCondition, null, elseBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Function for the if block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(ifCondition, elseBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void ifElseWithNullElseBlock() {
+
+    Predicate<Object> ifCondition = mock(Predicate.class);
+
+    Function<Object, Object> ifBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.ifElse("test", ifCondition, ifBlock, null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Function for the else block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(ifCondition, ifBlock);
+    }
+  }
+
+  @Test
+  public void ifElseWithNullTargetIsNullSafe() {
+
+    Predicate<Object> ifCondition = mock(Predicate.class);
+
+    Function<Object, Object> ifBlock = mock(Function.class);
+    Function<Object, Object> elseBlock = mock(Function.class);
+
+    doReturn(true).when(ifCondition).test(any());
+    doReturn("test").when(ifBlock).apply(any());
+
+    assertThat(CodeBlocks.ifElse(null, ifCondition, ifBlock, elseBlock)).isEqualTo("test");
+
+    verify(ifCondition, times(1)).test(isNull());
+    verify(ifBlock, times(1)).apply(isNull());
+    verifyNoMoreInteractions(ifCondition, ifBlock);
+    verifyNoInteractions(elseBlock);
+  }
+
+  @Test
+  public void tryCatchIsSuccessful() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Function<Throwable, RuntimeException> catchBlock = mock(Function.class);
+
+    doReturn("test").when(operation).run(any());
+
+    assertThat(CodeBlocks.tryCatch(operation, catchBlock)).isEqualTo("test");
+
+    verify(operation, times(1)).run(any());
+    verifyNoMoreInteractions(operation);
+    verifyNoInteractions(catchBlock);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryCatchThrowsRuntimeException() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Function<Throwable, RuntimeException> catchBlock = mock(Function.class);
+
+    doThrow(new TestException("test")).when(operation).run(any());
+
+    doAnswer(invocation -> {
+      throw new IllegalArgumentException(invocation.getArgument(0, Throwable.class).getMessage());
+    }).when(catchBlock).apply(any(Throwable.class));
+
+    try {
+      CodeBlocks.tryCatch(operation, catchBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("test");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+
+      InOrder order = inOrder(operation, catchBlock);
+
+      order.verify(operation, times(1)).run(any());
+      order.verify(catchBlock, times(1)).apply(isA(TestException.class));
+
+      verifyNoMoreInteractions(operation, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void tryCatchFinallyThrowsRuntimeException() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    Function<Throwable, RuntimeException> catchBlock = mock(Function.class);
+
+    doThrow(new TestError("test")).when(operation).run(any());
+
+    doAnswer(invocation -> {
+      throw new IllegalStateException(invocation.getArgument(0, Throwable.class).getMessage());
+    }).when(catchBlock).apply(any(Throwable.class));
+
+    try {
+      CodeBlocks.tryCatchFinally(operation, catchBlock, finallyBlock);
+    }
+    catch (IllegalStateException expected) {
+
+      assertThat(expected).hasMessage("test");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+
+      InOrder order = inOrder(operation, catchBlock, finallyBlock);
+
+      order.verify(operation, times(1)).run(any());
+      order.verify(catchBlock, times(1)).apply(isA(TestError.class));
+      order.verify(finallyBlock, times(1)).run();
+
+      verifyNoMoreInteractions(operation, finallyBlock, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryCatchFinallyWithNullTryBlock() {
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    Function<Throwable, RuntimeException> catchBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.tryCatchFinally(null, catchBlock, finallyBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The ThrowableOperation invoked in the try block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(finallyBlock, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryCatchFinallyWithNullFinallyBlock() {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Function<Throwable, RuntimeException> catchBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.tryCatchFinally(operation, catchBlock, null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Runnable invoked in the finally block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(operation, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryCatchFinallyWithNullCatchBlock() {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    try {
+      CodeBlocks.tryCatchFinally(operation, null, finallyBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Function converter invoked in the catch block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(operation, finallyBlock);
+    }
+  }
+
+  @Test
+  public void tryHandleIsSuccessful() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    doReturn("test").when(operation).run(any());
+
+    Function<Throwable, Object> catchBlock = mock(Function.class);
+
+    CodeBlocks.tryHandle(operation, catchBlock);
+
+    verify(operation, times(1)).run(any());
+    verifyNoMoreInteractions(operation);
+    verifyNoInteractions(catchBlock);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryHandleThrowsRuntimeException() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Function<Throwable, Object> catchBlock = mock(Function.class);
+
+    doThrow(new TestException("test")).when(operation).run(any());
+
+    doAnswer(invocation -> {
+      throw new IllegalArgumentException(invocation.getArgument(0, Throwable.class).getMessage());
+    }).when(catchBlock).apply(isA(Throwable.class));
+
+    try {
+      CodeBlocks.tryHandle(operation, catchBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("test");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+
+      InOrder order = inOrder(operation, catchBlock);
+
+      order.verify(operation, times(1)).run(any());
+      order.verify(catchBlock, times(1)).apply(isA(TestException.class));
+
+      verifyNoMoreInteractions(operation, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void tryHandleFinallyThrowsRuntimeException() throws Throwable {
+
+    ThrowableOperation<Object> operation = mock(ThrowableOperation.class);
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    Function<Throwable, Object> catchBlock = mock(Function.class);
+
+    doThrow(new TestError("test")).when(operation).run(any());
+
+    doAnswer(invocation -> {
+      throw new IllegalStateException(invocation.getArgument(0, Throwable.class).getMessage());
+    }).when(catchBlock).apply(isA(Throwable.class));
+
+    try {
+      CodeBlocks.tryHandleFinally(operation, catchBlock, finallyBlock);
+    }
+    catch (IllegalStateException expected) {
+
+      assertThat(expected).hasMessage("test");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+
+      InOrder order = inOrder(operation, catchBlock, finallyBlock);
+
+      order.verify(operation, times(1)).run(any());
+      order.verify(catchBlock, times(1)).apply(isA(TestError.class));
+      order.verify(finallyBlock, times(1)).run();
+
+      verifyNoMoreInteractions(operation, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryHandleFinallyWithNullTryBlock() {
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    Function<Throwable, Object> catchBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.tryHandleFinally(null, catchBlock, finallyBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The ThrowableOperation invoked in the try block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(finallyBlock, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryHandleFinallyWithNullFinallyBlock() {
+
+    ThrowableOperation<Object> tryBlock = mock(ThrowableOperation.class);
+
+    Function<Throwable, Object> catchBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.tryHandleFinally(tryBlock, catchBlock, null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Runnable invoked in the finally block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(tryBlock, catchBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void tryHandleFinallyWithNullCatchBlock() {
+
+    ThrowableOperation<Object> tryBlock = mock(ThrowableOperation.class);
+
+    Runnable finallyBlock = mock(Runnable.class);
+
+    try {
+      CodeBlocks.tryHandleFinally(tryBlock, null, finallyBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Function handler invoked in the catch block is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(tryBlock, finallyBlock);
+    }
+  }
+
+  @Test
+  public void whileLoopIsSuccessful() {
+
+    Predicate<Integer> whileCondition = number ->  number < 200;
+
+    Function<Integer, Integer> powerOfTwo = number -> number * number;
+
+    assertThat(CodeBlocks.whileLoop(2, whileCondition, powerOfTwo)).isEqualTo(256);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whileLoopWithNullWhileCondition() {
+
+    Function<Object, Object> whileBlock = mock(Function.class);
+
+    try {
+      CodeBlocks.whileLoop("test", null, whileBlock);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The condition for the while-loop is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(whileBlock);
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void whileLoopWithNullWhileBlock() {
+
+    Predicate<Object> whileCondition = mock(Predicate.class);
+
+    try {
+      CodeBlocks.whileLoop("test", whileCondition, null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Function of the while-loop is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verifyNoInteractions(whileCondition);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static final class TestError extends Error {
+
+    public TestError() { }
+
+    public TestError(String message) {
+      super(message);
+    }
+
+    public TestError(Throwable cause) {
+      super(cause);
+    }
+
+    public TestError(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
+
+  @SuppressWarnings("unused")
+  private static final class TestException extends Exception {
+
+    public TestException() { }
+
+    public TestException(String message) {
+      super(message);
+    }
+
+    public TestException(Throwable cause) {
+      super(cause);
+    }
+
+    public TestException(String message, Throwable cause) {
+      super(message, cause);
+    }
+  }
+}
