606ddab76df11fd53c3aae3fbe8420295af1aa2c
John Blum <jblum@vmware.com>
2022-01-02 20:51:18 -0800

    Reviewed and refactored the org.cp.elements.lang.CheckedExceptionsFactory class.

--- src/main/java/org/cp/elements/lang/CheckedExceptionsFactory.java
+++ src/main/java/org/cp/elements/lang/CheckedExceptionsFactory.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.lang;
 
 import static org.cp.elements.text.FormatUtils.format;
@@ -21,9 +20,11 @@ import static org.cp.elements.text.FormatUtils.format;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
+
 /**
- * The {@link CheckedExceptionsFactory} class is an object factory used to construct and initialize
- * common, "checked" {@link Exception Exceptions}.
+ * {@link Object} factory used to construct and initialize common, {@literal checked} {@link Exception Exceptions}.
  *
  * @author John Blum
  * @see java.lang.Exception
@@ -32,89 +33,143 @@ import java.util.concurrent.TimeoutException;
 @SuppressWarnings("unused")
 public abstract class CheckedExceptionsFactory {
 
+  protected static final String UNKNOWN_REASON_MESSAGE = "?";
+
+  protected static final Throwable DEFAULT_CAUSE = null;
+
   /**
-   * Constructs and initializes a new {@link CloneNotSupportedException} with the given {@link String message}
+   * Constructs a new instance of {@link CloneNotSupportedException} initialized with the given {@link String message}
    * formatted with the given {@link Object[] arguments}.
    *
-   * @param message {@link String} describing the {@link CloneNotSupportedException exception}.
+   * @param message {@link String} describing the {@link CloneNotSupportedException}; must not be {@literal null}.
    * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link CloneNotSupportedException} with the given {@link String message}.
+   * @return a new {@link CloneNotSupportedException} initialized with the given {@link String message}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
    * @see #newCloneNotSupportedException(Throwable, String, Object...)
    * @see java.lang.CloneNotSupportedException
    */
-  public static CloneNotSupportedException newCloneNotSupportedException(String message, Object... args) {
+  public static @NotNull CloneNotSupportedException newCloneNotSupportedException(@NotNull String message,
+      Object... args) {
+
     return newCloneNotSupportedException(null, message, args);
   }
 
   /**
-   * Constructs and initializes a new {@link CloneNotSupportedException} with the given {@link Throwable cause}
-   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   * Constructs a new instance of {@link CloneNotSupportedException} initialized with the given {@link Throwable cause}.
    *
-   * @param cause {@link Throwable} identified as the reason this {@link CloneNotSupportedException} was thrown.
-   * @param message {@link String} describing the {@link CloneNotSupportedException exception}.
-   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link CloneNotSupportedException} with the given {@link Throwable cause} and {@link String message}.
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link CloneNotSupportedException}.
+   * @return a new {@link CloneNotSupportedException} initialized with the given {@link Throwable cause}.
+   * @see #newCloneNotSupportedException(Throwable, String, Object...)
    * @see java.lang.CloneNotSupportedException
    */
-  public static CloneNotSupportedException newCloneNotSupportedException(Throwable cause,
-      String message, Object... args) {
+  public static @NotNull CloneNotSupportedException newCloneNotSupportedException(@Nullable Throwable cause) {
+    return newCloneNotSupportedException(cause, UNKNOWN_REASON_MESSAGE);
+  }
+
+  /**
+   * Constructs a new instance of {@link CloneNotSupportedException} initialized with the given {@link Throwable cause}
+   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   *
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link CloneNotSupportedException}.
+   * @param message {@link String} describing the {@link CloneNotSupportedException}; must not be {@literal null}.
+   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
+   * @return a new {@link CloneNotSupportedException} initialized with the given {@link String message}
+   * and {@link Throwable cause}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
+   * @see java.lang.CloneNotSupportedException
+   */
+  public static @NotNull CloneNotSupportedException newCloneNotSupportedException(@Nullable Throwable cause,
+      @NotNull String message, Object... args) {
 
     return (CloneNotSupportedException) new CloneNotSupportedException(format(message, args)).initCause(cause);
   }
 
   /**
-   * Constructs and initializes a new {@link IOException} with the given {@link String message}
+   * Constructs a new instance of {@link IOException} initialized with the given {@link String message}
    * formatted with the given {@link Object[] arguments}.
    *
-   * @param message {@link String} describing the {@link IOException exception}.
+   * @param message {@link String} describing the {@link IOException}; must not be {@literal null}.
    * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link IOException} with the given {@link String message}.
+   * @return a new {@link IOException} initialized with the given {@link String message}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
    * @see #newIOException(Throwable, String, Object...)
    * @see java.io.IOException
    */
-  public static IOException newIOException(String message, Object... args) {
+  public static @NotNull IOException newIOException(@NotNull String message, Object... args) {
     return newIOException(null, message, args);
   }
 
   /**
-   * Constructs and initializes a new {@link IOException} with the given {@link Throwable cause}
-   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   * Constructs a new instance of {@link IOException} initialized with the given {@link Throwable cause}.
    *
-   * @param cause {@link Throwable} identified as the reason this {@link IOException} was thrown.
-   * @param message {@link String} describing the {@link IOException exception}.
-   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link IOException} with the given {@link Throwable cause} and {@link String message}.
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link IOException}.
+   * @return a new {@link IOException} initialized with the given {@link Throwable cause}.
+   * @see #newIOException(Throwable, String, Object...)
    * @see java.io.IOException
    */
-  public static IOException newIOException(Throwable cause, String message, Object... args) {
+  public static @NotNull IOException newIOException(@Nullable Throwable cause) {
+    return newIOException(cause, UNKNOWN_REASON_MESSAGE);
+  }
+
+  /**
+   * Constructs a new instance of {@link IOException} initialized with the given {@link Throwable cause}
+   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   *
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link IOException}.
+   * @param message {@link String} describing the {@link IOException}; must not be {@literal null}.
+   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
+   * @return a new {@link IOException} initialized with the given {@link String message} and {@link Throwable cause}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
+   * @see java.io.IOException
+   */
+  public static @NotNull IOException newIOException(@Nullable Throwable cause,
+      @NotNull String message, Object... args) {
+
     return new IOException(format(message, args), cause);
   }
 
   /**
-   * Constructs and initializes a new {@link TimeoutException} with the given {@link String message}
+   * Constructs a new instance of {@link TimeoutException} initialized with the given {@link String message}
    * formatted with the given {@link Object[] arguments}.
    *
-   * @param message {@link String} describing the {@link TimeoutException exception}.
+   * @param message {@link String} describing the {@link TimeoutException}; must not be {@literal null}.
    * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link TimeoutException} with the given {@link String message}.
+   * @return a new {@link TimeoutException} initialized with the given {@link String message}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
    * @see #newTimeoutException(Throwable, String, Object...)
    * @see java.util.concurrent.TimeoutException
    */
-  public static TimeoutException newTimeoutException(String message, Object... args) {
+  public static @NotNull TimeoutException newTimeoutException(@NotNull String message, Object... args) {
     return newTimeoutException(null, message, args);
   }
 
   /**
-   * Constructs and initializes a new {@link TimeoutException} with the given {@link Throwable cause}
-   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   * Constructs a new instance of {@link TimeoutException} initialized with the given {@link Throwable cause}.
    *
-   * @param cause {@link Throwable} identified as the reason this {@link TimeoutException} was thrown.
-   * @param message {@link String} describing the {@link TimeoutException exception}.
-   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
-   * @return a new {@link TimeoutException} with the given {@link Throwable cause} and {@link String message}.
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link TimeoutException}.
+   * @return a new {@link TimeoutException} initialized with the given {@link Throwable cause}.
+   * @see #newTimeoutException(Throwable, String, Object...)
    * @see java.util.concurrent.TimeoutException
    */
-  public static TimeoutException newTimeoutException(Throwable cause, String message, Object... args) {
+  public static @NotNull TimeoutException newTimeoutException(@Nullable Throwable cause) {
+    return newTimeoutException(cause, UNKNOWN_REASON_MESSAGE);
+  }
+
+  /**
+   * Constructs a new instance of {@link TimeoutException} initialized with the given {@link Throwable cause}
+   * and {@link String message} formatted with the given {@link Object[] arguments}.
+   *
+   * @param cause {@link Throwable} used as the {@literal cause} of the {@link TimeoutException}.
+   * @param message {@link String} describing the {@link TimeoutException}; must not be {@literal null}.
+   * @param args {@link Object[] arguments} used to replace format placeholders in the {@link String message}.
+   * @return a new {@link TimeoutException} initialized with the given {@link String message}
+   * and {@link Throwable cause}.
+   * @throws NullPointerException if the {@link String message} is {@literal null}.
+   * @see java.util.concurrent.TimeoutException
+   */
+  public static @NotNull TimeoutException newTimeoutException(@Nullable Throwable cause,
+      @NotNull String message, Object... args) {
+
     return (TimeoutException) new TimeoutException(format(message, args)).initCause(cause);
   }
 }
---
+++ src/test/java/org/cp/elements/lang/CheckedExceptionsFactoryUnitTests.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.CheckedExceptionsFactory.UNKNOWN_REASON_MESSAGE;
+import static org.cp.elements.lang.CheckedExceptionsFactory.newCloneNotSupportedException;
+import static org.cp.elements.lang.CheckedExceptionsFactory.newIOException;
+import static org.cp.elements.lang.CheckedExceptionsFactory.newTimeoutException;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * Unit Tests for {@link CheckedExceptionsFactory}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.junit.runner.RunWith
+ * @see org.mockito.Mock
+ * @see org.mockito.Mockito
+ * @see org.mockito.junit.MockitoJUnitRunner
+ * @see org.cp.elements.lang.CheckedExceptionsFactory
+ * @since 1.0.0
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CheckedExceptionsFactoryUnitTests {
+
+  @Mock
+  private Throwable mockCause;
+
+  private void assertThrowable(Throwable throwable, Class<? extends Throwable> type, String message) {
+    assertThrowable(throwable, type, message, null);
+  }
+
+  private void assertThrowable(Throwable throwable, Class<? extends Throwable> type,
+      String message, Throwable cause) {
+
+    assertThat(throwable).isNotNull();
+    assertThat(throwable).isInstanceOf(type);
+    assertThat(throwable).hasCause(cause);
+    assertThat(throwable).hasMessage(message);
+  }
+
+  @Test
+  public void newCloneNotSupportedExceptionWithCause() {
+    assertThrowable(newCloneNotSupportedException(this.mockCause), CloneNotSupportedException.class,
+      UNKNOWN_REASON_MESSAGE, this.mockCause);
+  }
+
+  @Test
+  public void newCloneNotSupportedExceptionWithMessage() {
+    assertThrowable(newCloneNotSupportedException("%s is a {1}", "This", "test"), CloneNotSupportedException.class,
+      "This is a test");
+  }
+
+  @Test
+  public void newCloneNotSupportedExceptionWithMessageAndCause() {
+    assertThrowable(newCloneNotSupportedException(this.mockCause, "test"), CloneNotSupportedException.class,
+      "test", this.mockCause);
+  }
+
+  @SuppressWarnings("all")
+  @Test(expected = NullPointerException.class)
+  public void newCloneNotSuppoertedExceptionWithNullMessage() {
+    newCloneNotSupportedException(null, ObjectUtils.EMPTY_OBJECT_ARRAY);
+  }
+
+  @Test
+  public void newIOExceptionWithCause() {
+    assertThrowable(newIOException(this.mockCause), IOException.class, UNKNOWN_REASON_MESSAGE, this.mockCause);
+  }
+
+  @Test
+  public void newIOExceptionWithMessage() {
+    assertThrowable(newIOException("%s is a {1}", "This", "test"), IOException.class, "This is a test");
+  }
+
+  @Test
+  public void newIOExceptionWithMessageAndCause() {
+    assertThrowable(newIOException(this.mockCause, "test"), IOException.class, "test", this.mockCause);
+  }
+
+  @SuppressWarnings("all")
+  @Test(expected = NullPointerException.class)
+  public void newIOExceptionWithNullMessage() {
+    newIOException(null, ObjectUtils.EMPTY_OBJECT_ARRAY);
+  }
+
+  @Test
+  public void newTimeoutExceptionWithCause() {
+    assertThrowable(newTimeoutException(this.mockCause), TimeoutException.class,
+      UNKNOWN_REASON_MESSAGE, this.mockCause);
+  }
+
+  @Test
+  public void newTimeoutExceptionWithMessage() {
+    assertThrowable(newTimeoutException("%s is a {1}", "This", "test"), TimeoutException.class,
+      "This is a test");
+  }
+
+  @Test
+  public void newTimeoutExceptionWithMessageAndCause() {
+    assertThrowable(newTimeoutException(this.mockCause, "test"), TimeoutException.class,
+      "test", this.mockCause);
+  }
+
+  @SuppressWarnings("all")
+  @Test(expected = NullPointerException.class)
+  public void newTimeoutExceptionWithNullMessage() {
+    newTimeoutException(null, ObjectUtils.EMPTY_OBJECT_ARRAY);
+  }
+}
