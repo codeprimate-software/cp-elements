f73c0f232f62928860e2c8a4053d447c4cd5f7ec
John Blum <jblum@vmware.com>
2022-01-02 19:16:37 -0800

    Define the ThrowableOperation interface in the org.cp.elements.lang package.
    Implement the java.lang.Runnable, java.util.concurrent.Callable, java.util.function.Consumer and java.util.function.Supplier interfaces.

---
+++ src/main/java/org/cp/elements/lang/ThrowableOperation.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newRuntimeException;
+
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * Operation capable of throwing a {@link Throwable} during execution.
+ *
+ * Implementation of the {@link Callable}, {@link Consumer}, {@link Runnable} and {@link Supplier} interfaces.
+ *
+ * @author John Blum
+ * @see java.lang.FunctionalInterface
+ * @see java.lang.Runnable
+ * @see java.util.concurrent.Callable
+ * @see java.util.function.Consumer
+ * @see java.util.function.Supplier
+ * @since 1.0.0
+ */
+@FunctionalInterface
+public interface ThrowableOperation<T> extends Callable<T>, Consumer<T>, Runnable, Supplier<T> {
+
+  /**
+   * Alias for {@link #run(Object...)}.
+   *
+   * Ignores any return value computed and returned from {@link #run(Object...)}.
+   *
+   * @param target {@link Object} processed by {@literal this} operation.
+   * @throws RuntimeException wrapping the {@link Throwable cause} if {@literal this} operation results in an error.
+   * @see #run(Object...)
+   */
+  @Override
+  default void accept(T target) {
+
+    try {
+      run(target);
+    }
+    catch (Throwable cause) {
+      throw newRuntimeException(cause, "Accept failed to operate on target [%s]", target);
+    }
+  }
+
+  /**
+   * Alias for {@link #run(Object...)}.
+   *
+   * @return the {@link Object result} of {@literal this} operation.
+   * @throws Exception wrapping the {@link Throwable cause} if {@literal this} operation results in an error.
+   * @see #run(Object...)
+   */
+  @Override
+  default T call() throws Exception {
+
+    try {
+      return run(ObjectUtils.EMPTY_OBJECT_ARRAY);
+    }
+    catch (Throwable cause) {
+      throw newRuntimeException(cause, "Call failed to complete operation");
+    }
+  }
+
+  /**
+   * Alias for {@link #run(Object...)}.
+   *
+   * @return the {@link Object result} of {@literal this} operation.
+   * @throws RuntimeException wrapping the {@link Throwable cause} if {@literal this} operation results in an error.
+   * @see #run(Object...)
+   */
+  @Override
+  default T get() {
+
+    try {
+      return run(ObjectUtils.EMPTY_OBJECT_ARRAY);
+    }
+    catch (Throwable cause) {
+      throw newRuntimeException(cause, "Get failed to return the result of the operation");
+    }
+  }
+
+  /**
+   * Alias for {@link #run(Object...)}.
+   *
+   * Accepts no arguments and ignores the {@link Object result} of the operation.
+   *
+   * @throws RuntimeException wrapping the {@link Throwable cause} if {@literal this} operation results in an error.
+   * @see #run(Object...)
+   */
+  @Override
+  default void run() {
+
+    try {
+      run(ObjectUtils.EMPTY_OBJECT_ARRAY);
+    }
+    catch (Throwable cause) {
+      throw newRuntimeException(cause, "Run failed to complete operation");
+    }
+  }
+
+  /**
+   * Performs an operation on the given, optional arguments.
+   *
+   * @param args optional array of {@link Object arguments} to process.
+   * @return the {@link Object result} of the operation.
+   * @throws Throwable if the operation fails.
+   */
+  T run(Object... args) throws Throwable;
+
+}
---
+++ src/test/java/org/cp/elements/lang/ThrowableOperationUnitTests.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link ThrowableOperation}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.ThrowableOperation
+ * @since 1.0.0
+ */
+@SuppressWarnings("unchecked")
+public class ThrowableOperationUnitTests {
+
+  @Test
+  public void acceptInvokesRunWithArguments() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).accept(any());
+
+    mockOperation.accept("mock");
+
+    verify(mockOperation, times(1)).accept(eq("mock"));
+    verify(mockOperation, times(1)).run(eq("mock"));
+    verifyNoMoreInteractions(mockOperation);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void acceptHandlesThrowable() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).accept(any());
+    doThrow(new IllegalArgumentException("test")).when(mockOperation).run(any());
+
+    try {
+      mockOperation.accept("mock");
+    }
+    catch (RuntimeException expected) {
+
+      assertThat(expected).hasMessage("Accept failed to operate on target [mock]");
+      assertThat(expected).hasCauseInstanceOf(IllegalArgumentException.class);
+      assertThat(expected.getCause()).hasMessage("test");
+      assertThat(expected.getCause()).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verify(mockOperation, times(1)).accept(eq("mock"));
+      verify(mockOperation, times(1)).run(eq("mock"));
+      verifyNoMoreInteractions(mockOperation);
+    }
+  }
+
+  @Test
+  public void callInvokesRunWithArguments() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).call();
+    doReturn("mock").when(mockOperation).run(any());
+
+    assertThat(mockOperation.call()).isEqualTo("mock");
+
+    verify(mockOperation, times(1)).call();
+    verify(mockOperation, times(1)).run(any());
+    verifyNoMoreInteractions(mockOperation);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void callHandlesThrowable() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).call();
+    doThrow(new IllegalStateException("test")).when(mockOperation).run(any());
+
+    try {
+      mockOperation.call();
+    }
+    catch (RuntimeException cause) {
+
+      assertThat(cause).hasMessage("Call failed to complete operation");
+      assertThat(cause).hasCauseInstanceOf(IllegalStateException.class);
+      assertThat(cause.getCause()).hasMessage("test");
+      assertThat(cause.getCause()).hasNoCause();
+
+      throw cause;
+    }
+    finally {
+      verify(mockOperation, times(1)).call();
+      verify(mockOperation, times(1)).run(any());
+      verifyNoMoreInteractions(mockOperation);
+    }
+  }
+
+  @Test
+  public void getInvokesRunWithArguments() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).get();
+    doReturn("mock").when(mockOperation).run(any());
+
+    assertThat(mockOperation.get()).isEqualTo("mock");
+
+    verify(mockOperation, times(1)).get();
+    verify(mockOperation, times(1)).run(any());
+    verifyNoMoreInteractions(mockOperation);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void getHandlesThrowable() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).get();
+    doThrow(new UnsupportedOperationException("test")).when(mockOperation).run(any());
+
+    try {
+      mockOperation.get();
+    }
+    catch (RuntimeException cause) {
+
+      assertThat(cause).hasMessage("Get failed to return the result of the operation");
+      assertThat(cause).hasCauseInstanceOf(UnsupportedOperationException.class);
+      assertThat(cause.getCause()).hasMessage("test");
+      assertThat(cause.getCause()).hasNoCause();
+
+      throw cause;
+    }
+    finally {
+      verify(mockOperation, times(1)).get();
+      verify(mockOperation, times(1)).run(any());
+      verifyNoMoreInteractions(mockOperation);
+    }
+  }
+
+  @Test
+  public void runInvokesRunWithArguments() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).run();
+
+    mockOperation.run();
+
+    verify(mockOperation, times(1)).run();
+    verify(mockOperation, times(1)).run(any());
+    verifyNoMoreInteractions(mockOperation);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void runHandlesThrowable() throws Throwable {
+
+    ThrowableOperation<Object> mockOperation = mock(ThrowableOperation.class);
+
+    doCallRealMethod().when(mockOperation).run();
+    doThrow(new RuntimeException("test")).when(mockOperation).run(any());
+
+    try {
+      mockOperation.run();
+    }
+    catch (RuntimeException expected) {
+
+      assertThat(expected).hasMessage("Run failed to complete operation");
+      assertThat(expected).hasCauseInstanceOf(RuntimeException.class);
+      assertThat(expected.getCause()).hasMessage("test");
+      assertThat(expected.getCause()).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verify(mockOperation, times(1)).run();
+      verify(mockOperation, times(1)).run(any());
+      verifyNoMoreInteractions(mockOperation);
+    }
+  }
+}
