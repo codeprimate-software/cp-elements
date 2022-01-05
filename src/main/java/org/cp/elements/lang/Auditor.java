098cbe775c2bc58a7f3c896d50646d74f1a5e0f7
John Blum <jblum@vmware.com>
2022-01-04 17:13:17 -0800

    Create new org.cp.elements.lang.Auditor interface encapsulating the logic to audit and Auditable object.

---
+++ src/main/java/org/cp/elements/lang/Auditor.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import java.time.Instant;
+import java.util.function.Supplier;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
+
+/**
+ * Abstract Data Type (ADT) defining an {@link Auditor} capable of {@literal auditing} an {@link Auditable} object.
+ *
+ * @author John Blum
+ * @see java.time.Instant
+ * @see org.cp.elements.lang.Auditable
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public interface Auditor<USER, PROCESS> {
+
+  /**
+   * Gets the date and time (timestamp) used to mark an {@link Auditable} object when it changed.
+   *
+   * @return a new {@link Instant}.
+   * @see java.util.function.Supplier
+   * @see java.time.Instant
+   */
+  default @NotNull Supplier<Instant> getDateTime() {
+    return Instant::now;
+  }
+
+  /**
+   * Gets the process (application) responsible for changing the {@link Auditable} object.
+   *
+   * Returns {@literal null} by default.
+   *
+   * @return the process (application) responsible for changing the {@link Auditable} object.
+   * @see java.util.function.Supplier
+   */
+  default @NotNull Supplier<PROCESS> getProcess() {
+    return () -> null;
+  }
+
+  /**
+   * Gets the user responsible for changing the {@link Auditable} object.
+   *
+   * By default, the {@literal user.name} {@link System#getProperties() System property} is used
+   * to identify the user.
+   *
+   * @return a {@link String} containing the {@literal username}, as determined by the {@literal user.name}
+   * {@link System#getProperties() System property}, of the user responsible for changing the {@link Auditable} object.
+   * @see java.lang.System#getProperty(String)
+   * @see java.util.function.Supplier
+   */
+  @SuppressWarnings("unchecked")
+  default @NotNull Supplier<USER> getUser() {
+    return () -> (USER) System.getProperty("user.name");
+  }
+
+  /**
+   * Tries to audit the given {@link Object} if the {@link Object} is {@link Auditable}
+   * otherwise simply returns the given {@link Object}.
+   *
+   * @param target {@link Object} to audit.
+   * @return the given {@link Object}.
+   * @see #audit(Auditable)
+   */
+  @NullSafe
+  @SuppressWarnings("unchecked")
+  default @Nullable Object audit(@Nullable Object target) {
+
+    return target instanceof Auditable
+      ? audit((Auditable<USER, PROCESS, ?>) target)
+      : target;
+  }
+
+  /**
+   * Audits the given, required {@link Auditable} object.
+   *
+   * @param <T> {@link Class subtype} of {@link Auditable} object.
+   * @param <ID> {@link Class type} of the {@link Auditable} object's identifier.
+   * @param auditable {@link Auditable} object to audit.
+   * @return the given {@link Auditable} object.
+   * @throws IllegalArgumentException if the {@link Auditable} object is {@literal null}.
+   * @throws IllegalStateException if the configured user is not set.
+   * @see org.cp.elements.lang.Auditable
+   * @see #getDateTime()
+   * @see #getProcess()
+   * @see #getUser()
+   */
+  default @NotNull <ID extends Comparable<ID>, T extends Auditable<USER, PROCESS, ID>> T audit(@NotNull T auditable) {
+
+    Assert.notNull(auditable, "The Auditable object to audit is required");
+
+    Instant dateTime = getDateTime().get();
+
+    PROCESS process = getProcess().get();
+    USER user = requireUser(getUser().get());
+
+    boolean setCreatedProperties = auditable.isNew() || isCreatedPropertiesUnset(auditable);
+
+    if (setCreatedProperties) {
+      auditable.setCreatedBy(user);
+      auditable.setCreatedOn(dateTime);
+      auditable.setCreatedWith(process);
+    }
+
+    boolean setModifiedProperties = auditable.isModified();
+
+    if (setModifiedProperties) {
+      auditable.setModifiedBy(user);
+      auditable.setModifiedOn(dateTime);
+      auditable.setModifiedWith(process);
+    }
+
+    return auditable;
+  }
+
+  /**
+   * Determines whether the created properties of the given, required {@link Auditable} object have been properly set.
+   *
+   * Only the {@literal createdBy} and {@literal createdOn} properties are used to make the determination.
+   *
+   * @param auditable {@link Auditable} object who's created properties are evaluated.
+   * @return a boolean value indicating whether the created properties of the given, required {@link Auditable} object
+   * have been properly set.
+   * @see org.cp.elements.lang.Auditable
+   */
+  default boolean isCreatedPropertiesUnset(@NotNull Auditable<USER, PROCESS, ?> auditable) {
+    return auditable.getCreatedBy() == null || auditable.getCreatedOn() == null;
+  }
+
+  /**
+   * Requires that the {@link USER} be known when auditing.
+   *
+   * @param user {@link USER} object to evaluate.
+   * @return the given {@link USER} object.
+   * @throws IllegalStateException if the {@link USER} is not known.
+   */
+  default @NotNull USER requireUser(@Nullable USER user) {
+    Assert.state(user != null, "User is required");
+    return user;
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/AuditorUnitTests.java
@@ -0,0 +1,417 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.withSettings;
+
+import java.time.Instant;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+
+/**
+ * Unit Tests for {@link Auditor}
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.lang.Auditable
+ * @see org.cp.elements.lang.Auditor
+ * @since 1.0.0
+ */
+@SuppressWarnings("unchecked")
+public class AuditorUnitTests {
+
+  private Instant toInstant(int year, Month month, int day, int hour, int minute, int second) {
+    return ZonedDateTime.of(year, month.getValue(), day, hour, minute, second,
+      0, ZoneId.systemDefault()).toInstant();
+  }
+
+  @Test
+  public void auditAuditsAuditableObjectBySettingAllAuditProperties() {
+
+    Instant dateTime = toInstant(2022, Month.JANUARY, 4, 16, 30, 15);
+
+    Object processName = AuditorUnitTests.class.getName();
+    Object username = "root";
+
+    Supplier<Instant> dateTimeSupplier = () -> dateTime;
+    Supplier<Object> processNameSupplier = () -> processName;
+    Supplier<Object> usernameSupplier = () -> username;
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    Auditable<Object, Object, Integer> mockAuditable = mock(Auditable.class);
+
+    doReturn(true).when(mockAuditable).isNew();
+    doReturn(true).when(mockAuditable).isModified();
+    doReturn(dateTimeSupplier).when(mockAuditor).getDateTime();
+    doReturn(processNameSupplier).when(mockAuditor).getProcess();
+    doReturn(usernameSupplier).when(mockAuditor).getUser();
+    doCallRealMethod().when(mockAuditor).audit(isA(Auditable.class));
+    doCallRealMethod().when(mockAuditor).requireUser(any());
+
+    assertThat(mockAuditor.audit(mockAuditable)).isEqualTo(mockAuditable);
+
+    verify(mockAuditor, times(1)).audit(eq(mockAuditable));
+    verify(mockAuditor, times(1)).getDateTime();
+    verify(mockAuditor, times(1)).getProcess();
+    verify(mockAuditor, times(1)).getUser();
+    verify(mockAuditor, times(1)).requireUser(eq(username));
+    verify(mockAuditor, never()).isCreatedPropertiesUnset(any());
+    verify(mockAuditable, times(1)).isNew();
+    verify(mockAuditable, times(1)).setCreatedBy(eq(username));
+    verify(mockAuditable, times(1)).setCreatedOn(eq(dateTime));
+    verify(mockAuditable, times(1)).setCreatedWith(eq(processName));
+    verify(mockAuditable, times(1)).isModified();
+    verify(mockAuditable, times(1)).setModifiedBy(eq(username));
+    verify(mockAuditable, times(1)).setModifiedOn(eq(dateTime));
+    verify(mockAuditable, times(1)).setModifiedWith(eq(processName));
+    verifyNoMoreInteractions(mockAuditable, mockAuditor);
+  }
+
+  @Test
+  public void auditSetsCreatedPropertiesWhenTheAuditableIsNotNewAndNotModified() {
+
+    Instant dateTime = toInstant(2022, Month.JANUARY, 4, 17, 0, 45);
+
+    Object processName = AuditorUnitTests.class.getName();
+    Object username = "jblum";
+
+    Supplier<Instant> dateTimeSupplier = () -> dateTime;
+    Supplier<Object> processNameSupplier = () -> processName;
+    Supplier<Object> usernameSupplier = () -> username;
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    Auditable<Object, Object, Integer> mockAuditable = mock(Auditable.class);
+
+    doReturn(false).when(mockAuditable).isNew();
+    doReturn(false).when(mockAuditable).isModified();
+    doReturn("root").when(mockAuditable).getCreatedBy();
+    doReturn(dateTimeSupplier).when(mockAuditor).getDateTime();
+    doReturn(processNameSupplier).when(mockAuditor).getProcess();
+    doReturn(usernameSupplier).when(mockAuditor).getUser();
+    doCallRealMethod().when(mockAuditor).audit(isA(Auditable.class));
+    doCallRealMethod().when(mockAuditor).isCreatedPropertiesUnset(eq(mockAuditable));
+    doCallRealMethod().when(mockAuditor).requireUser(any());
+
+    assertThat(mockAuditor.audit(mockAuditable)).isEqualTo(mockAuditable);
+
+    verify(mockAuditor, times(1)).audit(eq(mockAuditable));
+    verify(mockAuditor, times(1)).getDateTime();
+    verify(mockAuditor, times(1)).getProcess();
+    verify(mockAuditor, times(1)).getUser();
+    verify(mockAuditor, times(1)).requireUser(eq(username));
+    verify(mockAuditor, times(1)).isCreatedPropertiesUnset(eq(mockAuditable));
+    verify(mockAuditable, times(1)).isNew();
+    verify(mockAuditable, times(1)).getCreatedBy();
+    verify(mockAuditable, times(1)).getCreatedOn();
+    verify(mockAuditable, times(1)).setCreatedBy(eq(username));
+    verify(mockAuditable, times(1)).setCreatedOn(eq(dateTime));
+    verify(mockAuditable, times(1)).setCreatedWith(eq(processName));
+    verify(mockAuditable, times(1)).isModified();
+    verify(mockAuditable, never()).setModifiedBy(eq(username));
+    verify(mockAuditable, never()).setModifiedOn(eq(dateTime));
+    verify(mockAuditable, never()).setModifiedWith(eq(processName));
+    verifyNoMoreInteractions(mockAuditable, mockAuditor);
+  }
+
+  @Test
+  public void auditSetsNoProperties() {
+
+    Instant old = toInstant(2021, Month.JANUARY, 1, 0, 0, 0);
+    Instant dateTime = toInstant(2022, Month.JANUARY, 5, 17, 1, 45);
+
+    Object processName = AuditorUnitTests.class.getName();
+    Object username = "jblum";
+
+    Supplier<Instant> dateTimeSupplier = () -> dateTime;
+    Supplier<Object> processNameSupplier = () -> processName;
+    Supplier<Object> usernameSupplier = () -> username;
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    Auditable<Object, Object, Integer> mockAuditable = mock(Auditable.class);
+
+    doReturn(false).when(mockAuditable).isNew();
+    doReturn(false).when(mockAuditable).isModified();
+    doReturn("root").when(mockAuditable).getCreatedBy();
+    doReturn(old).when(mockAuditable).getCreatedOn();
+    doReturn(dateTimeSupplier).when(mockAuditor).getDateTime();
+    doReturn(processNameSupplier).when(mockAuditor).getProcess();
+    doReturn(usernameSupplier).when(mockAuditor).getUser();
+    doCallRealMethod().when(mockAuditor).audit(isA(Auditable.class));
+    doCallRealMethod().when(mockAuditor).isCreatedPropertiesUnset(eq(mockAuditable));
+    doCallRealMethod().when(mockAuditor).requireUser(any());
+
+    assertThat(mockAuditor.audit(mockAuditable)).isEqualTo(mockAuditable);
+
+    verify(mockAuditor, times(1)).audit(eq(mockAuditable));
+    verify(mockAuditor, times(1)).getDateTime();
+    verify(mockAuditor, times(1)).getProcess();
+    verify(mockAuditor, times(1)).getUser();
+    verify(mockAuditor, times(1)).requireUser(eq(username));
+    verify(mockAuditor, times(1)).isCreatedPropertiesUnset(eq(mockAuditable));
+    verify(mockAuditable, times(1)).isNew();
+    verify(mockAuditable, times(1)).getCreatedBy();
+    verify(mockAuditable, times(1)).getCreatedOn();
+    verify(mockAuditable, never()).setCreatedBy(any());
+    verify(mockAuditable, never()).setCreatedOn(any());
+    verify(mockAuditable, never()).setCreatedWith(any());
+    verify(mockAuditable, never()).setModifiedBy(eq(username));
+    verify(mockAuditable, times(1)).isModified();
+    verify(mockAuditable, never()).setModifiedOn(eq(dateTime));
+    verify(mockAuditable, never()).setModifiedWith(eq(processName));
+    verifyNoMoreInteractions(mockAuditable, mockAuditor);
+  }
+
+  @Test
+  public void auditUntypedAuditable() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    Object mockAuditable = mock(Auditable.class);
+
+    doCallRealMethod().when(mockAuditor).audit(isA(Object.class));
+
+    mockAuditor.audit(mockAuditable);
+
+    verify(mockAuditor, times(1)).audit(isA(Object.class));
+    verify(mockAuditor, times(1)).audit(isA(Auditable.class));
+    verifyNoMoreInteractions(mockAuditor);
+    verifyNoInteractions(mockAuditable);
+  }
+
+  @Test
+  public void auditObject() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).audit(isA(Object.class));
+
+    assertThat(mockAuditor.audit("test")).isEqualTo("test");
+
+    verify(mockAuditor, times(1)).audit(isA(Object.class));
+    verify(mockAuditor, never()).audit(isA(Auditable.class));
+  }
+
+  @Test
+  public void auditNull() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).audit(isA(Object.class));
+
+    assertThat(mockAuditor.audit((Object) null)).isNull();
+
+    verify(mockAuditor, times(1)).audit(ArgumentMatchers.<Object>isNull());
+    verify(mockAuditor, never()).audit(isA(Auditable.class));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void auditNullAuditable() {
+
+    Auditor<Object, Object> mockAuditor = spy(new Auditor<Object, Object>() { });
+
+    try {
+      mockAuditor.audit((Auditable<Object, Object, Integer>) null);
+    }
+    catch (IllegalArgumentException expected) {
+
+      assertThat(expected).hasMessage("The Auditable object to audit is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verify(mockAuditor, times(1)).audit(isNull());
+      verifyNoMoreInteractions(mockAuditor);
+    }
+  }
+
+  @Test
+  public void getDateTimeIsCorrect() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).getDateTime();
+
+    Supplier<Instant> dateTimeSupplier = mockAuditor.getDateTime();
+
+    assertThat(dateTimeSupplier).isNotNull();
+
+    Instant dateTime = dateTimeSupplier.get();
+
+    assertThat(dateTime).isNotNull();
+    assertThat(dateTime).isBeforeOrEqualTo(Instant.now());
+
+    verify(mockAuditor, times(1)).getDateTime();
+    verifyNoMoreInteractions(mockAuditor);
+  }
+
+  @Test
+  public void getProcessIsCorrect() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).getProcess();
+
+    Supplier<?> processSupplier = mockAuditor.getProcess();
+
+    assertThat(processSupplier).isNotNull();
+
+    Object process = processSupplier.get();
+
+    assertThat(process).isNull();
+
+    verify(mockAuditor, times(1)).getProcess();
+    verifyNoMoreInteractions(mockAuditor);
+  }
+
+  @Test
+  public void getUserIsCorrect() {
+
+    Auditor<?, ?> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).getUser();
+
+    Supplier<?> userSupplier = mockAuditor.getUser();
+
+    assertThat(userSupplier).isNotNull();
+
+    Object user = userSupplier.get();
+
+    assertThat(user).isNotNull();
+    assertThat(user).asString().isNotEmpty();
+    assertThat(user).isEqualTo(System.getProperty("user.name"));
+
+    verify(mockAuditor, times(1)).getUser();
+    verifyNoMoreInteractions(mockAuditor);
+  }
+
+  private <USER, PROCESS> void testIsCreatedPropertiesUnsetWhenCreatedPropertiesAreNotSetReturnsTrue(
+      USER user, Instant dateTime) {
+
+    Auditable<USER, PROCESS, Integer> mockAuditable = mock(Auditable.class, withSettings().lenient());
+
+    doReturn(user).when(mockAuditable).getCreatedBy();
+    doReturn(dateTime).when(mockAuditable).getCreatedOn();
+    doReturn(AuditorUnitTests.class.getName()).when(mockAuditable).getCreatedWith();
+
+    Auditor<USER, PROCESS> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).isCreatedPropertiesUnset(any());
+
+    assertThat(mockAuditor.isCreatedPropertiesUnset(mockAuditable)).isTrue();
+
+    verify(mockAuditor, times(1)).isCreatedPropertiesUnset(eq(mockAuditable));
+    verify(mockAuditable, times(1)).getCreatedBy();
+    verify(mockAuditable, times(user != null ? 1 : 0)).getCreatedOn();
+    verifyNoMoreInteractions(mockAuditable, mockAuditor);
+  }
+
+  @Test
+  public void isCreatedPropertiesUnsetWhenCreatedByAndCreatedOnPropertiesAreNotSet() {
+    testIsCreatedPropertiesUnsetWhenCreatedPropertiesAreNotSetReturnsTrue(null, null);
+  }
+
+  @Test
+  public void isCreatedPropertiesUnsetWhenOnlyCreatedByIsSet() {
+    testIsCreatedPropertiesUnsetWhenCreatedPropertiesAreNotSetReturnsTrue("root", null);
+  }
+
+  @Test
+  public void isCreatedPropertiesUnsetWhenOnlyCreatedOnIsSet() {
+    testIsCreatedPropertiesUnsetWhenCreatedPropertiesAreNotSetReturnsTrue(null, Instant.now());
+  }
+
+  @Test
+  public void isCreatedPropertiesUnsetWhenCreatedByAndCreatedOnAreSetReturnsFalse() {
+
+    Auditable<Object, Object, Integer> mockAuditable = mock(Auditable.class, withSettings().lenient());
+
+    doReturn("root").when(mockAuditable).getCreatedBy();
+    doReturn(Instant.now()).when(mockAuditable).getCreatedOn();
+    doReturn(AuditorUnitTests.class.getName()).when(mockAuditable).getCreatedWith();
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).isCreatedPropertiesUnset(any());
+
+    assertThat(mockAuditor.isCreatedPropertiesUnset(mockAuditable)).isFalse();
+
+    verify(mockAuditor, times(1)).isCreatedPropertiesUnset(eq(mockAuditable));
+    verify(mockAuditable, times(1)).getCreatedBy();
+    verify(mockAuditable, times(1)).getCreatedOn();
+    verifyNoMoreInteractions(mockAuditable, mockAuditor);
+  }
+
+  @Test
+  public void requireUserWithUser() {
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).requireUser(any());
+
+    assertThat(mockAuditor.requireUser("root")).isEqualTo("root");
+
+    verify(mockAuditor, times(1)).requireUser(eq("root"));
+    verifyNoMoreInteractions(mockAuditor);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void requireUserWithNull() {
+
+    Auditor<Object, Object> mockAuditor = mock(Auditor.class);
+
+    doCallRealMethod().when(mockAuditor).requireUser(any());
+
+    try {
+      mockAuditor.requireUser(null);
+    }
+    catch (IllegalStateException expected) {
+
+      assertThat(expected).hasMessage("User is required");
+      assertThat(expected).hasNoCause();
+
+      throw expected;
+    }
+    finally {
+      verify(mockAuditor, times(1)).requireUser(isNull());
+      verifyNoMoreInteractions(mockAuditor);
+    }
+  }
+}
