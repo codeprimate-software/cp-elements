922075610c6b7686f36fdb5c892d1788eb168ccc
John Blum <jblum@vmware.com>
2022-01-04 18:52:43 -0800

    Create the org.cp.elements.lang.Versionable interface defining a contract for Objects that can be versioned.

---
+++ src/main/java/org/cp/elements/lang/Versionable.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.lang;
+
+/**
+ * Interface defining a contract for {@link Object Object} that can be {@literal versioned}.
+ *
+ * The {@link Version} interface can serve as an alternative to {@link Auditable} object metadata in transactional,
+ * optimistic locking use cases.
+ *
+ * @author John Blum
+ * @param <T> {@link Class type} of {@link Object} modeling the {@literal version}.
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public interface Versionable<T> {
+
+  /**
+   * Gets the {@link T version} of {@literal this} {@link Object}.
+   *
+   * @return the {@link T version} of {@literal this} {@link Object}.
+   */
+  T version();
+
+}
