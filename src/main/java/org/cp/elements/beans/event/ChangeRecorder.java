00d4218c1c2ed6ab691ea733696ab8a88a6c7cc3
John Blum <jblum@vmware.com>
2022-03-30 22:59:26 -0700

    Review and refactor the org.cp.elements.beans.event.ChangeRecorder class.
    * Rename ChangeTracker to ChangeRecorder.
    * Rename ChangeTrackerTest to ChangeRecorderUnitTests.

---
+++ src/main/java/org/cp/elements/beans/event/ChangeRecorder.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.beans.event;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.cp.elements.beans.Bean;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.Visitable;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * {@link PropertyChangeListener} implementation used by {@link Bean} implementations for recording and monitoring
+ * changes in state to the properties of the {@literal observed} {@link Bean}.
+ *
+ * @author John J. Blum
+ * @see java.beans.PropertyChangeEvent
+ * @see java.beans.PropertyChangeListener
+ * @see java.lang.Iterable
+ * @see org.cp.elements.beans.Bean
+ * @see org.cp.elements.lang.Visitable
+ * @since 1.0.0
+ */
+public class ChangeRecorder implements Iterable<String>, PropertyChangeListener, Visitable {
+
+  // Map of object state mapping property name to the property value's hash code, ordered (sorted) by property name.
+  private final Map<String, Integer> objectStateMap = new TreeMap<>();
+
+  /**
+   * Determines whether the {@link Bean} monitored by this {@link ChangeRecorder} has any modified properties.
+   *
+   * @return {@literal true} if the {@link Bean} monitored by {@literal this} {@link ChangeRecorder} has any
+   * modified properties.
+   * @see #isModified(String)
+   */
+  public boolean isModified() {
+    return !this.objectStateMap.isEmpty();
+  }
+
+  /**
+   * Determines whether the {@link String named} property of the {@link Bean} monitored by {@literal this}
+   * {@link ChangeRecorder} has been modified.
+   *
+   * @param propertyName {@link String} containing the {@literal name} of the property on the {@link Bean}
+   * monitored by {@literal this} {@link ChangeRecorder}.
+   * @return {@literal true} if the {@link String named} property on the {@link Bean} monitored by {@literal this}
+   * {@link ChangeRecorder} has been modified.
+   * @see #isModified()
+   */
+  public boolean isModified(@NotNull String propertyName) {
+    return this.objectStateMap.containsKey(propertyName);
+  }
+
+  /**
+   * Event handler method that is fired when a property of the {@link Bean} monitored by this {@link ChangeRecorder}
+   * has been modified.
+   *
+   * The {@link PropertyChangeEvent} encapsulates all the information pertaining to the property change
+   * including the {@link String name} of the property, the {@link Object old value} and {@link Object new value}
+   * along with the source {@link Bean} of the targeted change.
+   *
+   * @param event {@link PropertyChangeEvent} encapsulating information about the property change.
+   * @see java.beans.PropertyChangeEvent
+   * @see #isModified(String)
+   */
+  public void propertyChange(@NotNull PropertyChangeEvent event) {
+
+    String propertyName = event.getPropertyName();
+
+    if (isModified(propertyName)) {
+      if (this.objectStateMap.get(propertyName) == ObjectUtils.hashCode(event.getNewValue())) {
+        // NOTE: If the hash code of the property's old (original) value is equal to the hash code of the property's
+        // new value, then the state of the property, identified by the event, has been reverted to its old (original),
+        // persisted value.
+        this.objectStateMap.remove(propertyName);
+      }
+    }
+    else {
+      if (!ObjectUtils.equalsIgnoreNull(event.getOldValue(), event.getNewValue())) {
+        this.objectStateMap.put(propertyName, ObjectUtils.hashCode(event.getOldValue()));
+      }
+    }
+  }
+
+  /**
+   * Iterates over the set of properties on the {@link Bean} monitored by {@literal this} {@link ChangeRecorder}
+   * that have been modified.
+   *
+   * @return an {@link Iterator} over the modified properties on the {@link Bean} monitored by {@literal this}
+   * {@link ChangeRecorder}.
+   * @see java.util.Iterator
+   */
+  public @NotNull Iterator<String> iterator() {
+    return Collections.unmodifiableSet(this.objectStateMap.keySet()).iterator();
+  }
+
+  /**
+   * Clears the recorded, dirty state of the {@link Object}.
+   *
+   * @return {@literal true} if the dirty state of the {@link Object} was successfully cleared.
+   */
+  @SuppressWarnings("all")
+  public boolean clear() {
+    this.objectStateMap.clear();
+    return this.objectStateMap.isEmpty();
+  }
+}
---
+++ src/test/java/org/cp/elements/beans/event/ChangeRecorderUnitTests.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.beans.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.beans.PropertyChangeEvent;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link ChangeRecorder}.
+ *
+ * @author John J. Blum
+ * @see java.beans.PropertyChangeEvent
+ * @see org.junit.Test
+ * @see org.cp.elements.beans.event.ChangeRecorder
+ * @since 1.0.0
+ */
+public class ChangeRecorderUnitTests {
+
+  private static final Object SOURCE = new Object();
+
+  private @NotNull PropertyChangeEvent newPropertyChangeEvent(String propertyName, Object oldValue, Object newValue) {
+    return new PropertyChangeEvent(SOURCE, propertyName, oldValue, newValue);
+  }
+
+  @Test
+  public void propertyChangeIsRecordedCorrectly() {
+
+    ChangeRecorder changeRecorder = new ChangeRecorder();
+
+    assertThat(changeRecorder).isNotNull();
+    assertThat(changeRecorder.isModified()).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("myProperty", null, "test"));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("myProperty")).isTrue();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("myProperty", "test", "testing"));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("myProperty")).isTrue();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("myProperty", "testing", "tested"));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("myProperty")).isTrue();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("myProperty", "tested", "testing"));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("myProperty")).isTrue();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("myProperty", "tested", null));
+
+    assertThat(changeRecorder.isModified()).isFalse();
+    assertThat(changeRecorder.isModified("myProperty")).isFalse();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("yourProperty", null, null));
+
+    assertThat(changeRecorder.isModified()).isFalse();
+    assertThat(changeRecorder.isModified("myProperty")).isFalse();
+    assertThat(changeRecorder.isModified("yourProperty")).isFalse();
+  }
+
+  @Test
+  public void iterateBeanProperties()  {
+
+    ChangeRecorder changeRecorder = new ChangeRecorder();
+
+    assertThat(changeRecorder).isNotNull();
+    assertThat(changeRecorder.isModified()).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyOne", null, "test"));
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyTwo", null, null));
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyThree", "null", null));
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyFour", "null", "null"));
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyFive", "null", "nil"));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("propertyOne")).isTrue();
+    assertThat(changeRecorder.isModified("propertyTwo")).isFalse();
+    assertThat(changeRecorder.isModified("propertyThree")).isTrue();
+    assertThat(changeRecorder.isModified("propertyFour")).isFalse();
+    assertThat(changeRecorder.isModified("propertyFive")).isTrue();
+
+    Set<String> actualProperties = new HashSet<>(3);
+
+    for (String property : changeRecorder) {
+      actualProperties.add(property);
+    }
+
+    assertThat(actualProperties).hasSize(3);
+    assertThat(actualProperties).containsExactlyInAnyOrder("propertyOne", "propertyThree", "propertyFive");
+    assertThat(actualProperties).doesNotContain("propertyTwo", "propertyFour");
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void iteratorIsUnmodifiable() {
+
+    ChangeRecorder changeRecorder = new ChangeRecorder();
+
+    assertThat(changeRecorder).isNotNull();
+    assertThat(changeRecorder.isModified()).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyOne", null, "test"));
+    changeRecorder.propertyChange(newPropertyChangeEvent("propertyTwo", null, null));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.isModified("propertyOne")).isTrue();
+    assertThat(changeRecorder.isModified("propertyTwo")).isFalse();
+
+    Iterator<String> it = changeRecorder.iterator();
+
+    assertThat(it).isNotNull();
+    assertThat(it.hasNext()).isTrue();
+
+    try {
+      it.remove();
+    }
+    finally {
+      assertThat(it.next()).isEqualTo("propertyOne");
+      assertThat(it.hasNext()).isFalse();
+    }
+  }
+
+  @Test
+  public void clearIsSuccessful() {
+
+    ChangeRecorder changeRecorder = new ChangeRecorder();
+
+    assertThat(changeRecorder).isNotNull();
+    assertThat(changeRecorder.isModified()).isFalse();
+
+    changeRecorder.propertyChange(newPropertyChangeEvent("testProperty", 1, 2));
+
+    assertThat(changeRecorder.isModified()).isTrue();
+    assertThat(changeRecorder.clear()).isTrue();
+    assertThat(changeRecorder.isModified()).isFalse();
+  }
+}
