c774bb13729626cf9ccee3038a4f408f4e41e383
John Blum <jblum@vmware.com>
2022-04-09 23:06:10 -0700

    Review and refactor the org.cp.elements.enums.Weekday enum.

--- src/main/java/org/cp/elements/enums/Weekday.java
+++ src/main/java/org/cp/elements/enums/Weekday.java
@@ -13,16 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.enums;
 
-import static org.cp.elements.util.CollectionExtensions.from;
-
+import java.time.DayOfWeek;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.function.Predicate;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
+import org.cp.elements.util.CollectionExtensions;
 
 /**
- * The {@link Weekday} enum is an {@link Enum enumeration} for all the days in a week.
+ * An {@link Enum enumeration} for all the days in a week.
  *
  * @author John J. Blum
  * @see java.lang.Enum
@@ -32,86 +35,214 @@ import java.util.Calendar;
 @SuppressWarnings("unused")
 public enum Weekday {
 
-  SUNDAY("Sun", "Sunday", 1, Calendar.SUNDAY),
-  MONDAY("Mon", "Monday", 2, Calendar.MONDAY),
-  TUESDAY("Tue", "Tuesday", 3, Calendar.TUESDAY),
-  WEDNESDAY("Wed", "Wednesday", 4, Calendar.WEDNESDAY),
-  THURSDAY("Thu", "Thursday", 5, Calendar.THURSDAY),
-  FRIDAY("Fri", "Friday", 6, Calendar.FRIDAY),
-  SATURDAY("Sat", "Saturday", 7, Calendar.SATURDAY);
+  SUNDAY("Sun", "Sunday", Calendar.SUNDAY, DayOfWeek.SUNDAY, 1),
+  MONDAY("Mon", "Monday", Calendar.MONDAY, DayOfWeek.MONDAY, 2),
+  TUESDAY("Tue", "Tuesday", Calendar.TUESDAY, DayOfWeek.TUESDAY, 3),
+  WEDNESDAY("Wed", "Wednesday", Calendar.WEDNESDAY, DayOfWeek.WEDNESDAY, 4),
+  THURSDAY("Thu", "Thursday", Calendar.THURSDAY, DayOfWeek.THURSDAY, 5),
+  FRIDAY("Fri", "Friday", Calendar.FRIDAY, DayOfWeek.FRIDAY, 6),
+  SATURDAY("Sat", "Saturday", Calendar.SATURDAY, DayOfWeek.SATURDAY, 7);
 
-  public static Weekday valueOfAbbreviation(String abbreviation) {
+  /**
+   * Finds and matches a {@link Weekday} enumerated value for the given {@link String abbreviation}.
+   *
+   * @param abbreviation {@link String} containing the {@literal abbreviation} used to find and match
+   * the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the given {@link String abbreviation} or {@literal null}
+   * if no {@link Weekday} has the given {@link String abbreviation}.
+   * @see #valueOfPredicate(Predicate)
+   * @see #getAbbreviation()
+   */
+  public static @Nullable Weekday valueOfAbbreviation(@Nullable String abbreviation) {
+    return valueOfPredicate(day -> day.getAbbreviation().equalsIgnoreCase(abbreviation));
+  }
+
+  /**
+   * Finds and matches a {@link Weekday} enumerated value for the given {@link Calendar#DAY_OF_WEEK}.
+   *
+   * @param calendarDay {@link Calendar#DAY_OF_WEEK} used to find and match the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the given {@link Calendar#DAY_OF_WEEK} or {@literal null}
+   * if no {@link Weekday} has the given {@link Calendar#DAY_OF_WEEK}.
+   * @see java.util.Calendar#DAY_OF_WEEK
+   * @see #valueOfPredicate(Predicate)
+   * @see #getCalendarDay()
+   */
+  public static @Nullable Weekday valueOfCalendarDay(int calendarDay) {
+    return valueOfPredicate(day -> day.getCalendarDay() == calendarDay);
+  }
+
+  /**
+   * Finds and matches a {@link Weekday} enumerated value for the given {@link DayOfWeek}.
+   *
+   * @param dayOfWeek {@link DayOfWeek} used to find and match the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the given {@link DayOfWeek} or {@literal null}
+   * if no {@link Weekday} has the given {@link DayOfWeek}.
+   * @see #valueOfPredicate(Predicate)
+   * @see java.time.DayOfWeek
+   * @see #getDayOfWeek()
+   */
+  public static @Nullable Weekday valueOfDayOfWeek(@Nullable DayOfWeek dayOfWeek) {
+    return valueOfPredicate(day -> day.getDayOfWeek().equals(dayOfWeek));
+  }
+
+  /**
+   * Finds and matches a {@link Weekday} enumerated value for the given {@link String name}.
+   *
+   * @param name {@link String} containing the {@literal name} used to find and match the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the given {@link String name} or {@literal null}
+   * if no {@link Weekday} has the given {@link String name}.
+   * @see #valueOfPredicate(Predicate)
+   * @see #getName()
+   */
+  public static @Nullable Weekday valueOfName(@Nullable String name) {
+    return valueOfPredicate(day -> day.getName().equalsIgnoreCase(name));
+  }
+
+  /**
+   * Finds and matches a {@link Weekday} enumerated value for the given {@link Integer position}.
+   *
+   * @param position {@link Integer} containing the {@literal position} used to find and match
+   * the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the given {@link Integer position} or {@literal null}
+   * if no {@link Weekday} has the given {@link Integer position}.
+   * @see #valueOfPredicate(Predicate)
+   * @see #getPosition()
+   */
+  public static @Nullable Weekday valueOfPosition(int position) {
+    return valueOfPredicate(day -> day.getPosition() == position);
+  }
+
+  /**
+   * Finds the first {@link Weekday} enumerated value that matches the given, required {@link Predicate}.
+   *
+   * @param predicate {@link Predicate} used to find and match the desired {@link Weekday}.
+   * @return a {@link Weekday} matching the {@link Predicate} or {@literal null} if no {@link Weekday}
+   * is found matching the {@link Predicate}.
+   * @see java.util.function.Predicate
+   */
+  private static @Nullable Weekday valueOfPredicate(@NotNull Predicate<Weekday> predicate) {
 
     return Arrays.stream(values())
-      .filter(day -> day.getAbbreviation().equalsIgnoreCase(abbreviation))
+      .filter(predicate)
       .findFirst()
       .orElse(null);
   }
 
-  public static Weekday valueOfCalendarConstant(int calendarConstant) {
-
-    return Arrays.stream(values())
-      .filter(day -> day.getCalendarConstant() == calendarConstant)
-      .findFirst()
-      .orElse(null);
-  }
-
-  public static Weekday valueOfName(String name) {
-
-    return Arrays.stream(values())
-      .filter(day -> day.getName().equalsIgnoreCase(name))
-      .findFirst()
-      .orElse(null);
-  }
-
-  public static Weekday valueOfPosition(int position) {
-
-    return Arrays.stream(values())
-      .filter(day -> day.getPosition() == position)
-      .findFirst()
-      .orElse(null);
-  }
-
-  private final int calendarConstant;
+  private final int calendarDay;
   private final int position;
+
+  private final java.time.DayOfWeek dayOfWeek;
 
   private final String abbreviation;
   private final String name;
 
-  Weekday(String abbreviation, String name, int position, int calendarConstant) {
+  /**
+   * Constructs a new instance of {@link Weekday} initialized with weekday metadata and descriptors.
+   * @param abbreviation {@link String} specifying the {@literal abbreviation} for {@literal this} {@link Weekday}.
+   * @param name {@link String} containing the {@literal name} of {@literal this} {@link Weekday}.
+   * @param calendarDay {@link Calendar#DAY_OF_WEEK} value.
+   * @param dayOfWeek {@link DayOfWeek} value.
+   * @param position {@link Integer#TYPE} specifying the position of {@literal this} {@link Weekday} in a calendar week.
+   */
+  Weekday(@NotNull String abbreviation, @NotNull String name, int calendarDay, @NotNull DayOfWeek dayOfWeek,
+      int position) {
 
     this.abbreviation = abbreviation;
     this.name = name;
+    this.calendarDay = calendarDay;
+    this.dayOfWeek = dayOfWeek;
     this.position = position;
-    this.calendarConstant = calendarConstant;
   }
 
-  public String getAbbreviation() {
+  /**
+   * Gets the {@link String abbreviation} for {@literal this} {@link Weekday}.
+   *
+   * @return the {@link String abbreviation} for {@literal this} {@link Weekday}.
+   * @see #getName()
+   */
+  public @NotNull String getAbbreviation() {
     return this.abbreviation;
   }
 
-  public int getCalendarConstant() {
-    return this.calendarConstant;
+  /**
+   * Gets the {@link Calendar#DAY_OF_WEEK} for {@literal this} {@link Weekday}.
+   *
+   * @return the {@link Calendar#DAY_OF_WEEK} for {@literal this} {@link Weekday}.
+   * @see java.util.Calendar#DAY_OF_WEEK
+   */
+  public int getCalendarDay() {
+    return this.calendarDay;
   }
 
-  public String getName() {
+  /**
+   * Gets the {@link DayOfWeek} for {@literal this} {@link Weekday}.
+   *
+   * @return the {@link DayOfWeek} for {@literal this} {@link Weekday}.
+   * @see java.time.DayOfWeek
+   */
+  public @NotNull DayOfWeek getDayOfWeek() {
+    return this.dayOfWeek;
+  }
+
+  /**
+   * Gets the {@link String name} for {@literal this} {@link Weekday}.
+   *
+   * @return the {@link String name} for {@literal this} {@link Weekday}.
+   * @see #getAbbreviation()
+   */
+  public @NotNull String getName() {
     return this.name;
   }
 
+  /**
+   * Gets the {@link Integer position} for {@literal this} {@link Weekday}.
+   *
+   * @return the {@link Integer position} for {@literal this} {@link Weekday}.
+   */
   public int getPosition() {
     return this.position;
   }
 
+  /**
+   * Determines whether {@literal this} {@link Weekday} enumerated value is a day in the week not on the weekend.
+   *
+   * Will return {@literal true} if the {@link Weekday} is in the set of: [{@literal Monday}, {@literal Tuesday},
+   * {@literal Wednesday}, {@literal Thursday}, {@literal Friday}].
+   *
+   * Will return {@literal false} if the {@link Weekday} is in the set of: [{@literal Saturday}, {@literal Sundday}].
+   *
+   * @return a boolean value if {@literal this} {@link Weekday} enumerated value is a day in the week
+   * not on the weekend.
+   * @see #isWeekend()
+   */
   public boolean isWeekday() {
     return !isWeekend();
   }
 
+  /**
+   * Determines whether {@literal this} {@link Weekday} enumerated value is a day of the weekend.
+   *
+   * Will return {@literal true} if the {Weekday} is in the set of: [{@literal Sunday}, {@literal Saturday}].
+   *
+   * Will return {@literal false} if the {@link Weekday} is in the set of: [{@literal Monday}, {@literal Tuesday},
+   * {@literal Wednesday}, {@literal Thursday}, {@literal Friday}].
+   *
+   * @return a boolean value if {@literal this} {@link Weekday} enumerated value is a day of the weekend.
+   * @see #isWeekday()
+   */
   public boolean isWeekend() {
-    return from(SUNDAY, SATURDAY).toSet().contains(this);
+    return CollectionExtensions.from(SUNDAY, SATURDAY).toSet().contains(this);
   }
 
+  /**
+   * Returns a {@link String} representation of {@literal this} {@link Weekday}.
+   *
+   * @return a {@link String} describing {@literal this} {@link Weekday}.
+   * @see java.lang.String
+   * @see #getName()
+   */
   @Override
-  public String toString() {
+  public @NotNull String toString() {
     return getName();
   }
 }
---
+++ src/test/java/org/cp/elements/enums/WeekdayUnitTests.java
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.cp.elements.lang.StringUtils;
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link Weekday}.
+ *
+ * @author John J. Blum
+ * @see java.time.DayOfWeek
+ * @see java.util.Calendar
+ * @see org.junit.Test
+ * @see org.cp.elements.enums.Weekday
+ * @since 1.0.0
+ */
+public class WeekdayUnitTests {
+
+  private static final int[] CALENDAR_DAYS = {
+    Calendar.SUNDAY,
+    Calendar.MONDAY,
+    Calendar.TUESDAY,
+    Calendar.WEDNESDAY,
+    Calendar.THURSDAY,
+    Calendar.FRIDAY,
+    Calendar.SATURDAY
+  };
+
+  private static final DayOfWeek[] DAYS_OF_WEEK = {
+    DayOfWeek.SUNDAY,
+    DayOfWeek.MONDAY,
+    DayOfWeek.TUESDAY,
+    DayOfWeek.WEDNESDAY,
+    DayOfWeek.THURSDAY,
+    DayOfWeek.FRIDAY,
+    DayOfWeek.SATURDAY,
+  };
+
+  @Test
+  public void isWeekdayReturnsTrue() {
+
+    assertThat(Weekday.MONDAY.isWeekday()).isTrue();
+    assertThat(Weekday.TUESDAY.isWeekday()).isTrue();
+    assertThat(Weekday.WEDNESDAY.isWeekday()).isTrue();
+    assertThat(Weekday.THURSDAY.isWeekday()).isTrue();
+    assertThat(Weekday.FRIDAY.isWeekday()).isTrue();
+  }
+
+  @Test
+  public void isWeekdayReturnsFalse() {
+
+    assertThat(Weekday.SUNDAY.isWeekday()).isFalse();
+    assertThat(Weekday.SATURDAY.isWeekday()).isFalse();
+  }
+
+  @Test
+  public void isWeekendReturnsTrue() {
+
+    assertThat(Weekday.SUNDAY.isWeekend()).isTrue();
+    assertThat(Weekday.SATURDAY.isWeekend()).isTrue();
+  }
+
+  @Test
+  public void isWeekendReturnsFalse() {
+
+    assertThat(Weekday.MONDAY.isWeekend()).isFalse();
+    assertThat(Weekday.TUESDAY.isWeekend()).isFalse();
+    assertThat(Weekday.WEDNESDAY.isWeekend()).isFalse();
+    assertThat(Weekday.THURSDAY.isWeekend()).isFalse();
+    assertThat(Weekday.FRIDAY.isWeekend()).isFalse();
+  }
+
+  @Test
+  public void valueOfReturnsWeekday() {
+
+    AtomicInteger position = new AtomicInteger(0);
+
+    Arrays.stream(Weekday.values()).forEach(weekday -> {
+
+      String name = StringUtils.capitalize(weekday.name().toLowerCase());
+
+      assertThat(Weekday.valueOf(weekday.name())).isEqualTo(weekday);
+      assertThat(weekday.getAbbreviation()).isEqualTo(name.substring(0, 3));
+      assertThat(weekday.getName()).isEqualTo(name);
+      assertThat(weekday.getCalendarDay()).isEqualTo(CALENDAR_DAYS[position.get()]);
+      assertThat(weekday.getDayOfWeek()).isEqualTo(DAYS_OF_WEEK[position.get()]);
+      assertThat(weekday.getPosition()).isEqualTo(position.incrementAndGet());
+    });
+  }
+
+  @Test
+  public void valueOfAbbreviationIsCorrect() {
+
+    Arrays.stream(Weekday.values()).forEach(weekday ->
+      assertThat(Weekday.valueOfAbbreviation(weekday.getAbbreviation())).isEqualTo(weekday));
+  }
+
+  @Test
+  public void valueOfAbbreviationIsLenient() {
+
+    assertThat(Weekday.valueOfAbbreviation("fri")).isEqualTo(Weekday.FRIDAY);
+    assertThat(Weekday.valueOfAbbreviation("sAT")).isEqualTo(Weekday.SATURDAY);
+    assertThat(Weekday.valueOfAbbreviation("SUN")).isEqualTo(Weekday.SUNDAY);
+  }
+
+  @Test
+  public void valueOfAbbreviationIsNullSafeReturnsNull() {
+    assertThat(Weekday.valueOfAbbreviation(null)).isNull();
+  }
+
+  @Test
+  public void valueOfAbbreviationUsingNameReturnsNull() {
+
+    assertThat(Weekday.valueOfAbbreviation("Sunday")).isNull();
+    assertThat(Weekday.valueOfAbbreviation("Moonday")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidAbbreviationReturnsNull() {
+
+    assertThat(Weekday.valueOfAbbreviation("")).isNull();
+    assertThat(Weekday.valueOfAbbreviation("  ")).isNull();
+    assertThat(Weekday.valueOfAbbreviation("May")).isNull();
+    assertThat(Weekday.valueOfAbbreviation("2014")).isNull();
+  }
+
+  @Test
+  public void valueOfCalendarDayIsCorrect() {
+
+    int index = 0;
+
+    for (Weekday weekday : Weekday.values()) {
+      assertThat(Weekday.valueOfCalendarDay(CALENDAR_DAYS[index++])).isEqualTo(weekday);
+    }
+
+    assertThat(index).isEqualTo(7);
+  }
+
+  @Test
+  public void valueOfInvalidCalendarDayReturnsNull() {
+
+    assertThat(Weekday.valueOfCalendarDay(-1)).isNull();
+    assertThat(Weekday.valueOfCalendarDay(0)).isNull();
+    assertThat(Weekday.valueOfCalendarDay(8)).isNull();
+  }
+
+  @Test
+  public void valueOfDayOfWeekIsCorrect() {
+
+    int index = 0;
+
+    for (Weekday weekday : Weekday.values()) {
+      assertThat(Weekday.valueOfDayOfWeek(DAYS_OF_WEEK[index++])).isEqualTo(weekday);
+    }
+
+    assertThat(index).isEqualTo(7);
+  }
+
+  @Test
+  public void valueOfNullDayOfWeekReturnsNull() {
+    assertThat(Weekday.valueOfDayOfWeek(null)).isNull();
+  }
+
+  @Test
+  public void valueOfNameIsCorrect() {
+
+    Arrays.stream(Weekday.values()).forEach(weekday -> {
+      assertThat(Weekday.valueOfName(weekday.getName())).isEqualTo(weekday);
+      assertThat(Weekday.valueOfName(weekday.name())).isEqualTo(weekday);
+    });
+  }
+
+  @Test
+  public void valueOfNameIsLenient() {
+
+    assertThat(Weekday.valueOfName("friday")).isEqualTo(Weekday.FRIDAY);
+    assertThat(Weekday.valueOfName("sATurDaY")).isEqualTo(Weekday.SATURDAY);
+    assertThat(Weekday.valueOfName("SUNDAY")).isEqualTo(Weekday.SUNDAY);
+  }
+
+  @Test
+  public void valueOfNameIsNullSafeReturnsNull() {
+    assertThat(Weekday.valueOfName(null)).isNull();
+  }
+
+  @Test
+  public void valueOfNameUsingAbbreviationReturnsNull() {
+
+    assertThat(Weekday.valueOfName("Mon")).isNull();
+    assertThat(Weekday.valueOfName("Tues")).isNull();
+    assertThat(Weekday.valueOfName("Thurs")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidNamesReturnsNull() {
+
+    assertThat(Weekday.valueOfName("")).isNull();
+    assertThat(Weekday.valueOfName("  ")).isNull();
+    assertThat(Weekday.valueOfName("October")).isNull();
+    assertThat(Weekday.valueOfName("2014")).isNull();
+  }
+
+  @Test
+  public void valueOfPositionIsCorrect() {
+
+    AtomicInteger position = new AtomicInteger(0);
+
+    Arrays.stream(Weekday.values()).forEach(weekday ->  {
+      assertThat(weekday.getPosition()).isEqualTo(position.incrementAndGet());
+    });
+
+    assertThat(position.get()).isEqualTo(7);
+  }
+
+  @Test
+  public void valueOfInvalidPositionReturnsNull() {
+
+    assertThat(Weekday.valueOfPosition(-1)).isNull();
+    assertThat(Weekday.valueOfPosition(0)).isNull();
+    assertThat(Weekday.valueOfPosition(8)).isNull();
+  }
+}
