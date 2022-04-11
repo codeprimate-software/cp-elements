f1c320afed66d3035f20e5e41864ad1007b02edd
John Blum <jblum@vmware.com>
2022-04-10 23:01:53 -0700

    Review and refactor the org.cp.elements.enums.Order enum.

--- src/main/java/org/cp/elements/enums/Order.java
+++ src/main/java/org/cp/elements/enums/Order.java
@@ -13,13 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.enums;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The {@link Order} enum is an {@link Enum enumeration} for sort order, or order by.
+ * An {@link Enum enumeration} of {@literal sort orders} used in {@literal ORDER BY} clauses of query statements.
  *
  * @author John J. Blum
  * @see java.lang.Enum
@@ -31,18 +34,45 @@ public enum Order {
   ASCENDING("ASC", "Ascending"),
   DESCENDING("DESC", "Descending");
 
-  public static Order valueOfAbbreviation(String abbreviation) {
-
-    return Arrays.stream(values())
-      .filter(order -> order.getAbbreviation().equalsIgnoreCase(abbreviation))
-      .findFirst()
-      .orElse(null);
+  /**
+   * Factory method used to find and match an {@link Order} by {@link String abbreviation}.
+   *
+   * @param abbreviation {@link String} containing the {@literal abbreviation} of the {@link Order} to find.
+   * @return an {@link Order} matching the given {@link String abbreviation} or {@literal null}
+   * if no {@link Order} matches the given {@link String abbreviation}.
+   * @see #valueOfPredicate(Predicate)
+   * @see #getAbbreviation()
+   */
+  public static @Nullable Order valueOfAbbreviation(@Nullable String abbreviation) {
+    return valueOfPredicate(order -> order.getAbbreviation().equalsIgnoreCase(abbreviation));
   }
 
-  public static Order valueOfName(String name) {
+  /**
+   * Factory method used to find and match an {@link Order} by {@link String name}.
+   *
+   * @param name {@link String} containing the {@literal name} of the {@link Order} to find.
+   * @return an {@link Order} matching the given {@link String name} or {@literal null}
+   * if no {@link Order} matches by the given {@link String name}.
+   * @see #valueOfPredicate(Predicate)
+   * @see #getName()
+   */
+  public static @Nullable Order valueOfName(@Nullable String name) {
+    return valueOfPredicate(order -> order.getName().equalsIgnoreCase(name));
+  }
+
+  /**
+   * Factory method used to find and match an {@link Order} by the given {@link Predicate}.
+   *
+   * @param predicate {@link Predicate} used to find and match an {@link Order}.
+   * @return an {@link Order} matching the given {@link Predicate} or {@literal null}
+   * if no {@link Order} matches the given {@link Predicate}.
+   * @see java.util.function.Predicate
+   * @see #values()
+   */
+  private static @Nullable Order valueOfPredicate(@NotNull Predicate<Order> predicate) {
 
     return Arrays.stream(values())
-      .filter(order -> order.getName().equalsIgnoreCase(name))
+      .filter(predicate)
       .findFirst()
       .orElse(null);
   }
@@ -50,19 +80,45 @@ public enum Order {
   private final String abbreviation;
   private final String name;
 
-  Order(String abbreviation, String name) {
+  /**
+   * Constructs a new instance of {@link Order} initialized with the given metadata and descriptors.
+   *
+   * @param abbreviation {@link String} specifying the {@literal abbreviation} of {@literal this} {@link Order}.
+   * @param name {@link String} containing the {@literal name} of {@literal this} {@link Order}.
+   */
+  Order(@NotNull String abbreviation, @NotNull String name) {
+
     this.abbreviation = abbreviation;
     this.name = name;
   }
 
+  /**
+   * Get the {@link String abbreviation} for {@literal this} {@link Order}.
+   *
+   * @return the {@link String abbreviation} for {@literal this} {@link Order}.
+   * @see #getName()
+   */
   public String getAbbreviation() {
     return this.abbreviation;
   }
 
+  /**
+   * Get the {@link String name} of {@literal this} {@link Order}.
+   *
+   * @return the {@link String name} of {@literal this} {@link Order}.
+   * @see #getAbbreviation()
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Return a {@link String} representation of {@literal this} {@link Order}.
+   *
+   * @return a {@link String} describing {@literal this} {@link Order}.
+   * @see java.lang.Object#toString()
+   * @see #getName()
+   */
   @Override
   public String toString() {
     return getName();
---
+++ src/test/java/org/cp/elements/enums/OrderUnitTests.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+
+import org.cp.elements.lang.StringUtils;
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link Order}.
+ *
+ * @author John J. Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.enums.Order
+ * @since 1.0.0
+ */
+public class OrderUnitTests {
+
+  @Test
+  public void valueOfReturnsOrder() {
+
+    Arrays.stream(Order.values()).forEach(order -> {
+      assertThat(Order.valueOf(order.name())).isEqualTo(order);
+      assertThat(order.getName()).isEqualTo(StringUtils.capitalize(order.name().toLowerCase()));
+      assertThat(order.getAbbreviation()).startsWith(order.name().toUpperCase().substring(0, 3));
+    });
+  }
+
+  @Test
+  public void valueOfAbbreviationIsCorrect() {
+
+    Arrays.stream(Order.values()).forEach(order ->
+      assertThat(Order.valueOfAbbreviation(order.getAbbreviation())).isEqualTo(order));
+  }
+
+  @Test
+  public void valueOfAbbreviationIsLenient() {
+
+    assertThat(Order.valueOfAbbreviation("asc")).isEqualTo(Order.ASCENDING);
+    assertThat(Order.valueOfAbbreviation("Desc")).isEqualTo(Order.DESCENDING);
+    assertThat(Order.valueOfAbbreviation("ASC")).isEqualTo(Order.ASCENDING);
+  }
+
+  @Test
+  public void valueOfAbbreviationIsNullSafeReturnsNull() {
+    assertThat(Order.valueOfAbbreviation(null)).isNull();
+  }
+
+  @Test
+  public void valueOfAbbreviationUsingNameReturnsNull() {
+
+    assertThat(Order.valueOfAbbreviation("Ascending")).isNull();
+    assertThat(Order.valueOfAbbreviation("DESCending")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidAbbreviationsReturnsNull() {
+
+    assertThat(Order.valueOfAbbreviation("")).isNull();
+    assertThat(Order.valueOfAbbreviation("  ")).isNull();
+    assertThat(Order.valueOfAbbreviation("ascend")).isNull();
+    assertThat(Order.valueOfAbbreviation("Descend")).isNull();
+    assertThat(Order.valueOfAbbreviation("SIDE")).isNull();
+    assertThat(Order.valueOfAbbreviation("Sideways")).isNull();
+  }
+
+  @Test
+  public void valueOfNameIsCorrect() {
+
+    Arrays.stream(Order.values()).forEach(order ->
+      assertThat(Order.valueOfName(order.getName())).isEqualTo(order));
+  }
+
+  @Test
+  public void valueOfNameIsLenient() {
+
+    assertThat(Order.valueOfName("ascending")).isEqualTo(Order.ASCENDING);
+    assertThat(Order.valueOfName("Descending")).isEqualTo(Order.DESCENDING);
+    assertThat(Order.valueOfName("ASCENDING")).isEqualTo(Order.ASCENDING);
+  }
+
+  @Test
+  public void valueOfNameIsNullSafeReturnsNull() {
+    assertThat(Order.valueOfName(null)).isNull();
+  }
+
+  @Test
+  public void valueOfNameUsingAbbreviationReturnsNull() {
+
+    assertThat(Order.valueOfName("asc")).isNull();
+    assertThat(Order.valueOfName("Desc")).isNull();
+    assertThat(Order.valueOfName("ASC")).isNull();
+    assertThat(Order.valueOfName("side")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidNameReturnsNull() {
+
+    assertThat(Order.valueOfName("")).isNull();
+    assertThat(Order.valueOfName("  ")).isNull();
+    assertThat(Order.valueOfName("Horizontal")).isNull();
+    assertThat(Order.valueOfName("Sideways")).isNull();
+    assertThat(Order.valueOfName("Vertical")).isNull();
+  }
+}
