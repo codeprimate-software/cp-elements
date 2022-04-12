025324989375a8043eea77866e4a160bf29bfeda
John Blum <jblum@vmware.com>
2022-04-11 20:19:02 -0700

    Review and refactor the org.cp.elements.enums.Race enum.

--- src/main/java/org/cp/elements/enums/Race.java
+++ src/main/java/org/cp/elements/enums/Race.java
@@ -13,13 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.enums;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The {@link Race} enum is an {@link Enum enumeration} for different ethnicity and nationalities.
+ * An {@link Enum enumeration} for different races as defined by the OMB standards.
  *
  * @author John J. Blum
  * @see java.lang.Enum
@@ -29,26 +32,52 @@ import java.util.Arrays;
 public enum Race {
 
   AFRICAN_AMERICAN("Black", "African American"),
-  ALASKAN_NATIVE("Eskimo", "Alaskan Native"),
+  ALASKA_NATIVE("Alaskan", "Alaska Native"),
+  AMERICAN_INDIAN("Indian", "American Indian/Native American"),
   ASIAN("Asian", "Asian"),
-  EUROPEAN("European", "European"),
-  HISPANIC("Hispanic", "Hispanic"),
-  INDIAN("Indi", "Indian"),
-  NATIVE_AMERICAN("Indian", "Native American"),
+  NATIVE_HAWAIIAN("Hawaiian", "Native Hawaiian/Other Pacific Islander"),
   WHITE("White", "White");
 
-  public static Race valueOfAbbreviation(String abbreviation) {
-
-    return Arrays.stream(values())
-      .filter(race -> race.getAbbreviation().equalsIgnoreCase(abbreviation))
-      .findFirst()
-      .orElse(null);
+  /**
+   * Factory method used to find a {@link Race} by {@link String abbreviation}.
+   *
+   * @param abbreviation {@link String} containing the {@literal abbreviation} used to
+   * find and match a {@link Race} enum.
+   * @return a {@link Race} matching the given {@link String abbreviation} or {@literal null}
+   * if no {@link Race} matches the given {@link String abbreviation}.
+   * @see #valueOf(Predicate)
+   * @see #getAbbreviation()
+   */
+  public static @Nullable Race valueOfAbbreviation(@Nullable String abbreviation) {
+    return valueOf(race -> race.getAbbreviation().equalsIgnoreCase(abbreviation));
   }
 
-  public static Race valueOfName(String name) {
+  /**
+   * Factory method used to find a {@link Race} by {@link String name}.
+   *
+   * @param name {@link String} containing the {@literal name} used to find and match a {@link Race} enum.
+   * @return a {@link Race} matching the given {@link String name} or {@literal null}
+   * if no {@link Race} matches the given {@link String name}.
+   * @see #valueOf(Predicate)
+   * @see #getName()
+   */
+  public static @Nullable Race valueOfName(@Nullable String name) {
+    return valueOf(race -> race.getName().equalsIgnoreCase(name));
+  }
+
+  /**
+   * Factory method used to find a {@link Race} by the given, required {@link Predicate}.
+   *
+   * @param predicate {@link Predicate} used to find and match a {@link Race} enumerated value.
+   * @return a {@link Race} matching the given, required {@link Predicate} or {@literal null}
+   * if no {@link Race} is a match for the given, required {@link Predicate}.
+   * @see java.util.function.Predicate
+   * @see #values()
+   */
+  private static @Nullable Race valueOf(@NotNull Predicate<Race> predicate) {
 
     return Arrays.stream(values())
-      .filter(race -> race.getName().equalsIgnoreCase(name))
+      .filter(predicate)
       .findFirst()
       .orElse(null);
   }
@@ -56,19 +85,45 @@ public enum Race {
   private final String abbreviation;
   private final String name;
 
-  Race(String abbreviation, String name) {
+  /**
+   * Constructs a new instance of {@link Race} initialized with the given {@link String abbreviation}
+   * and {@link String name} for the {@link Race}.
+   *
+   * @param abbreviation {@link String} specifying the {@literal abbreviation} for {@literal this} {@link Race}.
+   * @param name {@link String} containing the {@literal name} of {@literal this} {@link Race}.
+   */
+  Race(@NotNull String abbreviation, @NotNull String name) {
     this.abbreviation = abbreviation;
     this.name = name;
   }
 
+  /**
+   * Get the {@link String abbreviation} for {@literal this} {@link Race}.
+   *
+   * @return the {@link String abbreviation} for {@literal this} {@link Race}.
+   * @see #getName()
+   */
   public String getAbbreviation() {
     return this.abbreviation;
   }
 
+  /**
+   * Get the {@link String name} of {@literal this} {@link Race}.
+   *
+   * @return the {@link String name} of {@literal this} {@link Race}.
+   * @see #getAbbreviation()
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Returns a {@link String} representation for {@literal this} {@link Race}.
+   *
+   * @return a {@link String} describing {@literal this} {@link Race}.
+   * @see java.lang.Object#toString()
+   * @see #getName()
+   */
   @Override
   public String toString() {
     return getName();
