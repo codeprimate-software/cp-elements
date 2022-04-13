db89b6d99149a3159858ed55f6d3d9246d0a5432
John Blum <jblum@vmware.com>
2022-04-12 18:16:00 -0700

    Review and refactor the org.cp.elements.enums.TimeUnit enum.

--- src/main/java/org/cp/elements/enums/TimeUnit.java
+++ src/main/java/org/cp/elements/enums/TimeUnit.java
@@ -13,13 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.enums;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
+
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The {@link TimeUnit} enum is an {@link Enum enumeration} for units of time.
+ * An {@link Enum enumeration} for units of time.
  *
  * @author John J. Blum
  * @see java.lang.Enum
@@ -39,22 +42,51 @@ public enum TimeUnit {
   MONTH("mon", "Month", "28-31 days"),
   YEAR("yr", "Year", "12 months, 365 days"),
   DECADE("dec", "Decade", "10 years"),
-  SCORE("Score", "Score", "20 years"),
+  SCORE("score", "Score", "20 years"),
   CENTURY("cent", "Century", "100 years"),
-  MILLENIA("Millenia", "Millenia", "1000 years");
+  MILLENNIA("millennia", "Millennia", "1000 years");
 
-  public static TimeUnit valueOfAbbreviation(String abbreviation) {
-
-    return Arrays.stream(values())
-      .filter(unit -> unit.getAbbreviation().equalsIgnoreCase(abbreviation))
-      .findFirst()
-      .orElse(null);
+  /**
+   * Factory method used to find a {@link TimeUnit} for the given {@link String abbreviation}.
+   *
+   * @param abbreviation {@link String} containing the {@literal abbreviation} used to find
+   * and match the desired {@link TimeUnit}.
+   * @return a {@link TimeUnit} matching the given {@link String abbreviation} or {@literal null}
+   * if no {@link TimeUnit} is a match for the given {@link String abbreviation}.
+   * @see #valueOf(Predicate)
+   * @see #getAbbreviation()
+   */
+  public static @Nullable TimeUnit valueOfAbbreviation(@Nullable String abbreviation) {
+    return valueOf(timeUnit -> timeUnit.getAbbreviation().equalsIgnoreCase(abbreviation));
   }
 
-  public static TimeUnit valueOfName(String name) {
+  /**
+   * Factory method used to find a {@link TimeUnit} for the given {@link String name}.
+   *
+   * @param name {@link String} containing the {@literal name} used to find and match the desired {@link TimeUnit}.
+   * @return a {@link TimeUnit} matching the given {@link String name} or {@literal null}
+   * if no {@link TimeUnit} is a match for the given {@link String name}.
+   * @see #valueOf(Predicate)
+   * @see #getName()
+   */
+  public static @Nullable TimeUnit valueOfName(@Nullable String name) {
+    return valueOf(timeUnit -> timeUnit.getName().equalsIgnoreCase(name));
+  }
+
+  /**
+   * Factory method used to find a {@link TimeUnit} matching the given, required {@link Predicate}.
+   *
+   * @param predicate {@link Predicate} used to find and match the desired {@link TimeUnit};
+   * must not be {@literal null}.
+   * @return a {@link TimeUnit} matching the given, required {@link Predicate} or {@literal null}
+   * if no {@link TimeUnit} is match for the given, required {@link Predicate}
+   * @see java.util.function.Predicate
+   * @see #values()
+   */
+  private static @Nullable TimeUnit valueOf(@NotNull Predicate<TimeUnit> predicate) {
 
     return Arrays.stream(values())
-      .filter(unit -> unit.getName().equalsIgnoreCase(name))
+      .filter(predicate)
       .findFirst()
       .orElse(null);
   }
@@ -63,26 +95,58 @@ public enum TimeUnit {
   private final String description;
   private final String name;
 
+  /**
+   * Constructs a new instance of {@link TimeUnit} initialized with the given, required {@link String abbreviation},
+   * {@link String name} and {@link String description} to describe the unit of time.
+   *
+   * @param abbreviation {@link String} specifying the {@literal abbreviation} for {@literal this} {@link TimeUnit}.
+   * @param name {@link String} containing the {@literal name} of {@literal this} {@link TimeUnit}.
+   * @param description {@link String} describing {@literal this} {@link TimeUnit}.
+   */
   TimeUnit(String abbreviation, String name, String description) {
     this.abbreviation = abbreviation;
     this.name = name;
     this.description = description;
   }
 
+  /**
+   * Gets the {@link String abbreviation} for {@literal this} {@link TimeUnit}.
+   *
+   * @return the {@link String abbreviation} for {@literal this} {@link TimeUnit}.
+   * @see #getName()
+   */
   public String getAbbreviation() {
     return this.abbreviation;
   }
 
+  /**
+   * Gets a {@link String} describing {@literal this} {@link TimeUnit}.
+   *
+   * @return a {@link String} describing {@literal this} {@link TimeUnit}.
+   */
   public String getDescription() {
     return this.description;
   }
 
+  /**
+   * Gets the {@link String name} of {@literal this} {@link TimeUnit}.
+   *
+   * @return the {@link String name} of {@literal this} {@link TimeUnit}.
+   * @see #getAbbreviation()
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Return a {@link String} representation of {@literal this} {@link TimeUnit}.
+   *
+   * @return a {@link String} describing {@literal this} {@link TimeUnit}.
+   * @see java.lang.Object#toString()
+   * @see #getDescription()
+   */
   @Override
   public String toString() {
-    return getName();
+    return getDescription();
   }
 }
---
+++ src/test/java/org/cp/elements/enums/TimeUnitUnitTests.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link TimeUnit}.
+ *
+ * @author John J. Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.enums.TimeUnit
+ * @since 1.0.0
+ */
+public class TimeUnitUnitTests {
+
+  @Test
+  public void valueOfReturnsTimeUnit() {
+
+    Arrays.stream(TimeUnit.values()).forEach(timeUnit ->
+      assertThat(TimeUnit.valueOf(timeUnit.name())).isEqualTo(timeUnit));
+  }
+
+  @Test
+  public void valueOfAbbreviationIsCorrect() {
+
+    Arrays.stream(TimeUnit.values()).forEach(timeUnit ->
+      assertThat(TimeUnit.valueOfAbbreviation(timeUnit.getAbbreviation())).isEqualTo(timeUnit));
+  }
+
+  @Test
+  public void valueOfAbbreviationIsLenient() {
+
+    assertThat(TimeUnit.valueOfAbbreviation("ms")).isEqualTo(TimeUnit.MILLISECOND);
+    assertThat(TimeUnit.valueOfAbbreviation("Mi")).isEqualTo(TimeUnit.MINUTE);
+    assertThat(TimeUnit.valueOfAbbreviation("DAY")).isEqualTo(TimeUnit.DAY);
+  }
+
+  @Test
+  public void valueOfAbbreviationIsNullSafeReturnsNull() {
+    assertThat(TimeUnit.valueOfAbbreviation(null)).isNull();
+  }
+
+  @Test
+  public void valueOfAbbreviationUsingNameReturnsNull() {
+    assertThat(TimeUnit.valueOfAbbreviation("Week")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidAbbreviationReturnsNull() {
+
+    assertThat(TimeUnit.valueOfAbbreviation("")).isNull();
+    assertThat(TimeUnit.valueOfAbbreviation("  ")).isNull();
+    assertThat(TimeUnit.valueOfAbbreviation("our")).isNull();
+    assertThat(TimeUnit.valueOfAbbreviation("sent")).isNull();
+    assertThat(TimeUnit.valueOfAbbreviation("wknd")).isNull();
+  }
+
+  @Test
+  public void valueOfNameIsCorrect() {
+
+    Arrays.stream(TimeUnit.values()).forEach(timeUnit -> {
+      assertThat(TimeUnit.valueOfName(timeUnit.name())).isEqualTo(timeUnit);
+      assertThat(TimeUnit.valueOfName(timeUnit.getName())).isEqualTo(timeUnit);
+    });
+  }
+
+  @Test
+  public void valueOfNameIsLenient() {
+
+    assertThat(TimeUnit.valueOfName("MicroSecond")).isEqualTo(TimeUnit.MICROSECOND);
+    assertThat(TimeUnit.valueOfName("milliSecond")).isEqualTo(TimeUnit.MILLISECOND);
+    assertThat(TimeUnit.valueOfName("second")).isEqualTo(TimeUnit.SECOND);
+    assertThat(TimeUnit.valueOfName("Minute")).isEqualTo(TimeUnit.MINUTE);
+    assertThat(TimeUnit.valueOfName("hOUR")).isEqualTo(TimeUnit.HOUR);
+    assertThat(TimeUnit.valueOfName("DAY")).isEqualTo(TimeUnit.DAY);
+    assertThat(TimeUnit.valueOfName("yEAR")).isEqualTo(TimeUnit.YEAR);
+  }
+
+  @Test
+  public void valueOfNameIsNullSafeReturnsNull() {
+    assertThat(TimeUnit.valueOfName(null)).isNull();
+  }
+
+  @Test
+  public void valueOfNameUsingAbbreviationReturnsNull() {
+    assertThat(TimeUnit.valueOfName("us")).isNull();
+  }
+
+  @Test
+  public void valueOfInvalidNameReturnsNull() {
+
+    assertThat(TimeUnit.valueOfName("")).isNull();
+    assertThat(TimeUnit.valueOfName("  ")).isNull();
+    assertThat(TimeUnit.valueOfName("daily")).isNull();
+    assertThat(TimeUnit.valueOfName("weekend")).isNull();
+  }
+}
