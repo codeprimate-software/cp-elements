79aec1464b5b521b2bbfacdf5ccec4f05b7019d0
John Blum <jblum@vmware.com>
2022-04-15 13:09:46 -0700

    Define the new org.cp.elements.data.cache.CacheException class as a RuntimeException thrown during caching operations.

---
+++ src/main/java/org/cp/elements/data/caching/CacheException.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.cp.elements.data.caching;
+
+/**
+ * A {@link RuntimeException} implementation defining a general classification of exceptions for caching.
+ *
+ * @author John Blum
+ * @see java.lang.RuntimeException
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class CacheException extends RuntimeException {
+
+  /**
+   * Default constructor used to construct a new, uninitialized instance of {@link CacheException}
+   * with no {@link String message} or {@link Throwable cause}.
+   */
+  public CacheException() { }
+
+  /**
+   * Constructs a new instance of {@link CacheException} initialized with the given {@link String message}
+   * to describe the error.
+   *
+   * @param message {@link String} containing a {@literal  description} of this {@link CacheException}.
+   */
+  public CacheException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new instance of {@link CacheException} initialized with the given {@link Throwable cause}
+   * used as the underlying reason for this {@link CacheException}.
+   *
+   * @param cause {@link Throwable} used as the reason (cause) for this {@link CacheException}.
+   */
+  public CacheException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a new instance of {@link CacheException} initialized with the given {@link String message}
+   * to describe the error along with the given {@link Throwable cause} used as the underlying reason for
+   * this {@link CacheException}.
+   *
+   * @param message {@link String} containing a {@literal  description} of this {@link CacheException}.
+   * @param cause {@link Throwable} used as the reason (cause) for this {@link CacheException}.
+   */
+  public CacheException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
