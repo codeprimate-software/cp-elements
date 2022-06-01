bd92cc4fc335689b65339ecf6bf9b7fca6eaef0c
John Blum <jblum@vmware.com>
2022-05-31 23:17:00 -0700

    Define the @Transient annotation in the org.cp.elements.lang.annotation package.
    The @Transient annotation is used to annotate POJO field(s) or property (properties) as non-persistent / non-serializable.

---
+++ src/main/java/org/cp/elements/lang/annotation/Transient.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * {@link Annotation} used to annotate an {@link Object} (POJO) field or property as {@literal transient},
+ * indicating that the field or property is not persistent and not serializable.
+ *
+ * This {@link Transient} {@link Annotation} is a compliment to Java's {@literal transient} keyword.
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Annotation
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.Inherited
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.Target
+ * @since 1.0.0
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD })
+@SuppressWarnings("unused")
+public @interface Transient {
+
+}
