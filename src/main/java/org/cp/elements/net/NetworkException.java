e829b6abe159d26efc06ecc046e89cf9c1c6f86b
John Blum <jblum@vmware.com>
2022-07-05 12:19:17 -0700

    Create new org.cp.elements.net.NetworkException class to capture general network errors.

---
+++ src/main/java/org/cp/elements/net/NetworkException.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.net;
+
+/**
+ * {@link RuntimeException} to indicate that an error occurred in the network.
+ *
+ * @author John Blum
+ * @see java.lang.RuntimeException
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class NetworkException extends RuntimeException {
+
+  /**
+   * Constructs a new, uninitialized instance of {@link NetworkException} with no {@link String message}
+   * and no {@link Throwable cause}.
+   */
+  public NetworkException() { }
+
+  /**
+   * Constructs a new instance of {@link NetworkException} initialized with the given {@link String message}
+   * describing the network error.
+   *
+   * @param message {@link String} containing the message describing the network error.
+   */
+  public NetworkException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new instance of {@link NetworkException} initialized with the given {@link Throwable cause}
+   * used as the reason why this network error occurred.
+   *
+   * @param cause {@link Throwable} used as the reason why this network error occurred.
+   */
+  public NetworkException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a new instance of {@link NetworkException} initialized with the given {@link String message}
+   * describing the network error along with the given {@link Throwable cause} used as the reason
+   * why this network error occurred.
+   *
+   * @param message {@link String} containing the message describing the network error.
+   * @param cause {@link Throwable} used as the reason why this network error occurred.
+   */
+  public NetworkException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
