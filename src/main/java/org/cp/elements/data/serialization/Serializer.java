c16815e48d93652c969a3d232ef606c338caa08c
John Blum <jblum@vmware.com>
2022-08-17 14:12:00 -0700

    Define the org.cp.elements.data.serialization.Serializer interface for de/serializizing Objects.

---
+++ src/main/java/org/cp/elements/data/serialization/Serializer.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.serialization;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
+import org.cp.elements.nio.BufferUtils;
+
+/**
+ * Interface defining a contract for serializing and deserializing {@link Object Objects}
+ * to and from a sequence of bytes.
+ *
+ * @author John Blum
+ * @see java.io.Serializable
+ * @see java.nio.ByteBuffer
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public interface Serializer {
+
+  /**
+   * Determines whether the given {@link Object} is {@link #serialize(Object) serializable}, or rather, whether
+   * the given {@link Object} can be serialized into a sequence of bytes by this {@link Serializer}.
+   *
+   * The default implementation simply checks whether the given {@link Object} is an instance of {@link Serializable}.
+   * However, an {@link Object} does not necessarily need to be {@link Serializable} to be
+   * {@link #serialize(Object) serialized} by a specific {@link Serializer} implementation; serializability is entirely
+   * dependent on the {@link Serializer} implementation.
+   *
+   * It is assumed that if a {@link Serializer} can {@link #serialize(Object)} a given {@link Object} that it should
+   * also subsequently be capable of {@link #deserialize(ByteBuffer)}  deserializing} a byte sequence from
+   * the given {@link Object}.
+   *
+   * @param target {@link Object} to evaluate whether it can be serialized by this {@link Serializer}.
+   * @return a boolean value indicating whether the given {@link Object} can be {@link #serialize(Object) serialized}
+   * by this {@link Serializer}.
+   * @see java.io.Serializable
+   */
+  @NullSafe
+  default boolean isSerializable(@Nullable Object target) {
+    return target instanceof Serializable;
+  }
+
+  /**
+   * Serializes the given {@link Object} directly into a byte array.
+   *
+   * @param target {@link Object} to serialize.
+   * @return an array of bytes from serializing the given, target {@link Object}.
+   * @throws IllegalArgumentException if the {@link Object} to serialize is {@literal null}.
+   * @see java.nio.ByteBuffer#array()
+   * @see #serialize(Object)
+   */
+  @NotNull default byte[] toByteArray(@NotNull Object target) {
+
+    Assert.notNull(target, "Object to serialize is required");
+
+    ByteBuffer buffer = serialize(target);
+
+    return buffer.isReadOnly()
+      ? BufferUtils.getByteArray(buffer)
+      : buffer.array();
+  }
+
+  /**
+   * Serializes the given, target {@link Object} into a sequence of bytes encapsulated by
+   * the returned {@link ByteBuffer}.
+   *
+   * @param target {@link Object} to serialize.
+   * @return an instance of {@link ByteBuffer} encapsulating the sequence of bytes
+   * from serializing the target {@link Object}.
+   * @see java.nio.ByteBuffer
+   */
+  ByteBuffer serialize(Object target);
+
+  /**
+   * Deserializes the given sequence of bytes encapsulated by the given {@link ByteBuffer} into an {@link Object}
+   * of the declared {@link T type}.
+   *
+   * @param <T> {@link Class type} of {@link Object} reconstructed from the sequence of bytes.
+   * @param bytes {@link ByteBuffer} encapsulating the sequence of bytes from
+   * the {@link #serialize(Object) serialized} {@link Object}.
+   * @return the deserialized {@link Object} of the given {@link T type}.
+   * @see java.nio.ByteBuffer
+   */
+  <T> T deserialize(ByteBuffer bytes);
+
+}
---
+++ src/test/java/org/cp/elements/data/serialization/SerializerUnitTests.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.serialization;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link Serializer}.
+ *
+ * @author John Blum
+ * @see java.io.Serializable
+ * @see java.nio.ByteBuffer
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.data.serialization.Serializer
+ * @since 1.0.0
+ */
+public class SerializerUnitTests {
+
+  @Test
+  public void isSerializableWithNonSerializableObject() {
+
+    Object target = spy(new Object());
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).isSerializable(any());
+
+    assertThat(serializer.isSerializable(target)).isFalse();
+
+    verify(serializer, times(1)).isSerializable(eq(target));
+    verifyNoMoreInteractions(serializer);
+    verifyNoInteractions(target);
+  }
+
+  @Test
+  public void isSerializableWithNullIsNullSafe() {
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).isSerializable(any());
+
+    assertThat(serializer.isSerializable(null)).isFalse();
+
+    verify(serializer, times(1)).isSerializable(isNull());
+    verifyNoMoreInteractions(serializer);
+  }
+
+  @Test
+  public void isSerializableWithSerializableObject() {
+
+    Serializable mockSerializable = mock(Serializable.class);
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).isSerializable(any());
+
+    assertThat(serializer.isSerializable(mockSerializable)).isTrue();
+
+    verify(serializer, times(1)).isSerializable(eq(mockSerializable));
+    verifyNoMoreInteractions(serializer);
+    verifyNoInteractions(mockSerializable);
+  }
+
+  @Test
+  public void toByteArrayWithSerializableObjectIsSuccessful() {
+
+    byte[] testBytes = "TEST".getBytes();
+
+    ByteBuffer buffer = ByteBuffer.wrap(testBytes);
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).toByteArray(any());
+    doReturn(buffer).when(serializer).serialize(eq("TEST"));
+
+    byte[] serializedBytes = serializer.toByteArray("TEST");
+
+    assertThat(serializedBytes).isNotNull();
+    assertThat(serializedBytes).isEqualTo(testBytes);
+    assertThat(new String(testBytes)).isEqualTo("TEST");
+
+    verify(serializer, times(1)).toByteArray(eq("TEST"));
+    verify(serializer, times(1)).serialize(eq("TEST"));
+    verifyNoMoreInteractions(serializer);
+  }
+
+  @Test
+  public void toByteArrayWithSerializableObjectReturningReadOnlyByteBufferIsSuccessful() {
+
+    byte[] testBytes = "MOCK".getBytes();
+
+    ByteBuffer buffer = ByteBuffer.wrap(testBytes).asReadOnlyBuffer();
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).toByteArray(any());
+    doReturn(buffer).when(serializer).serialize(eq("MOCK"));
+
+    byte[] serializedBytes = serializer.toByteArray("MOCK");
+
+    assertThat(serializedBytes).isNotNull();
+    assertThat(serializedBytes).isEqualTo(testBytes);
+    assertThat(new String(testBytes)).isEqualTo("MOCK");
+
+    verify(serializer, times(1)).toByteArray(eq("MOCK"));
+    verify(serializer, times(1)).serialize(eq("MOCK"));
+    verifyNoMoreInteractions(serializer);
+  }
+
+  @Test
+  public void toByteArrayWithNullTargetObjectThrowsIllegalArgumentException() {
+
+    Serializer serializer = mock(Serializer.class);
+
+    doCallRealMethod().when(serializer).toByteArray(any());
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> serializer.toByteArray(null))
+      .withMessage("Object to serialize is required")
+      .withNoCause();
+
+    verify(serializer, times(1)).toByteArray(isNull());
+    verifyNoMoreInteractions(serializer);
+  }
+}
