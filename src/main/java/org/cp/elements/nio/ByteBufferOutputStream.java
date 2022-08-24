587cc2204e7f3a0489db34b0fb950b30e01c5f21
John Blum <jblum@vmware.com>
2022-08-24 11:49:05 -0700

    Create the ByteBufferOutputStream class in the org.cp.elements.nio package implemented as a java.io.OutputStream writing to a java.nio.ByteBuffer.

---
+++ src/main/java/org/cp/elements/nio/ByteBufferOutputStream.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.nio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * An {@link OutputStream} implementation that writes to a Java {@link ByteBuffer}.
+ *
+ * @author John Blum
+ * @see java.io.OutputStream
+ * @see java.nio.ByteBuffer
+ * @since 1.0.0
+ */
+public class ByteBufferOutputStream extends OutputStream {
+
+  protected static final int CAPACITY_INCREMENT = 8192;
+
+  protected static final float LOAD_FACTOR = 0.85f;
+
+  /**
+   * Factory method used to construct a new instance of {@link ByteBufferOutputStream} to write data into the given,
+   * required {@link ByteBuffer}.
+   *
+   * @param byteBuffer {@link ByteBuffer} into which this {@link OutputStream} will write data;
+   * must not be {@literal null} and must be {@link ByteBuffer#isReadOnly() writable}.
+   * @throws IllegalArgumentException if the {@link ByteBuffer} is {@literal null}.
+   * @throws IllegalStateException if the {@link ByteBuffer} is {@link ByteBuffer#isReadOnly() read-only}.
+   * @see #ByteBufferOutputStream(ByteBuffer)
+   * @see java.nio.ByteBuffer
+   */
+  public static @NotNull ByteBufferOutputStream into(@NotNull ByteBuffer byteBuffer) {
+    return new ByteBufferOutputStream(byteBuffer);
+  }
+
+  private volatile boolean closed = false;
+
+  private ByteBuffer byteBuffer;
+
+  /**
+   * Constructs a new instance of {@link ByteBufferOutputStream} initialized with
+   * the given, required {@link ByteBuffer}.
+   *
+   * @param byteBuffer {@link ByteBuffer} into which this {@link OutputStream} will write data;
+   * must not be {@literal null} and must be {@link ByteBuffer#isReadOnly() writable}.
+   * @throws IllegalArgumentException if the {@link ByteBuffer} is {@literal null}.
+   * @throws IllegalStateException if the {@link ByteBuffer} is {@link ByteBuffer#isReadOnly() read-only}.
+   * @see java.nio.ByteBuffer
+   */
+  public ByteBufferOutputStream(@NotNull ByteBuffer byteBuffer) {
+    setByteBuffer(byteBuffer);
+  }
+
+  /**
+   * Configures the {@link ByteBuffer} used by this {@link OutputStream} to write data into.
+   *
+   * @param byteBuffer {@link ByteBuffer} into which this {@link OutputStream} will write data;
+   * must not be {@literal null} and must be {@link ByteBuffer#isReadOnly() writable}.
+   * @throws IllegalArgumentException if the {@link ByteBuffer} is {@literal null}.
+   * @throws IllegalStateException if the {@link ByteBuffer} is {@link ByteBuffer#isReadOnly() read-only}.
+   * @see java.nio.ByteBuffer
+   */
+  private void setByteBuffer(@NotNull ByteBuffer byteBuffer) {
+
+    Assert.notNull(byteBuffer, "ByteBuffer is required");
+    Assert.state(!byteBuffer.isReadOnly(), "ByteBuffer must not be read-only");
+
+    this.byteBuffer = byteBuffer;
+  }
+
+  /**
+   * Closes the {@link OutputStream} and returns the backing {@link ByteBuffer} into which this {@link OutputStream}
+   * wrote data.
+   *
+   * @return the backing {@link ByteBuffer}.
+   * @see java.nio.ByteBuffer
+   * @see #close()
+   */
+  public @NotNull ByteBuffer getByteBuffer() {
+    close();
+    return this.byteBuffer;
+  }
+
+  /**
+   * Determines whether this {@link OutputStream} has been closed.
+   *
+   * @return a boolean value indicating whether this {@link OutputStream} has been closed.
+   * @see #isOpen()
+   */
+  public boolean isClosed() {
+    return this.closed;
+  }
+
+  /**
+   * Determines whether this {@link OutputStream} has not been closed yet.
+   *
+   * @return a boolean value indicating whether this {@link OutputStream} has not been closed yet.
+   * @see #isClosed()
+   */
+  public boolean isOpen() {
+    return !isClosed();
+  }
+
+  /**
+   * Asserts that this {@link OutputStream} has not been closed yet.
+   *
+   * @throws IllegalStateException if this {@link OutputStream} has been closed.
+   * @see #isOpen()
+   * @see #isClosed()
+   */
+  protected void assertNotClosed() {
+    Assert.state(isOpen(), "The ByteBufferOutputStream was closed");
+  }
+
+  /**
+   * Closes this {@link OutputStream} preventing any further I/O operations (e.g. {@literal writes})
+   * from occurring on the backing {@link ByteBuffer}.
+   */
+  @Override
+  public void close() {
+    this.closed = true;
+  }
+
+  protected byte convertToByte(int value) {
+    return (byte) (value & 0x000000FF);
+  }
+
+  protected void reallocateBuffer() {
+
+    assertNotClosed();
+
+    ByteBuffer byteBuffer = this.byteBuffer;
+
+    if (BufferUtils.computeLoadFactor(byteBuffer) > LOAD_FACTOR) {
+      setByteBuffer(BufferUtils.copy(byteBuffer, CAPACITY_INCREMENT));
+    }
+  }
+
+  @Override
+  public void write(int byteToWrite) throws IOException {
+
+    assertNotClosed();
+    reallocateBuffer();
+    this.byteBuffer.put(convertToByte(byteToWrite));
+  }
+}
---
+++ src/test/java/org/cp/elements/nio/ByteBufferOutputStreamUnitTests.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.nio;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.cp.elements.util.ArrayUtils;
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link ByteBufferOutputStream}.
+ *
+ * @author John Blum
+ * @see java.io.OutputStream
+ * @see java.nio.ByteBuffer
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.nio.ByteBufferOutputStream
+ * @since 1.0.0
+ */
+public class ByteBufferOutputStreamUnitTests {
+
+  @Test
+  public void constructNewByteBufferOutputStreamWithNonNullByteBuffer() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = new ByteBufferOutputStream(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void constructNewByteBufferOutputStreamWithNullByteBuffer() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> new ByteBufferOutputStream(null))
+      .withMessage("ByteBuffer is required")
+      .withNoCause();
+  }
+
+  @Test
+  public void constructNewByteBufferOutputStreamWithReadOnlyByteBuffer() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(true).when(mockByteBuffer).isReadOnly();
+
+    assertThatIllegalStateException()
+      .isThrownBy(() -> new ByteBufferOutputStream(mockByteBuffer))
+      .withMessage("ByteBuffer must not be read-only")
+      .withNoCause();
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void intoIsCorrect() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void closeClosedTheOutputStream() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.isClosed()).isFalse();
+    assertThat(outputStream.isOpen()).isTrue();
+
+    outputStream.close();
+
+    assertThat(outputStream.isClosed()).isTrue();
+    assertThat(outputStream.isOpen()).isFalse();
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void closePreventsOutputStreamWriteOperations() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+
+    outputStream.close();
+
+    assertThat(outputStream.isClosed()).isTrue();
+    assertThat(outputStream.isOpen()).isFalse();
+
+    assertThatIllegalStateException()
+      .isThrownBy(() -> outputStream.write(0x0A))
+      .withMessage("The ByteBufferOutputStream was closed")
+      .withNoCause();
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void gettingByteBufferClosesOutputStream() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.isClosed()).isFalse();
+    assertThat(outputStream.isOpen()).isTrue();
+    assertThat(outputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+    assertThat(outputStream.isClosed()).isTrue();
+    assertThat(outputStream.isOpen()).isFalse();
+
+    verify(mockByteBuffer, times(1)).isReadOnly();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void convertToByteIsCorrect() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(false).when(mockByteBuffer).isReadOnly();
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(mockByteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.convertToByte(0x00)).isEqualTo(Integer.valueOf(0x00).byteValue());
+    assertThat(outputStream.convertToByte(0x01)).isEqualTo(Integer.valueOf(0x01).byteValue());
+    assertThat(outputStream.convertToByte(0x08)).isEqualTo(Integer.valueOf(0x08).byteValue());
+    assertThat(outputStream.convertToByte(0xFF)).isEqualTo(Integer.valueOf(0xFF).byteValue());
+    assertThat(outputStream.convertToByte(0xB00BFAB)).isEqualTo(Integer.valueOf(0xAB).byteValue());
+    assertThat(outputStream.convertToByte(0xB00BCAFE)).isEqualTo(Integer.valueOf(0xFE).byteValue());
+  }
+
+  @Test
+  public void writeReallocatesByteBuffer() throws IOException {
+
+    byte[] array = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+
+    ByteBuffer byteBuffer = ByteBuffer.allocate(2);
+
+    assertThat(byteBuffer).isNotNull();
+    assertThat(byteBuffer.isDirect()).isFalse();
+    assertThat(byteBuffer.isReadOnly()).isFalse();
+    assertThat(byteBuffer.capacity()).isEqualTo(2);
+
+    ByteBufferOutputStream outputStream = ByteBufferOutputStream.into(byteBuffer);
+
+    assertThat(outputStream).isNotNull();
+    assertThat(outputStream.isOpen()).isTrue();
+
+    outputStream.write(array);
+
+    ByteBuffer buffer = outputStream.getByteBuffer();
+
+    assertThat(buffer).isNotNull();
+    assertThat(buffer).isNotSameAs(byteBuffer);
+    assertThat(buffer.isDirect()).isFalse();
+    assertThat(buffer.isReadOnly()).isFalse();
+    assertThat(buffer.capacity()).isGreaterThanOrEqualTo(array.length);
+    assertThat(outputStream.isClosed()).isTrue();
+
+    byte[] copy = buffer.array();
+
+    assertThat(ArrayUtils.subArray(BufferUtils.toBigByteArray(copy), 2, 15))
+      .containsExactly(BufferUtils.toBigByteArray(array));
+  }
+}
