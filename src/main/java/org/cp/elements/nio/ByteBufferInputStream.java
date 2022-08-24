7e211b8d20b234179ec5828ae6a7d2e1fbbdac9d
John Blum <jblum@vmware.com>
2022-08-24 11:47:52 -0700

    Create the ByteBufferInputStream class in the org.cp.elements.nio package implemented as a java.io.InputStream reading from a java.nio.ByteBuffer.

---
+++ src/main/java/org/cp/elements/nio/ByteBufferInputStream.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.nio;
+
+import static org.cp.elements.lang.LangExtensions.assertThat;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIndexOutOfBoundsException;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newNullPointerException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * An {@link InputStream} implementation that reads from a Java {@link ByteBuffer}.
+ *
+ * @author John Blum
+ * @see java.io.InputStream
+ * @see java.nio.ByteBuffer
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class ByteBufferInputStream extends InputStream {
+
+  /**
+   * Factory method used to construct a new instance of {@link ByteBufferInputStream} backed by
+   * the given, required {@link ByteBuffer}.
+   *
+   * @param byteBuffer {@link ByteBuffer} used to back the {@link InputStream}; must not be {@literal null}.
+   * @return a new {@link ByteBufferInputStream} initialized with the given, required {@link ByteBuffer}.
+   * @throws IllegalArgumentException if the {@link ByteBuffer} is {@literal null}.
+   * @see #ByteBufferInputStream(ByteBuffer)
+   * @see java.nio.ByteBuffer
+   */
+  public static @NotNull ByteBufferInputStream from(@NotNull ByteBuffer byteBuffer) {
+    return new ByteBufferInputStream(byteBuffer);
+  }
+
+  private final ByteBuffer byteBuffer;
+
+  /**
+   * Constructs a new instance of {@link ByteBufferInputStream} backed by the given, required {@link ByteBuffer}.
+   *
+   * @param byteBuffer {@link ByteBuffer} used to back the {@link InputStream}; must not be {@literal null}.
+   * @throws IllegalArgumentException if the {@link ByteBuffer} is {@literal null}.
+   * @see java.nio.ByteBuffer
+   */
+  public ByteBufferInputStream(@NotNull ByteBuffer byteBuffer) {
+    this.byteBuffer = ObjectUtils.requireObject(byteBuffer, "ByteBuffer is required");
+    this.byteBuffer.mark();
+  }
+
+  /**
+   * Gets the {@link ByteBuffer} used to back this {@link InputStream}.
+   *
+   * @return the {@link ByteBuffer} used to back this {@link InputStream}.
+   * @see java.nio.ByteBuffer
+   */
+  protected @NotNull ByteBuffer getByteBuffer() {
+    return this.byteBuffer;
+  }
+
+  /**
+   * Determines the number of bytes still available to be read from the backing {@link ByteBuffer}
+   * using this {@link InputStream}.
+   *
+   * @return the number of bytes still available to be read.
+   * @see java.nio.ByteBuffer#remaining()
+   */
+  @Override
+  public int available() {
+    return getByteBuffer().remaining();
+  }
+
+  /**
+   * Marks this {@link InputStream} to the current {@link ByteBuffer#position()}.
+   *
+   * @param readLimit ignored. This {@link InputStream} is backed by a {@link ByteBuffer} and therefore can recall
+   * any number of bytes read before {@link #reset()} is called and the {@code readLimit} is reached.
+   * @see java.nio.ByteBuffer#mark()
+   */
+  @Override
+  public synchronized void mark(int readLimit) {
+    getByteBuffer().mark();
+  }
+
+  /**
+   * Return {@literal true} to indicate {@link #mark(int)} is supported.
+   *
+   * @return {@literal true}.
+   */
+  @Override
+  public boolean markSupported() {
+    return true;
+  }
+
+  /**
+   * Read a single byte from the {@link ByteBuffer} using this {@link InputStream}.
+   *
+   * @return an {@link Integer} value containing the byte read from the backing {@link ByteBuffer}.
+   * @throws IOException if an error occurs reading from the backing {@link ByteBuffer}.
+   */
+  @Override
+  public int read() throws IOException {
+    return available() > 0 ? getByteBuffer().get() : -1;
+  }
+
+  /**
+   * Reads at most {@code length} bytes from the backing {@link ByteBuffer} using this {@link InputStream}
+   * into the given, required byte array starting at the given offset.
+   *
+   * @param buffer byte array written from the bytes read from this {@link InputStream}; must not be {@literal null}.
+   * @param offset {@link Integer} referring to the index in the byte array to start writing bytes.
+   * @param length {@link Integer} specifying the number of bytes to write into the byte array
+   * from this {@link InputStream}.
+   * @return an {@link Integer} indicating the number of bytes read from this {@link InputStream} into the byte array.
+   * Returns {@literal 0} if the length of the byte array is {@literal 0}. Returns {@literal -1} if there are no
+   * {@link #available()} bytes in this {@link InputStream}.
+   * @throws NullPointerException if the byte array is {@literal null}.
+   * @throws IndexOutOfBoundsException if {@code offset} is less than {@literal 0} or greater than equal to
+   * the byte array length, or the length is less than {@literal 0} or greater than
+   * the byte array length - {@code offset}.
+   */
+  @Override
+  public int read(@NotNull byte[] buffer, int offset, int length) throws IOException {
+
+    Assert.notNull(buffer, newNullPointerException("Byte array cannot be null"));
+
+    if (buffer.length > 0) {
+
+      assertThat(offset)
+        .throwing(newIndexOutOfBoundsException("Offset [%d] must be greater than equal to 0 and less than [%d]",
+          offset, buffer.length))
+        .isGreaterThanEqualToAndLessThan(0, buffer.length);
+
+      assertThat(length)
+        .throwing(newIndexOutOfBoundsException("Length [%d] must be greater than equal to 0 and less than equal to [%d]",
+          length, buffer.length - offset))
+        .isGreaterThanEqualToAndLessThanEqualTo(0, buffer.length - offset);
+
+      int numberOfBytesToRead = Math.min(available(), length);
+
+      if (numberOfBytesToRead > 0) {
+        getByteBuffer().get(buffer, offset, numberOfBytesToRead);
+        return numberOfBytesToRead;
+      }
+
+      return -1;
+    }
+
+    return 0;
+  }
+
+  /**
+   * Resets the underlying, backing {@link ByteBuffer} to the last {@link ByteBuffer#mark() marked position}.
+   *
+   * @see java.nio.ByteBuffer#reset()
+   */
+  @Override
+  public synchronized void reset() {
+    getByteBuffer().reset();
+  }
+
+  /**
+   * Skips the given number of bytes read from this {@link InputStream}.
+   *
+   * Effectively moves the {@link ByteBuffer#position()} ahead by the number of available or given number of bytes,
+   * which ever is smaller.
+   *
+   * @param numberOfBytes {@link Long} specifying the number of bytes to skip in this {@link InputStream}.
+   * @return an {@link Integer} indicating the actual number of bytes skipped.
+   * @see java.nio.ByteBuffer#position(int)
+   * @see #available()
+   */
+  @Override
+  public long skip(long numberOfBytes) {
+
+    if (numberOfBytes > 0) {
+      int actualNumberOfBytes = (int) Math.min(available(), numberOfBytes);
+      ByteBuffer byteBuffer = getByteBuffer();
+      byteBuffer.position(byteBuffer.position() + actualNumberOfBytes);
+      return actualNumberOfBytes;
+    }
+
+    return 0;
+  }
+}
---
+++ src/test/java/org/cp/elements/nio/ByteBufferInputStreamUnitTests.java
@@ -0,0 +1,370 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.nio;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.cp.elements.lang.ThrowableAssertions.assertThatIndexOutOfBoundsException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link ByteBufferInputStream}.
+ *
+ * @author John Blum
+ * @see java.io.InputStream
+ * @see java.nio.ByteBuffer
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.nio.ByteBufferInputStream
+ * @since 1.0.0
+ */
+public class ByteBufferInputStreamUnitTests {
+
+  @Test
+  public void constructNewByteBufferInputStreamWithNonNullByteBuffer() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    ByteBufferInputStream inputStream = new ByteBufferInputStream(mockByteBuffer);
+
+    assertThat(inputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+
+    verify(mockByteBuffer, times(1)).mark();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void constructNewByteBufferInputStreamWithNullByteBuffer() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> new ByteBufferInputStream(null))
+      .withMessage("ByteBuffer is required")
+      .withNoCause();
+  }
+
+  @Test
+  public void fromByteBufferIsSuccessful() {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(mockByteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+
+    verify(mockByteBuffer, times(1)).mark();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void availableIsCorrect() throws IOException {
+
+    byte[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(array);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+    assertThat(inputStream.available()).isEqualTo(array.length);
+
+    int count = 0;
+
+    while (inputStream.read() != -1) {
+      assertThat(inputStream.available()).isEqualTo(array.length - ++count);
+    }
+
+    assertThat(count).isEqualTo(array.length);
+    assertThat(inputStream.available()).isZero();
+  }
+
+  @Test
+  public void markIsCorrect() throws IOException {
+
+    byte[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(array);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+    assertThat(inputStream.available()).isEqualTo(array.length);
+
+    int count = 0;
+
+    for (byte data : array) {
+      assertThat(inputStream.read()).isEqualTo(data);
+      if (++count == 3) {
+        inputStream.mark(0);
+      }
+    }
+
+    assertThat(count).isEqualTo(array.length);
+    assertThat(inputStream.read()).isEqualTo(-1);
+    assertThat(inputStream.available()).isZero();
+
+    inputStream.reset();
+
+    assertThat(inputStream.available()).isEqualTo(6);
+
+    count = 0;
+
+    for (int index = 3; index < array.length; index++) {
+      assertThat(inputStream.read()).isEqualTo(array[index]);
+      if (++count == 3) {
+        inputStream.mark(10);
+      }
+    }
+
+    assertThat(count).isEqualTo(array.length - 3);
+    assertThat(inputStream.read()).isEqualTo(-1);
+    assertThat(inputStream.available()).isZero();
+
+    inputStream.reset();
+
+    assertThat(inputStream.available()).isEqualTo(3);
+    assertThat(inputStream.read()).isEqualTo(7);
+  }
+
+  @Test
+  public void markSupportedReturnsTrue() {
+    assertThat(ByteBufferInputStream.from(mock(ByteBuffer.class)).markSupported()).isTrue();
+  }
+
+  @Test
+  public void readsAllBytesFromByteBuffer() throws IOException {
+
+    String data = "This is data for the ByteBufferInputStream.read() test case!";
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(data.getBytes());
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+
+    byte[] buffer = new byte[byteBuffer.capacity()];
+
+    for (int index = 0, byteValue = inputStream.read(); byteValue != -1; index++, byteValue = inputStream.read()) {
+      buffer[index] = (byte) byteValue;
+    }
+
+    assertThat(new String(buffer)).isEqualTo(data);
+    assertThat(inputStream.read()).isEqualTo(-1);
+  }
+
+  @Test
+  public void readIntoByteArrayWihtAvailableBytesReturnsActualNumberOfBytesRead() throws IOException {
+
+    byte[] array = { 1, 2, 3 };
+    byte[] buffer = new byte[array.length * 10];
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(array);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+    assertThat(inputStream.available()).isEqualTo(3);
+    assertThat(inputStream.read(buffer)).isEqualTo(3);
+    assertThat(inputStream.available()).isZero();
+
+    for (int index = 0; index < buffer.length; index++) {
+      if (index < array.length) {
+        assertThat(buffer[index]).isEqualTo(array[index]);
+      }
+      else {
+        assertThat(buffer[index]).isZero();
+      }
+    }
+  }
+
+  @Test
+  public void readIntoByteArrayWithAvailableBytesReturnsGivenLengthBytesRead() throws IOException {
+
+    byte[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF };
+    byte[] buffer = new byte[data.length * 2];
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(data);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+    assertThat(inputStream.read(buffer, 5, 5)).isEqualTo(5);
+
+    int dataIndex = 0;
+
+    for (int index = 0; index < buffer.length; index++) {
+      if (index < 5 || index > 9) {
+        assertThat(buffer[index]).isZero();
+      }
+      else {
+        assertThat(buffer[index]).isEqualTo(data[dataIndex++]);
+      }
+    }
+  }
+
+  @Test
+  public void readIntoByteArrayWithUnavailableBytesReturnsMinusOne() throws IOException {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    doReturn(0).when(mockByteBuffer).remaining();
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(mockByteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+    assertThat(inputStream.read(new byte[10], 0, 5)).isEqualTo(-1);
+
+    verify(mockByteBuffer, times(1)).mark();
+    verify(mockByteBuffer, times(1)).remaining();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void readIntoZeroLengthByteArrayReturnsZero() throws IOException {
+
+    ByteBuffer mockByteBuffer = mock(ByteBuffer.class);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(mockByteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(mockByteBuffer);
+    assertThat(inputStream.read(new byte[0], 0, 10)).isZero();
+
+    verify(mockByteBuffer, times(1)).mark();
+    verifyNoMoreInteractions(mockByteBuffer);
+  }
+
+  @Test
+  public void readIntoByteArrayWithInvalidOffset() {
+
+    assertThatIndexOutOfBoundsException()
+      .isThrownBy(args -> ByteBufferInputStream.from(mock(ByteBuffer.class)).read(new byte[5], 7, 5))
+      .havingMessage("Offset [7] must be greater than equal to 0 and less than [5]")
+      .withNoCause();
+  }
+
+  @Test
+  public void readIntoByteArrayWithNegativeOffset() {
+
+    assertThatIndexOutOfBoundsException()
+      .isThrownBy(args -> ByteBufferInputStream.from(mock(ByteBuffer.class)).read(new byte[10], -5, 5))
+      .havingMessage("Offset [-5] must be greater than equal to 0 and less than [10]")
+      .withNoCause();
+  }
+
+  @Test
+  public void readIntoByteArrayWithInvalidLength() {
+
+    assertThatIndexOutOfBoundsException()
+      .isThrownBy(args -> ByteBufferInputStream.from(mock(ByteBuffer.class)).read(new byte[10], 6, 5))
+      .havingMessage("Length [5] must be greater than equal to 0 and less than equal to [4]")
+      .withNoCause();
+  }
+
+  @Test
+  public void readIntoByteArrayWithNegativeLength() {
+
+    assertThatIndexOutOfBoundsException()
+      .isThrownBy(args -> ByteBufferInputStream.from(mock(ByteBuffer.class)).read(new byte[10], 5, -5))
+      .havingMessage("Length [-5] must be greater than equal to 0 and less than equal to [5]")
+      .withNoCause();
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void readIntoNullByteArrayThrowsNullPointerException() {
+
+    assertThatNullPointerException()
+      .isThrownBy(() -> ByteBufferInputStream.from(mock(ByteBuffer.class)).read(null, 0, 10))
+      .withMessage("Byte array cannot be null")
+      .withNoCause();
+  }
+
+  @Test
+  public void resetIsCorrect() throws IOException {
+
+    byte[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA };
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(array);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+
+    inputStream.reset();
+
+    for (int index = 0; index < 5; index++) {
+      assertThat(inputStream.read()).isEqualTo(array[index]);
+    }
+
+    assertThat(inputStream.read()).isEqualTo(array[5]);
+
+    inputStream.reset();
+
+    for (byte data : array) {
+      assertThat(inputStream.read()).isEqualTo(data);
+    }
+
+    assertThat(inputStream.available()).isZero();
+
+    inputStream.reset();
+
+    assertThat(inputStream.available()).isEqualTo(array.length);
+  }
+
+  @Test
+  public void skipIsCorrect() throws IOException {
+
+    byte[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA };
+
+    ByteBuffer byteBuffer = ByteBuffer.wrap(array);
+
+    ByteBufferInputStream inputStream = ByteBufferInputStream.from(byteBuffer);
+
+    assertThat(inputStream).isNotNull();
+    assertThat(inputStream.getByteBuffer()).isEqualTo(byteBuffer);
+    assertThat(inputStream.skip(3)).isEqualTo(3);
+
+    for (int index = 3; index < 6; index++) {
+      assertThat(inputStream.read()).isEqualTo(array[index]);
+    }
+
+    assertThat(inputStream.available()).isEqualTo(4);
+    assertThat(inputStream.skip(1)).isEqualTo(1);
+    assertThat(inputStream.available()).isEqualTo(3);
+    assertThat(inputStream.read()).isEqualTo(8);
+    assertThat(inputStream.available()).isEqualTo(2);
+    assertThat(inputStream.skip(3)).isEqualTo(2);
+    assertThat(inputStream.available()).isZero();
+  }
+}
