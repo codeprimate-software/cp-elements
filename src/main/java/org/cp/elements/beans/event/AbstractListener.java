445965a5807804fa2c22fb93307c80bc67042d6e
John Blum <jblum@vmware.com>
2022-09-06 10:50:19 -0700

    Define the AbstractListener class in the org.cp.elements.beans.event package as a abstract base class and EventListener implementation for all JavaBean/POJO PropertyChangeEvent based listeners.

---
+++ src/main/java/org/cp/elements/beans/event/AbstractListener.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.beans.event;
+
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EventListener;
+import java.util.Iterator;
+import java.util.List;
+
+import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
+import org.cp.elements.util.ArrayUtils;
+import org.cp.elements.util.CollectionUtils;
+
+/**
+ * Abstract base class and {@link EventListener} implementation used to listen for JavaBean, {@literal POJO}
+ * {@link PropertyChangeEvent property change events}.
+ *
+ * @author John Blum
+ * @see java.beans.PropertyChangeEvent
+ * @see java.lang.Iterable
+ * @see java.util.EventListener
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public abstract class AbstractListener implements EventListener, Iterable<String> {
+
+  private final List<String> propertyNames;
+
+  /**
+   * Constructs a new instance of {@link AbstractListener} initialized with the array of {@link String property names}.
+   *
+   * @param propertyNames array of {@link String property names} for which this listener is interested and processes
+   * {@link PropertyChangeEvent property change events}.
+   * @see #AbstractListener(Iterable)
+   */
+  public AbstractListener(String... propertyNames) {
+    this(Arrays.asList(ArrayUtils.nullSafeArray(propertyNames, String.class)));
+  }
+
+  /**
+   * Constructs a new instance of {@link AbstractListener} initialized with the {@link Iterable}
+   * of {@link String property names}.
+   *
+   * @param propertyNames {@link Iterable} of {@link String property names} for which this listener
+   * is interested and processes {@link PropertyChangeEvent property change events}.
+   * @see java.lang.Iterable
+   */
+  public AbstractListener(Iterable<String> propertyNames) {
+    this.propertyNames = Collections.unmodifiableList(CollectionUtils.addAll(new ArrayList<>(),
+      CollectionUtils.nullSafeIterable(propertyNames)));
+  }
+
+  /**
+   * Gets the {@link List} of {@link String property names} processed (handled) by this listener.
+   *
+   * @return the {@link List} of {@link String property names} processed (handled) by this listener.
+   * @see java.util.List
+   */
+  protected List<String> getPropertyNames() {
+    return this.propertyNames;
+  }
+
+  /**
+   * Iterates over the {@link String property names} of this listener.
+   *
+   * @return an {@link Iterator} over the {@link String property names} of this listener.
+   * @see #getPropertyNames()
+   * @see java.util.Iterator
+   */
+  @Override
+  public Iterator<String> iterator() {
+    return getPropertyNames().iterator();
+  }
+
+  /**
+   * Processes (handles) the {@link PropertyChangeEvent} if this listener is interested in the property
+   * that is the source of the given {@link PropertyChangeEvent}.
+   *
+   * @param event {@link PropertyChangeEvent} to process.
+   * @see #canHandle(PropertyChangeEvent)
+   * @see java.beans.PropertyChangeEvent
+   * @see #doHandle(PropertyChangeEvent)
+   */
+  @NullSafe
+  protected void handle(@NotNull PropertyChangeEvent event) {
+
+    if (canHandle(event)) {
+      doHandle(event);
+    }
+  }
+
+  /**
+   * Determines whether this listener is interested in the JavaBean/POJO property
+   * that is the source of the given {@link PropertyChangeEvent}.
+   *
+   * @param event {@link PropertyChangeEvent} to evaluate.
+   * @return a boolean value to determine whether this listener is interested in the JavaBean/POJO property
+   * that is the source of the given {@link PropertyChangeEvent}.
+   * @see #canHandle(String)
+   * @see java.beans.PropertyChangeEvent
+   */
+  @NullSafe
+  protected boolean canHandle(@Nullable PropertyChangeEvent event) {
+    return event != null && canHandle(event.getPropertyName());
+  }
+
+  /**
+   * Determines whether this listener is interested in the {@link String named} JavaBean/POJO property.
+   *
+   * @param propertyName {@link String} containing the {@literal name} of the property.
+   * @return a boolean value indicating whether this listener is interested in the {@link String named}
+   * JavaBean/POJO property.
+   * @see #getPropertyNames()
+   */
+  @NullSafe
+  protected boolean canHandle(@Nullable String propertyName) {
+    return StringUtils.hasText(propertyName) && getPropertyNames().contains(propertyName);
+  }
+
+  /**
+   * Processes (handles) the given {@link PropertyChangeEvent}.
+   *
+   * @param event {@link PropertyChangeEvent} to process; never {@literal null}.
+   * @see java.beans.PropertyChangeEvent
+   */
+  protected abstract void doHandle(@NotNull PropertyChangeEvent event);
+
+}
---
+++ src/test/java/org/cp/elements/beans/event/AbstractListenerUnitTests.java
@@ -0,0 +1,265 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.beans.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.beans.PropertyChangeEvent;
+import java.util.Arrays;
+import java.util.List;
+
+import org.cp.elements.lang.Constants;
+import org.cp.elements.lang.StringUtils;
+
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+
+/**
+ * Unit Tests for {@link AbstractListener}.
+ *
+ * @author John Blum
+ * @see java.beans.PropertyChangeEvent
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.beans.event.AbstractListener
+ * @since 1.0.0
+ */
+public class AbstractListenerUnitTests {
+
+  @Test
+  public void constructsListenerWithArrayOfPropertyNames() {
+
+    String[] propertyNames = { "propertyOne", "propertyTwo" };
+
+    AbstractListener listener = new TestListener(propertyNames);
+
+    assertThat(listener).isNotNull();
+    assertThat(listener).containsExactly(propertyNames);
+  }
+
+  @Test
+  public void constructsListenerWithIterableOfPropertyNames() {
+
+    List<String> propertyNames = Arrays.asList("propertyOne", "propertyTwo");
+
+    AbstractListener listener = new TestListener(propertyNames);
+
+    assertThat(listener).isNotNull();
+    assertThat(listener).containsExactly(propertyNames.toArray(new String[0]));
+  }
+
+  @Test
+  public void constructListenerWithNoPropertyNames() {
+
+    AbstractListener listener = new TestListener();
+
+    assertThat(listener).isNotNull();
+    assertThat(listener).isEmpty();
+  }
+
+  @Test
+  public void constructListenerWithNullArrayOfPropertyNamesIsNullSafe() {
+
+    AbstractListener listener = new TestListener((String[]) null);
+
+    assertThat(listener).isNotNull();
+    assertThat(listener).isEmpty();
+  }
+
+  @Test
+  public void constructListenerWithNullIterableOfPropertyNamesIsNullSafe() {
+
+    AbstractListener listener = new TestListener((Iterable<String>) null);
+
+    assertThat(listener).isNotNull();
+    assertThat(listener).isEmpty();
+  }
+
+  @Test
+  public void handlesPropertyChangeEvent() {
+
+    PropertyChangeEvent mockEvent = mock(PropertyChangeEvent.class);
+
+    AbstractListener listener = spy(new TestListener());
+
+    doReturn(true).when(listener).canHandle(any(PropertyChangeEvent.class));
+    doNothing().when(listener).doHandle(any());
+
+    listener.handle(mockEvent);
+
+    verify(listener, times(1)).handle(eq(mockEvent));
+    verify(listener, times(1)).canHandle(eq(mockEvent));
+    verify(listener, times(1)).doHandle(mockEvent);
+    verifyNoMoreInteractions(listener);
+    verifyNoInteractions(mockEvent);
+  }
+
+  @Test
+  public void doesNotHandlePropertyChangeEvent() {
+
+    PropertyChangeEvent mockEvent = mock(PropertyChangeEvent.class);
+
+    AbstractListener listener = spy(new TestListener());
+
+    doReturn(false).when(listener).canHandle(any(PropertyChangeEvent.class));
+
+    listener.handle(mockEvent);
+
+    verify(listener, times(1)).handle(eq(mockEvent));
+    verify(listener, times(1)).canHandle(eq(mockEvent));
+    verify(listener, never()).doHandle(any());
+    verifyNoMoreInteractions(listener);
+    verifyNoInteractions(mockEvent);
+  }
+
+  @Test
+  public void canHandlePropertyChangeEvent() {
+
+    PropertyChangeEvent mockEvent = mock(PropertyChangeEvent.class);
+
+    doReturn("mockProperty").when(mockEvent).getPropertyName();
+
+    AbstractListener listener = spy(new TestListener());
+
+    doReturn(true).when(listener).canHandle(anyString());
+
+    assertThat(listener.canHandle(mockEvent)).isTrue();
+
+    verify(listener, times(1)).canHandle(eq(mockEvent));
+    verify(listener, times(1)).canHandle(eq("mockProperty"));
+    verify(mockEvent, times(1)).getPropertyName();
+    verifyNoMoreInteractions(listener, mockEvent);
+  }
+
+  @Test
+  public void cannotHandlePropertyChangeEvent() {
+
+    PropertyChangeEvent mockEvent = mock(PropertyChangeEvent.class);
+
+    doReturn("mockProperty").when(mockEvent).getPropertyName();
+
+    AbstractListener listener = spy(new TestListener());
+
+    doReturn(false).when(listener).canHandle(anyString());
+
+    assertThat(listener.canHandle(mockEvent)).isFalse();
+
+    verify(listener, times(1)).canHandle(eq(mockEvent));
+    verify(listener, times(1)).canHandle(eq("mockProperty"));
+    verify(mockEvent, times(1)).getPropertyName();
+    verifyNoMoreInteractions(listener, mockEvent);
+  }
+
+  @Test
+  public void cannotHandleNullPropertyChangeEventIsNullSafe() {
+
+    AbstractListener listener = spy(new TestListener());
+
+    assertThat(listener.canHandle((PropertyChangeEvent) null)).isFalse();
+
+    verify(listener, times(1)).canHandle(ArgumentMatchers.<PropertyChangeEvent>isNull());
+    verifyNoMoreInteractions(listener);
+  }
+
+  @Test
+  public void canHandleNamedProperty() {
+
+    AbstractListener listener = spy(new TestListener("mockProperty"));
+
+    assertThat(listener.canHandle("mockProperty")).isTrue();
+
+    verify(listener, times(1)).canHandle(eq("mockProperty"));
+    verify(listener, times(1)).getPropertyNames();
+    verifyNoMoreInteractions(listener);
+  }
+
+  @Test
+  public void cannotHandleNamedProperty() {
+
+    AbstractListener listener = spy(new TestListener("mockProperty"));
+
+    assertThat(listener.canHandle("testProperty")).isFalse();
+
+    verify(listener, times(1)).canHandle(eq("testProperty"));
+    verify(listener, times(1)).getPropertyNames();
+    verifyNoMoreInteractions(listener);
+  }
+
+  public void testHandleInvalidPropertyName(String propertyName) {
+
+    AbstractListener listener = spy(new TestListener("mockProperty"));
+
+    assertThat(listener.canHandle(propertyName)).isFalse();
+
+    if (StringUtils.hasText(propertyName)) {
+      verify(listener, times(1)).getPropertyNames();
+    }
+
+    verify(listener, times(1)).canHandle(ArgumentMatchers.<String>any());
+    verifyNoMoreInteractions(listener);
+  }
+
+  @Test
+  public void cannotHandleNonExitingProperty() {
+
+    testHandleInvalidPropertyName("mockedProperty");
+    testHandleInvalidPropertyName("nonExistingProperty");
+    testHandleInvalidPropertyName("testProperty");
+  }
+
+  @Test
+  public void cannotHandleBlankProperty() {
+    testHandleInvalidPropertyName("  ");
+  }
+
+  @Test
+  public void cannotHandleEmptyProperty() {
+    testHandleInvalidPropertyName("");
+  }
+
+  @Test
+  public void cannotHandleNullProperty() {
+    testHandleInvalidPropertyName(null);
+  }
+
+  static class TestListener extends AbstractListener {
+
+    TestListener(String... propertyNames) {
+      super(propertyNames);
+    }
+
+    TestListener(Iterable<String> propertyNames) {
+      super(propertyNames);
+    }
+
+    @Override
+    protected void doHandle(PropertyChangeEvent event) {
+      throw new UnsupportedOperationException(Constants.NOT_IMPLEMENTED);
+    }
+  }
+}
