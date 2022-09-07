9cec59f62c3997298db151783513b66c7ab63f76
John Blum <jblum@vmware.com>
2022-09-07 12:33:14 -0700

    Define the CacheNotFoundException class in the org.cp.elements.data.caching package.

---
+++ src/main/java/org/cp/elements/data/caching/CacheNotFoundException.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.caching;
+
+/**
+ * A {@link RuntimeException} used to indicate a {@link Cache} by {@link String name} could not be found.
+ *
+ * @author John Blum
+ * @see java.lang.RuntimeException
+ * @see org.cp.elements.data.caching.CacheException
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class CacheNotFoundException extends CacheException {
+
+  /**
+   * Constructs a new, uninitialized instance of {@link CacheNotFoundException}.
+   */
+  public CacheNotFoundException() { }
+
+  /**
+   * Constructs a new instance of {@link CacheNotFoundException} initialized with the given {@link String message}
+   * describing the {@link RuntimeException}.
+   *
+   * @param message {@link String} containing a description of the {@link RuntimeException}.
+   */
+  public CacheNotFoundException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new instance of {@link CacheNotFoundException} initialized with the given {@link Throwable}
+   * used as the cause of this {@link RuntimeException}.
+   *
+   * @param cause {@link Throwable} uses as the reason this {@link RuntimeException} was thrown.
+   */
+  public CacheNotFoundException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a new instance of {@link CacheNotFoundException} initialized with the given {@link String message}
+   * describing the {@link RuntimeException} along with the given {@link Throwable} used as the cause
+   * of this {@link RuntimeException}.
+   *
+   * @param message {@link String} containing a description of the {@link RuntimeException}.
+   * @param cause {@link Throwable} uses as the reason this {@link RuntimeException} was thrown.
+   */
+  public CacheNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
