e17afc00b83a971f91711ad2cc6bc70345eb4705
John Blum <jblum@vmware.com>
2022-10-03 09:51:29 -0700

    Add JavaSerializationSerializer class in the org.cp.elements.data.seralization package implementing the Serializer interface with Java Serialization.

---
+++ src/main/java/org/cp/elements/data/serialization/provider/JavaSerializationSerializer.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.serialization.provider;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+
+import org.cp.elements.data.serialization.DeserializationException;
+import org.cp.elements.data.serialization.SerializationException;
+import org.cp.elements.data.serialization.Serializer;
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.nio.ByteBufferInputStream;
+import org.cp.elements.nio.ByteBufferOutputStream;
+
+/**
+ * {@link Serializer} implementation that used Java Serialization.
+ *
+ * @author John Blum
+ * @see java.io.ObjectInputStream
+ * @see java.io.ObjectOutputStream
+ * @see org.cp.elements.data.serialization.Serializer
+ * @see org.cp.elements.nio.ByteBufferInputStream
+ * @see org.cp.elements.nio.ByteBufferOutputStream
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class JavaSerializationSerializer implements Serializer {
+
+  private static final int DEFAULT_BUFFER_SIZE = 4096;
+
+  public static final JavaSerializationSerializer INSTANCE = new JavaSerializationSerializer();
+
+  @Override
+  public @NotNull ByteBuffer serialize(@NotNull Object target) {
+
+    Assert.notNull(target, "The object to serialize is required");
+
+    ByteBufferOutputStream byteBufferOutput =
+      new ByteBufferOutputStream(ByteBuffer.allocate(DEFAULT_BUFFER_SIZE));
+
+    try (ObjectOutputStream objectOutput = new ObjectOutputStream(byteBufferOutput)) {
+      objectOutput.writeObject(target);
+      objectOutput.flush();
+      return byteBufferOutput.getByteBuffer();
+    }
+    catch (IOException cause) {
+      throw new SerializationException(String.format("Failed to serialize object [%s]", target), cause);
+    }
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public @NotNull <T> T deserialize(@NotNull ByteBuffer bytes) {
+
+    Assert.notNull(bytes, "The ByteBuffer containing the bytes of the object to deserialize is required");
+
+    try (ObjectInputStream objectInput = new ObjectInputStream(new ByteBufferInputStream(bytes))) {
+      return (T) objectInput.readObject();
+    }
+    catch (IOException | ClassNotFoundException cause) {
+      throw new DeserializationException("Failed to deserialize object from bytes", cause);
+    }
+  }
+}
---
+++ src/test/java/org/cp/elements/data/serialization/provider/JavaSerializationSerializerIntegrationTests.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.serialization.provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.cp.elements.lang.ThrowableAssertions.assertThatThrowableOfType;
+
+import java.io.EOFException;
+import java.io.NotSerializableException;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
+import org.cp.elements.data.serialization.DeserializationException;
+import org.cp.elements.data.serialization.SerializationException;
+import org.cp.elements.security.model.User;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Integration Tests for {@link JavaSerializationSerializer}.
+ *
+ * @author John Blum
+ * @see java.io.Serializable
+ * @see java.nio.ByteBuffer
+ * @see org.junit.Test
+ * @see org.cp.elements.data.serialization.provider.JavaSerializationSerializer
+ * @since 1.0.0
+ */
+public class JavaSerializationSerializerIntegrationTests {
+
+  @Test
+  public void serializesAndDeserializesObject() {
+
+    TestUser jonDoe = TestUser.as("jonDoe").identifiedBy(2);
+
+    ByteBuffer jonDoeBytes = JavaSerializationSerializer.INSTANCE.serialize(jonDoe);
+
+    assertThat(jonDoeBytes).isNotNull();
+    assertThat(jonDoeBytes.limit()).isGreaterThan(0);
+
+    TestUser deserializedJonDoe = JavaSerializationSerializer.INSTANCE.deserialize(jonDoeBytes);
+
+    assertThat(deserializedJonDoe).isNotNull();
+    assertThat(deserializedJonDoe).isEqualTo(jonDoe);
+    assertThat(deserializedJonDoe).isNotSameAs(jonDoe);
+    assertThat(deserializedJonDoe.getId()).isEqualTo(2);
+  }
+
+  @Test
+  public void serializeNonSerializableObject() {
+
+    NonSerializableObject target = new NonSerializableObject();
+
+    assertThatThrowableOfType(SerializationException.class)
+      .isThrownBy(args -> JavaSerializationSerializer.INSTANCE.serialize(target))
+      .havingMessage("Failed to serialize object [%s]", target)
+      .causedBy(NotSerializableException.class)
+      .havingMessage("%s", target.getClass().getName())
+      .withNoCause();
+  }
+
+  @Test
+  public void serializeNullObjectThrowsIllegalArgumentException() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> JavaSerializationSerializer.INSTANCE.serialize(null))
+      .withMessage("The object to serialize is required")
+      .withNoCause();
+  }
+
+  @Test
+  public void deserializeNullByteBufferThrowsIllegalArgumentException() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> JavaSerializationSerializer.INSTANCE.deserialize(null))
+      .withMessage("The ByteBuffer containing the bytes of the object to deserialize is required")
+      .withNoCause();
+  }
+
+  @Test
+  public void deserializeEmptyByteBuffer() {
+
+    ByteBuffer bytes = ByteBuffer.allocate(0);
+
+    assertThatThrowableOfType(DeserializationException.class)
+      .isThrownBy(args -> JavaSerializationSerializer.INSTANCE.deserialize(bytes))
+      .havingMessage("Failed to deserialize object from bytes")
+      .causedBy(EOFException.class)
+      .withNoCause();
+  }
+
+  static class NonSerializableObject { }
+
+  @Getter
+  @ToString(of = "name")
+  @EqualsAndHashCode(of = "name")
+  @RequiredArgsConstructor(staticName = "as")
+  static class TestUser implements User<Integer>, Serializable {
+
+    @Setter
+    private Integer id;
+
+    @lombok.NonNull
+    private final String name;
+
+  }
+}
