1d998c7f36e8618ab404fb3ed124cdb23cd75b97
John Blum <jblum@vmware.com>
2022-10-10 15:18:48 -0700

    Refactor the o.c.elements.lang.reflect.DelegatingMethodInterceptor.
    * Annotate API with Elements' @NotNull and @Nullable annotations.
    * Format source code.
    * Edit Javadoc.

--- src/main/java/org/cp/elements/lang/reflect/support/DelegatingMethodInterceptor.java
+++ src/main/java/org/cp/elements/lang/reflect/support/DelegatingMethodInterceptor.java
@@ -13,21 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.lang.reflect.support;
-
-import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
 
 import java.lang.reflect.Method;
 import java.util.Optional;
 
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 import org.cp.elements.lang.concurrent.GuardedBy;
 import org.cp.elements.lang.reflect.MethodInterceptor;
 import org.cp.elements.lang.reflect.MethodInvocation;
 
 /**
- * The {@link DelegatingMethodInterceptor} class is a wrapper for an existing {@link MethodInterceptor} that delegates
- * all {@link Method} intercepting operations to the configured delegate.
+ * {@link DelegatingMethodInterceptor} is a wrapper for an existing {@link MethodInterceptor} that delegates
+ * all {@link Method} invocations and intercepting operations to the configured delegate.
  *
  * @author John Blum
  * @param <T> {@link Class} type of the target {@link Object}.
@@ -40,46 +40,46 @@ public class DelegatingMethodInterceptor<T> implements MethodInterceptor<T> {
   private MethodInterceptor<T> delegate;
 
   /**
-   * Factory method used to construct an uninitialized instance of the {@link DelegatingMethodInterceptor}.
+   * Factory method used to construct a new, uninitialized instance of {@link DelegatingMethodInterceptor}.
    *
-   * @return a new, uninitialized instance of the {@link DelegatingMethodInterceptor}.
+   * @return a new, uninitialized instance of {@link DelegatingMethodInterceptor}.
    * @see org.cp.elements.lang.reflect.support.DelegatingMethodInterceptor
    * @see #DelegatingMethodInterceptor()
    */
-  public static DelegatingMethodInterceptor newDelegatingMethodInterceptor() {
-    return new DelegatingMethodInterceptor();
+  public static @NotNull <T> DelegatingMethodInterceptor<T> newDelegatingMethodInterceptor() {
+    return new DelegatingMethodInterceptor<>();
   }
 
   /**
-   * Factory method used to construct an instance of the {@link DelegatingMethodInterceptor} class initialized with
+   * Factory method used to construct a new instance of {@link DelegatingMethodInterceptor} initialized with
    * the given {@link MethodInterceptor} used as the delegate in all {@link MethodInterceptor} operations.
    *
-   * @param <T> {{@link Class} type of the target {@link Object}.
+   * @param <T> {{@link Class type} of the {@link Object target}.
    * @param delegate {@link MethodInterceptor} wrapped as the delegate for the {@link DelegatingMethodInterceptor}.
-   * @return a new instance of {@link DelegatingMethodInterceptor} initialized with
-   * the given {@link MethodInterceptor} delegate.
+   * @return a new {@link DelegatingMethodInterceptor} initialized with the given {@link MethodInterceptor} delegate.
    * @see org.cp.elements.lang.reflect.MethodInterceptor
    */
-  public static <T> DelegatingMethodInterceptor newDelegatingMethodInterceptor(MethodInterceptor<T> delegate) {
-    return new DelegatingMethodInterceptor<T>(delegate);
+  public static @NotNull <T> DelegatingMethodInterceptor<T> newDelegatingMethodInterceptor(
+      @Nullable MethodInterceptor<T> delegate) {
+
+    return new DelegatingMethodInterceptor<>(delegate);
   }
 
   /**
    * Default constructor for {@link DelegatingMethodInterceptor}constructing an uninitialized instance
    * of {@link DelegatingMethodInterceptor}.
    */
-  public DelegatingMethodInterceptor() {
-  }
+  public DelegatingMethodInterceptor() { }
 
   /**
-   * Constructs an instance of the {@link DelegatingMethodInterceptor} initialized with
+   * Constructs a new instance of {@link DelegatingMethodInterceptor} initialized with
    * the given {@link MethodInterceptor} serving as the delegate.
    *
    * @param delegate {@link MethodInterceptor} wrapped by this {@link DelegatingMethodInterceptor}.
    * May be {@literal null} but must be {@literal non-null} when the delegate is used.
    * @see org.cp.elements.lang.reflect.MethodInterceptor
    */
-  public DelegatingMethodInterceptor(MethodInterceptor<T> delegate) {
+  public DelegatingMethodInterceptor(@Nullable MethodInterceptor<T> delegate) {
     this.delegate =  delegate;
   }
 
@@ -90,7 +90,7 @@ public class DelegatingMethodInterceptor<T> implements MethodInterceptor<T> {
    * @see org.cp.elements.lang.reflect.MethodInterceptor
    */
   @GuardedBy("this")
-  public synchronized void setDelegate(MethodInterceptor<T> delegate) {
+  public synchronized void setDelegate(@Nullable MethodInterceptor<T> delegate) {
     this.delegate = delegate;
   }
 
@@ -102,9 +102,9 @@ public class DelegatingMethodInterceptor<T> implements MethodInterceptor<T> {
    * @see org.cp.elements.lang.reflect.MethodInterceptor
    */
   @GuardedBy("this")
-  protected synchronized MethodInterceptor<T> getDelegate() {
-    return Optional.ofNullable(this.delegate).orElseThrow(
-      () -> newIllegalStateException("The delegate MethodInterceptor was not properly initialized"));
+  protected synchronized @NotNull MethodInterceptor<T> getDelegate() {
+    return ObjectUtils.requireState(this.delegate,
+      "The delegate MethodInterceptor was not properly initialized");
   }
 
   /**
