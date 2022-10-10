cb914a5e7555bbaf45e961a8f563c08adb0cab35
John Blum <jblum@vmware.com>
2022-10-10 15:20:27 -0700

    Refactor the o.c.elements.lang.reflect.MethodInvokingMethodInterceptor class.
    * Annotate API with Elements' @NotNull and @Nullable annotations.
    * Format source code.
    * Edit Javadoc.

--- src/main/java/org/cp/elements/lang/reflect/support/MethodInvokingMethodInterceptor.java
+++ src/main/java/org/cp/elements/lang/reflect/support/MethodInvokingMethodInterceptor.java
@@ -13,12 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.lang.reflect.support;
 
 import java.lang.reflect.Method;
 import java.util.Optional;
 
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 import org.cp.elements.lang.reflect.MethodInterceptor;
 import org.cp.elements.lang.reflect.MethodInvocation;
 
@@ -33,27 +34,27 @@ import org.cp.elements.lang.reflect.MethodInvocation;
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
-public class MethodInvokingMethodInterceptor implements MethodInterceptor {
-
-  private final Object target;
+public class MethodInvokingMethodInterceptor implements MethodInterceptor<Object> {
 
   /**
-   * Factory method used to construct an instance of {@link MethodInvokingMethodInterceptor} initialized
+   * Factory method used to construct a new instance of {@link MethodInvokingMethodInterceptor} initialized
    * with the given target {@link Object} on which the {@link Method} invocation will be called.
    *
    * @param target {@link Object} on which the {@link Method} will be invoked.
-   * @return a new instance of {@link MethodInvokingMethodInterceptor} initialized with the given target {@link Object}.
+   * @return a new instance of {@link MethodInvokingMethodInterceptor} initialized with the given {@link Object target}.
    * @see org.cp.elements.lang.reflect.support.MethodInvokingMethodInterceptor
    * @see #MethodInvokingMethodInterceptor(Object)
    * @see java.lang.Object
    */
-  public static MethodInvokingMethodInterceptor newMethodInvokingMethodInterceptor(Object target) {
+  public static @NotNull MethodInvokingMethodInterceptor newMethodInvokingMethodInterceptor(@Nullable Object target) {
     return new MethodInvokingMethodInterceptor(target);
   }
 
+  private final Object target;
+
   /**
-   * Default constructor used to construct an instance of {@link MethodInvokingMethodInterceptor}
-   * with no target {@link Object}.
+   * Default constructor used to construct a new instance of {@link MethodInvokingMethodInterceptor}
+   * with no {@link Object target}.
    *
    * This constructor is used to invoke {@link java.lang.reflect.Modifier#STATIC} {@link Method methods}.
    *
@@ -64,13 +65,13 @@ public class MethodInvokingMethodInterceptor implements MethodInterceptor {
   }
 
   /**
-   * Constructs an instance of the {@link MethodInvokingMethodInterceptor} initialized with
+   * Constructs a new instance of {@link MethodInvokingMethodInterceptor} initialized with
    * the given target {@link Object} on which the {@link Method} will be invoked.
    *
    * @param target {@link Object} used as the target of the {@link Method} invocation.
    * @see java.lang.Object
    */
-  public MethodInvokingMethodInterceptor(Object target) {
+  public MethodInvokingMethodInterceptor(@Nullable Object target) {
     this.target = target;
   }
 
@@ -78,6 +79,7 @@ public class MethodInvokingMethodInterceptor implements MethodInterceptor {
    * Return the target {@link Object} on which the {@link Method} will be intercepted.
    *
    * @return the target {@link Object} on which the {@link Method} will be intercepted.
+   * @see java.lang.Object
    */
   @Override
   public Object getTarget() {
@@ -93,9 +95,11 @@ public class MethodInvokingMethodInterceptor implements MethodInterceptor {
    * @return an {@link Optional} return value containing the result of the interception.
    * @see org.cp.elements.lang.reflect.MethodInvocation
    * @see java.util.Optional
+   * @see #getTarget()
    */
   @Override
-  public Optional intercept(MethodInvocation methodInvocation) {
+  @SuppressWarnings("unchecked")
+  public Optional<Object> intercept(@NotNull MethodInvocation methodInvocation) {
     return methodInvocation.makeAccessible().invoke(getTarget());
   }
 }
