c58c9c3fe612270a86d5b8428d5921bdb7b3fab8
John Blum <jblum@vmware.com>
2022-10-16 16:49:51 -0700

    Define the ConfigurationProperties annotation in the org.cp.elements.context.configure.annotation package.

---
+++ src/main/java/org/cp/elements/context/configure/annotation/ConfigurationProperties.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.context.configure.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Java {@link Annotation} defining a {@link Class type} annotated with this {@link Annotation}
+ * as a source of configuration properties.
+ *
+ * @author John Blum
+ * @see java.lang.annotation.Annotation
+ * @see java.lang.annotation.Documented
+ * @see java.lang.annotation.Inherited
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.Target
+ * @since 1.0.0
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE })
+@SuppressWarnings("unused")
+public @interface ConfigurationProperties {
+
+  /**
+   * The {@link String prefix} defining the subset of configuration properties referencable from this {@link Object}.
+   *
+   * Default to {@link String empty}, implying no configuration property prefix.
+   *
+   * @return {@link String} containing the configuration properties prefix.
+   */
+  String propertyPrefix() default "";
+
+}
