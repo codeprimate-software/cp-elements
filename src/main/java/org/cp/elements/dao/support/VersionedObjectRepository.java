1b3516f9ad06c64e9dca9bbb9b71b22085b74683
John Blum <jblum@vmware.com>
2022-10-27 22:28:49 -0700

    Create the VersionedObjectRepository interface in the org.cp.elements.dao.support package.

---
+++ src/main/java/org/cp/elements/dao/support/VersionedObjectRepository.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.dao.support;
+
+import java.util.Optional;
+
+import org.cp.elements.lang.Integers;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.Versioned;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * Data Access Object (DAO) and Repository interface defining a single query method used to determine
+ * if a {@link Versioned POJO} is present in the data store with the given {@literal version}.
+ *
+ * {@literal Versioning} can be used for {@literal Optimistic Concurrency Control}, or {@literal Optimistic Locking}
+ * in place of {@link org.cp.elements.lang.Auditable audited metadata}, to determine if a record for a persistent entity
+ * has been change concurrently in the backend data store by another program (application process).
+ *
+ * A typical {@literal SQL} query implementation used to determine existence or presence of a persistent entity,
+ * or {@link Versioned object} with the given {@literal version} corresponds to a record in the database, would be:
+ *
+ * <pre>
+ * <code>
+ *   SELECT [DISTINCT] 1 FROM tableName WHERE version = ?
+ * </code>
+ * </pre>
+ *
+ * The use of the {@literal DISTINCT} keyword in the {@literal SQL} query is optional and unnecessary when
+ * the {@literal version} is unique, which should be the case for {@literal Optimistic Concurrency Control}.
+ *
+ * Subsequently, an {@literal Optimistic Lock} is typically enforced using the following {@literal SQL UPDATE}
+ * statement:
+ *
+ * <pre>
+ * <code>
+ *   UPDATE tableName SET <fields> WHERE version = <currentVersion>
+ * </code>
+ * </pre>
+ *
+ * The {@literal currentVersion} in this case would be the {@literal version} sourced from the {@link Versioned object}
+ * that the caller is currently processing.
+ *
+ * @author John Blum
+ * @param <VERSION> {@link Class type} of the {@link Versioned POJO's} version property.
+ * @see java.lang.FunctionalInterface
+ * @see org.cp.elements.lang.Versioned
+ * @see <a href="https://en.wikipedia.org/wiki/Optimistic_concurrency_control">Optimistic Concurrency Control (aka Optimistic Locking)</a>
+ * @since 1.0.0
+ */
+@FunctionalInterface
+@SuppressWarnings("unused")
+public interface VersionedObjectRepository<VERSION> {
+
+  /**
+   * Query method used to query the backend data store and determine whether a record for a persistent entity
+   * with the given {@literal version} exists, or is present.
+   *
+   * The {@link Integer value} may indicate a count of the number of records in the backend data store that
+   * have the given {@literal version}. However, when the {@literal version} is used for {@literal optimistic locking},
+   * then the returned {@link Integer value} will be equal to {@literal 1} when a record with the given {@literal version}
+   * exists in the backend data store and {@literal null} will be returned when no record with the given {@literal version}
+   * exists in the backend data store, likely because the record was changed and the {@literal version} was updated.
+   *
+   * @param version {@link VERSION version} to query for existence.
+   * @return an {@link Optional} {@link Integer value} indicating whether a record for a persistent entity
+   * with the given {@literal version} exists, or is present in the backend data store.
+   * @see java.util.Optional
+   * @see java.lang.Integer
+   */
+  Optional<Integer> existsByVersion(VERSION version);
+
+  /**
+   * Query method used to query the backend data store and determine whether a record for the given
+   * {@link Versioned persistent entity} with the given {@literal version} exists, or is present.
+   *
+   * @param object {@link Versioned persistent entity} to query; must not be {@literal null}.
+   * @return a boolean value indicating whether a record for the given {@link Versioned persistent entity}
+   * with the given {@literal version} exists, or is present in the backend data store.
+   * @throws IllegalArgumentException if the {@link Versioned object} is {@literal null}.
+   * @see org.cp.elements.lang.Versioned
+   * @see #existsByVersion(Object)
+   */
+  default boolean existsByVersion(@NotNull Versioned<VERSION> object) {
+
+    return existsByVersion(ObjectUtils.requireObject(object, "Versioned object is required").getVersion())
+      .filter(Integers::isGreaterThanZero)
+      .isPresent();
+  }
+}
---
+++ src/test/java/org/cp/elements/dao/support/VersionedObjectRepositoryUnitTests.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.dao.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import org.cp.elements.lang.Versioned;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
+
+import org.mockito.ArgumentMatchers;
+
+/**
+ * Unit Tests for {@link VersionedObjectRepository}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.dao.support.VersionedObjectRepository
+ * @see org.cp.elements.lang.Versioned
+ * @since 1.0.0
+ */
+public class VersionedObjectRepositoryUnitTests {
+
+  @SuppressWarnings("unchecked")
+  private void testExistsByVersion(@Nullable Integer result,
+      @NotNull Consumer<Boolean> repositoryExistsByVersionResultAssertion) {
+
+    String version = UUID.randomUUID().toString();
+
+    Versioned<String> mockVersionedObject = mock(Versioned.class);
+
+    doReturn(version).when(mockVersionedObject).getVersion();
+
+    VersionedObjectRepository<String> versionedObjectRepository = mock(VersionedObjectRepository.class);
+
+    doCallRealMethod().when(versionedObjectRepository).existsByVersion(any(Versioned.class));
+    doReturn(Optional.ofNullable(result)).when(versionedObjectRepository).existsByVersion(anyString());
+
+    repositoryExistsByVersionResultAssertion.accept(versionedObjectRepository.existsByVersion(mockVersionedObject));
+
+    verify(mockVersionedObject, times(1)).getVersion();
+    verify(versionedObjectRepository, times(1)).existsByVersion(eq(mockVersionedObject));
+    verify(versionedObjectRepository, times(1)).existsByVersion(eq(version));
+    verifyNoMoreInteractions(mockVersionedObject, versionedObjectRepository);
+  }
+
+  @Test
+  public void existsByVersionWhenIntegerIsGreaterThanOneReturnsTrue() {
+    IntStream.range(2, 100).forEach(value ->
+      testExistsByVersion(value, result -> assertThat(result).isTrue()));
+  }
+
+  @Test
+  public void existsByVersionWhenIntegerIsMinusOneReturnsFalse() {
+    testExistsByVersion(-1, result -> assertThat(result).isFalse());
+  }
+
+  @Test
+  public void existsByVersionWhenIntegerIsOneReturnsTrue() {
+    testExistsByVersion(1, result -> assertThat(result).isTrue());
+  }
+
+  @Test
+  public void existsByVersionWhenIntegerIsZeroReturnsFalse() {
+    testExistsByVersion(0, result -> assertThat(result).isFalse());
+  }
+
+  @Test
+  public void existsByVersionWhenNullReturnsFalse() {
+    testExistsByVersion(null, result -> assertThat(result).isFalse());
+  }
+
+  @Test
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  public void existByVersionWithNullVersionedObject() {
+
+    VersionedObjectRepository<?> versionedObjectRepository = mock(VersionedObjectRepository.class);
+
+    doCallRealMethod().when(versionedObjectRepository).existsByVersion(ArgumentMatchers.<Versioned>any());
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> versionedObjectRepository.existsByVersion((Versioned) null))
+      .withMessage("Versioned object is required")
+      .withNoCause();
+
+    verify(versionedObjectRepository, times(1))
+      .existsByVersion(ArgumentMatchers.<Versioned>isNull());
+
+    verifyNoMoreInteractions(versionedObjectRepository);
+  }
+}
