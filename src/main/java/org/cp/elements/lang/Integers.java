1dfc32a192b45aa48216847fbf8a494da9c47759
John Blum <jblum@vmware.com>
2022-10-27 21:51:16 -0700

    Create the Integers class in the org.cp.elements.lang package.
    The Integers class defines useful Integer constants, such as 0 and 1 along with utility methods for processing Integers, such as isZero(:Integer).

---
+++ src/main/java/org/cp/elements/lang/Integers.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang;
+
+import static org.cp.elements.lang.LangExtensions.is;
+
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
+
+/**
+ * Abstract base class for processing {@link Integer} values.
+ *
+ * @author John Blum
+ * @see java.lang.Integer
+ * @since 1.0.0
+ */
+public abstract class Integers {
+
+  public static final Integer ZERO = 0;
+  public static final Integer ONE = 1;
+  public static final Integer TWO = 2;
+
+  /**
+   * Null-safe method used to determine whether the given {@link Integer value} is greater than {@literal 0}.
+   *
+   * @param value {@link Integer value} to evaluate.
+   * @return a boolean value indicating whether the given {@link Integer value} is greater than {@literal 0}.
+   * @see java.lang.Integer
+   */
+  @NullSafe
+  public static boolean isGreaterThanZero(@Nullable Integer value) {
+    return value != null && is(value).greaterThan(ZERO);
+  }
+
+  /**
+   * Null-safe method used to determine whether the given {@link Integer value} is equal to {@literal 1}.
+   *
+   * @param value {@link Integer value} to evaluate.
+   * @return a boolean valued indicating whether the given {@link Integer value} is equal to {@literal 1}.
+   * @see java.lang.Integer
+   */
+  @NullSafe
+  public static boolean isOne(@Nullable Integer value) {
+    return ONE.equals(value);
+  }
+
+  /**
+   * Null-safe method used to determine whether the given {@link Integer value} is equal to {@literal 0}.
+   *
+   * @param value {@link Integer value} to evaluate.
+   * @return a boolean valued indicating whether the given {@link Integer value} is equal to {@literal 0}.
+   * @see java.lang.Integer
+   */
+  @NullSafe
+  public static boolean isZero(@Nullable Integer value) {
+    return ZERO.equals(value);
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/IntegersUnitTests.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Unit Tests for {@link Integers}.
+ *
+ * @author John Blum
+ * @see java.lang.Integer
+ * @see org.junit.Test
+ * @see org.cp.elements.lang.Integers
+ * @since 1.0.0
+ */
+public class IntegersUnitTests {
+
+  @Test
+  public void minusOneIsNotGreaterThanZero() {
+    assertThat(Integers.isGreaterThanZero(-1)).isFalse();
+  }
+
+  @Test
+  public void nullIsNotGreaterThanZeroIsNullSafe() {
+    assertThat(Integers.isGreaterThanZero(null)).isFalse();
+  }
+
+  @Test
+  public void oneIsGreaterThanZero() {
+    assertThat(Integers.isGreaterThanZero(1)).isTrue();
+  }
+
+  @Test
+  public void twoIsGreaterThanZero() {
+    assertThat(Integers.isGreaterThanZero(2)).isTrue();
+  }
+
+  @Test
+  public void zeroIsNotGreaterThanZero() {
+    assertThat(Integers.isGreaterThanZero(0)).isFalse();
+  }
+
+  @Test
+  public void minusOneIsNotOne() {
+    assertThat(Integers.isOne(-1)).isFalse();
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void nullIsNotOneIsNullSafe() {
+    assertThat(Integers.isOne(null)).isFalse();
+  }
+
+  @Test
+  public void oneIsOne() {
+    assertThat(Integers.isOne(1)).isTrue();
+  }
+
+  @Test
+  public void twoIsNotOne() {
+    assertThat(Integers.isOne(2)).isFalse();
+  }
+
+  @Test
+  public void zeroIsNotOne() {
+    assertThat(Integers.isOne(0)).isFalse();
+  }
+
+  @Test
+  public void minusZeroIsZero() {
+    assertThat(Integers.isZero(-0)).isTrue();
+  }
+
+  @Test
+  @SuppressWarnings("all")
+  public void nullIsNotZeroIsNullSafe() {
+    assertThat(Integers.isZero(null)).isFalse();
+  }
+
+  @Test
+  public void oneIsNotZero() {
+    assertThat(Integers.isZero(1)).isFalse();
+  }
+
+  @Test
+  public void twoIsNotZero() {
+    assertThat(Integers.isZero(2)).isFalse();
+  }
+
+  @Test
+  public void zeroIsZero() {
+    assertThat(Integers.isZero(0)).isTrue();
+  }
+}
