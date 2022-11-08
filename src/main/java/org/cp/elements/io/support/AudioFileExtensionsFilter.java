8fe1fa692e3e03e4bb75963cc675c8e5ddf5ecc6
John Blum <jblum@vmware.com>
2022-11-07 17:23:16 -0800

    Review and refactor the org.cp.elements.io.support.AudioFileExtensionsFilter class.
    Rename AudioFileExtensionsFilterTests to AudioFileExtensionsFilterUnitTests.

--- src/main/java/org/cp/elements/io/support/AudioFileExtensionsFilter.java
+++ src/main/java/org/cp/elements/io/support/AudioFileExtensionsFilter.java
@@ -13,16 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.io.support;
+
+import java.io.File;
 
 import org.cp.elements.io.FileExtensionFilter;
 
 /**
- * The AudioFileExtensionsFilter class is a {@link FileExtensionFilter} implementation
- * that filters {@link java.io.File}s by audio file types.
+ * {@link FileExtensionFilter} implementation that filters {@link File Files} by audio file types.
  *
  * @author John J. Blum
+ * @see java.io.File
  * @see org.cp.elements.io.FileExtensionFilter
  * @since 1.0.0
  */
@@ -41,7 +42,7 @@ public class AudioFileExtensionsFilter extends FileExtensionFilter {
   };
 
   /**
-   * Constructs an instance of the {@link AudioFileExtensionsFilter} class that filters {@link java.io.File}s
+   * Constructs a new instance of {@link AudioFileExtensionsFilter} that filters {@link File Files}
    * by audio file types.
    *
    * @see org.cp.elements.io.FileExtensionFilter#FileExtensionFilter(String...)
--- src/test/java/org/cp/elements/io/support/AudioFileExtensionsFilterUnitTests.java
+++ src/test/java/org/cp/elements/io/support/AudioFileExtensionsFilterUnitTests.java
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.io.support;
 
 import org.cp.elements.io.FileExtensionFilter;
+import org.cp.elements.test.annotation.SubjectUnderTest;
 
 /**
- * Test suite of test cases testing the contract and functionality of the {@link AudioFileExtensionsFilter} class.
+ * Unit Tests for {@link AudioFileExtensionsFilter}.
  *
  * @author John J. Blum
  * @see java.io.File
@@ -28,9 +28,10 @@ import org.cp.elements.io.FileExtensionFilter;
  * @see org.cp.elements.io.support.AudioFileExtensionsFilter
  * @since 1.0.0
  */
-public class AudioFileExtensionsFilterTests extends AbstractFileExtensionFilterTests {
+public class AudioFileExtensionsFilterUnitTests extends AbstractFileExtensionFilterTests {
 
-  private AudioFileExtensionsFilter fileExtensionFilter = new AudioFileExtensionsFilter();
+  @SubjectUnderTest
+  private final AudioFileExtensionsFilter fileExtensionFilter = new AudioFileExtensionsFilter();
 
   @Override
   protected String[] expectedFileExtensions() {
@@ -39,11 +40,12 @@ public class AudioFileExtensionsFilterTests extends AbstractFileExtensionFilterT
 
   @Override
   protected FileExtensionFilter fileExtensionFilter() {
-    return fileExtensionFilter;
+    return this.fileExtensionFilter;
   }
 
   @Override
   protected String[] unexpectedFileExtensions() {
+
     return new String[] {
       "absolute/path/to/a/image/file.avi",
       "absolute/path/to/a/image/file.jpg",
