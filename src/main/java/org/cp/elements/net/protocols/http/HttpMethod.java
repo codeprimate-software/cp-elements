46e38ebb13709bbfaf5baafc194a5d71a233741c
John Blum <jblum@vmware.com>
2022-11-10 12:17:05 -0800

    Review and refactor the org.cp.elements.net.protocols.http.HttpMethod enum.
    Rename HttpMethodTests to HttpMethodUnitTests.

--- src/main/java/org/cp/elements/net/protocols/http/HttpMethod.java
+++ src/main/java/org/cp/elements/net/protocols/http/HttpMethod.java
@@ -13,23 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.net.protocols.http;
 
+import java.util.function.Predicate;
+
 import org.cp.elements.dao.CrudOperation;
+import org.cp.elements.function.FunctionUtils;
 import org.cp.elements.lang.ObjectUtils;
 import org.cp.elements.lang.StringUtils;
-import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The HttpMethod enum is an enumeration of all HTTP protocol methods (POST, GET, PUT, DELETE, HEADERS, etc).
+ * An {@link Enum Enumeration} of all HTTP methods:
+ * [ {@literal POST}, {@literal GET}, {@literal PUT}, {@literal DELETE}, ... ].
+ *
+ * Additionally, The {@link HttpMethod} is mapped to a corresponding and equivalent {@link CrudOperation}.
  *
  * @author John J. Blum
+ * @see java.lang.Enum
  * @see org.cp.elements.dao.CrudOperation
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
 public enum HttpMethod {
+
   POST(CrudOperation.CREATE),
   GET(CrudOperation.READ),
   PUT(CrudOperation.UPDATE),
@@ -42,63 +50,72 @@ public enum HttpMethod {
   private final CrudOperation crudOperation;
 
   /**
-   * Constructs an instance of the {@link HttpMethod} enum initialized with the corresponding CRUD operation.
+   * Constructs a new instance of {@link HttpMethod} initialized with an equivalent {@link CrudOperation CRUD operation}.
    *
-   * @param crudOperation {@link CrudOperation} corresponding to this HTTP method.
+   * @param crudOperation {@link CrudOperation} equivalent to this HTTP method.
    * @see org.cp.elements.dao.CrudOperation
    */
-  HttpMethod(CrudOperation crudOperation) {
+  HttpMethod(@Nullable CrudOperation crudOperation) {
     this.crudOperation = crudOperation;
   }
 
   /**
-   * Returns an {@link HttpMethod} enumerated value corresponding to the given CRUD operation
-   * or null if no match was found.
+   * Factory method used to search for and return an instance of {@link HttpMethod} matching the given,
+   * required {@link Predicate} or returns {@literal null} if not match was found.
    *
-   * @param crudOperation CRUD operation used to match the HTTP method.
-   * @return an {@link HttpMethod} enumerated value corresponding to the given CRUD operation
-   * or null if no match was found.
+   * @param httpMethodPredicate {@link Predicate} used to search for and match the requested {@link HttpMethod};
+   * must not be {@literal null}.
+   * @return an {@link HttpMethod} matching the given, required {@link Predicate} or returns {@literal null}
+   * if not match was found.
+   * @see java.util.function.Predicate
+   */
+  private static @Nullable HttpMethod valueOf(@NotNull Predicate<HttpMethod> httpMethodPredicate) {
+
+    for (HttpMethod httpMethod : values()) {
+      if (FunctionUtils.nullSafePredicateMatchNone(httpMethodPredicate).test(httpMethod)) {
+        return httpMethod;
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Returns an {@link HttpMethod} enumerated value corresponding to the given {@link CrudOperation CRUD operation}
+   * or {@literal null} if no match was found.
+   *
+   * @param crudOperation {@link CrudOperation CRUD operation} used to match the {@link HttpMethod HTTP method}.
+   * @return an {@link HttpMethod} enumerated value corresponding to the given {@link CrudOperation CRUD operation}
+   * or {@literal null} if no match was found.
    * @see org.cp.elements.dao.CrudOperation
+   * @see #valueOf(Predicate)
    * @see #crudOperation()
    */
-  @NullSafe
-  public static HttpMethod valueOf(CrudOperation crudOperation) {
-    for (HttpMethod httpMethod : values()) {
-      if (ObjectUtils.equals(httpMethod.crudOperation(), crudOperation)) {
-        return httpMethod;
-      }
-    }
-
-    return null;
+  public static @Nullable HttpMethod valueOf(@Nullable CrudOperation crudOperation) {
+    return valueOf(httpMethod -> ObjectUtils.equals(httpMethod.crudOperation(), crudOperation));
   }
 
   /**
-   * Returns an {@link HttpMethod} enumerated value matching the case insensitive name of the HTTP method
-   * or null if no match was found.
+   * Returns an {@link HttpMethod} enumerated value matching the case-insensitive {@link String name}
+   * of the {@link HttpMethod HTTP method} or {@literal null} if no match was found.
    *
-   * @param name name of the HTTP method.
-   * @return an {@link HttpMethod} enumerated value matching the case insensitive name of the HTTP method
-   * or null if no match is found.
+   * @param name {@link String} containing the {@literal name} of the {@link HttpMethod HTTP method}.
+   * @return an {@link HttpMethod} enumerated value matching the case-insensitive {@link String name}
+   * of the {@link HttpMethod HTTP method} or {@literal null} if no match was found.
+   * @see #valueOf(Predicate)
    * @see #name()
    */
-  @NullSafe
-  public static HttpMethod valueOfIgnoreCase(String name) {
-    for (HttpMethod httpMethod : values()) {
-      if (httpMethod.name().equalsIgnoreCase(StringUtils.trim(name))) {
-        return httpMethod;
-      }
-    }
-
-    return null;
+  public static @Nullable HttpMethod valueOfNameIgnoreCase(@Nullable String name) {
+    return valueOf(httpMethod -> httpMethod.name().equalsIgnoreCase(StringUtils.trim(name)));
   }
 
   /**
-   * Gets the corresponding CRUD operation for this HTTP method.
+   * Gets the equivalent {@link CrudOperation CRUD operation} for this {@link HttpMethod HTTP method}.
    *
-   * @return a {@link CrudOperation} corresponding to this {@link HttpMethod}.
+   * @return the equivalent {@link CrudOperation CRUD operation} for this {@link HttpMethod HTTP method}.
    * @see org.cp.elements.dao.CrudOperation
    */
-  public CrudOperation crudOperation() {
-    return crudOperation;
+  public @Nullable CrudOperation crudOperation() {
+    return this.crudOperation;
   }
 }
---
+++ src/test/java/org/cp/elements/net/protocols/http/HttpMethodUnitTests.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.net.protocols.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import org.cp.elements.dao.CrudOperation;
+
+/**
+ * Unit Tests for {@link HttpMethod}.
+ *
+ * @author John J. Blum
+ * @see org.junit.Test
+ * @see org.cp.elements.net.protocols.http.HttpMethod
+ * @since 1.0.0
+ */
+public class HttpMethodUnitTests {
+
+  @Test
+  public void httpMethodWithCrudOperation() {
+
+    assertThat(HttpMethod.POST.crudOperation()).isEqualTo(CrudOperation.CREATE);
+    assertThat(HttpMethod.GET.crudOperation()).isEqualTo(CrudOperation.READ);
+    assertThat(HttpMethod.PUT.crudOperation()).isEqualTo(CrudOperation.UPDATE);
+    assertThat(HttpMethod.DELETE.crudOperation()).isEqualTo(CrudOperation.DELETE);
+  }
+
+  @Test
+  public void httpMethodWithNoCrudOperationReturnsNull() {
+
+    assertThat(HttpMethod.CONNECT.crudOperation()).isNull();
+    assertThat(HttpMethod.HEAD.crudOperation()).isNull();
+    assertThat(HttpMethod.OPTIONS.crudOperation()).isNull();
+    assertThat(HttpMethod.TRACE.crudOperation()).isNull();
+  }
+
+  @Test
+  public void valueOfCrudOperationReturnsHttpMethod() {
+
+    assertThat(HttpMethod.valueOf(CrudOperation.CREATE)).isEqualTo(HttpMethod.POST);
+    assertThat(HttpMethod.valueOf(CrudOperation.READ)).isEqualTo(HttpMethod.GET);
+    assertThat(HttpMethod.valueOf(CrudOperation.UPDATE)).isEqualTo(HttpMethod.PUT);
+    assertThat(HttpMethod.valueOf(CrudOperation.DELETE)).isEqualTo(HttpMethod.DELETE);
+  }
+
+  @Test
+  public void valueOfCrudOperationWithNullIsNullSafeReturnsNull() {
+    assertThat(HttpMethod.valueOf((CrudOperation) null)).isNull();
+  }
+
+  @Test
+  public void valueOfNameIgnoreCaseReturnsHttpMethod() {
+
+    assertThat(HttpMethod.valueOfNameIgnoreCase("CONNECT")).isEqualTo(HttpMethod.CONNECT);
+    assertThat(HttpMethod.valueOfNameIgnoreCase(" Delete")).isEqualTo(HttpMethod.DELETE);
+    assertThat(HttpMethod.valueOfNameIgnoreCase("get  ")).isEqualTo(HttpMethod.GET);
+    assertThat(HttpMethod.valueOfNameIgnoreCase("  HEad   ")).isEqualTo(HttpMethod.HEAD);
+    assertThat(HttpMethod.valueOfNameIgnoreCase("   OPTionS ")).isEqualTo(HttpMethod.OPTIONS);
+    assertThat(HttpMethod.valueOfNameIgnoreCase(" PuT")).isEqualTo(HttpMethod.PUT);
+    assertThat(HttpMethod.valueOfNameIgnoreCase(" POST ")).isEqualTo(HttpMethod.POST);
+    assertThat(HttpMethod.valueOfNameIgnoreCase("tRACE")).isEqualTo(HttpMethod.TRACE);
+    assertThat(HttpMethod.valueOfNameIgnoreCase("trACE")).isEqualTo(HttpMethod.TRACE);
+  }
+
+  @Test
+  public void valueOfNameIgnoreCaseWithInvalidNamesReturnsNull() {
+
+    assertThat(HttpMethod.valueOfNameIgnoreCase("DISCONNECT")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("Remove")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("GETit")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("giveMe")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("grab")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("PUTS")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("OPT")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("OPTS")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("OPTNS")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("Postage")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("PUTT")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("debug")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("info")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("warn")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("error")).isNull();
+  }
+
+  @Test
+  public void valueOfNameIgnoreCaseWithNoNamesReturnsNull() {
+
+    assertThat(HttpMethod.valueOfNameIgnoreCase("  ")).isNull();
+    assertThat(HttpMethod.valueOfNameIgnoreCase("")).isNull();
+  }
+
+  @Test
+  public void valueOfNameIgnoreCaseWithNullIsNullSafeReturnsNull() {
+    assertThat(HttpMethod.valueOfNameIgnoreCase(null)).isNull();
+  }
+}
