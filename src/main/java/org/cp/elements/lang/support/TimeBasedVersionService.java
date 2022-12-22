6f42e0046cc4ba478c7de3e63e1c276ebfa56d70
John Blum <jblum@vmware.com>
2022-12-22 13:55:06 -0800

    Create the new TimeBasedVersionService class implementing the VersionService interface using an java.time.Instant in the org.cp.elements.lang.support package.

---
+++ src/main/java/org/cp/elements/lang/support/TimeBasedVersionService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang.support;
+
+import java.time.Instant;
+
+import org.cp.elements.lang.VersionService;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * {@link VersionService} implementation based on {@link Instant time}.
+ *
+ * @author John Blum
+ * @see java.time.Instant
+ * @see org.cp.elements.lang.VersionService
+ * @since 1.0.0
+ */
+public class TimeBasedVersionService implements VersionService<Instant> {
+
+  @Override
+  public @NotNull Instant newVersion() {
+    return Instant.now();
+  }
+}
---
+++ src/test/java/org/cp/elements/lang/support/TimeBasedVersionServiceIUnitTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.lang.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import org.cp.elements.lang.VersionService;
+import org.cp.elements.lang.concurrent.ThreadUtils;
+
+/**
+ * Unit Tests for {@link TimeBasedVersionService}.
+ *
+ * @author John Blum
+ * @see java.time.Instant
+ * @see org.junit.Test
+ * @see org.cp.elements.lang.support.TimeBasedVersionService
+ * @since 1.0.0
+ */
+public class TimeBasedVersionServiceIUnitTests {
+
+  private static final int COUNT = 100;
+
+  @Test
+  public void newVersionIsUnique() {
+
+    VersionService<Instant> versionService = new TimeBasedVersionService();
+
+    assertThat(versionService.newVersion()).isNotNull();
+
+    Set<Instant> versionNumbers = new TreeSet<>();
+
+    IntStream.range(0, COUNT).forEach(count -> {
+      versionNumbers.add(versionService.newVersion());
+      ThreadUtils.sleep(10L, 0);
+    });
+
+    assertThat(versionNumbers).hasSize(COUNT);
+
+    Instant previousVersionNumber = Instant.now().minus(1, ChronoUnit.DAYS);
+
+    for (Instant versionNumber : versionNumbers) {
+      assertThat(versionNumber).isAfter(previousVersionNumber);
+      previousVersionNumber = versionNumber;
+    }
+  }
+}
