f20c8e9ea323e675f11fde3b251caf1f38dbeacd
John Blum <jblum@vmware.com>
2023-04-06 21:58:54 -0700

    Review and refactor the org.cp.elements.data.struct.tabular.Table interface.

--- src/main/java/org/cp/elements/data/struct/tabular/Table.java
+++ src/main/java/org/cp/elements/data/struct/tabular/Table.java
@@ -23,13 +23,18 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Predicate;
 
 import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Integers;
 import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.NotNull;
 import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.Nullable;
 
 /**
- * The {@link Table} interface is an Abstract Data Type (ADT) modeling a tabular data structure.
+ * Abstract Data Type (ADT) modeling a {@literal tabular}, or {@literal table} data structure,
+ * containing {@literal rows} and {@literal columns}.
  *
  * @author John J. Blum
+ * @see java.util.function.Predicate
  * @see org.cp.elements.data.struct.tabular.Column
  * @see org.cp.elements.data.struct.tabular.Row
  * @see org.cp.elements.data.struct.tabular.View
@@ -45,6 +50,7 @@ public interface Table extends View {
    * @return a boolean value indicating whether the added {@link Column}
    * successfully modified the structure of this {@link Table}.
    * @see org.cp.elements.data.struct.tabular.Column
+   * @see #add(Row)
    */
   boolean add(Column column);
 
@@ -55,79 +61,106 @@ public interface Table extends View {
    * @return a boolean value indicating whether the added {@link Row}
    * successfully modified the structure of this {@link Table}.
    * @see org.cp.elements.data.struct.tabular.Row
+   * @see #add(Column)
    */
   boolean add(Row row);
 
   /**
    * Removes the {@link Column} at the given {@link Integer index} from this {@link Table}.
    *
-   * @param index {@link Integer} value indicating the index of the {@link Column} to remove.
+   * @param index {@link Integer value} specifying the {@literal index} of the {@link Column} to remove.
    * @return a boolean value indicating whether the {@link Column} at {@link Integer index}
-   * was successfully removed.
-   * @throws IndexOutOfBoundsException if the given {@link Integer index} is not a valid {@link Column} index
-   * in this {@link Table}.
+   * was successfully removed from this {@link Table}.
+   * @throws IndexOutOfBoundsException if the given {@link Integer index} is not a valid {@link Column}
+   * {@literal index} in this {@link Table}.
+   * @see #removeColumn(String)
+   * @see #remove(Column)
    */
   boolean removeColumn(int index);
 
   /**
    * Removes the {@link Column} with the given {@link String name} from this {@link Table}.
    *
-   * @param name {@link String} containing the name of the {@link Column} to remove.
+   * @param name {@link String} containing the {@literal name} of the {@link Column} to remove.
    * @return a boolean value indicating whether the given {@link Column} with {@link String name}
-   * was successfully removed.
-   * the {@link Column} {@link String name} is out of bounds.
+   * was successfully removed from this {@link Table}.
    * @see #indexOf(String)
    * @see #removeColumn(int)
+   * @see #remove(Column)
    */
   @NullSafe
-  default boolean removeColumn(String name) {
+  default boolean removeColumn(@NotNull String name) {
 
-    return Optional.of(indexOf(name))
-      .filter(index -> index > -1)
-      .map(this::removeColumn)
-      .orElse(false);
+    int columnIndex = indexOf(name);
+
+    return columnIndex > Integers.MINUS_ONE && removeColumn(columnIndex);
   }
 
   /**
    * Removes the given {@link Column} from this {@link Table}.
    *
    * @param column {@link Column} to remove.
-   * @return a boolean value indicating whether the given {@link Column} was successfully removed.
+   * @return a boolean value indicating whether the given {@link Column}
+   * was successfully removed from this {@link Table}.
    * @see org.cp.elements.data.struct.tabular.Column
-   * @see #indexOf(Column)
+   * @see #removeColumn(String)
    * @see #removeColumn(int)
+   * @see #indexOf(Column)
    */
   @NullSafe
-  default boolean remove(Column column) {
+  default boolean remove(@NotNull Column column) {
 
-    return Optional.of(indexOf(column))
-      .filter(index -> index > -1)
-      .map(this::removeColumn)
-      .orElse(false);
+    int columnIndex = indexOf(column);
+
+    return columnIndex > Integers.MINUS_ONE && removeColumn(columnIndex);
+  }
+
+  /**
+   * Removes the given {@link Row} from this {@link Table}.
+   *
+   * @param row {@link Row} to remove.
+   * @return a boolean value indicating whether the given {@link Row}
+   * was successfully removed from this {@link Table}.
+   * @see org.cp.elements.data.struct.tabular.Row
+   * @see #removeRows(Predicate)
+   * @see #removeRow(int)
+   * @see #indexOf(Row)
+   */
+  @NullSafe
+  default boolean remove(@NotNull Row row) {
+
+    int rowIndex = indexOf(row);
+
+    return rowIndex > Integers.MINUS_ONE && removeRow(rowIndex);
   }
 
   /**
    * Removes the {@link Row} at the given {@link Integer index} from this {@link Table}.
    *
-   * @param index {@link Integer} value indicating the index of the {@link Row} to remove.
+   * @param index {@link Integer value} specifying the {@literal index} of the {@link Row}
+   * to remove from this {@link Table}.
    * @return a boolean value indicating whether the given {@link Row} at {@link Integer index}
-   * was successfully removed.
+   * was successfully removed from this {@link Table}.
+   * @see #removeRows(Predicate)
+   * @see #remove(Row)
    */
   boolean removeRow(int index);
 
   /**
    * Removes all {@link Row Rows} from this {@link Table} matching the given {@link Predicate}.
    *
-   * @param predicate {@link Predicate} used to match {@link Row Rows} to remove from this {@link Table}.
-   * @return a boolean value indicating whether the remove operation modified the structure of this {@link Table}.
-   * @throws IllegalArgumentException if {@link Predicate} is {@literal null}.
-   * @throws IllegalStateException if the removal of a {@link Row} matching the {@link Predicate}
-   * was not successful.
+   * @param predicate {@link Predicate} used to match {@link Row Rows} to remove from this {@link Table};
+   * must not be {@literal null}.
+   * @return a boolean value indicating whether the remove operation resulted in removing {@link Row Rows}
+   * and modified the structure of this {@link Table}.
+   * @throws IllegalArgumentException if the given {@link Predicate} is {@literal null}.
+   * @throws IllegalStateException if the removal of a {@link Row} matching the {@link Predicate} was not successful.
    * @see java.util.function.Predicate
+   * @see #removeRow(int)
    * @see #remove(Row)
    * @see #rows()
    */
-  default boolean removeRows(Predicate<Row> predicate) {
+  default boolean removeRows(@NotNull Predicate<Row> predicate) {
 
     Assert.notNull(predicate, "Predicate is required");
 
@@ -135,51 +168,32 @@ public interface Table extends View {
 
     for (Iterator<Row> rows = rows().iterator(); rows.hasNext(); ) {
 
-      Optional.of(rows.next())
-        .filter(predicate)
-        .ifPresent(row -> {
-          rows.remove();
-          result.set(true);
-        });
+      Row row = rows.next();
+
+      if (predicate.test(row)) {
+        rows.remove();
+        result.set(true);
+      }
     }
 
     return result.get();
   }
 
   /**
-   * Removes the given {@link Row} from this {@link Table}.
-   *
-   * @param row {@link Row} to remove.
-   * @return a boolean value indicating whether the given {@link Row} was successfully removed.
-   * @see org.cp.elements.data.struct.tabular.Row
-   * @see #indexOf(Row)
-   * @see #removeRow(int)
-   */
-  @NullSafe
-  default boolean remove(Row row) {
-
-    return Optional.ofNullable(row)
-      .map(this::indexOf)
-      .filter(index -> index > -1)
-      .map(this::removeRow)
-      .orElse(false);
-  }
-
-  /**
    * Sets the {@link Object value} at the given {@link Row} and {@link Column} {@link Integer indexes}
    * in this {@link Table}.
    *
-   * @param rowIndex {@link Integer} value indicating the {@link Row} index.
-   * @param columnIndex {@link Integer} value indicating the {@link Column} index.
-   * @param value the {@link Object value} to set at the referenced {@link Row} and {@link Column}
-   * in this {@link Table}.
-   * @return the current {@link Object} value at the given {@link Row} and {@link Column}.
-   * @throws IndexOutOfBoundsException if either the {@link Row} or {@link Column} {@link Integer index}
-   * are invalid.
+   * @param rowIndex {@link Integer value} specifying the {@link Row} {@literal index}.
+   * @param columnIndex {@link Integer value} specifying the {@link Column} {@literal index}.
+   * @param value {@link Object value} to set at the referenced {@link Row} and {@link Column} in this {@link Table}.
+   * @return the current {@link Object value} at the given {@link Row} and {@link Column} in this {@link Table}.
+   * @throws IndexOutOfBoundsException if either the {@link Row} or {@link Column} {@link Integer indexes} are invalid.
    * @see org.cp.elements.data.struct.tabular.Row#setValue(int, Object)
+   * @see #setValue(int, String, Object)
+   * @see #setValue(int, Column, Object)
    * @see #getRow(int)
    */
-  default Object setValue(int rowIndex, int columnIndex, Object value) {
+  default @Nullable Object setValue(int rowIndex, int columnIndex, @Nullable Object value) {
     return getRow(rowIndex).setValue(columnIndex, value);
   }
 
@@ -187,24 +201,24 @@ public interface Table extends View {
    * Sets the {@link Object value} at the given {@link Row} {@link Integer index}
    * and {@link String named} {@link Column} in this {@link Table}.
    *
-   * @param rowIndex {@link Integer} value indicating the {@link Row} index.
-   * @param columnName {@link String} containing the name of the {@link Column}.
-   * @param value the {@link Object value} to set at the referenced {@link Row} and {@link Column}
-   * in this {@link Table}.
-   * @return the current {@link Object} value at the given {@link Row} and {@link Column}.
+   * @param rowIndex {@link Integer value} specifying the {@link Row} {@literal index}.
+   * @param columnName {@link String} containing the {@literal name} of the {@link Column}.
+   * @param value {@link Object value} to set at the referenced {@link Row} and {@link Column} in this {@link Table}.
+   * @return the current {@link Object value} at the given {@link Row} and {@link Column} in this {@link Table}.
    * @throws IllegalArgumentException if the {@link String named} {@link Column}
    * is not a valid {@link Column} in this {@link Table}.
    * @throws IndexOutOfBoundsException if the {@link Row} {@link Integer index} is not valid.
+   * @see #setValue(int, Column, Object)
    * @see #setValue(int, int, Object)
    * @see #indexOf(String)
    */
   @NullSafe
-  default Object setValue(int rowIndex, String columnName, Object value) {
+  default @Nullable Object setValue(int rowIndex, String columnName, @Nullable Object value) {
 
     return Optional.ofNullable(columnName)
       .filter(StringUtils::hasText)
       .map(this::indexOf)
-      .filter(columnIndex -> columnIndex > -1)
+      .filter(columnIndex -> columnIndex > Integers.MINUS_ONE)
       .map(columnIndex -> setValue(rowIndex, columnIndex, value))
       .orElseThrow(() -> newIllegalArgumentException("Column [%s] is not valid", columnName));
   }
@@ -213,24 +227,24 @@ public interface Table extends View {
    * Sets the {@link Object value} at the given {@link Row} {@link Integer index} and {@link Column}
    * in this {@link Table}.
    *
-   * @param rowIndex {@link Integer} value indicating the {@link Row} index.
-   * @param column {@link Column} in this {@link Table}.
-   * @param value the {@link Object value} to set at the referenced {@link Row} and {@link Column}
-   * in this {@link Table}.
-   * @return the current {@link Object} value at the given {@link Row} and {@link Column}.
-   * @throws IllegalArgumentException if {@link Column} is {@literal null}
+   * @param rowIndex {@link Integer value} specifying the {@link Row} {@literal index}.
+   * @param column {@link Column} in this {@link Table}; must not be {@literal null}.
+   * @param value {@link Object value} to set at the referenced {@link Row} and {@link Column} in this {@link Table}.
+   * @return the current {@link Object value} at the given {@link Row} and {@link Column} from this {@link Table}.
+   * @throws IllegalArgumentException if the given {@link Column} is {@literal null}
    * or is not a valid {@link Column} in this {@link Table}.
-   * @throws IndexOutOfBoundsException if the {@link Row} {@link Integer index} is not valid.
+   * @throws IndexOutOfBoundsException if the given {@link Row} {@link Integer index} is not valid.
    * @see org.cp.elements.data.struct.tabular.Column
+   * @see #setValue(int, String, Object)
    * @see #setValue(int, int, Object)
    * @see #indexOf(Column)
    */
   @NullSafe
-  default Object setValue(int rowIndex, Column column, Object value) {
+  default @Nullable Object setValue(int rowIndex, @NotNull Column column, @Nullable Object value) {
 
     return Optional.ofNullable(column)
       .map(this::indexOf)
-      .filter(columnIndex -> columnIndex > -1)
+      .filter(columnIndex -> columnIndex > Integers.MINUS_ONE)
       .map(columnIndex -> setValue(rowIndex, columnIndex, value))
       .orElseThrow(() -> newIllegalArgumentException("Column [%s] is not valid", column));
   }
