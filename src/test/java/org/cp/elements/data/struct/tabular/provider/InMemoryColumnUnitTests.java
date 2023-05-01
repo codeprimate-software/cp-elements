6ca7ef88811f59b3149e0415f938e4d83bf2f5de
John Blum <jblum@vmware.com>
2023-04-30 18:22:54 -0700

    Review and refactor unit tests for InMemoryTable.InMemoryColumn class.

---
+++ src/test/java/org/cp/elements/data/struct/tabular/provider/InMemoryColumnUnitTests.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.struct.tabular.provider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.cp.elements.lang.ThrowableAssertions.assertThatUnsupportedOperationException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
+
+import org.cp.elements.data.struct.tabular.Column;
+import org.cp.elements.data.struct.tabular.View;
+import org.cp.elements.data.struct.tabular.provider.InMemoryTable.InMemoryColumn;
+import org.cp.elements.lang.ThrowableOperation;
+
+/**
+ * Unit Tests for {@link InMemoryTable.InMemoryColumn}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.data.struct.tabular.Column
+ * @see org.cp.elements.data.struct.tabular.Table
+ * @see org.cp.elements.data.struct.tabular.View
+ * @see org.cp.elements.data.struct.tabular.provider.InMemoryTable
+ * @see org.cp.elements.data.struct.tabular.provider.InMemoryTable.InMemoryColumn
+ * @since 1.0.0
+ */
+public class InMemoryColumnUnitTests {
+
+  @SuppressWarnings("unchecked")
+  private <T> Column<T> mockColumn(String name) {
+
+    Column<T> mockColumn = mock(Column.class, name);
+
+    doReturn(name).when(mockColumn).getName();
+    doReturn(Object.class).when(mockColumn).getType();
+
+    return mockColumn;
+  }
+
+  @Test
+  public void constructInMemoryColumn() {
+
+    Column<?> mockColumn = mockColumn("MockColumn");
+
+    InMemoryTable table = mock(InMemoryTable.class);
+
+    InMemoryColumn<?> column = table.new InMemoryColumn<>(mockColumn);
+
+    assertThat(column).isNotNull();
+    assertThat(column.getName()).isEqualTo("MockColumn");
+    assertThat(column.getType()).isEqualTo(Object.class);
+    assertThat(column.getView()).isPresent();
+    assertThat(column.getView().orElse(null)).isEqualTo(table);
+
+    verify(mockColumn, times(1)).getName();
+    verify(mockColumn, times(1)).getType();
+    verify(mockColumn, times(1)).getAlias();
+    verify(mockColumn, times(1)).getDefaultValue();
+    verify(mockColumn, times(1)).getDescription();
+    verifyNoMoreInteractions(mockColumn);
+    verifyNoMoreInteractions(table);
+  }
+
+  @Test
+  public void constructInMemoryColumnWithNullColumnThrowsIllegalArgumentException() {
+
+    InMemoryTable table = mock(InMemoryTable.class);
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> table.new InMemoryColumn<>(null))
+      .withMessage("The Column to copy is required")
+      .withNoCause();
+
+    verifyNoInteractions(table);
+  }
+
+  @Test
+  public void viewIsSameAsContainingInMemoryTable() {
+
+    InMemoryTable table = mock(InMemoryTable.class);
+
+    Column<?> mockColumn = mockColumn("MockColumn");
+    Column<?> column = table.new InMemoryColumn<>(mockColumn);
+
+    assertThat(column).isNotNull();
+    assertThat(column.getName()).isEqualTo("MockColumn");
+    assertThat(column.getType()).isEqualTo(Object.class);
+    assertThat(column.getView()).isPresent();
+    assertThat(column.getView().orElse(null)).isSameAs(table);
+
+    verifyNoInteractions(table);
+  }
+
+  @Test
+  public void setViewThrowsUnSupportedOperationException() {
+
+    Column<?> mockColumn = mockColumn("MockColumn");
+
+    InMemoryTable table = mock(InMemoryTable.class);
+
+    View mockView = mock(View.class);
+
+    assertThat(table).isNotNull();
+
+    InMemoryColumn<?> column = table.new InMemoryColumn<>(mockColumn);
+
+    assertThat(column).isNotNull();
+    assertThat(column.getName()).isEqualTo("MockColumn");
+    assertThat(column.getView()).isPresent();
+    assertThat(column.getView().orElse(null)).isEqualTo(table);
+
+    assertThatUnsupportedOperationException()
+      .isThrownBy(ThrowableOperation.fromRunnable(() -> column.setView(mockView)))
+      .havingMessage("The View for this Column [MockColumn] cannot be changed")
+      .withNoCause();
+
+    assertThat(column.getView()).isPresent();
+    assertThat(column.getView().orElse(null)).isEqualTo(table);
+
+    verifyNoInteractions(table, mockView);
+  }
+}
