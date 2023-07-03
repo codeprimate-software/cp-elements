2151effa310cbbaee2b8582ae0afdfb15a8a8eaa
John Blum <jblum@vmware.com>
2023-07-02 17:47:31 -0700

    Create ThrowableFunction class in org.cp.elements.function package implementing the Java Function interface with possibility of throwing an exception.

---
+++ src/main/java/org/cp/elements/function/ThrowableFunction.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.function;
+
+import static org.cp.elements.lang.ElementsExceptionsFactory.newFunctionExecutionException;
+
+import java.util.function.Function;
+
+/**
+ * Java {@link Function} implementation capable of throwing an {@link Throwable exception} during execution.
+ *
+ * @author John Blum
+ * @see java.lang.FunctionalInterface
+ * @see java.util.function.Function
+ * @since 2.0.0
+ */
+@FunctionalInterface
+public interface ThrowableFunction<T, R> extends Function<T, R> {
+
+  @Override
+  default R apply(T input) {
+
+    try {
+      return applyWithThrows(input);
+    }
+    catch (Throwable cause) {
+      throw newFunctionExecutionException(cause, "Failed to execute Function [%s]", getClass().getSimpleName());
+    }
+  }
+
+  /**
+   * Executes the logic of this {@link Function} with the possibility that an {@link Throwable exception} may be thrown.
+   *
+   * @param input {@link Object} of {@link T type T} passed as the argument to this {@link Function}.
+   * @return the {@link R computation (result)} of this {@link Function}.
+   * @throws Throwable if the {@link Function} execution fails.
+   * @see #apply(Object)
+   */
+  R applyWithThrows(T input) throws Throwable;
+
+}
---
+++ src/test/java/org/cp/elements/function/ThrowableFunctionUnitTests.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2011-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.ThrowableAssertions.assertThatThrowableOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit Tests for {@link ThrowableFunction}.
+ *
+ * @author John Blum
+ * @see org.junit.jupiter.api.Test
+ * @see org.mockito.Mockito
+ * @see org.cp.elements.function.ThrowableFunction
+ * @since 2.0.0
+ */
+public class ThrowableFunctionUnitTests {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void throwableFunctionReturnsValue() throws Throwable {
+
+    ThrowableFunction<Object, Object> mockFunction = mock(ThrowableFunction.class);
+
+    doCallRealMethod().when(mockFunction).apply(any());
+    doAnswer(invocation -> invocation.getArgument(0)).when(mockFunction).applyWithThrows(any());
+
+    assertThat(mockFunction.apply("test")).isEqualTo("test");
+
+    verify(mockFunction, times(1)).apply(eq("test"));
+    verify(mockFunction, times(1)).applyWithThrows(eq("test"));
+    verifyNoMoreInteractions(mockFunction);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void throwableFunctionThrowsFunction() throws Throwable {
+
+    ThrowableFunction<Object, Object> mockFunction = mock(ThrowableFunction.class);
+
+    doCallRealMethod().when(mockFunction).apply(any());
+    doThrow(new Exception("ERROR")).when(mockFunction).applyWithThrows(any());
+
+    assertThatThrowableOfType(FunctionExecutionException.class)
+      .isThrownBy(args -> mockFunction.apply("test"))
+      .havingMessage("Failed to execute Function [%s]", mockFunction.getClass().getSimpleName())
+      .causedBy(Exception.class)
+      .havingMessage("ERROR")
+      .withNoCause();
+
+    verify(mockFunction, times(1)).apply(eq("test"));
+    verify(mockFunction, times(1)).applyWithThrows(eq("test"));
+    verifyNoMoreInteractions(mockFunction);
+  }
+}
