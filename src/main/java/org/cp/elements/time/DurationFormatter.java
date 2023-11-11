4a3c9d91e1392ea1ae65a045e5d7afd4af49629a
John Blum <jblum0@gmail.com>
2023-11-11 15:18:13 -0800

    Create DurationFormatter class in org.cp.elements.time package used to parse Strings as a Duration.

---
+++ src/main/java/org/cp/elements/time/DurationFormatter.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.time;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalArgumentException;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.Nameable;
+import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * {@link Enum Enumeration} of different formatters for {@link Duration}.
+ *
+ * @author John Blum
+ * @see java.lang.Enum
+ * @see java.time.Duration
+ * @since 2.0.0
+ */
+public enum DurationFormatter {
+
+  TIME_UNIT("\\d+[a-zA-Z]{1,2}");
+
+  private final Pattern pattern;
+
+  DurationFormatter(String durationPattern) {
+    this.pattern = Pattern.compile(durationPattern);
+  }
+
+  /**
+   * Gets the configured Regular Expression (REGEX) {@link Pattern} used to parse and match a {@link Duration}
+   * formatted as a {@link String}.
+   *
+   * @return the configured Regular Expression (REGEX) {@link Pattern} used to parse and match a {@link Duration}
+   * formatted as a {@link String}.
+   * @see java.util.regex.Pattern
+   */
+  @NotNull Pattern getPattern() {
+    return this.pattern;
+  }
+
+  /**
+   * Parses the given, required {@link String text} as a {@link Duration}.
+   *
+   * @param text {@link String} containing the {@link String} formatted time as a representation of {@link Duration}.
+   * @return the computed {@link Duration} from the {@link String text}.
+   * @see java.time.Duration
+   */
+  public @NotNull Duration parse(String text) {
+
+    Assert.hasText(text, "Text [%s] to parse as a Duration is required", text);
+
+    Matcher matcher = getPattern().matcher(text);
+
+    Duration total = Duration.ZERO;
+
+    while (matcher.find()) {
+      String value = matcher.group();
+      total = total.plus(Unit.parseUnit(value).toDuration(value));
+    }
+
+    return total;
+  }
+
+  /**
+   * {@link Enum Enumeration} of units of time.
+   *
+   * @see java.lang.Enum
+   * @see org.cp.elements.lang.Nameable
+   */
+  public enum Unit implements Nameable<String> {
+
+    NANOSECONDS("ns", ChronoUnit.NANOS),
+    MICROSECONDS("us", ChronoUnit.MICROS),
+    MILLISECONDS("ms", ChronoUnit.MILLIS),
+    SECONDS("s", ChronoUnit.SECONDS),
+    MINUTES("m", ChronoUnit.MINUTES),
+    HOURS("h", ChronoUnit.HOURS),
+    DAYS("d", ChronoUnit.DAYS);
+
+    /**
+     * Factory method used to find a {@link DurationFormatter.Unit} for the given, required {@link ChronoUnit}.
+     *
+     * @param chronoUnit {@link ChronoUnit} used to match a {@link DurationFormatter.Unit}.
+     * @return a {@link DurationFormatter.Unit} for the given, required {@link ChronoUnit}.
+     * @throws IllegalArgumentException if no {@link Unit} matches the given {@link ChronoUnit}.
+     * @see java.time.temporal.ChronoUnit
+     */
+    public static @NotNull Unit valueOfChronoUnit(@NotNull ChronoUnit chronoUnit) {
+
+      return Arrays.stream(values())
+        .filter(unit -> unit.getUnit().equals(chronoUnit))
+        .findFirst()
+        .orElseThrow(() -> newIllegalArgumentException("No Unit %s matches the given ChronoUnit [%s]",
+          Arrays.toString(values()), chronoUnit));
+    }
+
+    /**
+     * Factory method used to find a {@link DurationFormatter.Unit} by the given, required {@link String symbol}.
+     *
+     * @param symbol {@link String} containing the symbol used to match the given {@link Unit}.
+     * @return a {@link DurationFormatter.Unit} by the given, required {@link String symbol}.
+     * @throws IllegalArgumentException if no {@link Unit} matches the given {@link String symbol}.
+     */
+    public static @NotNull Unit valueOfSymbol(@NotNull String symbol) {
+
+      return Arrays.stream(values())
+        .filter(unit -> unit.getSymbol().equalsIgnoreCase(StringUtils.trim(symbol)))
+        .findFirst()
+        .orElseThrow(() -> newIllegalArgumentException("No Unit %s matches the given Symbol [%s]",
+          Arrays.toString(values()), symbol));
+    }
+
+    private final ChronoUnit unit;
+
+    private final String symbol;
+
+    Unit(String symbol, ChronoUnit unit) {
+      this.symbol = symbol;
+      this.unit = unit;
+    }
+
+    static @NotNull Unit parseUnit(@NotNull String value) {
+
+      String symbol = parseSymbol(value);
+
+      return Arrays.stream(values())
+        .filter(unit -> unit.getSymbol().equalsIgnoreCase(symbol))
+        .findFirst()
+        .orElseThrow(() -> newIllegalStateException("Value [%s] does not contain a valid time unit", value));
+    }
+
+    private static String parse(String value, Predicate<Character> predicate) {
+
+      Assert.hasText(value, "Value [%s] is required", value);
+
+      StringBuilder builder = new StringBuilder();
+
+      for (char character : value.toCharArray()) {
+        if (predicate.test(character)) {
+          builder.append(character);
+        }
+      }
+
+      return builder.toString();
+    }
+
+    private static String parseSymbol(String value) {
+      return parse(value, Character::isLetter);
+    }
+
+    private static Long parseTime(String value) {
+      return Long.parseLong(parse(value, Character::isDigit));
+    }
+
+    /**
+     * Gets the {@link String name} of this {@link Unit}.
+     *
+     * @return the {@link String name} of this {@link Unit}.
+     */
+    public @NotNull String getName() {
+      return StringUtils.capitalize(name().toLowerCase());
+    }
+
+    /**
+     * Gets the {@link String symbol} representing of this {@link Unit}.
+     *
+     * @return the {@link String symbol} representing of this {@link Unit}.
+     */
+    public @NotNull String getSymbol() {
+      return this.symbol;
+    }
+
+    /**
+     * Gets the Java {@link ChronoUnit} matching this {@link Unit}.
+     *
+     * @return the Java {@link ChronoUnit} matching this {@link Unit}.
+     * @see java.time.temporal.ChronoUnit
+     */
+    public @NotNull ChronoUnit getUnit() {
+      return this.unit;
+    }
+
+    /**
+     * Computes a {@link Duration} from the given {@link String} representation.
+     *
+     * @param value {@link String} containing the value to parse as a {@link Duration}.
+     * @return a {@link Duration} from the given {@link String} representation.
+     * @see java.time.Duration
+     * @see #parseTime(String)
+     * @see #getUnit()
+     */
+    public @NotNull Duration toDuration(@NotNull String value) {
+      return Duration.of(parseTime(value), getUnit());
+    }
+  }
+}
---
+++ src/test/java/org/cp/elements/time/DurationFormatterUnitTests.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.time;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.cp.elements.lang.LangExtensions.assertThat;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit Tests for {@link DurationFormatter}.
+ *
+ * @author John Blum
+ * @see org.junit.jupiter.api.Test
+ * @see org.cp.elements.time.DurationFormatter
+ * @since 2.0.0
+ */
+class DurationFormatterUnitTests {
+
+  private Duration durationOf(long days, long hours, long minutes, long seconds,
+      long milliseconds, long microseconds, long nanoseconds) {
+
+    return Duration.ofDays(days)
+      .plus(Duration.ofHours(hours))
+      .plus(Duration.ofMinutes(minutes))
+      .plus(Duration.ofSeconds(seconds))
+      .plus(Duration.ofMillis(milliseconds))
+      .plus(Duration.of(microseconds, ChronoUnit.MICROS))
+      .plus(Duration.ofNanos(nanoseconds));
+  }
+
+  @Test
+  void daysHoursMinutesSecondsMillisecondsMicrosecondsNanosecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("2d16h29m10s451ms256125us8192156ns");
+    Duration expected = durationOf(2, 16, 29, 10, 451, 256_125, 8_192_156);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void daysHoursMinutesSecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("5d12h30m15s");
+    Duration expected = durationOf(5, 12, 30, 15, 0, 0, 0);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void daysMinutesMillisecondsNanosecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("21d42m999ms2124480ns");
+    Duration expected = durationOf(21, 0, 42, 0, 999, 0, 2_124_480);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void daysSecondsNanosecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("8d22s16156248ns");
+    Duration expected = durationOf(8, 0, 0, 22, 0, 0, 16_156_248);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void hoursSecondsMicrosecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("18h59s512125us");
+    Duration expected = durationOf(0, 18, 0, 59, 0, 512_125, 0);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void minutesSecondsMillisecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("30m29s999ms");
+    Duration expected = durationOf(0, 0, 30, 29, 999, 0, 0);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void secondsMilliseconds() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("45s555ms");
+    Duration expected = durationOf(0, 0, 0, 45, 555, 0, 0);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void millisecondsMicrosecondsNanosecondsIsCorrect() {
+
+    Duration actual = DurationFormatter.TIME_UNIT.parse("456ms128654us1248842ns");
+    Duration expected = durationOf(0, 0, 0, 0, 456, 128_654, 1_248_842);
+
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  void durationFormatterUnitValueOfChronoUnitIsCorrect() {
+
+    Arrays.stream(DurationFormatter.Unit.values()).forEach(unit ->
+      assertThat(DurationFormatter.Unit.valueOfChronoUnit(unit.getUnit())).isEqualTo(unit));
+  }
+
+  @Test
+  void durationFormatterUnitValueOfInvalidChronoUnitThrowsIllegalArgumentException() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> DurationFormatter.Unit.valueOfChronoUnit(ChronoUnit.ERAS))
+      .withMessage("No Unit %s matches the given ChronoUnit [%s]",
+        Arrays.toString(DurationFormatter.Unit.values()), ChronoUnit.ERAS)
+      .withNoCause();
+  }
+
+  @Test
+  void durationFormatterUnitValueOfSymbolIsCorrect() {
+
+    Arrays.stream(DurationFormatter.Unit.values()).forEach(unit ->
+      assertThat(DurationFormatter.Unit.valueOfSymbol(unit.getSymbol())).isEqualTo(unit));
+  }
+
+  @Test
+  void durationFormatterUnitValueOfInvalidSymbolThrowsIllegalArgumentException() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> DurationFormatter.Unit.valueOfSymbol("yr"))
+      .withMessage("No Unit %s matches the given Symbol [yr]", Arrays.toString(DurationFormatter.Unit.values()))
+      .withNoCause();
+  }
+}
