7802725ba77ff4ae242fcc289001b9e19a921a50
John Blum <jblum0@gmail.com>
2024-03-06 13:22:41 -0800

    Edit Javadoc in EqualityException.

--- src/main/java/org/cp/elements/lang/EqualityException.java
+++ src/main/java/org/cp/elements/lang/EqualityException.java
@@ -13,11 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.cp.elements.lang;
 
 /**
- * The EqualityException class is an IllegalArgumentException indicating that two objects are not equal.
+ * Java {@link IllegalArgumentException} thrown when two {@link Object objects} are not equal.
  *
  * @author John J. Blum
  * @see java.lang.IllegalArgumentException
@@ -27,37 +26,38 @@ package org.cp.elements.lang;
 public class EqualityException extends IllegalArgumentException {
 
   /**
-   * Default constructor creating an instance of the EqualityException.
+   * Constructs a new instance of {@link EqualityException}.
    */
   public EqualityException() {
   }
 
   /**
-   * Constructor to create an instance of the EqualityException with the given message to describe the equality
-   * comparison error.
+   * Constructs a new {@link EqualityException} initialized with the given {@link String message}
+   * describing the equality error.
    *
-   * @param message a String value describing the nature of the equality comparison error.
+   * @param message {@link String} describing the equality error.
    */
   public EqualityException(final String message) {
     super(message);
   }
 
   /**
-   * Constructor to create an instance of the EqualityException with the given Throwable to indicate the cause
-   * of the equality comparison error.
+   * Constructs a new {@link EqualityException} initialized with the given {@link Throwable}
+   * used as the cause of this equality error.
    *
-   * @param cause the Throwable indicated as the cause of this equality comparison error.
+   * @param cause {@link Throwable} used as the cause of this equality error.
    */
   public EqualityException(final Throwable cause) {
     super(cause);
   }
 
   /**
-   * Constructor to create an instance of the EqualityException with a message to describe the equality comparison error
-   * and a Throwable to indicate the probable cause of the equality comparison error.
+   * Constructs a new {@link EqualityException} initialized with the given {@link String message}
+   * describing the equality error along with the given {@link Throwable} used as the cause
+   * of this equality error.
    *
-   * @param message a String value describing the nature of the equality comparison error.
-   * @param cause the Throwable indicated as the cause of this equality comparison error.
+   * @param message {@link String} describing the equality error.
+   * @param cause {@link Throwable} used as the cause of this equality error.
    */
   public EqualityException(final String message, final Throwable cause) {
     super(message, cause);
