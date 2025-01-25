32cb8e322421a3943842a461c762f037f3a68eac
John Blum <jblum0@gmail.com>
2025-01-24 23:20:18 -0800

    Create a Sum QueryFunction to sum the values in a set.

---
+++ src/main/java/org/cp/elements/data/oql/functions/Sum.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.data.oql.functions;
+
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+import org.cp.elements.data.oql.QueryFunction;
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.annotation.NotNull;
+
+/**
+ * {@link QueryFunction} used to calculate a {@literal sum}.
+ *
+ * @author John Blum
+ * @see java.lang.Comparable
+ * @see org.cp.elements.data.oql.QueryFunction
+ * @since 2.0.0
+ */
+public class Sum<S> implements QueryFunction<S, BigDecimal> {
+
+  public static <S> Sum<S> of(@NotNull Function<S, ? extends Number> function) {
+    return new Sum<>(function);
+  }
+
+  private final Function<S, ? extends Number> function;
+
+  protected Sum(@NotNull Function<S, ? extends Number> function) {
+    this.function = ObjectUtils.requireObject(function, "Function is required");
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public BigDecimal apply(S... targets) {
+
+    BigDecimal sum = BigDecimal.ZERO;
+
+    for (S target : targets) {
+
+      Number targetValue = this.function.apply(target);
+
+      if (targetValue != null) {
+        sum = sum.add(BigDecimal.valueOf(targetValue.doubleValue()));
+      }
+    }
+
+    return sum;
+  }
+}
