17ddf5bcdff04acbb4aec4c07f05c1cc4bbbc426
John Blum <jblum0@gmail.com>
2025-02-15 16:03:41 -0800

    Create ThrowableSupplier interface in the org.cp.elements.function package.

---
+++ src/main/java/org/cp/elements/function/ThrowableSupplier.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.function;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
+
+import java.util.function.Supplier;
+
+/**
+ * {@link Supplier} implementation capable of throwing an {@link Exception} when supplying a value.
+ *
+ * @author John Blum
+ * @param <T> {@link Class type} of {@link Object value} supplied by this {@link Supplier}.
+ * @see java.lang.FunctionalInterface
+ * @see java.util.function.Supplier
+ * @since 2.0.0
+ */
+@FunctionalInterface
+public interface ThrowableSupplier<T> extends Supplier<T> {
+
+  @Override
+  default T get() {
+
+    try {
+      return getThrowingException();
+    }
+    catch (Exception cause) {
+      throw newIllegalStateException(cause, "Failed to get supplied value");
+    }
+  }
+
+  /**
+   * Return a {@link T value} computed by this {@link Supplier}.
+   *
+   * @return a {@link T value} computed by this {@link Supplier}.
+   * @throws Exception if {@link T value} cannot be computed.
+   * @see #get()
+   */
+  T getThrowingException() throws Exception;
+
+}
---
+++ src/test/java/org/cp/elements/function/ThrowableSupplierUnitTests.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.cp.elements.lang.ThrowableAssertions.assertThatThrowableOfType;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.Test;
+
+import org.cp.elements.lang.ThrowableOperation;
+
+/**
+ * Unit Tests for {@link ThrowableSupplier}.
+ *
+ * @author John Blum
+ * @see org.cp.elements.function.ThrowableSupplier
+ * @see org.junit.jupiter.api.Test
+ * @see org.mockito.Mockito
+ * @since 2.0.0
+ */
+class ThrowableSupplierUnitTests {
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void getCallsGetThrowingException() throws Exception {
+
+    ThrowableSupplier<Object> mockThrowableConsumer = mock(ThrowableSupplier.class);
+
+    doCallRealMethod().when(mockThrowableConsumer).get();
+    doReturn("test").when(mockThrowableConsumer).getThrowingException();
+
+    assertThat(mockThrowableConsumer.get()).isEqualTo("test");
+
+    verify(mockThrowableConsumer, times(1)).get();
+    verify(mockThrowableConsumer, times(1)).getThrowingException();
+    verifyNoMoreInteractions(mockThrowableConsumer);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  void getThrowsException() throws Exception {
+
+    ThrowableSupplier<Object> mockThrowableConsumer = mock(ThrowableSupplier.class);
+
+    doCallRealMethod().when(mockThrowableConsumer).get();
+    doThrow(new Exception("TEST")).when(mockThrowableConsumer).getThrowingException();
+
+    assertThatThrowableOfType(IllegalStateException.class)
+      .isThrownBy(ThrowableOperation.fromSupplier(mockThrowableConsumer))
+      .havingMessage("Failed to get supplied value")
+      .causedBy(Exception.class)
+      .havingMessage("TEST")
+      .withNoCause();
+
+    verify(mockThrowableConsumer, times(1)).get();
+    verify(mockThrowableConsumer, times(1)).getThrowingException();
+    verifyNoMoreInteractions(mockThrowableConsumer);
+  }
+}
