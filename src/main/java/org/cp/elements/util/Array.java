bd757554717d5a786b0c30e8b138d433473b5489
John Blum <jblum0@gmail.com>
2025-03-02 14:36:18 -0800

    Create Array class modeling a Java array in the org.cp.elements.util package.

---
+++ src/main/java/org/cp/elements/util/Array.java
@@ -0,0 +1,306 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.util;
+
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newIllegalStateException;
+import static org.cp.elements.lang.RuntimeExceptionsFactory.newUnsupportedOperationException;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import org.cp.elements.lang.Assert;
+import org.cp.elements.lang.TypeResolver;
+import org.cp.elements.lang.annotation.NotNull;
+import org.cp.elements.util.stream.StreamUtils;
+import org.cp.elements.util.stream.Streamable;
+
+/**
+ * Abstract Data Type (ADT) modeling and wrapping any Java array.
+ *
+ * @author John Blum
+ * @param <T> {@link Class type} of {@link Object elements} in the array.
+ * @see org.cp.elements.util.stream.Streamable
+ * @see java.lang.FunctionalInterface
+ * @see java.lang.Iterable
+ * @since 2.0.0
+ */
+@FunctionalInterface
+@SuppressWarnings("unused")
+public interface Array<T> extends Iterable<T>, Streamable<T> {
+
+  /**
+   * Factory method used to construct a new {@link Array} with no elements.
+   *
+   * @param <T> {@link Class type} of the {@link Object elements} in the array.
+   * @return a new, empty {@link Array}.
+   */
+  static <T> Array<T> empty() {
+    return Collections::emptyIterator;
+  }
+
+  /**
+   * Factory method used to construct a new, immutable {@link Array} from the given array of {@link T elements}.
+   *
+   * @param <T> {@link Class type} of {@link Object elements} in the array.
+   * @param array Java array from which to construct a new, immutable {@link Array}.
+   * @return a new, immutable {@link Array} from the given Java array.
+   * @throws IllegalArgumentException if {@link T array} is {@literal null}.
+   * @see #of(Object[])
+   */
+  @SafeVarargs
+  static <T> Array<T> immutableOf(T... array) {
+
+    return new AbstractArray<>(array) {
+
+      @Override
+      public Array<T> set(int index, T value) {
+        throw newIllegalStateException("Array is immutable");
+      }
+    };
+  }
+
+  /**
+   * Factory method used to construct a new {@link Array} from the given array of {@link T elements}.
+   *
+   * @param <T> {@link Class type} of {@link Object elements} in the array.
+   * @param array Java array from which to construct a new {@link Array}.
+   * @return a new {@link Array} from the given Java array.
+   * @throws IllegalArgumentException if {@link T array} is {@literal null}.
+   */
+  @SafeVarargs
+  static <T> Array<T> of(T... array) {
+    return AbstractArray.of(array);
+  }
+
+  /**
+   * Factory method used to construct a new {@link Array} containing a single element.
+   *
+   * @param <T> {@link Class type} of {@link Object elements} in the array.
+   * @param element {@link T object} to add to the array.
+   * @return a new, single element {@link Array}.
+   * @throws IllegalArgumentException if {@link T element} is {@literal null}.
+   * @see #of(Object[])
+   */
+  static <T> Array<T> singleton(T element) {
+    Assert.notNull(element, "Element is required");
+    return of(element);
+  }
+
+  /**
+   * Determines whether this {@link Array} is empty.
+   *
+   * @return a boolean value indicating whether this {@link Array} is empty.
+   * @see #isNotEmpty()
+   * @see #length()
+   */
+  default boolean isEmpty() {
+    return length() == 0;
+  }
+
+  /**
+   * Determines whether this {@link Array} is empty.
+   *
+   * @return a boolean value indicating whether this {@link Array} is empty.
+   * @see #isEmpty()
+   */
+  default boolean isNotEmpty() {
+    return !isEmpty();
+  }
+
+  /**
+   * Finds all {@link T elements} in this {@link Array} matching the given {@link Predicate} as an {@link Array}.
+   *
+   * @param predicate {@link Predicate} used to match {@link T elements} from this {@link Array}.
+   * @return all elements in this {@link Array} matching the given {@link Predicate} as an {@link Array}.
+   * @see java.util.function.Predicate
+   * @see #findOne(Predicate)
+   */
+  @SuppressWarnings("unchecked")
+  default Array<T> findBy(@NotNull Predicate<T> predicate) {
+
+    Assert.notNull(predicate, "Predicate is required");
+
+    List<T> list = stream().filter(predicate).toList();
+    Class<T> componentType = (Class<T>) TypeResolver.getInstance().resolveType(this).getComponentType();
+    T[] array = ArrayUtils.asArray(list, componentType);
+
+    return Array.of(array);
+  }
+
+  /**
+   * Finds an {@link Optional} single {@link T element} from this {@link Array} matching the given {@link Predicate}.
+   *
+   * @param predicate {@link Predicate} used to match the single {@link T element}.
+   * @return an {@link Optional} single {@link T element} from this {@link Array} matching the given {@link Predicate}.
+   * @throws IllegalArgumentException if {@link Predicate} is {@literal null}.
+   * @see #findBy(Predicate)
+   * @see java.util.Optional
+   */
+  default Optional<T> findOne(Predicate<T> predicate) {
+    Assert.notNull(predicate, "Predicate is required");
+    return stream().filter(predicate).findFirst();
+  }
+
+  /**
+   * Gets the {@link T element} at the given {@link Integer index} in this {@link Array}.
+   *
+   * @param index {@link Integer} referring to the index of the element to return.
+   * @return the {@link T element} at the given {@link Integer index} in this {@link Array}.
+   * @see #set(int, Object)
+   */
+  default T get(int index) {
+    return toArray()[index];
+  }
+
+  /**
+   * Sets the element at {@link Integer index} in this {@link Array} to the given {@link T value}.
+   *
+   * @param index {@link Integer} referring to the element to set.
+   * @param value {@link T value} to set the element to.
+   * @return a new {@link Array}.
+   * @see #get(int)
+   */
+  default Array<T> set(int index, T value) {
+    T[] array = toArray();
+    array[index] = value;
+    return Array.of(array);
+  }
+
+  /**
+   * Returns the {@link Integer number of elements} contained in this {@link Array}.
+   *
+   * @return the {@link Integer number of elements} contained in this {@link Array}.
+   */
+  default int length() {
+    return Long.valueOf(stream().count()).intValue();
+  }
+
+  /**
+   * Requires that a single {@link T element} matching the given {@link Predicate} be found in this {@link Array}.
+   *
+   * @param predicate {@link Predicate} used to match an {@link T element}; required.
+   * @return a single {@link T element} from this {@link Array} matching the {@link Predicate}.
+   * @throws IllegalArgumentException if {@link Predicate} is {@literal null}.
+   * @throws NoSuchElementException if a single {@link T element} cannot be found matching the {@link Predicate}.
+   * @see java.util.function.Predicate
+   * @see #findOne(Predicate)
+   */
+  default T requireOne(@NotNull Predicate<T> predicate) {
+    return findOne(predicate).orElseThrow(() -> new NoSuchElementException("Element not found"));
+  }
+
+  /**
+   * Returns a {@link Stream} over the {@link T elements} in this {@link Array}.
+   *
+   * @return a {@link Stream} over the {@link T elements} in this {@link Array}.
+   * @see java.util.stream.Stream
+   */
+  @Override
+  default Stream<T> stream() {
+    return StreamUtils.stream(this);
+  }
+
+  @SuppressWarnings("unchecked")
+  default <V> Array<V> transform(@NotNull Function<T, V> function) {
+    Assert.notNull(function, "Function is required");
+    V[] array = (V[]) stream().map(function).toArray();
+    return Array.of(array);
+  }
+
+  /**
+   * Returns this {@link Array} as a Java array containing elements of type {@link T}.
+   * <p>
+   * This operation simply makes a shallow copy of this {@link Array}.
+   *
+   * @return this {@link Array} as a Java array containing elements of type {@link T}.
+   * @throws UnsupportedOperationException by default.
+   */
+  default T[] toArray() {
+    throw newUnsupportedOperationException("Getting a Java array from Array is not supported");
+  }
+
+  /**
+   * Abstract base class for all {@link Array} implementations.
+   */
+  abstract class AbstractArray<T> implements Array<T> {
+
+    /**
+     * Factory method used to construct a new {@link AbstractArray} from the given Java array.
+     *
+     * @param <T> {@link Class type} of {@link Object elements} in the array.
+     * @param array Java array used to construct and initialize the new {@link Array}; required.
+     * @return a new {@link AbstractArray} constructed and initalized from the given Java array.
+     * @throws IllegalArgumentException if {@link T array} is {@literal null}.
+     */
+    @SafeVarargs
+    static <T> AbstractArray<T> of(T... array) {
+      Assert.notNull(array, "Array is required");
+      return new AbstractArray<T>(array) { };
+    }
+
+    private final T[] array;
+
+    /**
+     * Construct a new {@link AbstractArray} from the given Java array.
+     *
+     * @param array Java array used to contruct an initialize a new {@link Array}.
+     * @throws IllegalArgumentException if {@link T array} is {@literal null}.
+     */
+    protected AbstractArray(T[] array) {
+      Assert.notNull(array, "Array is required");
+      this.array = ArrayUtils.asTypedArray(array, resolveComponentType(array));
+    }
+
+    @SuppressWarnings("unchecked")
+    private Class<T> resolveComponentType(T[] array) {
+
+      Class<?> componentType = TypeResolver.getInstance().resolveType(array);
+
+      Class<?> resolvedComponentType = Object.class.equals(componentType)
+        ? array.length > 0 ? array[0].getClass() : componentType
+        : componentType;
+
+      return (Class<T>) resolvedComponentType;
+    }
+
+    @Override
+    public T get(int index) {
+      return this.array[index];
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    public Iterator<T> iterator() {
+      return CollectionUtils.unmodifiableIterator(ArrayUtils.asIterator(this.array));
+    }
+
+    @Override
+    public int length() {
+      return this.array.length;
+    }
+
+    @Override
+    public T[] toArray() {
+      return ArrayUtils.shallowCopy(this.array);
+    }
+  }
+}
---
+++ src/test/java/org/cp/elements/util/ArrayUnitTests.java
@@ -0,0 +1,371 @@
+/*
+ * Copyright 2017-Present Author or Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cp.elements.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+
+import org.cp.elements.lang.NumberUtils;
+
+/**
+ * Unit Tests for {@link Array}.
+ *
+ * @author John Blum
+ * @see org.cp.elements.util.Array
+ * @see org.junit.jupiter.api.Test
+ * @since 2.0.0
+ */
+public class ArrayUnitTests {
+
+  @Test
+  void ofEmptyArray() {
+
+    Array<?> emptyArray = Array.empty();
+
+    assertThat(emptyArray).isNotNull();
+    assertThat(emptyArray).isEmpty();
+  }
+
+  @Test
+  void ofImmutableArray() {
+
+    Array<Integer> array = Array.immutableOf(1);
+
+    assertThat(array).isNotNull();
+    assertThat(array.get(0)).isOne();
+
+    assertThatIllegalStateException()
+      .isThrownBy(() -> array.set(0, 2))
+      .withMessage("Array is immutable")
+      .withNoCause();
+
+    assertThat(array.get(0)).isOne();
+  }
+
+  @Test
+  void ofJavaIntegerArray() {
+
+    Integer[] integers = { 1, 2, 3 };
+
+    Array<Integer> array = Array.of(integers);
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(integers.length);
+    assertThat(array).containsExactly(1, 2, 3);
+  }
+
+  @Test
+  void ofJavaStringArray() {
+
+    String[] strings = { "test", "testing", "tested" };
+
+    Array<String> array = Array.of(strings);
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(strings.length);
+    assertThat(array).containsExactly("test", "testing", "tested");
+  }
+
+  @Test
+  void ofNullArray() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> Array.of((Object[]) null))
+      .withMessage("Array is required")
+      .withNoCause();
+  }
+
+  @Test
+  void singleton() {
+
+    Array<String> array = Array.singleton("test");
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(1);
+    assertThat(array).containsExactly("test");
+  }
+
+  @Test
+  void nullSingleton() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> Array.singleton(null))
+      .withMessage("Element is required")
+      .withNoCause();
+  }
+
+  @Test
+  void arrayWithNullElements() {
+
+    Array<Object> array = Array.of(1, null, 2, 3, null, null);
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(6);
+    assertThat(array).containsExactly(1, null, 2, 3, null, null);
+  }
+
+  @Test
+  void isEmpty() {
+
+    Array<?> emptyArray = Array.empty();
+
+    assertThat(emptyArray).isNotNull();
+    assertThat(emptyArray.isEmpty()).isTrue();
+    assertThat(emptyArray.isNotEmpty()).isFalse();
+  }
+
+  @Test
+  void isNotEmptyForMultiElementArray() {
+
+    Array<?> array = Array.of(1, 2, 3);
+
+    assertThat(array).isNotNull();
+    assertThat(array.isEmpty()).isFalse();
+    assertThat(array.isNotEmpty()).isTrue();
+  }
+
+  @Test
+  void isNotEmptyForSingleElementArray() {
+
+    Array<?> array = Array.singleton(1);
+
+    assertThat(array).isNotNull();
+    assertThat(array.isEmpty()).isFalse();
+    assertThat(array.isNotEmpty()).isTrue();
+  }
+
+  @Test
+  void getAndSet() {
+
+    Array<String> array = Array.of("test");
+
+    assertThat(array).isNotNull();
+    assertThat(array.get(0)).isEqualTo("test");
+
+    Array<String> newArray = array.set(0, "testing");
+
+    assertThat(newArray).isNotNull().isNotSameAs(array);
+    assertThat(newArray.get(0)).isEqualTo("testing");
+    assertThat(array.get(0)).isEqualTo("test");
+
+    newArray = newArray.set(0, null);
+
+    assertThat(newArray).isNotNull().isNotSameAs(array);
+    assertThat(newArray.get(0)).isNull();
+    assertThat(array.get(0)).isEqualTo("test");
+
+    newArray= newArray.set(0, "testing");
+
+    assertThat(newArray).isNotNull().isNotSameAs(array);
+    assertThat(newArray.get(0)).isEqualTo("testing");
+    assertThat(array.get(0)).isEqualTo("test");
+  }
+
+  @Test
+  void findByPredicate() {
+
+    Array<Integer> array = Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    Array<Integer> evenArray = array.findBy(NumberUtils::isEven);
+
+    assertThat(evenArray).isNotNull();
+    assertThat(evenArray).hasSize(5);
+    assertThat(evenArray).containsExactly(0, 2, 4, 6, 8);
+  }
+
+  @Test
+  void findByFindsNone() {
+
+    Array<Integer> array = Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    Array<Integer> emptyArray = array.findBy(NumberUtils::isNegative);
+
+    assertThat(emptyArray).isNotNull();
+    assertThat(emptyArray).isEmpty();
+  }
+
+  @Test
+  void findByFindsOne() {
+
+    Array<Integer> array = Array.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    Array<Integer> singleElementArray = array.findBy(number -> number == 9);
+
+    assertThat(singleElementArray).isNotNull();
+    assertThat(singleElementArray).hasSize(1);
+    assertThat(singleElementArray).containsExactly(9);
+  }
+
+  @Test
+  void findByWithNullPredicate() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> Array.empty().findBy(null))
+      .withMessage("Predicate is required")
+      .withNoCause();
+  }
+
+  @Test
+  void findOneFindsNone() {
+
+    Array<Integer> array = Array.of(1, 3, 5, 7, 9);
+
+    Optional<Integer> element = array.findOne(NumberUtils::isEven);
+
+    assertThat(element).isNotNull();
+    assertThat(element).isNotPresent();
+  }
+
+  @Test
+  void findOneFindsSingleElement() {
+
+    Array<Integer> array = Array.of(1, 2, 3, 5, 7, 9);
+
+    Optional<Integer> two = array.findOne(NumberUtils::isEven);
+
+    assertThat(two).isNotNull();
+    assertThat(two).isPresent();
+    assertThat(two.orElse(null)).isEqualTo(2);
+  }
+
+  @Test
+  void findOneWithNullPredicate() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> Array.empty().findOne(null))
+      .withMessage("Predicate is required")
+      .withNoCause();
+  }
+
+  @Test
+  void lengthOfEmptyArray() {
+    assertThat(Array.empty().length()).isZero();
+  }
+
+  @Test
+  void lengthOfMultiElementArray() {
+    assertThat(Array.of(1, 2, 3).length()).isEqualTo(3);
+  }
+
+  @Test
+  void lengthOfSingleElementArray() {
+    assertThat(Array.singleton(1).length()).isOne();
+  }
+
+  @Test
+  void requireOneWhenFound() {
+
+    Integer two = Array.of(1, 2, 3).requireOne(NumberUtils::isEven);
+
+    assertThat(two).isNotNull();
+    assertThat(two).isEqualTo(2);
+  }
+
+  @Test
+  void requireOneWhenMissing() {
+
+    assertThatExceptionOfType(NoSuchElementException.class)
+      .isThrownBy(() -> Array.<Integer>empty().requireOne(NumberUtils::isOdd))
+      .withMessage("Element not found")
+      .withNoCause();
+  }
+
+  @Test
+  void requireOneWithNullPredicate() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> Array.empty().requireOne(null))
+      .withMessage("Predicate is required")
+      .withNoCause();
+  }
+
+  @Test
+  void stream() {
+
+    Array<Integer> array = Array.of(1, 2, 3);
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(3);
+    assertThat(array).containsExactly(1, 2, 3);
+
+    Stream<Integer> stream = array.stream();
+
+    assertThat(stream).isNotNull();
+    assertThat(stream).containsExactly(1, 2, 3);
+  }
+
+  @Test
+  void transformStringsToUppercase() {
+
+    Array<String> array = Array.of("test", "testing", "tested");
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(3);
+    assertThat(array).containsExactly("test", "testing", "tested");
+
+    Array<String> uppercaseStringArray = array.transform(String::toUpperCase);
+
+    assertThat(uppercaseStringArray).isNotNull();
+    assertThat(uppercaseStringArray).hasSameSizeAs(array);
+    assertThat(uppercaseStringArray).containsExactly("TEST", "TESTING", "TESTED");
+  }
+
+  @Test
+  void transformToStronglyTypeIntegerArray() {
+
+    Array<String> array = Array.of("1", "2", "3");
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSize(3);
+    assertThat(array).containsExactly("1", "2", "3");
+
+    Array<Integer> numberArray = array.transform(Integer::parseInt);
+
+    assertThat(numberArray).isNotNull();
+    assertThat(numberArray).hasSameSizeAs(array);
+    assertThat(numberArray).containsExactly(1, 2, 3);
+
+    Integer[] integerArray = numberArray.toArray();
+
+    assertThat(integerArray).isNotNull();
+    assertThat(integerArray).hasSameSizeAs(numberArray);
+    assertThat(integerArray).containsExactly(1, 2, 3);
+  }
+
+  @Test
+  void toArray() {
+
+    Integer[] integerArray = { 1, 2, 3 };
+
+    Array<Integer> array = Array.of(integerArray);
+
+    assertThat(array).isNotNull();
+    assertThat(array).hasSameSizeAs(integerArray);
+    assertThat(array).containsExactly(integerArray);
+
+    Integer[] theIntegerArray = array.toArray();
+
+    assertThat(theIntegerArray).isNotNull();
+    assertThat(theIntegerArray).isNotSameAs(integerArray);
+    assertThat(theIntegerArray).hasSameSizeAs(integerArray);
+    assertThat(theIntegerArray).containsExactly(integerArray);
+  }
+}
