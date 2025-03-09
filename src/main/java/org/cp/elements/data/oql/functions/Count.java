4e0bde47036b153d6f5d8108c173f3dca83fdb45
John Blum <jblum0@gmail.com>
2025-03-09 13:30:53 -0700

    Edit Javadoc in Count query function.

--- src/main/java/org/cp/elements/data/oql/functions/Count.java
+++ src/main/java/org/cp/elements/data/oql/functions/Count.java
@@ -18,6 +18,8 @@ package org.cp.elements.data.oql.functions;
 import org.cp.elements.data.oql.QueryFunction;
 import org.cp.elements.data.support.Iterables;
 import org.cp.elements.lang.StringUtils;
+import org.cp.elements.lang.annotation.NullSafe;
+import org.cp.elements.lang.annotation.ThreadSafe;
 import org.cp.elements.util.stream.StreamUtils;
 
 /**
@@ -25,30 +27,58 @@ import org.cp.elements.util.stream.StreamUtils;
  *
  * @author John Blum
  * @param <T> {@link Class type} of {@link Object} on which this {@link Count function} is applied.
+ * @see org.cp.elements.lang.annotation.ThreadSafe
  * @see org.cp.elements.data.oql.QueryFunction
  * @since 2.0.0
  */
+@ThreadSafe
 @SuppressWarnings("unused")
 public class Count<T> implements QueryFunction<T, Long> {
 
   protected static final String DEFAULT_NAME = "Count";
 
+  /**
+   * Factory method used to construct a new {@link Count} query function to count all the elements.
+   *
+   * @param <T> {@link Class type} of the {@link Object} being counted.
+   * @return a new {@link Count} query function.
+   */
   public static <T> Count<T> all() {
     return new Count<>();
   }
 
   private String name;
 
+  /**
+   * Returns the {@link String name} given to this query function.
+   *
+   * @return the {@link String name} given to this query function.
+   */
   @Override
   public String getName() {
     return StringUtils.defaultIfBlank(this.name, DEFAULT_NAME);
   }
 
+  /**
+   * Counts all the elements in the {@link Iterable result set}.
+   *
+   * @param resultSet {@link Iterable} of {@link T Objects} to count.
+   * @return a {@link Long count} of all the elments in the {@link Iterable result set}; returns {@literal 0}
+   * if the {@link Iterable result set} is {@literal null} or {@literal empty}.
+   * @see java.lang.Iterable
+   */
+  @NullSafe
   @Override
   public Long apply(Iterable<T> resultSet) {
     return StreamUtils.stream(Iterables.nullSafeIterable(resultSet)).count();
   }
 
+  /**
+   * Builder method used to assign a {@link String name} to this query function.
+   *
+   * @param name {@link String} containing the name given to this query function.
+   * @return this query function.
+   */
   public Count<T> named(String name) {
     this.name = name;
     return this;
