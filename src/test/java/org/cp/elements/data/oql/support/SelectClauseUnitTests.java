73ed6b7aeaa97788bb0cc22bb3c3bd8c780cd50c
John Blum <jblum0@gmail.com>
2025-04-11 00:27:54 -0700

    Add additional unit tests for OQL SelectClause.

--- src/test/java/org/cp/elements/data/oql/support/SelectClauseUnitTests.java
+++ src/test/java/org/cp/elements/data/oql/support/SelectClauseUnitTests.java
@@ -16,11 +16,20 @@
 package org.cp.elements.data.oql.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.cp.elements.data.oql.Oql;
+import org.cp.elements.security.model.User;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -44,6 +53,50 @@ class SelectClauseUnitTests {
     SelectClause<?, ?> select = SelectClause.select(this.mockProjection);
 
     assertThat(select).isNotNull();
+    assertThat(select.isDistinct()).isEqualTo(Oql.Select.DEFAULT_DISTINCT);
     assertThat(select.getProjection()).isInstanceOf(SelectClause.ProjectionWrapper.class);
+    assertThat(select.getProjection())
+      .asInstanceOf(InstanceOfAssertFactories.type(SelectClause.ProjectionWrapper.class))
+      .extracting(SelectClause.ProjectionWrapper::getProjection)
+      .isEqualTo(this.mockProjection);
+  }
+
+  @Test
+  void selectDistinct() {
+
+    doReturn(User.class).when(this.mockProjection).getFromType();
+
+    SelectClause<?, ?> select = SelectClause.select(this.mockProjection);
+
+    assertThat(select).isNotNull();
+    assertThat(select.isDistinct()).isFalse();
+    assertThat(select.getProjection()).isInstanceOf(SelectClause.ProjectionWrapper.class);
+
+    Oql.From<?, ?> from = select.distinct().from(Collections.emptySet());
+
+    assertThat(select.isDistinct()).isTrue();
+    assertThat(from).isNotNull();
+    assertThat(from.getSelection()).isEqualTo(select);
+
+    verify(this.mockProjection, times(1)).getFromType();
+    verifyNoMoreInteractions(this.mockProjection);
+  }
+
+  @Test
+  void selectClauseDistinctBuilderWithNoSelect() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> new SelectClause.DistinctBuilder<>(null))
+      .withMessage("Select is required")
+      .withNoCause();
+  }
+
+  @Test
+  void selectWithNoProjection() {
+
+    assertThatIllegalArgumentException()
+      .isThrownBy(() -> SelectClause.select(null))
+      .withMessage("Projection is required")
+      .withNoCause();
   }
 }
