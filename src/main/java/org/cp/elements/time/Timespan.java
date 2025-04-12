e0924c78187f7fc99b31c09e1630c745c547e233
John Blum <jblum0@gmail.com>
2025-04-11 23:57:37 -0700

    Update Copyright to 2011 for all source files.

--- src/main/java/org/cp/elements/time/Timespan.java
+++ src/main/java/org/cp/elements/time/Timespan.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-Present Author or Authors.
+ * Copyright 2011-Present Author or Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
